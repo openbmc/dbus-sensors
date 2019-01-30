e533f08d7f68abef73115f275f2e617f2fcfbfd9
Qiang XU <qiang.xu@linux.intel.com>
2019-01-30 10:55:09 +0800

    Add chassis intrusion sensor daemon
    Chassis intrusion signal is read from PCH via I2C in current platform.
    Create a new daemon to poll its status and expose to dbus.
    
    Related patches to run test:
    - meta-phosphor: dbus-sensors: Enable new service of intrusion sensor
    https://gerrit.openbmc-project.xyz/#/c/openbmc/meta-phosphor/+/17063/
    - redfish: chassis: add property of physical security
    https://gerrit.openbmc-project.xyz/#/c/openbmc/bmcweb/+/17691/
    
    Tested-by:
    - Check intrusion status value in dbus and redfish
    
    Change-Id: Id5e67abbd80bbf2ef502db49fa183d92d0d31bda
    Signed-off-by: Qiang XU <qiang.xu@linux.intel.com>

---
+++ include/ChassisIntrusionSensor.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <sdbusplus/asio/object_server.hpp>
+
+class ChassisIntrusionSensor
+{
+  public:
+    ChassisIntrusionSensor(
+        boost::asio::io_service& io,
+        std::shared_ptr<sdbusplus::asio::dbus_interface> iface);
+
+    ~ChassisIntrusionSensor();
+
+    void start(int busId, int slaveAddr);
+
+  private:
+    std::shared_ptr<sdbusplus::asio::dbus_interface> mIface;
+
+    // intrusion status. 0: not intruded, 1: intruded
+    int mValue;
+    int mOldValue;
+
+    // only support PCH register now
+    int mBusId;
+    int mSlaveAddr;
+
+    int mOverriddenValue = -1;
+    bool mOverridenState = false;
+    bool mInternalSet = false;
+
+    bool mInitialized = false;
+
+    boost::asio::deadline_timer mPollTimer;
+
+    bool i2cReadFromPch();
+    void pollPchInfo();
+    int setSensorValue(const int& req, int& propertyValue);
+    void updateValue(const int newValue);
+};
