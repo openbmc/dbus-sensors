59224b9eee3f76a3a41a3319d0f0d4f86654d72b
Nikhil Potade <nikhil.potade@linux.intel.com>
2019-03-13 10:52:21 -0700

    Enabling support NVMe FRU and NVMe sensor support
    This commit introduces the support for NVMe drives for Fru and sensors.
    
    All the NVMe drives which are detected by the FRU manager and are
    present in the inventory are scanned at regular interval for reading
    the temperature values of the NVMe devices.
    
    Tested how:
    Using the application provided by libmctp which queries the drive
    sensor values, cross verified that the temperature values received by
    the dbus-sensor are the same.
    
    Signed-off-by: Nikhil Potade <nikhil.potade@linux.intel.com>
    Change-Id: Icb119b424234d548c8ff5cda9c7a9517ec9696bb

---
+++ include/NVMeSensor.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <crc32c.h>
+#include <libmctp-smbus.h>
+#include <libmctp.h>
+
+#include <Thresholds.hpp>
+#include <boost/algorithm/string/replace.hpp>
+#include <boost/asio/basic_stream_socket.hpp>
+#include <boost/asio/buffer.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/posix/stream_descriptor.hpp>
+#include <boost/optional/optional.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+#include <sensor.hpp>
+
+static constexpr double maxReading = 127;
+static constexpr double minReading = -60;
+static int DEBUG = 0;
+
+struct NVMeContext
+{
+    NVMeContext(boost::asio::io_service& io, int bus, int rootBus);
+    NVMeContext(const NVMeContext&);
+    static void rxMessage(uint8_t eid, void* data, void* msg, size_t len);
+    ~NVMeContext(){};
+
+    int bus; // Bus this drive is on
+    // MCTP specific
+    std::unique_ptr<struct mctp_binding_smbus,
+                    std::function<void(mctp_binding_smbus*)>>
+        smbus;
+    struct mctp* mctp;
+    mctp_eid_t eid;
+    int rootBus; // Root bus for this drive
+    std::shared_ptr<boost::asio::ip::tcp::socket> nvmeSlaveSocket;
+};
+
+class NVMeSensor : public Sensor
+{
+  public:
+    NVMeSensor(const std::string& path,
+               sdbusplus::asio::object_server& objectServer,
+               std::shared_ptr<sdbusplus::asio::connection>& conn,
+               boost::asio::io_service& io, const std::string& sensorName,
+               std::vector<thresholds::Threshold>&& _thresholds,
+               const std::string& sensorConfiguration, size_t bus);
+
+    void updateNVMeReading(const double& value);
+    virtual ~NVMeSensor();
+
+  private:
+    thresholds::ThresholdTimer thresholdTimer;
+    int bus; // Bus this drive is on
+
+    void checkThresholds(void) override;
+};
+
+using MctpDeviceData = std::shared_ptr<struct NVMeContext>;
+using SensorInfo = std::unique_ptr<NVMeSensor>;
+
+extern std::vector<std::pair<SensorInfo, MctpDeviceData>> nvmeDeviceList;
+extern size_t lastQueriedDeviceIndex;
+
+void readAndProcessNVMeSensor(NVMeSensor& sensorInfo,
+                              struct NVMeContext& nvmeDevice,
+                              boost::asio::io_service& io,
+                              boost::asio::deadline_timer& mctpResponseTimer);
+int nvmeMessageTransmit(struct mctp* mctp, uint8_t eid,
+                        struct nvme_mi_msg_request* req);
+
+int verifyIntegrity(uint8_t* msg, size_t len);
