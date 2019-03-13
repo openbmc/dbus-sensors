02e4b9cae5f62b0f68bc80f93015e680a39fc622
Nikhil Potade <nikhil.potade@linux.intel.com>
2019-03-13 10:52:21 -0700

    Enabling NVMe sensor support
    This commit introduces the support for NVMe drives for sensors.
    
    All the NVMe drives which are detected by the FRU manager and are
    present in the inventory are scanned at regular interval for reading
    the temperature values of the NVMe devices.
    
    Tested how:
    Inserted the drives in the system and verified that the temperature
    values are returned by the drive using redfish interface
    
    Signed-off-by: Nikhil Potade <nikhil.potade@linux.intel.com>
    Change-Id: Icb119b424234d548c8ff5cda9c7a9517ec9696bb

---
+++ src/NVMeSensor.cpp
@@ -0,0 +1,416 @@
+/*
+// Copyright (c) 2017 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#include "NVMeSensor.hpp"
+
+#include "NVMeDevice.hpp"
+
+#include <unistd.h>
+
+#include <iostream>
+#include <limits>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+#include <string>
+
+static size_t lastQueriedDeviceIndex = -1;
+
+size_t getDeviceInfoToQuery()
+{
+    if (!nvmeDeviceList.empty())
+    {
+        lastQueriedDeviceIndex++;
+
+        if (lastQueriedDeviceIndex >= nvmeDeviceList.size())
+        {
+            lastQueriedDeviceIndex = 0;
+        }
+    }
+    else
+    {
+        lastQueriedDeviceIndex = -1;
+    }
+
+    return lastQueriedDeviceIndex;
+}
+
+void pollNVMeDevices(boost::asio::io_service& io,
+                     boost::asio::deadline_timer& scanTimer,
+                     boost::asio::deadline_timer& mctpResponseTimer)
+{
+    scanTimer.expires_from_now(boost::posix_time::seconds(1));
+    scanTimer.async_wait([&io, &scanTimer, &mctpResponseTimer](
+                             const boost::system::error_code& errorCode) {
+        if (errorCode)
+        {
+            std::cerr << "Error:" << errorCode.message() << "\n";
+        }
+        else
+        {
+            size_t index = getDeviceInfoToQuery();
+            if (index >= 0)
+            {
+                readAndProcessNVMeSensor(io, mctpResponseTimer, index);
+            }
+        }
+
+        pollNVMeDevices(io, scanTimer, mctpResponseTimer);
+    });
+}
+
+void readResponse(struct NVMeContext& nvmeDevice,
+                  boost::asio::deadline_timer& mctpResponseTimer)
+{
+    nvmeDevice.nvmeSlaveSocket->async_wait(
+        boost::asio::ip::tcp::socket::wait_error,
+        [&mctpResponseTimer,
+         &nvmeDevice](const boost::system::error_code& errorCode) {
+            if (errorCode)
+            {
+                return;
+            }
+
+            int rc = mctp_smbus_read(nvmeDevice.smbus.get());
+
+            if (rc == MCTP_READ_RETRY)
+            {
+                if (DEBUG)
+                {
+                    std::cout
+                        << "Initiating a re-read for bus: " << nvmeDevice.bus
+                        << " , rootBus: " << nvmeDevice.rootBus << "\n";
+                }
+                readResponse(nvmeDevice, mctpResponseTimer);
+            }
+
+            if (rc != MCTP_READ_RETRY)
+            {
+                if (DEBUG)
+                {
+                    std::cout << "Cancelling the timer now\n";
+                }
+
+                mctpResponseTimer.cancel();
+            }
+        });
+}
+
+void readAndProcessNVMeSensor(boost::asio::io_service& io,
+                              boost::asio::deadline_timer& mctpResponseTimer,
+                              size_t index)
+{
+    struct nvme_mi_msg_request requestMsg = {0};
+    requestMsg.header.opcode = NVME_MI_OPCODE_HEALTH_STATUS_POLL;
+    requestMsg.header.dword0 = 0;
+    requestMsg.header.dword1 = 0;
+
+    int mctpResponseTimeout = 1;
+
+    NVMeContext& nvmeDevice = *nvmeDeviceList[index].second;
+    nvmeDevice.sindex = index;
+
+    mctpResponseTimer.expires_from_now(
+        boost::posix_time::seconds(mctpResponseTimeout));
+    mctpResponseTimer.async_wait(
+        [&nvmeDevice](const boost::system::error_code& errorCode) {
+            if (errorCode != boost::asio::error::operation_aborted)
+            {
+                nvmeDevice.nvmeSlaveSocket->cancel();
+            }
+            else if (errorCode)
+            {
+                return;
+            }
+        });
+
+    readResponse(nvmeDevice, mctpResponseTimer);
+
+    if (DEBUG)
+    {
+        std::cout << "Sending message to read data from Drive on bus: "
+                  << nvmeDevice.bus << " , rootBus: " << nvmeDevice.rootBus
+                  << " device index:" << nvmeDevice.sindex << "\n";
+    }
+
+    int rc = nvmeMessageTransmit(nvmeDevice.mctp, nvmeDevice.eid, &requestMsg);
+
+    if (rc != 0)
+    {
+        std::cerr << "Error sending request message to NVMe device\n";
+    }
+}
+
+int nvmeMessageTransmit(struct mctp* mctp, uint8_t eid,
+                        struct nvme_mi_msg_request* req)
+{
+    uint8_t messageBuf[NVME_MI_MSG_BUFFER_SIZE] = {};
+
+    if (req == nullptr || mctp == nullptr)
+    {
+        std::cerr << "Bad request\n";
+        return -1;
+    }
+
+    req->header.flags |= NVME_MI_HDR_MESSAGE_TYPE_MI_COMMAND
+                         << NVME_MI_HDR_FLAG_MSG_TYPE_SHIFT;
+    req->header.message_type =
+        NVME_MI_MESSAGE_TYPE | NVME_MI_MCTP_INTEGRITY_CHECK;
+
+    size_t msgSize;
+    uint32_t integrity;
+    msgSize = NVME_MI_MSG_REQUEST_HEADER_SIZE + req->request_data_len +
+              sizeof(integrity);
+
+    if (sizeof(messageBuf) < msgSize)
+    {
+        return EXIT_FAILURE;
+    }
+
+    messageBuf[0] = req->header.message_type;
+    messageBuf[1] = req->header.flags;
+    // Reserved bits 2-3
+
+    messageBuf[4] = req->header.opcode;
+    // reserved bits 5-7
+    messageBuf[8] = req->header.dword0 & 0xff;
+    messageBuf[9] = (req->header.dword0 >> 8) & 0xff;
+    messageBuf[10] = (req->header.dword0 >> 16) & 0xff;
+    messageBuf[11] = (req->header.dword0 >> 24) & 0xff;
+
+    messageBuf[12] = req->header.dword1 & 0xff;
+    messageBuf[13] = (req->header.dword1 >> 8) & 0xff;
+    messageBuf[14] = (req->header.dword1 >> 16) & 0xff;
+    messageBuf[15] = (req->header.dword1 >> 24) & 0xff;
+
+    std::copy(req->request_data, req->request_data + req->request_data_len,
+              messageBuf +
+                  static_cast<uint8_t>(NVME_MI_MSG_REQUEST_HEADER_SIZE));
+
+    msgSize = NVME_MI_MSG_REQUEST_HEADER_SIZE + req->request_data_len;
+    integrity = crc32c(messageBuf,
+                       NVME_MI_MSG_REQUEST_HEADER_SIZE + req->request_data_len);
+    messageBuf[msgSize] = integrity & 0xff;
+    messageBuf[msgSize + 1] = (integrity >> 8) & 0xff;
+    messageBuf[msgSize + 2] = (integrity >> 16) & 0xff;
+    messageBuf[msgSize + 3] = (integrity >> 24) & 0xff;
+    msgSize += sizeof(integrity);
+
+    return mctp_message_tx(mctp, eid, messageBuf, msgSize);
+}
+
+int verifyIntegrity(uint8_t* msg, size_t len)
+{
+    uint32_t msgIntegrity = {0};
+    if (len < NVME_MI_MSG_RESPONSE_HEADER_SIZE + sizeof(msgIntegrity))
+    {
+        std::cerr << "Not enough bytes for nvme header and trailer\n";
+        return -1;
+    }
+
+    msgIntegrity = (msg[len - 4]) + (msg[len - 3] << 8) + (msg[len - 2] << 16) +
+                   (msg[len - 1] << 24);
+
+    uint32_t calculateIntegrity = crc32c(msg, len - sizeof(msgIntegrity));
+    if (msgIntegrity != calculateIntegrity)
+    {
+        std::cerr << "CRC mismatch. Got=" << msgIntegrity
+                  << " Expected=" << calculateIntegrity << "\n";
+        return -1;
+    }
+    return 0;
+}
+
+static double getTemperatureReading(double tempByte)
+{
+    double readingValue = maxReading;
+
+    if (tempByte <= maxReading || tempByte >= minReading)
+    {
+        readingValue = tempByte;
+    }
+    else if (tempByte == 0x80 || tempByte == 0x81)
+    {
+        // 0x80 = No temperature data or temperature data is more the 5 s old
+        // 0x81 = Temperature sensor failure
+        readingValue = maxReading;
+    }
+
+    return readingValue;
+}
+
+void NVMeContext::rxMessage(uint8_t eid, void* data, void* msg, size_t len)
+{
+    struct nvme_mi_msg_response_header header
+    {
+    };
+    uint8_t* messageData{nullptr};
+    size_t messageLength{};
+
+    int count{0};
+
+    if (data == nullptr)
+    {
+        std::cout << "Index not set for the NVMe drive to be updated \n";
+        return;
+    }
+
+    if (msg == nullptr)
+    {
+        std::cerr << "Bad message received\n";
+        return;
+    }
+
+    if (len <= 0)
+    {
+        std::cerr << "Received message not long enough\n";
+        return;
+    }
+
+    messageData = static_cast<uint8_t*>(msg);
+
+    if ((*messageData & NVME_MI_MESSAGE_TYPE_MASK) != NVME_MI_MESSAGE_TYPE)
+    {
+        std::cerr << "Got unknown type message_type="
+                  << (*messageData & NVME_MI_MESSAGE_TYPE_MASK) << "\n";
+        return;
+    }
+
+    if (len < NVME_MI_MSG_RESPONSE_HEADER_SIZE + sizeof(uint32_t))
+    {
+        std::cerr << "Not enough bytes for NVMe header and trailer\n";
+        return;
+    }
+
+    if (verifyIntegrity(messageData, len) != 0)
+    {
+        std::cerr << "Verification of message integrity failed\n";
+        return;
+    }
+
+    header.message_type = messageData[0];
+    header.flags = messageData[1];
+    header.status = messageData[4];
+
+    if (header.status == NVME_MI_HDR_STATUS_MORE_PROCESSING_REQUIRED)
+    {
+        return;
+    }
+
+    if (header.status != NVME_MI_HDR_STATUS_SUCCESS)
+    {
+        std::cerr << "Command failed with status= " << header.status << "\n";
+        return;
+    }
+
+    messageData += NVME_MI_MSG_RESPONSE_HEADER_SIZE;
+    messageLength = len - NVME_MI_MSG_RESPONSE_HEADER_SIZE - sizeof(uint32_t);
+    if (((header.flags >> NVME_MI_HDR_FLAG_MSG_TYPE_SHIFT) &
+         NVME_MI_HDR_FLAG_MSG_TYPE_MASK) != NVME_MI_HDR_MESSAGE_TYPE_MI_COMMAND)
+    {
+        std::cerr << "Not MI type comamnd\n";
+        return;
+    }
+
+    if (messageLength < NVME_MI_HEALTH_STATUS_POLL_MSG_MIN)
+    {
+        std::cerr << "Got improperly sized health status poll\n";
+        return;
+    }
+
+    size_t* index = (size_t*)data;
+
+    if (*index >= 0 && *index <= nvmeDeviceList.size())
+    {
+        NVMeSensor& sensorInfo = nvmeDeviceList[*index].first;
+        if (DEBUG)
+        {
+            std::cout << "Temperature Reading: "
+                      << getTemperatureReading(messageData[5])
+                      << " Celsius for device at index: " << *index << "\n";
+        }
+
+        sensorInfo.updateNVMeReading(getTemperatureReading(messageData[5]));
+    }
+}
+
+NVMeContext::NVMeContext(boost::asio::io_service& io, int bus, int rootBus) :
+    bus(bus), rootBus(rootBus)
+{
+    eid = 0;
+    struct mctp_binding_smbus* smbusTmp = mctp_smbus_init();
+
+    smbus = std::unique_ptr<struct mctp_binding_smbus,
+                            std::function<void(mctp_binding_smbus*)>>(
+        smbusTmp, [](mctp_binding_smbus*) {
+            std::cerr << "Delete libmctp raw ptr\n";
+            // Call libmctp free() here
+        });
+
+    smbusTmp = nullptr;
+
+    mctp = mctp_init();
+
+    int r = mctp_smbus_open_bus(smbus.get(), bus, rootBus);
+    if (r < 0)
+    {
+        std::cerr << "Error while opening the bus\n";
+        return;
+    }
+
+    mctp_smbus_register_bus(smbus.get(), mctp, eid);
+
+    mctp_set_rx_all(mctp, rxMessage, &sindex);
+
+    nvmeSlaveSocket = std::make_shared<boost::asio::ip::tcp::socket>(io);
+
+    nvmeSlaveSocket->assign(boost::asio::ip::tcp::v4(),
+                            mctp_smbus_get_in_fd(smbus.get()));
+}
+
+NVMeSensor::NVMeSensor(const std::string& path,
+                       sdbusplus::asio::object_server& objectServer,
+                       std::shared_ptr<sdbusplus::asio::connection>& conn,
+                       boost::asio::io_service& io,
+                       const std::string& sensorName,
+                       std::vector<thresholds::Threshold>&& _thresholds,
+                       const std::string& sensorConfiguration, size_t bus) :
+    Sensor(boost::replace_all_copy(sensorName, " ", "_"),
+           std::move(_thresholds), sensorConfiguration,
+           "xyz.openbmc_project.Configuration.NVMe", maxReading, minReading),
+    bus(bus)
+{
+    sensorInterface = objectServer.add_interface(
+        "/xyz/openbmc_project/sensors/temperature/" + name,
+        "xyz.openbmc_project.Sensor.Value");
+
+    setInitialProperties(conn);
+    // setup match
+    setupPowerMatch(conn);
+}
+
+NVMeSensor::~NVMeSensor()
+{
+}
+
+void NVMeSensor::checkThresholds(void)
+{
+    thresholds::checkThresholds(this);
+}
+
+void NVMeSensor::updateNVMeReading(const double& value)
+{
+    updateValue(value);
+}
---
+++ src/NVMeSensorMain.cpp
@@ -0,0 +1,232 @@
+/*
+// Copyright (c) 2017 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#include <NVMeSensor.hpp>
+#include <Utils.hpp>
+#include <VariantVisitors.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/container/flat_set.hpp>
+#include <regex>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+
+static std::string sensorType = "xyz.openbmc_project.Configuration.NVME1000";
+static std::regex driveRegex(R"((Pcie|M2|U2)_Slot)");
+static std::regex busRegex(R"(\w[^-]*$)");
+static std::regex rootBusPattern(R"(.?\/?(\d+)-\d+)");
+
+std::vector<std::pair<NVMeSensor, std::shared_ptr<struct NVMeContext>>>
+    nvmeDeviceList;
+
+void printNvmeDeviceList()
+{
+    for (auto it = nvmeDeviceList.begin(); it != nvmeDeviceList.end(); it++)
+    {
+        std::cout << "-------------------------------"
+                  << "\nBus: " << it->second->bus
+                  << "\nRootBus: " << it->second->rootBus
+                  << "\nNvmeSlaveSocket: " << it->second->nvmeSlaveSocket
+                  << "\nIn_fd: "
+                  << mctp_smbus_get_in_fd(it->second->smbus.get()) << "\n";
+    }
+}
+
+void createSensors(
+    boost::asio::io_service& io, sdbusplus::asio::object_server& objectServer,
+    std::shared_ptr<sdbusplus::asio::connection>& dbusConnection,
+    const std::unique_ptr<boost::container::flat_set<std::string>>&
+        sensorsChanged)
+{
+    ManagedObjectType sensorConfigurations;
+    bool useCache = false;
+
+    if (!getSensorConfiguration(sensorType, dbusConnection,
+                                sensorConfigurations, useCache))
+    {
+        std::cerr << "Error communicating with Entity Manager";
+        return;
+    }
+    useCache = true;
+
+    std::vector<std::filesystem::path> paths;
+    if (!findFiles(std::filesystem::path("/dev/i2c-mux"),
+                   R"((Pcie|M2|U2)_Slot)", paths))
+    {
+        return;
+    }
+
+    // iterate through all found NVMe drives and match them with configuration
+    for (auto& path : paths)
+    {
+        std::smatch match;
+        const SensorData* sensorData = nullptr;
+        const std::string* interfacePath = nullptr;
+        const std::pair<std::string, boost::container::flat_map<
+                                         std::string, BasicVariantType>>*
+            baseConfiguration;
+
+        std::string busPath = std::filesystem::read_symlink(path);
+
+        std::regex_search(busPath, match, busRegex);
+
+        if (match.empty())
+        {
+            continue;
+        }
+
+        std::string busStr = *(match.begin());
+        size_t bus = std::stoul(busStr);
+        int rootBus = 0;
+
+        for (const std::pair<sdbusplus::message::object_path, SensorData>&
+                 sensor : sensorConfigurations)
+        {
+            // clear it out each loop
+            baseConfiguration = nullptr;
+
+            // find base configuration
+            auto sensorBase = sensor.second.find(sensorType);
+            if (sensorBase != sensor.second.end())
+            {
+                baseConfiguration = &(*sensorBase);
+            }
+
+            if (baseConfiguration == nullptr)
+            {
+                continue;
+            }
+            auto findBus = baseConfiguration->second.find("Bus");
+            if (findBus == baseConfiguration->second.end())
+            {
+                continue;
+            }
+
+            unsigned int busNumber =
+                std::visit(VariantToUnsignedIntVisitor(), findBus->second);
+
+            if (busNumber != bus)
+            {
+                continue;
+            }
+            sensorData = &(sensor.second);
+            interfacePath = &(sensor.first.str);
+            break;
+        }
+        if (sensorData == nullptr)
+        {
+            std::cerr << "failed to find match for " << path.string() << "\n";
+            continue;
+        }
+
+        if (baseConfiguration == nullptr)
+        {
+            std::cerr << "error finding base configuration for" << path.string()
+                      << "\n";
+            continue;
+        }
+
+        auto findSensorName = baseConfiguration->second.find("Name");
+        if (findSensorName == baseConfiguration->second.end())
+        {
+            std::cerr << "could not determine configuration name for "
+                      << path.string() << "\n";
+            continue;
+        }
+
+        std::string rootPath =
+            "/sys/bus/i2c/devices/i2c-" + std::to_string(bus) + "/mux_device";
+
+        if (std::filesystem::exists(rootPath) &&
+            std::filesystem::is_symlink(rootPath))
+        {
+            std::smatch match{};
+            std::string search =
+                std::filesystem::read_symlink(rootPath).string();
+            if (std::regex_search(search, match, rootBusPattern))
+            {
+                rootBus = stoi(match.str(1));
+            }
+        }
+
+        std::vector<thresholds::Threshold> sensorThresholds;
+        std::string sensorName = std::get<std::string>(findSensorName->second);
+
+        NVMeSensor sensor = NVMeSensor(
+            path.string(), objectServer, dbusConnection, io, sensorName,
+            std::move(sensorThresholds), *interfacePath, bus);
+
+        nvmeDeviceList.emplace_back(
+            std::make_pair<NVMeSensor, std::shared_ptr<struct NVMeContext>>(
+                std::move(sensor),
+                std::make_shared<struct NVMeContext>(io, bus, rootBus)));
+
+        if (DEBUG)
+        {
+            printNvmeDeviceList();
+        }
+    }
+}
+
+int main(int argc, char** argv)
+{
+    boost::asio::io_service io;
+    auto systemBus = std::make_shared<sdbusplus::asio::connection>(io);
+    systemBus->request_name("xyz.openbmc_project.NVMeSensor");
+    sdbusplus::asio::object_server objectServer(systemBus);
+    std::vector<std::unique_ptr<sdbusplus::bus::match::match>> matches;
+    std::unique_ptr<boost::container::flat_set<std::string>> sensorsChanged =
+        std::make_unique<boost::container::flat_set<std::string>>();
+
+    boost::asio::deadline_timer mctpResponseTimer(io);
+
+    io.post([&]() { createSensors(io, objectServer, systemBus, nullptr); });
+
+    boost::asio::deadline_timer filterTimer(io);
+    std::function<void(sdbusplus::message::message&)> eventHandler =
+        [&](sdbusplus::message::message& message) {
+            if (message.is_method_error())
+            {
+                std::cerr << "callback method error\n";
+                return;
+            }
+            sensorsChanged->insert(message.get_path());
+            // this implicitly cancels the timer
+            filterTimer.expires_from_now(boost::posix_time::seconds(1));
+
+            filterTimer.async_wait([&](const boost::system::error_code& ec) {
+                if (ec)
+                {
+                    std::cerr << "Error: " << ec.message() << "\n";
+                    return;
+                }
+
+                createSensors(io, objectServer, systemBus, sensorsChanged);
+            });
+        };
+
+    auto match = std::make_unique<sdbusplus::bus::match::match>(
+        static_cast<sdbusplus::bus::bus&>(*systemBus),
+        "type='signal',member='PropertiesChanged',path_namespace='" +
+            std::string(inventoryPath) + "',arg0namespace='" + sensorType + "'",
+        eventHandler);
+    matches.emplace_back(std::move(match));
+
+    boost::asio::deadline_timer scanTimer(io);
+
+    io.post([&]() { pollNVMeDevices(io, scanTimer, mctpResponseTimer); });
+
+    io.run();
+}
