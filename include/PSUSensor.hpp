a7dd6ef0510441d214cde4c4820a8bb91809f7fb
Cheng C Yang <cheng.c.yang@linux.intel.com>
2019-04-09 23:46:30 +0800

    Add PSU PWM sensor to support FSC
    Add PSU PWM sensor to allow FSC control PSU fan pwm.
    
    Tested By:
    Change property Target to 100 in dbus interface xyz.openbmc_project.Control.FanPwm
    path /xyz/openbmc_project/control/fanpwm/Pwm_PSU1_Fan_1, the value in
    /sys/class/hwmon/hwmonx/fan1_target also become 100, and value property in interface
    /xyz/openbmc_project/sensors/fan_pwm/Pwm_PSU1_Fan_1 become 39.2157
    
    Run ipmitool sensor list, bellow sensors will show like:
    Pwm PSU1 Fan 1   | 39.200     | unspecified | ok    | na        | na        | na        | na        | na        | na
    Pwm PSU2 Fan 1   | 0.000      | unspecified | ok    | na        | na        | na        | na        | na        | na
    
    Signed-off-by: Cheng C Yang <cheng.c.yang@linux.intel.com>
    Change-Id: I6a4d50912a190297dbcf4a4e5c511607f71cb0c0

--- include/PSUSensor.hpp
+++ include/PSUSensor.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <PwmSensor.hpp>
 #include <Thresholds.hpp>
 #include <sdbusplus/asio/object_server.hpp>
 #include <sensor.hpp>
@@ -46,4 +47,17 @@ class PSUProperty
     double maxReading;
     double minReading;
     unsigned int sensorScaleFactor;
+};
+
+class PSUPWMSensor : public PwmSensor
+{
+  public:
+    PSUPWMSensor(const std::string& targetPath,
+                 sdbusplus::asio::object_server& objectServer,
+                 const std::string& sensorConfiguration,
+                 const std::string& pwmName);
+
+    void setValue(uint32_t value) override;
+    uint32_t getValue(bool errThrow) override;
+    std::string targetPath;
 };
--- include/PwmSensor.hpp
+++ include/PwmSensor.hpp
@@ -10,6 +10,8 @@ class PwmSensor
               const std::string& sensorConfiguration);
     ~PwmSensor();
 
+    virtual void setValue(uint32_t value);
+    virtual uint32_t getValue(bool errThrow = true);
   private:
     std::string sysPath;
     sdbusplus::asio::object_server& objectServer;
@@ -17,6 +19,4 @@ class PwmSensor
     std::shared_ptr<sdbusplus::asio::dbus_interface> sensorInterface;
     std::shared_ptr<sdbusplus::asio::dbus_interface> controlInterface;
     std::shared_ptr<sdbusplus::asio::dbus_interface> association;
-    void setValue(uint32_t value);
-    uint32_t getValue(bool errThrow = true);
 };
