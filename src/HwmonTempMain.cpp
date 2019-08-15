3546adb92b6fb36322e9bfb60c722ca2af9478d6
Patrick Venture <venture@google.com>
2019-08-14 18:35:29 -0700

    hwmontemp: add tmp175
    Signed-off-by: Patrick Venture <venture@google.com>
    Change-Id: I79d3e2dfadd9af26e5fd7f80bdd3417d3b1202a4

--- src/HwmonTempMain.cpp
+++ src/HwmonTempMain.cpp
@@ -28,11 +28,12 @@
 static constexpr bool DEBUG = false;
 
 namespace fs = std::filesystem;
-static constexpr std::array<const char*, 6> sensorTypes = {
+static constexpr std::array<const char*, 7> sensorTypes = {
     "xyz.openbmc_project.Configuration.TMP75",
     "xyz.openbmc_project.Configuration.TMP421",
     "xyz.openbmc_project.Configuration.TMP441",
     "xyz.openbmc_project.Configuration.TMP112",
+    "xyz.openbmc_project.Configuration.TMP175",
     "xyz.openbmc_project.Configuration.EMC1413",
     "xyz.openbmc_project.Configuration.MAX31725"};
 
