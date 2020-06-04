4ee716c129a49edb2db2f7010d1740ccdb19f8ea
Adrian Ambrożewicz <adrian.ambrozewicz@linux.intel.com>
2020-06-04 13:59:55 +0200

    Support retrieving 'utilization' from ME
    ME exposes sensors which express host CPU utilization in range 0-255.
    This change introduces following things:
    - 'MESensor' sensor type introduced alongside legacy 'METemp' for clarity
    - ability to expose 'utilization' sensor with range 0-100 (percent)
    - scaling utilization value from 0-255 to 0-100
    
    Testing:
    - added following configuration on platform:
    {
      "Address": 190,
      "Class": "MESensor",
      "Name": "Host CPU Core Utilization",
      "SensorType": "utilization",
      "Type": "IpmbSensor"
    }
    - configuration properly loaded
    - sensors appear on D-Bus and Redfish:
    {
        "@odata.id": "/redfish/v1/Chassis/WC_Baseboard/Sensors/Host_CPU_Core_Utilization",
        "@odata.type": "#Sensor.v1_0_0.Sensor",
        "Id": "Host_CPU_Core_Utilization",
        "Name": "Host CPU Core Utilization",
        "Reading": 98.82352941176471,
        "ReadingRangeMax": 100.0,
        "ReadingRangeMin": 0.0,
        "ReadingUnits": "Percent",
        "Status": {
            "Health": "OK",
            "State": "Enabled"
        }
    }
    
    Change-Id: I49a14820e1b72f9776bd24ba1584d81ee0c95cd5
    Signed-off-by: Adrian Ambrożewicz <adrian.ambrozewicz@linux.intel.com>

--- include/IpmbSensor.hpp
+++ include/IpmbSensor.hpp
@@ -25,7 +25,8 @@ enum class IpmbSubType
     temp,
     curr,
     power,
-    volt
+    volt,
+    util
 };
 
 struct IpmbSensor : public Sensor
@@ -35,7 +36,8 @@ struct IpmbSensor : public Sensor
                const std::string& sensorConfiguration,
                sdbusplus::asio::object_server& objectServer,
                std::vector<thresholds::Threshold>&& thresholds,
-               uint8_t deviceAddress, std::string& sensorTypeName);
+               uint8_t deviceAddress, std::string& sensorTypeName,
+               const double maxValue, const double minValue);
     ~IpmbSensor();
 
     void checkThresholds(void) override;
--- src/IpmbSensor.cpp
+++ src/IpmbSensor.cpp
@@ -65,11 +65,11 @@ IpmbSensor::IpmbSensor(std::shared_ptr<sdbusplus::asio::connection>& conn,
                        const std::string& sensorConfiguration,
                        sdbusplus::asio::object_server& objectServer,
                        std::vector<thresholds::Threshold>&& thresholdData,
-                       uint8_t deviceAddress, std::string& sensorTypeName) :
+                       uint8_t deviceAddress, std::string& sensorTypeName,
+                       const double maxValue, const double minValue) :
     Sensor(boost::replace_all_copy(sensorName, " ", "_"),
            std::move(thresholdData), sensorConfiguration,
-           "xyz.openbmc_project.Configuration.ExitAirTemp", ipmbMaxReading,
-           ipmbMinReading),
+           "xyz.openbmc_project.Configuration.ExitAirTemp", maxValue, minValue),
     deviceAddress(deviceAddress), readState(PowerState::on),
     objectServer(objectServer), dbusConnection(conn), waitTimer(io)
 {
@@ -341,6 +341,13 @@ void IpmbSensor::read(void)
 
                 /* Adjust value as per scale and offset */
                 value = (value * scaleVal) + offsetVal;
+
+                /* Scale to percent */
+                if (type == IpmbType::meSensor && subType == IpmbSubType::util)
+                {
+                    value = value * 100 / ipmbMaxReading;
+                }
+
                 updateValue(value);
                 read();
                 errorCount = 0; // success
@@ -402,10 +409,21 @@ void createSensors(
                     }
 
                     auto& sensor = sensors[name];
+
+                    double maxValue = ipmbMaxReading;
+                    double minValue = ipmbMinReading;
+                    if (sensorClass == "MESensor" &&
+                        sensorTypeName == "utilization")
+                    {
+                        // Utilization need to be scaled back to percent
+                        maxValue = 100;
+                        minValue = 0;
+                    }
+
                     sensor = std::make_unique<IpmbSensor>(
                         dbusConnection, io, name, pathPair.first, objectServer,
                         std::move(sensorThresholds), deviceAddress,
-                        sensorTypeName);
+                        sensorTypeName, maxValue, minValue);
 
                     /* Initialize scale and offset value */
                     sensor->scaleVal = 1;
@@ -451,7 +469,8 @@ void createSensors(
                     {
                         sensor->type = IpmbType::mpsVR;
                     }
-                    else if (sensorClass == "METemp")
+                    else if (sensorClass == "METemp" ||
+                             sensorClass == "MESensor")
                     {
                         sensor->type = IpmbType::meSensor;
                     }
@@ -472,6 +491,10 @@ void createSensors(
                     else if (sensorTypeName == "current")
                     {
                         sensor->subType = IpmbSubType::curr;
+                    }
+                    else if (sensorTypeName == "utilization")
+                    {
+                        sensor->subType = IpmbSubType::util;
                     }
                     else
                     {
