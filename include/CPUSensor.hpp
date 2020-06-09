90fe4e030ade2d6a2ed02923effdba4ccfc295e2
Zbigniew Kurzynski <zbigniew.kurzynski@intel.com>
2020-06-09 12:42:15 +0200

    Refactoring of the updateValue method
    Purpose of these changes is to make the method created to update any dbus
    parameters, not only the `Value`, available for all classes that inherit
    from the Sensor class.
    
    This commit does following changes:
    * method areDifferent is moved from the CPUSensor class to the Sensor.
    * method genericUpdateValue is renamed to updateProperty and moved from
      the CPUSensor class to the Sensor.
    * method updateValue is changed to utilize the updateProperty method.
    
    Tested:
        Manual tests where done on CPUSensor. No differences detected comparing
        to results taken before these changes.
    
    Signed-off-by: Zbigniew Kurzynski <zbigniew.kurzynski@intel.com>
    Change-Id: I736526c0a128ffe094e156018fca6a883659f82a

--- include/CPUSensor.hpp
+++ include/CPUSensor.hpp
@@ -49,9 +49,6 @@ class CPUSensor : public Sensor
     void handleResponse(const boost::system::error_code& err);
     void checkThresholds(void) override;
     void updateMinMaxValues(void);
-    bool areDifferent(const double& lVal, const double& rVal);
-    void genericUpdateValue(double& oldValue, const double& newValue,
-                            const char* dbusParamName);
 };
 
 extern boost::container::flat_map<std::string, std::unique_ptr<CPUSensor>>
