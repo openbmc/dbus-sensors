26d944967a4d4635e923e6dfd8ac1ed54f2e1bf9
Josh Lehan <krellan@google.com>
2020-09-02 11:48:14 -0700

    ExternalSensor: New service to support external sensors
    This code was mostly copied from HwmonTempSensor
    and the initial proof of concept from
    https://gerrit.openbmc-project.xyz/c/openbmc/dbus-sensors/+/35476
    
    External sensors are designed to be placeholders,
    with no hwmon or other hardware support locally.
    Thus, there is no detection or kernel driver load,
    so no change is necessary to entity-manager.
    
    The expectation is that these sensors will be updated
    by something external to the BMC, such as the host,
    which can be useful for including host-based sensors
    into phosphor-pid-control thermal zones.
    
    Added systemd service file, similar to the others
    Added CMakeLists blocks, similar to the others
    Also cleaned up some nearby formatting in CMakeLists
    
    Tested: It works, and with some work elsewhere,
    the external sensors now appear in the IPMI SDR.
    
    Here is example JSON for use with entity-manager:
    {
      "Name": "My_Thermal_Zone",
      "Prefix": "margin",
      "EntityId": "0x1E",
      "EntityInstance": 0,
      "Type": "ExternalSensor"
    },
    
    Signed-off-by: Josh Lehan <krellan@google.com>
    Change-Id: Id499e1cd6c7918179bb1729e5ce8546cf4121dfa

---
+++ src/ExternalSensorMain.cpp
@@ -0,0 +1,230 @@
+#include "ExternalSensor.hpp"
+#include "Utils.hpp"
+
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/algorithm/string/replace.hpp>
+#include <boost/container/flat_map.hpp>
+#include <boost/container/flat_set.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+#include <sdbusplus/bus/match.hpp>
+
+#include <array>
+#include <filesystem>
+#include <fstream>
+#include <functional>
+#include <memory>
+#include <regex>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <variant>
+#include <vector>
+
+static constexpr bool DEBUG = false;
+
+static constexpr std::array<const char*, 1> sensorTypes = {
+    "xyz.openbmc_project.Configuration.ExternalSensor",
+};
+
+// Copied from HwmonTempSensor and inspired by
+// https://gerrit.openbmc-project.xyz/c/openbmc/dbus-sensors/+/35476
+
+// The ExternalSensor is a trivial sensor,
+// merely a placeholder, not corresponding to any hwmon file,
+// or anything else on the local filesystem.
+// It is intended to be settable by something external to the BMC,
+// so that the host (or something else external) can write to it,
+// perhaps by using an IPMI connection.
+
+// Do not validate configuration against hwmon or any driver,
+// instead, blindly accept all configuration from the JSON parameters:
+// MinValue, MaxValue, Prefix, Name, PowerState
+
+// The purpose of "Prefix" is to provide additional, optional,
+// path components, as required by phosphor-pid-control usage,
+// for example: "/xyz/openbmc_project/extsensors/margin/fleeting0"
+// In this case, set Prefix="margin" and Name="fleeting0" in JSON.
+
+void createSensors(
+    boost::asio::io_service& io, sdbusplus::asio::object_server& objectServer,
+    boost::container::flat_map<std::string, std::shared_ptr<ExternalSensor>>&
+        sensors,
+    std::shared_ptr<sdbusplus::asio::connection>& dbusConnection,
+    const std::shared_ptr<boost::container::flat_set<std::string>>&
+        sensorsChanged)
+{
+    auto getter = std::make_shared<GetSensorConfiguration>(
+        dbusConnection,
+        [&io, &objectServer, &sensors, &dbusConnection,
+         sensorsChanged](const ManagedObjectType& sensorConfigurations) {
+            bool firstScan = (sensorsChanged == nullptr);
+
+            for (const std::pair<sdbusplus::message::object_path, SensorData>&
+                     sensor : sensorConfigurations)
+            {
+                const std::string* interfacePath = &(sensor.first.str);
+                const SensorData* sensorData = &(sensor.second);
+
+                const char* sensorType = nullptr;
+                const SensorBaseConfiguration* baseConfiguration = nullptr;
+                const SensorBaseConfigMap* baseConfigMap = nullptr;
+
+                for (const char* type : sensorTypes)
+                {
+                    auto sensorBase = sensorData->find(type);
+                    if (sensorBase != sensorData->end())
+                    {
+                        sensorType = type;
+                        baseConfiguration = &(*sensorBase);
+                        break;
+                    }
+                }
+                if (baseConfiguration == nullptr)
+                {
+                    std::cerr << "error finding base configuration for "
+                              << *interfacePath << "\n";
+                    continue;
+                }
+
+                baseConfigMap = &(baseConfiguration->second);
+                auto configurationMin = baseConfigMap->find("MinValue");
+                auto configurationMax = baseConfigMap->find("MaxValue");
+
+                // Range of signed byte seems reasonable default
+                double minValue = std::numeric_limits<int8_t>::min();
+                double maxValue = std::numeric_limits<int8_t>::max();
+
+                if (configurationMin != baseConfigMap->end())
+                {
+                    minValue = std::get<double>(configurationMin->second);
+                }
+                if (configurationMax != baseConfigMap->end())
+                {
+                    maxValue = std::get<double>(configurationMax->second);
+                }
+
+                auto findSensorName = baseConfigMap->find("Name");
+                if (findSensorName == baseConfigMap->end())
+                {
+                    std::cerr << "could not determine configuration name for "
+                              << *interfacePath << "\n";
+                    continue;
+                }
+
+                std::string sensorName =
+                    std::get<std::string>(findSensorName->second);
+
+                auto findPrefix = baseConfigMap->find("Prefix");
+                std::string sensorPrefix;
+                if (findPrefix != baseConfigMap->end())
+                {
+                    sensorPrefix = std::get<std::string>(findPrefix->second);
+                }
+
+                // on rescans, only update sensors we were signaled by
+                auto findSensor = sensors.find(sensorName);
+                if (!firstScan && (findSensor != sensors.end()))
+                {
+                    bool found = false;
+                    for (auto it = sensorsChanged->begin();
+                         it != sensorsChanged->end(); it++)
+                    {
+                        if (boost::ends_with(*it, findSensor->second->name))
+                        {
+                            sensorsChanged->erase(it);
+                            findSensor->second = nullptr;
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found)
+                    {
+                        continue;
+                    }
+                }
+
+                std::vector<thresholds::Threshold> sensorThresholds;
+                if (!parseThresholdsFromConfig(*sensorData, sensorThresholds))
+                {
+                    std::cerr << "error populating thresholds for "
+                              << sensorName << "\n";
+                }
+
+                auto findPowerOn = baseConfiguration->second.find("PowerState");
+                PowerState readState = PowerState::always;
+                if (findPowerOn != baseConfiguration->second.end())
+                {
+                    std::string powerState = std::visit(
+                        VariantToStringVisitor(), findPowerOn->second);
+                    setReadState(powerState, readState);
+                }
+
+                auto& sensorEntry = sensors[sensorName];
+                sensorEntry = nullptr;
+
+                sensorEntry = std::make_shared<ExternalSensor>(
+                    sensorType, objectServer, dbusConnection, io, sensorName,
+                    sensorPrefix, std::move(sensorThresholds), *interfacePath,
+                    maxValue, minValue, readState);
+            }
+        });
+
+    getter->getConfiguration(
+        std::vector<std::string>(sensorTypes.begin(), sensorTypes.end()));
+}
+
+int main()
+{
+    boost::asio::io_service io;
+    auto systemBus = std::make_shared<sdbusplus::asio::connection>(io);
+    systemBus->request_name("xyz.openbmc_project.ExternalSensor");
+    sdbusplus::asio::object_server objectServer(systemBus);
+    boost::container::flat_map<std::string, std::shared_ptr<ExternalSensor>>
+        sensors;
+    std::vector<std::unique_ptr<sdbusplus::bus::match::match>> matches;
+    auto sensorsChanged =
+        std::make_shared<boost::container::flat_set<std::string>>();
+
+    io.post([&]() {
+        createSensors(io, objectServer, sensors, systemBus, nullptr);
+    });
+
+    boost::asio::deadline_timer filterTimer(io);
+    std::function<void(sdbusplus::message::message&)> eventHandler =
+        [&](sdbusplus::message::message& message) {
+            if (message.is_method_error())
+            {
+                std::cerr << "callback method error\n";
+                return;
+            }
+            sensorsChanged->insert(message.get_path());
+            // this implicitly cancels the timer
+            filterTimer.expires_from_now(boost::posix_time::seconds(1));
+
+            filterTimer.async_wait([&](const boost::system::error_code& ec) {
+                if (ec)
+                {
+                    if (ec != boost::asio::error::operation_aborted)
+                    {
+                        std::cerr << "callback error: " << ec.message() << "\n";
+                    }
+                    return;
+                }
+                createSensors(io, objectServer, sensors, systemBus,
+                              sensorsChanged);
+            });
+        };
+
+    for (const char* type : sensorTypes)
+    {
+        auto match = std::make_unique<sdbusplus::bus::match::match>(
+            static_cast<sdbusplus::bus::bus&>(*systemBus),
+            "type='signal',member='PropertiesChanged',path_namespace='" +
+                std::string(inventoryPath) + "',arg0namespace='" + type + "'",
+            eventHandler);
+        matches.emplace_back(std::move(match));
+    }
+
+    io.run();
+}
