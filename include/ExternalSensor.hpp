b742ceb9ccc0d91a938cb58e69355502adde9795
Josh Lehan <krellan@google.com>
2020-09-02 11:48:14 -0700

    ExternalSensor: New service to support external sensors
    This code was mostly copied from HwmonTempSensor
    and the initial proof of concept from
    https://gerrit.openbmc-project.xyz/c/openbmc/dbus-sensors/+/35476
    
    External sensors are designed to be placeholders,
    with no hwmon or other hardware support locally.
    Thus, there is no detection or kernel driver load,
    so no change is necessary to entity-manager.
    
    The expectation is that these sensors will be updated
    by something external to the BMC, such as the host,
    which can be useful for including host-based sensors
    into phosphor-pid-control thermal zones.
    
    Added systemd service file, similar to the others
    Added CMakeLists blocks, similar to the others
    Also cleaned up some nearby formatting in CMakeLists
    
    Tested: It works, and with some work elsewhere,
    the external sensors now appear in the IPMI SDR.
    
    Here is example JSON for use with entity-manager:
    {
      "Name": "My_Thermal_Zone",
      "Prefix": "margin",
      "EntityId": "0x1E",
      "EntityInstance": 0,
      "Type": "ExternalSensor"
    },
    
    Signed-off-by: Josh Lehan <krellan@google.com>
    Change-Id: Id499e1cd6c7918179bb1729e5ce8546cf4121dfa

---
+++ include/ExternalSensor.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Thresholds.hpp"
+#include "sensor.hpp"
+
+#include <sdbusplus/asio/object_server.hpp>
+
+#include <string>
+#include <vector>
+
+class ExternalSensor :
+    public Sensor,
+    public std::enable_shared_from_this<ExternalSensor>
+{
+  public:
+    ExternalSensor(const std::string& objectType,
+                   sdbusplus::asio::object_server& objectServer,
+                   std::shared_ptr<sdbusplus::asio::connection>& conn,
+                   const std::string& sensorName,
+                   const std::string& sensorPrefix,
+                   std::vector<thresholds::Threshold>&& _thresholds,
+                   const std::string& sensorConfiguration,
+                   const double& maxReading, const double& minReading,
+                   const PowerState& powerState);
+    virtual ~ExternalSensor();
+
+  private:
+    sdbusplus::asio::object_server& objServer;
+
+    void checkThresholds(void) override;
+};
