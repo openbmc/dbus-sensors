41e2d32a55d06d3a82bb3a2bb22d8cc77c2f8d6d
Josh Lehan <krellan@google.com>
2020-09-02 11:48:14 -0700

    ExternalSensor: New service to support external sensors
    This code was mostly copied from HwmonTempSensor
    and the initial proof of concept from
    https://gerrit.openbmc-project.xyz/c/openbmc/dbus-sensors/+/35476
    
    External sensors are designed to be placeholders,
    with no hwmon or other hardware support locally.
    Thus, there is no detection or kernel driver load,
    so no change is necessary to entity-manager.
    
    The expectation is that these sensors will be updated
    by something external to the BMC, such as the host,
    which can be useful for including host-based sensors
    into phosphor-pid-control thermal zones.
    
    Added systemd service file, similar to the others
    Added CMakeLists blocks, similar to the others
    Also cleaned up some nearby formatting in CMakeLists
    
    Tested: It works, and with some work elsewhere,
    the external sensors now appear in the IPMI SDR.
    
    Here is example JSON for use with entity-manager:
    {
      "Name": "My_Thermal_Zone",
      "Prefix": "margin",
      "EntityId": "0x1E",
      "EntityInstance": 0,
      "Type": "ExternalSensor"
    },
    
    Signed-off-by: Josh Lehan <krellan@google.com>
    Change-Id: Id499e1cd6c7918179bb1729e5ce8546cf4121dfa

---
+++ src/ExternalSensor.cpp
@@ -0,0 +1,71 @@
+#include "ExternalSensor.hpp"
+
+#include <unistd.h>
+
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/algorithm/string/replace.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+
+#include <iostream>
+#include <istream>
+#include <limits>
+#include <memory>
+#include <string>
+#include <vector>
+
+ExternalSensor::ExternalSensor(
+    const std::string& objectType, sdbusplus::asio::object_server& objectServer,
+    std::shared_ptr<sdbusplus::asio::connection>& conn,
+    boost::asio::io_service& io, const std::string& sensorName,
+    const std::string& sensorPrefix,
+    std::vector<thresholds::Threshold>&& _thresholds,
+    const std::string& sensorConfiguration, const double& maxReading,
+    const double& minReading, const PowerState& powerState) :
+    Sensor(boost::replace_all_copy(sensorName, " ", "_"),
+           std::move(_thresholds), sensorConfiguration, objectType, maxReading,
+           minReading, powerState),
+    std::enable_shared_from_this<ExternalSensor>(), objServer(objectServer)
+{
+    // This matches the syntax expected by phosphor-pid-control
+    // Optionally insert another path component before the sensor name
+    std::string objectPath = "/xyz/openbmc_project/extsensors/";
+    if (!(sensorPrefix.empty()))
+    {
+        objectPath += sensorPrefix;
+        objectPath += '/';
+    }
+    objectPath += sensorName;
+
+    sensorInterface = objectServer.add_interface(
+        objectPath, "xyz.openbmc_project.Sensor.Value");
+
+    if (thresholds::hasWarningInterface(thresholds))
+    {
+        thresholdInterfaceWarning = objectServer.add_interface(
+            objectPath, "xyz.openbmc_project.Sensor.Threshold.Warning");
+    }
+    if (thresholds::hasCriticalInterface(thresholds))
+    {
+        thresholdInterfaceCritical = objectServer.add_interface(
+            objectPath, "xyz.openbmc_project.Sensor.Threshold.Critical");
+    }
+
+    association =
+        objectServer.add_interface(objectPath, association::interface);
+    setInitialProperties(conn);
+}
+
+ExternalSensor::~ExternalSensor()
+{
+    objServer.remove_interface(association);
+    objServer.remove_interface(thresholdInterfaceCritical);
+    objServer.remove_interface(thresholdInterfaceWarning);
+    objServer.remove_interface(sensorInterface);
+}
+
+void ExternalSensor::checkThresholds(void)
+{
+    thresholds::checkThresholds(this);
+}
