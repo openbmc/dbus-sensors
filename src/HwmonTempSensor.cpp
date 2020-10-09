0b8725389e30b51a7fadf77f1447246c595e3800
Ed Tanous <ed@tanous.net>
2020-10-09 12:46:52 -0700

    add clang-tidy
    This commit implements a clang-tidy file, and makes some changes to get
    it to pass.  Most changes are naming or mechanical in nature.
    
    Tested:
    Clang-tidy now passes.
    
    Signed-off-by: Ed Tanous <ed@tanous.net>
    Change-Id: Ia441e4801b6c8725421d160c531c5df141f255d4

--- src/HwmonTempSensor.cpp
+++ src/HwmonTempSensor.cpp
@@ -14,10 +14,9 @@
 // limitations under the License.
 */
 
-#include "HwmonTempSensor.hpp"
-
 #include <unistd.h>
 
+#include <HwmonTempSensor.hpp>
 #include <boost/algorithm/string/predicate.hpp>
 #include <boost/algorithm/string/replace.hpp>
 #include <boost/asio/read_until.hpp>
@@ -44,11 +43,11 @@ HwmonTempSensor::HwmonTempSensor(
     sdbusplus::asio::object_server& objectServer,
     std::shared_ptr<sdbusplus::asio::connection>& conn,
     boost::asio::io_service& io, const std::string& sensorName,
-    std::vector<thresholds::Threshold>&& _thresholds,
+    std::vector<thresholds::Threshold>&& thresholds,
     const std::string& sensorConfiguration, const PowerState powerState) :
-    Sensor(boost::replace_all_copy(sensorName, " ", "_"),
-           std::move(_thresholds), sensorConfiguration, objectType, maxReading,
-           minReading, conn, powerState),
+    Sensor(boost::replace_all_copy(sensorName, " ", "_"), std::move(thresholds),
+           sensorConfiguration, objectType, maxReading, minReading, conn,
+           powerState),
     std::enable_shared_from_this<HwmonTempSensor>(), objServer(objectServer),
     inputDev(io, open(path.c_str(), O_RDONLY)), waitTimer(io), path(path)
 {
