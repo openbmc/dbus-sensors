d4999c42f7f7bf83b8e1584cdfb78a98b88ab716
Zhikui Ren <zhikui.ren@intel.com>
2020-12-03 15:14:49 -0800

    Fix ExitAirTempSensor calculation
    When calculating maxRPM, variables tachMaxPercent and tachMinPercent
    are in percentage. Both tachMaxReading and tachMinReading are already
    specified as percentage in configuration file.
    
    When cfm is less than 50% of QMin, exit air temp formula does not
    work correctly. Make exit air temp not available.
    
    Use steady_clock to ensure time elapsed is non-negative.
    
    Increase the error print count to 5 during exit air temp computation.
    This change allows power reading related error messages to be logged
    in addition to fan sensor reading errors during same computation cycle.
    
    This patch also includes minor clean up that moves powerReading
    to private member.
    
    Tested:
    Before the change, exit air temp reports critical event occasionally on power up.
    Examples:
    Exit_Air_Temp sensor crossed a critical high threshold going high. Reading=13938.000000 Threshold=85.000000
    Exit_Air_Temp sensor crossed a critical low threshold going low. Reading=-119240.000000 Threshold=0.000000
    
    Same AC cycle test was run with the change, the issue is resolved.
    
    Signed-off-by: Zhikui Ren <zhikui.ren@intel.com>
    Change-Id: Idf9c2aa916ac741ff047c5baea51a664c101c33d

--- src/ExitAirTempSensor.cpp
+++ src/ExitAirTempSensor.cpp
@@ -38,6 +38,7 @@
 #include <vector>
 
 constexpr const double altitudeFactor = 1.14;
+
 constexpr const char* exitAirIface =
     "xyz.openbmc_project.Configuration.ExitAirTempSensor";
 constexpr const char* cfmIface = "xyz.openbmc_project.Configuration.CFMSensor";
@@ -649,7 +650,7 @@ double ExitAirTempSensor::getTotalCFM(void)
 
 bool ExitAirTempSensor::calculate(double& val)
 {
-    constexpr size_t maxErrorPrint = 1;
+    constexpr size_t maxErrorPrint = 5;
     static bool firstRead = false;
     static size_t errorPrint = maxErrorPrint;
 
@@ -657,6 +658,20 @@ bool ExitAirTempSensor::calculate(double& val)
     if (cfm <= 0)
     {
         std::cerr << "Error getting cfm\n";
+        return false;
+    }
+
+    // cfm is out of range, exit air temp can't be computed correctly
+    constexpr const double cfmLimitFactor = 0.5;
+    if (cfm < (qMin * cfmLimitFactor))
+    {
+        if (errorPrint > 0)
+        {
+            errorPrint--;
+            std::cerr << "cfm " << cfm << " is too low, expected qMin " << qMin
+                      << "\n";
+        }
+        val = 0;
         return false;
     }
 
@@ -756,7 +771,7 @@ bool ExitAirTempSensor::calculate(double& val)
         alpha = alphaS + ((alphaF - alphaS) * (cfm - qMin) / (qMax - qMin));
     }
 
-    auto time = std::chrono::system_clock::now();
+    auto time = std::chrono::steady_clock::now();
     if (!firstRead)
     {
         firstRead = true;
@@ -884,13 +899,9 @@ void createSensor(sdbusplus::asio::object_server& objectServer,
                         sensor->c2 =
                             loadVariant<double>(entry.second, "C2") / 100;
                         sensor->tachMinPercent =
-                            loadVariant<double>(entry.second,
-                                                "TachMinPercent") /
-                            100;
+                            loadVariant<double>(entry.second, "TachMinPercent");
                         sensor->tachMaxPercent =
-                            loadVariant<double>(entry.second,
-                                                "TachMaxPercent") /
-                            100;
+                            loadVariant<double>(entry.second, "TachMaxPercent");
                         sensor->createMaxCFMIface();
                         sensor->setupMatches();
 
