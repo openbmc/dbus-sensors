1a92d41977ddb7c14dab9b0f4096563681a51544
Jeff Lin <JeffLin2@quantatw.com>
2020-12-04 20:29:00 +0800

    PSUSensor: Add Offset and PowerState attribute
    1. Add Offset attribute for labels
    2. Add PowerState attribute with default "Always"
       We have some PSUSensor no need to be monitored when power is not on.
    
    Tested:
    1. Test case of varifying Offset and PollRate for specific label:
       {
          "Address": "0x68",
          "Bus": "33",
          "Labels": [
                "vout1",
                "iout1",
                "pout1",
                "temp1"
          ],
          "Name": "brick2_p12v",
          "PollRate": 100.0,
          "Thresholds": [
                ...
          ],
          ...
          "iout1_Max": 108.0,
          "iout1_Min": 0.0,
          "iout1_Offset": -0.25,
          ...
       }
       - Check SDR and value of brick2_p12v Output Current and verify pass.
    2. Test case for PowerState set "On" in configuration:
       {
          "Address": "0x59",
          "Bus": "38",
          "Labels": [
                "vout1",
                "vout3",
                "vout4",
                "vout5",
                "vout6"
          ],
          "Name": "mobo_stb_p3v3",
          "PowerState": "On",
          "Thresholds": [
                ...
          ],
          "Type": "MAX34451"
       }
       - The value update successfully when host state is running.
       - Change host state to off will make the value be "nan".
       - The sensor without setting "PowerState" will always update.
    
    Signed-off-by: Jeff Lin <JeffLin2@quantatw.com>
    Change-Id: If4deefb63f075568ee48902f4b186971ca83a15e

--- include/PSUSensor.hpp
+++ include/PSUSensor.hpp
@@ -20,7 +20,8 @@ class PSUSensor : public Sensor, public std::enable_shared_from_this<PSUSensor>
               std::vector<thresholds::Threshold>&& thresholds,
               const std::string& sensorConfiguration,
               std::string& sensorTypeName, unsigned int factor, double max,
-              double min, const std::string& label, size_t tSize);
+              double min, double offset, PowerState readState,
+              const std::string& label, size_t tSize);
     ~PSUSensor() override;
     void setupRead(void);
 
@@ -35,6 +36,7 @@ class PSUSensor : public Sensor, public std::enable_shared_from_this<PSUSensor>
     size_t errCount;
     unsigned int sensorFactor;
     uint8_t minMaxReadCounter;
+    double sensorOffset;
     void handleResponse(const boost::system::error_code& err);
     void checkThresholds(void) override;
     void updateMinMaxValues(void);
@@ -47,9 +49,11 @@ class PSUSensor : public Sensor, public std::enable_shared_from_this<PSUSensor>
 class PSUProperty
 {
   public:
-    PSUProperty(std::string name, double max, double min, unsigned int factor) :
-        labelTypeName(std::move(name)), maxReading(max), minReading(min),
-        sensorScaleFactor(factor)
+    PSUProperty(std::string name, double max, double min, unsigned int factor,
+                double offset) :
+        labelTypeName(std::move(name)),
+        maxReading(max), minReading(min), sensorScaleFactor(factor),
+        sensorOffset(offset)
     {}
     ~PSUProperty() = default;
 
@@ -57,4 +61,5 @@ class PSUProperty
     double maxReading;
     double minReading;
     unsigned int sensorScaleFactor;
+    double sensorOffset;
 };
