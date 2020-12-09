6857c62763f7ac86d9ad7360581e59fd0dd14d70
Sathish Sivasekar <sathish-s@hcl.com>
2020-12-09 14:41:01 +0530

    IpmbSensor: Add support for new IPMB Device
    A new IpmbType for SDR sensor based on the entity configuration
    "Class" to define the IPMB send Request commands for each sensor
    based on the sensor's number received.
    
    In OCP multi-host platform, multiple IPMB bus support is present.
    IpmbBusIndex is added to the constructor to define the bus in
    which BMC will send and receive messages and update all the
    sensor values.
    
    TESTED: Tested on Facebook YosemiteV2 hardware and verified all
    the IPMB based SDR Type 1 sensor value.
    
    Signed-off-by: Sathish Sivasekar <sathish-s@hcl.com>
    Change-Id: I3a08cf9a2b57da398e7b413ddcb6db6aeaa739b9

--- src/IpmbSensor.cpp
+++ src/IpmbSensor.cpp
@@ -64,13 +64,14 @@ IpmbSensor::IpmbSensor(std::shared_ptr<sdbusplus::asio::connection>& conn,
                        sdbusplus::asio::object_server& objectServer,
                        std::vector<thresholds::Threshold>&& thresholdData,
                        uint8_t deviceAddress, uint8_t hostSMbusIndex,
-                       const float pollRate, std::string& sensorTypeName) :
+                       uint8_t ipmbBusIndex, const float pollRate,
+                       std::string& sensorTypeName) :
     Sensor(escapeName(sensorName), std::move(thresholdData),
            sensorConfiguration, "IpmbSensor", false, false, ipmbMaxReading,
            ipmbMinReading, conn, PowerState::on),
     deviceAddress(deviceAddress), hostSMbusIndex(hostSMbusIndex),
-    sensorPollMs(static_cast<int>(pollRate * 1000)), objectServer(objectServer),
-    waitTimer(io)
+    ipmbBusIndex(ipmbBusIndex), sensorPollMs(static_cast<int>(pollRate * 1000)),
+    objectServer(objectServer), waitTimer(io)
 {
     std::string dbusPath = sensorPathPrefix + sensorTypeName + "/" + name;
 
@@ -162,9 +163,16 @@ void IpmbSensor::runInitCmd()
 
 void IpmbSensor::loadDefaults()
 {
-    if (type == IpmbType::meSensor)
+    if (type == IpmbType::meSensor || type == IpmbType::IpmbDevice)
     {
-        commandAddress = meAddress;
+        if (type == IpmbType::meSensor)
+        {
+            commandAddress = meAddress;
+        }
+        else
+        {
+            commandAddress = ipmbBusIndex << ipmbLeftShift;
+        }
         netfn = ipmi::sensor::netFn;
         command = ipmi::sensor::getSensorReading;
         commandData = {deviceAddress};
@@ -464,6 +472,10 @@ bool IpmbSensor::sensorClassType(const std::string& sensorClass)
     {
         type = IpmbType::meSensor;
     }
+    else if (sensorClass == "IpmbDevice")
+    {
+        type = IpmbType::IpmbDevice;
+    }
     else
     {
         std::cerr << "Invalid class " << sensorClass << "\n";
@@ -602,8 +614,6 @@ void createSensors(
                 {
                     ipmbBusIndex = std::visit(VariantToUnsignedIntVisitor(),
                                               findBusType->second);
-                    std::cerr << "Ipmb Bus Index for " << name << " is "
-                              << static_cast<int>(ipmbBusIndex) << "\n";
                 }
 
                 /* Default sensor type is "temperature" */
@@ -635,7 +645,8 @@ void createSensors(
                         sensor = std::make_shared<IpmbSensor>(
                             dbusConnection, io, name, path, objectServer,
                             std::move(sensorThreshold), deviceAddress,
-                            hostSMbusIndex, pollRate, sensorTypeName);
+                            hostSMbusIndex, ipmbBusIndex, pollRate,
+                            sensorTypeName);
 
                         sensor->parseConfigValues(cfg);
                         if (!(sensor->sensorClassType(sensorClass)))
@@ -652,7 +663,7 @@ void createSensors(
                 sensor = std::make_shared<IpmbSensor>(
                     dbusConnection, io, name, path, objectServer,
                     std::move(sensorThresholds), deviceAddress, hostSMbusIndex,
-                    pollRate, sensorTypeName);
+                    ipmbBusIndex, pollRate, sensorTypeName);
 
                 sensor->parseConfigValues(cfg);
                 if (!(sensor->sensorClassType(sensorClass)))
