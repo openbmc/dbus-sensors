0bfc489ef9326bc70e51a409fd18d0a112c216c0
Sathish Sivasekar <sathish-s@hcl.com>
2020-12-09 14:41:01 +0530

    dbus-sensors: Added support for ipmb-sdr based on ipmb-fru detection dynamically.
    This patch has the below implementation
    1. To get the ipmb devices from the ipmb-fru
    2. Based on the detection using ipmi sdr commands the sensor lists are populated.
    3. Dbus entries are created based on the populated sensor list.
    4. Periodic update of the sensor values
    
    TESTED : Built Facebook YosemiteV2 images and loaded on the target hardware.
    Verified all the ipmb-sdr supported device data.
    
    Signed-off-by: Sathish Sivasekar <sathish-s@hcl.com>
    Depends On: I02255d7374379749234cbc97e3974330deab95fd
    Change-Id: I3a08cf9a2b57da398e7b413ddcb6db6aeaa739b9

--- include/IpmbSensor.hpp
+++ include/IpmbSensor.hpp
@@ -16,7 +16,8 @@ enum class IpmbType
     PXE1410CVR,
     IR38363VR,
     ADM1278HSC,
-    mpsVR
+    mpsVR,
+    SDRThresSensor
 };
 
 enum class IpmbSubType
@@ -34,7 +35,8 @@ enum class ReadingFormat
     byte3,
     elevenBit,
     elevenBitShift,
-    linearElevenBit
+    linearElevenBit,
+    sdrThres
 };
 
 namespace ipmi
@@ -110,4 +112,98 @@ struct IpmbSensor : public Sensor
   private:
     sdbusplus::asio::object_server& objectServer;
     boost::asio::deadline_timer waitTimer;
+};
+
+std::vector<std::string> Sensor_Unit{"Meters", "DegreesC", "RPMS", "Joules",
+                                     "Volts",  "Amperes",  "Watts"};
+
+class IpmbSDR
+{
+  public:
+    std::vector<std::string> sensorReadName;
+    std::vector<std::string> sensorUnit;
+    std::vector<double> thresUpperCri;
+    std::vector<double> thresLowerCri;
+    std::vector<uint16_t> mValue;
+    std::vector<uint16_t> bValue;
+    std::vector<uint8_t> sensorNumber;
+    std::vector<uint8_t> sensorSDRType;
+    std::vector<int8_t> rExp;
+    std::vector<int8_t> bExp;
+    std::vector<uint8_t> negRead;
+    std::vector<uint8_t> sensCap;
+    std::vector<uint8_t> getSdrData;
+
+    std::string sensorName;
+    std::string strUnit;
+    std::string hostName;
+
+    uint16_t recordCount;
+    uint16_t validRecordCount;
+    uint8_t sdrCommandAddress;
+    uint8_t curRecord;
+    uint8_t devAddr;
+    uint8_t command;
+    uint8_t cmdAddr;
+    uint8_t resrvIDLSB = 0;
+    uint8_t resrvIDMSB = 0;
+    uint8_t nextRecordIDLSB = 0;
+    uint8_t nextRecordIDMSB = 0;
+
+    void ipmbGetSdrInfo(
+        std::shared_ptr<sdbusplus::asio::connection>& dbusConnection);
+    void ipmbSdrRsrv(
+        std::shared_ptr<sdbusplus::asio::connection>& dbusConnection);
+    void ipmbGetSdr(
+        std::shared_ptr<sdbusplus::asio::connection>& dbusConnection);
+    void sdrDataProcess();
+    void sdrRead(std::shared_ptr<sdbusplus::asio::connection>& dbusConnection,
+                 std::unique_ptr<IpmbSensor>& sensor);
+    double dataConversion(double, uint8_t);
+
+    uint8_t maxPosReadingMargin = 127;
+    uint16_t thermalConst = 256;
+
+    uint8_t netfnStorageReq = 0x0a;
+    uint8_t cmdStorageGetSdrInfo = 0x20;
+    uint8_t cmdStorageRsrvSdr = 0x22;
+    uint8_t cmdStorageGetSdr = 0x23;
+
+    uint8_t sdrType01 = 1;
+    uint8_t sdrType02 = 2;
+    uint8_t sdrType03 = 3;
+
+    uint8_t cntType01 = 4;
+    uint8_t cntType02 = 3;
+    uint8_t cntType03 = 2;
+    uint8_t perLoopByte = 16;
+
+    uint8_t sdrNxtRecLSB = 0;
+    uint8_t sdrNxtRecMSB = 1;
+    uint8_t sdrType = 5;
+    uint8_t sdrSenNum = 9;
+
+    uint8_t sdrAdrType01 = 56;
+    uint8_t sdrAdrType02 = 38;
+    uint8_t sdrAdrType03 = 21;
+
+    uint8_t sdrLenBit = 0x1F;
+    uint8_t sdrLenType01 = 53;
+    uint8_t sdrLenType02 = 35;
+    uint8_t sdrLenType03 = 20;
+
+    uint8_t sdrThresAcce = 0x0C;
+    uint8_t sdrSensCapab = 13;
+    uint8_t sdrSensNoThres = 0;
+
+    uint8_t sdrUnitType01 = 25;
+    uint8_t sdrUpCriType01 = 43;
+    uint8_t sdrLoCriType01 = 46;
+
+    uint8_t mDataByte = 28;
+    uint8_t mTolDataByte = 29;
+    uint8_t bDataByte = 30;
+    uint8_t bAcuDataByte = 31;
+    uint8_t rbExpDataBye = 33;
+    uint8_t bitShiftMsb = 6;
 };
