4046d761e1ebe6161077f5c7ab6ea63e76069be1
Ed Tanous <edtanous@google.com>
2021-02-18 15:33:51 -0800

    Fix bug in name escaping for PSUSensor
    PSU sensor has a bug where if the name includes any invalid characters,
    it crashes when attempting to launch.  This is a problem, and very much
    not expected behavior.
    
    The root cause of this is that PSU combine event is creating a dbus API
    path from the object name, but neglects to escape it properly.
    
    The previous method to escape this was inlined inside sensor.hpp, so
    that has been promoted to a new utility function, escapeForDbus which
    can return an escaped name string to use in paths.
    
    Through the course of adding this to the sensors namespace and including
    it, it turns out that "sensors" is overloaded, and including it causes
    both compile time errors from the overloaded name, and link time errors
    from the lack of "inline" on the utility methods.  This commit also as a
    matter of cleanup moves everything in SensorPaths.hpp into an
    alternative file, SensorPaths.cpp, and includes that file in sensor
    utils.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: Id9ed915e7e0e13ee7bded67a785ab0fa58982ca8

--- include/SensorPaths.hpp
+++ include/SensorPaths.hpp
@@ -1,55 +1,18 @@
 #pragma once
 
 #include <cstring>
+#include <regex>
 #include <string>
 
-namespace sensors
+namespace sensor_paths
 {
-
-// This is the leading prefix of the object path for sensors.
-// The full path is /xyz/openbmc_project/sensors/<Measure>/<Name>
-// Note C++20 would allow this to be "constexpr std::string"
-const char* objectPathPrefix = "/xyz/openbmc_project/sensors/";
 
 // This is an allowlist of the units a sensor can measure. Should be in sync
 // with
 // phosphor-dbus-interfaces/blob/master/xyz/openbmc_project/Sensor/Value.interface.yaml#L35
 
-std::string getPathForUnits(const std::string& units)
-{
-    if (units == "DegreesC")
-    {
-        return "temperature";
-    }
-    if (units == "RPMS")
-    {
-        return "fan_tach";
-    }
-    if (units == "Volts")
-    {
-        return "voltage";
-    }
-    if (units == "Meters")
-    {
-        return "altitude";
-    }
-    if (units == "Amperes")
-    {
-        return "current";
-    }
-    if (units == "Watts")
-    {
-        return "power";
-    }
-    if (units == "Joules")
-    {
-        return "energy";
-    }
-    if (units == "Percent")
-    {
-        return "Utilization";
-    }
-    return "";
-}
+std::string getPathForUnits(const std::string& units);
 
-} // namespace sensors
+std::string escapePathForDbus(std::string name);
+
+} // namespace sensor_paths
---
+++ src/SensorPaths.cpp
@@ -0,0 +1,54 @@
+#include <cstring>
+#include <regex>
+#include <string>
+
+namespace sensor_paths
+{
+
+// This is an allowlist of the units a sensor can measure. Should be in sync
+// with
+// phosphor-dbus-interfaces/blob/master/xyz/openbmc_project/Sensor/Value.interface.yaml#L35
+
+std::string getPathForUnits(const std::string& units)
+{
+    if (units == "DegreesC")
+    {
+        return "temperature";
+    }
+    if (units == "RPMS")
+    {
+        return "fan_tach";
+    }
+    if (units == "Volts")
+    {
+        return "voltage";
+    }
+    if (units == "Meters")
+    {
+        return "altitude";
+    }
+    if (units == "Amperes")
+    {
+        return "current";
+    }
+    if (units == "Watts")
+    {
+        return "power";
+    }
+    if (units == "Joules")
+    {
+        return "energy";
+    }
+    if (units == "Percent")
+    {
+        return "Utilization";
+    }
+    return "";
+}
+
+std::string escapePathForDbus(std::string name)
+{
+    return std::regex_replace(name, std::regex("[^a-zA-Z0-9_/]+"), "_");
+}
+
+} // namespace sensor_paths
