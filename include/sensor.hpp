d4ce8462dc8c98e7f06edf2be3a4b08fe8fc2341
Josh Lehan <krellan@google.com>
2021-03-17 13:29:51 -0700

    Add external write hook to Sensor
    Adds a member variable to Sensor, a lambda,
    initialized to an empty function by default.
    
    By assigning this variable to their own lambda,
    a user of Sensor can receive notifications whenever an external write
    happens, without having to modify any code within Sensor itself.
    
    This is necessary to support ExternalSensor, but is generic enough
    to help other dbus-sensors daemons, should the need ever arise.
    
    Tested: In conjunction with ExternalSensor, works for me,
    external writes correctly cause the lambda to be executed.
    
    Signed-off-by: Josh Lehan <krellan@google.com>
    Change-Id: I67571327d870e7c718fa4f25e91a6598daabcf3d

--- include/sensor.hpp
+++ include/sensor.hpp
@@ -77,6 +77,12 @@ struct Sensor
     size_t errCount;
     std::unique_ptr<SensorInstrumentation> instrumentation;
 
+    // This member variable provides a hook that can be used to receive
+    // notification whenever this Sensor's value is externally set
+    // via D-Bus. If interested, replace this variable with your own
+    // lambda. See ExternalSensor for an example of usage.
+    std::function<void()> externalSetHook = []() {};
+
     void updateInstrumentation(double readValue)
     {
         // Do nothing if this feature is not enabled
@@ -172,6 +178,9 @@ struct Sensor
             // check thresholds for external set
             value = newValue;
             checkThresholds();
+
+            // Trigger the hook, as an external set has just happened
+            externalSetHook();
         }
         else if (!overriddenState)
         {
