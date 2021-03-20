768eab1ae9bac5dec7e7ce873444b1d5637f0e08
Josh Lehan <krellan@google.com>
2021-03-19 21:04:54 -0700

    sensorPaths: Use library function to escape D-Bus names
    Instead of rolling our own incomplete function to escape arbitrary
    names to make them compatible with D-Bus naming restrictions, there
    is an already-existing library function to perform this escape, and
    as a nice bonus, it is in a library that we already include.
    
    https://www.freedesktop.org/software/systemd/man/sd_bus_path_encode.html
    
    The Sensor constructor has been changed, to meet the API requirements
    of the sd_bus_path_encode() function, as it requires both prefix and
    name to be passed in, as it does its own concatenation of them, to
    produce the final object path.
    
    This object path is saved in "objectPath", a new variable in Sensor,
    which simplifies the subclasses of Sensor, as they no longer have to
    build and maintain these strings in their own constructors.
    
    Tested: I added test cases to the start of a sensor daemon, just to
    get it to quickly run, and it worked for me, here they are.
    
    Name "Hello World!" -> Escaped "Hello_20World_21"
    Name "o_o" -> Escaped "o_5fo"
    Name "1 Hot Part" -> Escaped "_31_20Hot_20Part"
    Name "Temperature 1" -> Escaped "Temperature_201"
    Name "Temperature-Aux" -> Escaped "Temperature_2dAux"
    Name "Interior/Slash" -> Escaped "Interior_2fSlash"
    Name "/LeadingSlash" -> Escaped "_2fLeadingSlash"
    Name "TrailingSlash/" -> Escaped "TrailingSlash_2f"
    Name "__5f" -> Escaped "_5f_5f5f"
    Name "_5f" -> Escaped "_5f5f"
    Name "5f" -> Escaped "_35f"
    Name "Slot 5f" -> Escaped "Slot_205f"
    Name "S5f" -> Escaped "S5f"
    Name "^_ _^" -> Escaped "_5e_5f_20_5f_5e"
    Name "" -> Escaped "_"
    Name "_" -> Escaped "_5f"
    
    Change-Id: I981fe4d03356a38f95fd18bc8aed55d5a490f199
    Signed-off-by: Josh Lehan <krellan@google.com>

--- src/SensorPaths.cpp
+++ src/SensorPaths.cpp
@@ -1,5 +1,6 @@
+#include <systemd/sd-bus.h>
+
 #include <cstring>
-#include <regex>
 #include <string>
 
 namespace sensor_paths
@@ -46,9 +47,30 @@ std::string getPathForUnits(const std::string& units)
     return "";
 }
 
-std::string escapePathForDbus(const std::string& name)
+// Escapes unacceptable characters in an object name, making it safe for use
+// in a D-Bus object path. The prefix is also required here, although it
+// remains unchanged, to meet the API requirements of sd_bus_path_encode():
+// https://www.freedesktop.org/software/systemd/man/sd_bus_path_encode.html
+// https://github.com/systemd/systemd/tree/main/src/libsystemd/sd-bus/sd-bus.c
+// Returns: String, concatenation of the (unchanged) prefix, the '/' (slash)
+// character, and the escaped object name. Returns empty string if error.
+std::string escapePathForDbus(const std::string& prefix,
+                              const std::string& name)
 {
-    return std::regex_replace(name, std::regex("[^a-zA-Z0-9_/]+"), "_");
+    char* outbuf = nullptr;
+    int ret = sd_bus_path_encode(prefix.c_str(), name.c_str(), &outbuf);
+
+    // Error checking
+    if ((ret < 0) || !outbuf)
+    {
+        return {};
+    }
+
+    // Convert C string to C++ string
+    std::string outstr(outbuf);
+    free(outbuf);
+
+    return outstr;
 }
 
 } // namespace sensor_paths
