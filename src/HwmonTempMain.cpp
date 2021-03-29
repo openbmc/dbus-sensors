5f527bfbb9b0ae1b9cba9bcf62e41385b1bb9c33
Rashmica Gupta <rashmica.g@gmail.com>
2021-03-29 20:16:09 +1100

    hwmontemp: Add DPS310 and SI7020 iio sensors
    Signed-off-by: Rashmica Gupta <rashmica.g@gmail.com>
    Change-Id: I240ef31a8f696f57b935adbf44aa2798f837120c

--- src/HwmonTempMain.cpp
+++ src/HwmonTempMain.cpp
@@ -40,7 +40,8 @@ static constexpr bool debug = false;
 static constexpr float pollRateDefault = 0.5;
 
 namespace fs = std::filesystem;
-static constexpr std::array<const char*, 13> sensorTypes = {
+static constexpr std::array<const char*, 15> sensorTypes = {
+    "xyz.openbmc_project.Configuration.DPS310",
     "xyz.openbmc_project.Configuration.EMC1412",
     "xyz.openbmc_project.Configuration.EMC1413",
     "xyz.openbmc_project.Configuration.EMC1414",
@@ -49,6 +50,7 @@ static constexpr std::array<const char*, 13> sensorTypes = {
     "xyz.openbmc_project.Configuration.MAX6581",
     "xyz.openbmc_project.Configuration.MAX6654",
     "xyz.openbmc_project.Configuration.SBTSI",
+    "xyz.openbmc_project.Configuration.SI7020",
     "xyz.openbmc_project.Configuration.TMP112",
     "xyz.openbmc_project.Configuration.TMP175",
     "xyz.openbmc_project.Configuration.TMP421",
