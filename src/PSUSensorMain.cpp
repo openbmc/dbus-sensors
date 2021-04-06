678f53b88861f14da4884f57d8e172a0604a1569
Paul Fertser <fercerpav@gmail.com>
2021-04-06 07:06:28 +0000

    PSUSensor: whitelist adm1275
    The kernel driver that handles adm1075, adm1272, adm1275, adm1276,
    adm1278, adm1293 and adm1294 is called "adm1275" and uses this name in
    hwmon nodes that it creates, so it needs to be white-listed.
    
    Signed-off-by: Paul Fertser <fercerpav@gmail.com>
    Change-Id: Ie0a89c02279dc01e7ab54097f22548b29536375a

--- src/PSUSensorMain.cpp
+++ src/PSUSensorMain.cpp
@@ -40,8 +40,9 @@
 
 static constexpr bool debug = false;
 
-static constexpr std::array<const char*, 22> sensorTypes = {
+static constexpr std::array<const char*, 23> sensorTypes = {
     "xyz.openbmc_project.Configuration.ADM1272",
+    "xyz.openbmc_project.Configuration.ADM1275",
     "xyz.openbmc_project.Configuration.ADM1278",
     "xyz.openbmc_project.Configuration.DPS800",
     "xyz.openbmc_project.Configuration.INA219",
@@ -65,10 +66,10 @@ static constexpr std::array<const char*, 22> sensorTypes = {
     "xyz.openbmc_project.Configuration.TPS546D24"};
 
 static std::vector<std::string> pmbusNames = {
-    "adm1272",   "adm1278",   "dps800",    "ina219",   "ina230",   "isl68137",
-    "isl68220",  "isl68223",  "isl69243",  "isl69260", "max16601", "max20710",
-    "max20730",  "max20734",  "max20796",  "max34451", "pmbus",    "pxe1610",
-    "raa228000", "raa228228", "raa229004", "tps546d24"};
+    "adm1272",  "adm1275",   "adm1278",   "dps800",    "ina219",   "ina230",
+    "isl68137", "isl68220",  "isl68223",  "isl69243",  "isl69260", "max16601",
+    "max20710", "max20730",  "max20734",  "max20796",  "max34451", "pmbus",
+    "pxe1610",  "raa228000", "raa228228", "raa229004", "tps546d24"};
 
 namespace fs = std::filesystem;
 
