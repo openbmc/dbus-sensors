dbeb34a0dfb458e786bb9431de22415f9ad537f2
ThuBaNguyen <thu@os.amperecomputing.com>
2021-04-24 21:40:05 +0700

    amperecpu: Add Ampere CPU daemon
    Ampere SoCs use the SCP register map interface to communicate with the
    BMC firmware to report the host sensors, states, events and also the
    errors. The current CPUSensors daemon is OEM daemon which was
    reporting the Intel CPU sensors. This daemon used peci interface.
    This requires new daemon to handle Ampere CPU sensors, events and
    errors. In this first commit, AmpereCPU sources will handle the host
    sensors, match the sensor paths in hwmon driver path with the sensors
    configuration and create the sensors dbus objects.
    
    Below is some info about Ampere CPU daemon:
        1. The daemon name is xyz.openbmc_project.amperecpusensor.service.
        2. The supports device type is "smpro_hwmon".
        3. The host sensors dbus objects will be add to
        xyz.openbmc_project.AmpereCPUSensor dbus interface.
    
    Tested:
        1. Add Ampere CPU configuration in entity-manger with
        type="smpro_hwmon".
        2. Power on the host.
        3. Check the host sensors dbus objects in dbus interface
        xyz.openbmc_project.AmpereCPUSensor.
    
    Signed-off-by: ThuBaNguyen <thu@os.amperecomputing.com>
    Change-Id: If55d9bfad5bfc7022865365d7f718df259f118a9

---
+++ include/AmpereCPU.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <Thresholds.hpp>
+#include <boost/asio/streambuf.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+#include <sensor.hpp>
+
+#include <memory>
+#include <string>
+#include <utility>
+
+class AmpereCPUSensor :
+    public Sensor,
+    public std::enable_shared_from_this<AmpereCPUSensor>
+{
+  public:
+    AmpereCPUSensor(const std::string& path, const std::string& objectType,
+                    sdbusplus::asio::object_server& objectServer,
+                    std::shared_ptr<sdbusplus::asio::connection>& conn,
+                    boost::asio::io_service& io, const std::string& sensorName,
+                    std::vector<thresholds::Threshold>&& thresholds,
+                    const std::string& sensorConfiguration,
+                    std::string& sensorTypeName, unsigned int factor,
+                    double max, double min, const std::string& label,
+                    size_t tSize, PowerState readState);
+    ~AmpereCPUSensor() override;
+    void setupRead(void);
+
+  private:
+    sdbusplus::asio::object_server& objServer;
+    boost::asio::posix::stream_descriptor inputDev;
+    boost::asio::deadline_timer waitTimer;
+    std::shared_ptr<boost::asio::streambuf> readBuf;
+    std::string path;
+    unsigned int sensorFactor;
+    void handleResponse(const boost::system::error_code& err);
+    void checkThresholds(void) override;
+
+    int fd;
+    static constexpr unsigned int sensorPollMs = 1000;
+    static constexpr size_t warnAfterErrorCount = 10;
+};
+
+class AmpereCPUProperty
+{
+  public:
+    AmpereCPUProperty(std::string name, double max, double min,
+                      unsigned int factor) :
+        labelTypeName(std::move(name)),
+        maxReading(max), minReading(min), sensorScaleFactor(factor)
+    {}
+    ~AmpereCPUProperty() = default;
+
+    std::string labelTypeName;
+    double maxReading;
+    double minReading;
+    unsigned int sensorScaleFactor;
+};
