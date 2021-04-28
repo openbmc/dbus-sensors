f06a9037641e9168024576366f8e21867f81b558
Helen Huang <he.huang@linux.intel.com>
2021-04-28 16:18:05 +0800

    Update starttimer of ThresholdTimer
    In the timer callback, checking if the timer is valid at the first step.
    If the timer has been already canceled, the timer handler should return
    immediately to avoid the further operation on dangling pointer 'pair'.
    
    Change-Id: I4d4c3970a5279aec9c02df4337d5c254f340a9f6
    Signed-off-by: Helen Huang <he.huang@linux.intel.com>

--- src/Thresholds.cpp
+++ src/Thresholds.cpp
@@ -368,7 +368,6 @@ void ThresholdTimer::startTimer(const Threshold& threshold, bool assert,
     pair->second.expires_from_now(boost::posix_time::seconds(waitTime));
     pair->second.async_wait([this, pair, threshold, assert,
                              assertValue](boost::system::error_code ec) {
-        pair->first.used = false;
 
         if (ec == boost::asio::error::operation_aborted)
         {
@@ -379,6 +378,7 @@ void ThresholdTimer::startTimer(const Threshold& threshold, bool assert,
             std::cerr << "timer error: " << ec.message() << "\n";
             return;
         }
+        pair->first.used = false;
         if (sensor->readingStateGood())
         {
             assertThresholds(sensor, assertValue, threshold.level,
