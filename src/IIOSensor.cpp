605950b3c2f3cfbcbe4309f8b3474e7f63cc478a
Bruce Mitchell <bruce.mitchell@linux.vnet.ibm.com>
2021-05-20 13:20:01 -0500

    dbus-sensors: Adds support for DPS310 Sensors
    Infineon DPS310 pressure and ambient temperature sensors for
    dbus-sensors with altitude "sensor" to follow.  This DPS310
    is on the Nisqually Backplane.
    
    Tested: busctl introspect --no-pager xyz.openbmc_project.IIOSensor \
    /xyz/openbmc_project/sensors/pressure/Station_Pressure returned
    results consistent with location of the Rainier system.
    
    Author: Eddie James <eajames@linux.ibm.com>
    Signed-off-by: Bruce Mitchell <bruce.mitchell@linux.vnet.ibm.com>
    Change-Id: I18e46f2317d2bafde643d900d0d339e16ae4ae60

---
+++ src/IIOSensor.cpp
@@ -0,0 +1,196 @@
+#include <unistd.h>
+
+#include <IIOSensor.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/algorithm/string/replace.hpp>
+#include <boost/asio/read_until.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+
+#include <iostream>
+#include <istream>
+#include <limits>
+#include <memory>
+#include <string>
+#include <vector>
+
+// The pressure units we receive are kilopascal.
+// From: https://www.kernel.org/doc/Documentation/ABI/testing/sysfs-bus-iio
+// Units after application of scale and offset are kilopascal.
+// 1 kilopascal = 10 hectopascal = 1000 Pascals
+// The standard unit for used in atmospheric pressure measurements or
+// readings is the hectopascal (hPa), in meteorology, for atmospheric
+// pressure, the modern equivalent of the traditional millibar.
+// However we tend to not put prefixes on dbus APIs, so our pressure
+// is in Pascals.
+static constexpr unsigned int pressureScaleFactor = 1000;
+static constexpr unsigned int temperatureScaleFactor = 1000;
+
+// the DPS310 can read up to 120000 Pascals
+static constexpr double maxReading = 120000;
+// Zero Kelvins is -273.15 DegreesC
+static constexpr double minReading = -273.15;
+
+IIOSensor::IIOSensor(const std::string& path, const std::string& objectType,
+                     sdbusplus::asio::object_server& objectServer,
+                     std::shared_ptr<sdbusplus::asio::connection>& conn,
+                     boost::asio::io_service& io, const std::string& sensorName,
+                     std::vector<thresholds::Threshold>&& thresholdsIn,
+                     const float pollRate,
+                     const std::string& sensorConfiguration,
+                     const PowerState powerState,
+                     const std::string& sensorType) :
+    Sensor(boost::replace_all_copy(sensorName, " ", "_"),
+           std::move(thresholdsIn), sensorConfiguration, objectType, maxReading,
+           minReading, conn, powerState),
+    std::enable_shared_from_this<IIOSensor>(), objServer(objectServer),
+    inputDev(io, open(path.c_str(), O_RDONLY)), waitTimer(io), path(path),
+    sensorPollMs(static_cast<unsigned int>(pollRate * 1000))
+{
+    const char* units;
+
+    if (sensorType.compare("temperature") == 0)
+    {
+        units = "DegreesC";
+    }
+    else if (sensorType.compare("pressure") == 0)
+    {
+        units = "Pascals";
+    }
+    // This should NEVER happen.
+    else
+    {
+        units = "Unknown";
+    }
+
+    sensorInterface = objectServer.add_interface(
+        "/xyz/openbmc_project/sensors/" + sensorType + "/" + name,
+        "xyz.openbmc_project.Sensor.Value");
+
+    if (thresholds::hasWarningInterface(thresholds))
+    {
+        thresholdInterfaceWarning = objectServer.add_interface(
+            "/xyz/openbmc_project/sensors/" + sensorType + "/" + name,
+            "xyz.openbmc_project.Sensor.Threshold.Warning");
+    }
+    if (thresholds::hasCriticalInterface(thresholds))
+    {
+        thresholdInterfaceCritical = objectServer.add_interface(
+            "/xyz/openbmc_project/sensors/" + sensorType + "/" + name,
+            "xyz.openbmc_project.Sensor.Threshold.Critical");
+    }
+    association = objectServer.add_interface("/xyz/openbmc_project/sensors/" +
+                                                 sensorType + "/" + name,
+                                             association::interface);
+    setInitialProperties(conn, units);
+}
+
+IIOSensor::~IIOSensor()
+{
+    // close the input dev to cancel async operations
+    inputDev.close();
+    waitTimer.cancel();
+    objServer.remove_interface(thresholdInterfaceWarning);
+    objServer.remove_interface(thresholdInterfaceCritical);
+    objServer.remove_interface(sensorInterface);
+    objServer.remove_interface(association);
+}
+
+void IIOSensor::setupRead(void)
+{
+    std::weak_ptr<IIOSensor> weakRef = weak_from_this();
+
+    boost::asio::async_read_until(inputDev, readBuf, '\n',
+                                  [weakRef](const boost::system::error_code& ec,
+                                            std::size_t /*bytes_transfered*/) {
+                                      std::shared_ptr<IIOSensor> self =
+                                          weakRef.lock();
+                                      if (self)
+                                      {
+                                          self->handleResponse(ec);
+                                      }
+                                  });
+}
+
+void IIOSensor::handleResponse(const boost::system::error_code& err)
+{
+    if ((err == boost::system::errc::bad_file_descriptor) ||
+        (err == boost::asio::error::misc_errors::not_found))
+    {
+        std::cerr << "IIO sensor " << name << " removed " << path << "\n";
+        return; // we're being destroyed
+    }
+    std::istream responseStream(&readBuf);
+    if (!err)
+    {
+        std::string response;
+        std::getline(responseStream, response);
+        try
+        {
+            rawValue = std::stod(response);
+            double nvalue;
+            std::string my_pressure = "in_pressure";
+            std::string my_temperature = "in_temp";
+            if (path.find(my_pressure) != std::string::npos)
+            {
+                nvalue = rawValue * pressureScaleFactor;
+            }
+            else if (path.find(my_temperature) != std::string::npos)
+            {
+                nvalue = rawValue / temperatureScaleFactor;
+            }
+            // This should NEVER happen.
+            else
+            {
+                nvalue = rawValue * 1.0;
+            }
+            updateValue(nvalue);
+        }
+        catch (const std::invalid_argument&)
+        {
+            incrementError();
+        }
+    }
+    else
+    {
+        incrementError();
+    }
+
+    responseStream.clear();
+    inputDev.close();
+    int fd = open(path.c_str(), O_RDONLY);
+    if (fd < 0)
+    {
+        std::cerr << "IIO sensor " << name << " not valid " << path << "\n";
+        return; // we're no longer valid
+    }
+    inputDev.assign(fd);
+    waitTimer.expires_from_now(boost::posix_time::milliseconds(sensorPollMs));
+    std::weak_ptr<IIOSensor> weakRef = weak_from_this();
+    waitTimer.async_wait([weakRef](const boost::system::error_code& ec) {
+        std::shared_ptr<IIOSensor> self = weakRef.lock();
+        if (ec == boost::asio::error::operation_aborted)
+        {
+            if (self)
+            {
+                std::cerr << "IIO sensor " << self->name << " read cancelled "
+                          << self->path << "\n";
+            }
+            else
+            {
+                std::cerr << "IIO sensor read cancelled, no self\n";
+            }
+            return; // we're being canceled
+        }
+        if (self)
+        {
+            self->setupRead();
+        }
+    });
+}
+
+void IIOSensor::checkThresholds(void)
+{
+    thresholds::checkThresholds(this);
+}
