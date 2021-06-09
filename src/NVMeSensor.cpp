openbmc/dbus-sensors

commit 1263c3dad3a723f4ebbd05e4031e4d5ff45f349c

Modify set-value function from external
    Move Special mode from bmcweb to dbus-sensors, this will be handled via dbus-sensor when the user set-value f$
    This is unlikely to break any users because the Intel special mode function is no change, only move to dbus-s$

    The set value function is to filter the external set, excluding the internal set.
    In the external set, if in INSECURE_UNRESTRICTED_SENSOR_OVERRIDE, no need to check before set value else add $

    bmcweb needs to be merged at the same time.
    bmcweb changes are pushed to Gerrit:
    https://gerrit.openbmc-project.xyz/c/openbmc/bmcweb/+/42448

    the mailing list discussion links:
    https://lists.ozlabs.org/pipermail/openbmc/2021-March/025597.html

Signed-off-by: Bruce Lee <Bruce_Lee@quantatw.com>
Change-Id: I0919e4d2343e81045010a2b0d4bf01dc272bcb07

src/NVMeSensor.cpp
@@ -31,9 +31,9 @@ NVMeSensor::NVMeSensor(sdbusplus::asio::object_server& objectServer,
                        const int busNumber) :
     Sensor(boost::replace_all_copy(sensorName, " ", "_"),
            std::move(thresholdsIn), sensorConfiguration,
-           "xyz.openbmc_project.Configuration.NVMe", maxReading, minReading,
-           conn, PowerState::on),
-    bus(busNumber), objServer(objectServer)
+           "xyz.openbmc_project.Configuration.NVMe", false, maxReading,
+           minReading, conn, PowerState::on),
+    objServer(objectServer), bus(busNumber)
 {
     sensorInterface = objectServer.add_interface(
         "/xyz/openbmc_project/sensors/temperature/" + name,

include/Utils.hpp
@@ -316,3 +316,5 @@ std::optional<std::tuple<std::string, std::string, std::string>>
     splitFileName(const std::filesystem::path& filePath);
 std::optional<double> readFile(const std::string& thresholdFile,
                                const double& scaleFactor);
+void setupManufacturingModeMatch(sdbusplus::asio::connection& conn);
+bool getManufacturingMode();

include/dbus-sensor_config.h.in
@@ -1,0 +1,9 @@
+#pragma once
+
+#include <cstdint>
+
+// clang-format off
+constexpr const bool validateUnsecureFeature = @VALIDATION_UNSECURE_FEATURE@;
+
+constexpr const bool insecureSensorOverride = @INSECURE_UNRESTRICTED_SENSOR_OVERRIDE@;
+// clang-format on

include/meson.build
@@ -1,0 +1,6 @@
+conf_data = configuration_data()
+conf_data.set10('VALIDATION_UNSECURE_FEATURE', get_option('validate-unsecure-feature').enabled())
+conf_data.set10('INSECURE_UNRESTRICTED_SENSOR_OVERRIDE', get_option('insecure-sensor-override').enabled())
+configure_file(input: 'dbus-sensor_config.h.in',
+               output: 'dbus-sensor_config.h',
+               configuration: conf_data)

include/sensor.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "dbus-sensor_config.h"
 
 #include <SensorPaths.hpp>
 #include <Thresholds.hpp>
@@ -39,12 +40,13 @@ struct Sensor
     Sensor(const std::string& name,
            std::vector<thresholds::Threshold>&& thresholdData,
            const std::string& configurationPath, const std::string& objectType,
-           const double max, const double min,
+           bool isSettable, const double max, const double min,
            std::shared_ptr<sdbusplus::asio::connection>& conn,
            PowerState readState = PowerState::always) :
         name(sensor_paths::escapePathForDbus(name)),
         configurationPath(configurationPath), objectType(objectType),
-        maxValue(max), minValue(min), thresholds(std::move(thresholdData)),
+        isSensorSettable(isSettable), maxValue(max), minValue(min),
+        thresholds(std::move(thresholdData)),
         hysteresisTrigger((max - min) * 0.01),
         hysteresisPublish((max - min) * 0.0001), dbusConnection(conn),
         readState(readState), errCount(0),
@@ -57,6 +59,7 @@ struct Sensor
     std::string name;
     std::string configurationPath;
     std::string objectType;
+    bool isSensorSettable;
     double maxValue;
     double minValue;
     std::vector<thresholds::Threshold> thresholds;
@@ -173,6 +176,18 @@ struct Sensor
     {
         if (!internalSet)
         {
+            if (insecureSensorOverride == false)
+            { // insecure sesnor override.
+                if (isSensorSettable == false)
+                { // sensor is not settable.
+                    if (getManufacturingMode() == false)
+                    { // manufacture mode is not enable.
+                        throw sdbusplus::exception::SdBusError(
+                            -EACCES, "Not allow set porperty value.");
+                    }
+                }
+            }
+
             oldValue = newValue;
             overriddenState = true;
             // check thresholds for external set

meson.build
@@ -66,7 +66,7 @@ pwmsensor_a = static_library(
     include_directories: 'include',
 )
 
-
+subdir('include')
 subdir('service_files')
 subdir('src')
 

src/ADCSensor.cpp
@@ -54,7 +54,7 @@ ADCSensor::ADCSensor(const std::string& path,
                      std::optional<BridgeGpio>&& bridgeGpio) :
     Sensor(boost::replace_all_copy(sensorName, " ", "_"),
            std::move(thresholdsIn), sensorConfiguration,
-           "xyz.openbmc_project.Configuration.ADC",
+           "xyz.openbmc_project.Configuration.ADC", false,
            maxVoltageReading / scaleFactor, minVoltageReading / scaleFactor,
            conn, readState),
     std::enable_shared_from_this<ADCSensor>(), objServer(objectServer),

src/ADCSensorMain.cpp
@@ -406,5 +406,6 @@ int main()
             "',arg0namespace='xyz.openbmc_project.Inventory.Item'",
         cpuPresenceHandler));
 
+    setupManufacturingModeMatch(*systemBus);
     io.run();
 }

commit 20558230c2818727e333247cbf9170a495ffcd7f

NVMeSensor: Fix ordering of member initializer list
Resolves:

In file included from ../src/NVMeSensor.cpp:17:
../include/NVMeSensor.hpp: In constructor ‘NVMeSensor::NVMeSensor(sdbusplus::asio::object_server&, boost::asio::io_service&, std::shared_ptr<sdbusplus::asio::connection>&, const string&, std::vector<thresholds::Threshold>&&, const string&, int)’:
../include/NVMeSensor.hpp:22:37: error: ‘NVMeSensor::objServer’ will be initialized after [-Werror=reorder]
   22 |     sdbusplus::asio::object_server& objServer;
      |                                     ^~~~~~~~~
../include/NVMeSensor.hpp:19:9: error:   ‘int NVMeSensor::bus’ [-Werror=reorder]
   19 |     int bus;
      |         ^~~
../src/NVMeSensor.cpp:25:1: error:   when initialized here [-Werror=reorder]
   25 | NVMeSensor::NVMeSensor(sdbusplus::asio::object_server& objectServer,
      | ^~~~~~~~~~

Fixes: 1263c3dad3a7 ("Modify set-value function from external")
Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
Change-Id: I7780400cc97349ac8a3553d8515ebf8d8668b5a0

src/NVMeSensor.cpp
@@ -33,7 +33,7 @@ NVMeSensor::NVMeSensor(sdbusplus::asio::object_server& objectServer,
            std::move(thresholdsIn), sensorConfiguration,
            "xyz.openbmc_project.Configuration.NVMe", false, maxReading,
            minReading, conn, PowerState::on),
-    objServer(objectServer), bus(busNumber)
+    bus(busNumber), objServer(objectServer)
 {
     sensorInterface = objectServer.add_interface(
         "/xyz/openbmc_project/sensors/temperature/" + name,