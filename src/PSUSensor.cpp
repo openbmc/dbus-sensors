680b2ea28381e72bdbf5752b4641ffa0a44eb620
Paul Fertser <fercerpav@gmail.com>
2021-07-20 08:29:09 +0000

    PSUSensor: improve error logging
    PSU sub-events might be asserted by to different alarm flags as returned
    by the kernel so when debugging a hardware or driver issue it's
    important to know what condition exactly triggered.
    
    Same about errors reading data from PMBUS sensors, for debugging it's
    essential to learn precisely what happened.
    
    Example output:
    
    System error 11 (Resource temporarily unavailable) reading from /sys/class/hwmon/hwmon12/temp1_input, line: 199
    PSUSubEvent asserted by /sys/class/hwmon/hwmon10/curr3_crit_alarm
    PSUSubEvent asserted by /sys/class/hwmon/hwmon12/curr3_crit_alarm
    System error 6 (No such device or address) reading from /sys/class/hwmon/hwmon8/curr3_input, line: 199
    PSUSubEvent asserted by /sys/class/hwmon/hwmon15/curr3_crit_alarm
    
    Monitoring with strace confirms psusensor happily continues to read from
    the file descriptors in question after the glitches.
    
    Signed-off-by: Paul Fertser <fercerpav@gmail.com>
    Change-Id: I2f1adac52ad8393d455c3c1a04e7f9bac479f68f

--- src/PSUSensor.cpp
+++ src/PSUSensor.cpp
@@ -30,6 +30,7 @@
 #include <memory>
 #include <string>
 #include <vector>
+#include <cerrno>
 
 static constexpr const char* sensorPathPrefix = "/xyz/openbmc_project/sensors/";
 
@@ -167,7 +168,7 @@ void PSUSensor::handleResponse(const boost::system::error_code& err)
     if ((err == boost::system::errc::bad_file_descriptor) ||
         (err == boost::asio::error::misc_errors::not_found))
     {
-        std::cerr << "Bad file descriptor from\n";
+        std::cerr << "Bad file descriptor for " << path << "\n";
         return;
     }
 
@@ -195,7 +196,9 @@ void PSUSensor::handleResponse(const boost::system::error_code& err)
     }
     else
     {
-        std::cerr << "System error: " << errno << " line: " << __LINE__ << "\n";
+        std::cerr << "System error " << errno << " (" << sys_errlist[errno]
+                  << ") reading from " << path << ", line: " << __LINE__
+                  << "\n";
         incrementError();
     }
 
