9f14e1e9146a51c765a2b0ed469672441000eca4
Bruce Mitchell <bruce.mitchell@linux.vnet.ibm.com>
2021-07-29 18:05:49 -0500

    (wip) DPS310 temperature pressure, SI7020 temperature
    Infineon DPS310 pressure and temperature sensors for the Blyth
    and Storm King Panel, as well as the Silicon Laboratories
    SI7020 type sensors.  This provide OpenBMC with the ability
    to have the DPS310 pressure and temperature sensors also the
    Silicon Laboratories SI7020 type sensors provide OpenBMC
    with temperature sensor on dbus via dbus-sensors through
    HwmonTempSensor.  And adds "pressure" as a type of sensor.
    Changed sensorScaleFactor to have offsetValue and scaleValue
    for _raw IIO devices, scaleValue is a multiplier where
    sensorScaleFactor was a divisor.  Pulling this into HwmonTempSensor
    came about from this Discord discussion:
    https://discord.com/channels/775381525260664832/775381525260664836/869641817220595772
    
    Temperature has "Name", pressure has "Name1".
    
    Tested: on Rainier's Blyth and Everest's Storm King Op Panels
    busctl tree --no-pager xyz.openbmc_project.HwmonTempSensor
    busctl introspect --no-pager xyz.openbmc_project.HwmonTempSensor \
    /xyz/openbmc_project/sensors/temperature/Ambient_0_Temp
    busctl introspect --no-pager xyz.openbmc_project.HwmonTempSensor \
    /xyz/openbmc_project/sensors/temperature/Ambient_1_Temp
    busctl introspect --no-pager xyz.openbmc_project.HwmonTempSensor \
    /xyz/openbmc_project/sensors/temperature/Ambient_2_Temp
    busctl introspect --no-pager xyz.openbmc_project.HwmonTempSensor \
    /xyz/openbmc_project/sensors/temperature/PCIE_0_Temp
    busctl introspect --no-pager xyz.openbmc_project.HwmonTempSensor \
    /xyz/openbmc_project/sensors/temperature/PCIE_1_Temp
    busctl introspect --no-pager xyz.openbmc_project.HwmonTempSensor \
    /xyz/openbmc_project/sensors/pressure/Station_Pressure
    Results consistent with location of the systems.
    
    Signed-off-by: Bruce Mitchell <bruce.mitchell@linux.vnet.ibm.com>
    Change-Id: I5c0d56b486a671ee507c7bfe18fea72e7eaf9ebe

--- include/HwmonTempSensor.hpp
+++ include/HwmonTempSensor.hpp
@@ -18,9 +18,12 @@ class HwmonTempSensor :
                     std::shared_ptr<sdbusplus::asio::connection>& conn,
                     boost::asio::io_service& io, const std::string& sensorName,
                     std::vector<thresholds::Threshold>&& thresholds,
+                    const double offsetValue, const double scaleValue,
+                    const double minReadingThreshold,
+                    const double maxReadingThreshold, const std::string& units,
                     const float pollRate,
                     const std::string& sensorConfiguration,
-                    const PowerState powerState);
+                    const PowerState powerState, const std::string& sensorType);
     ~HwmonTempSensor() override;
     void setupRead(void);
 
@@ -30,6 +33,11 @@ class HwmonTempSensor :
     boost::asio::deadline_timer waitTimer;
     boost::asio::streambuf readBuf;
     std::string path;
+    double offsetValue;
+    double scaleValue;
+    double minReadingThreshold;
+    double maxReadingThreshold;
+    std::string units;
     unsigned int sensorPollMs;
 
     void handleResponse(const boost::system::error_code& err);
