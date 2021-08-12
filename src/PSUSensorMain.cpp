999e862f7f44d8086bc6d0477ccff6daa72486b3
Willy Tu <wltu@google.com>
2021-08-12 00:07:35 -0700

    dbus-sensors: Send PSU sensor warning/critical event signal
    The signal can be picked up by phosphor-sel-logger to create SEL for
    the system to pick with IPMI handler.
    
    Tested: Verified with max34451 connected to BMC
    ```
    $ trigger_sensor_event(){
      export HWMON=`grep -l 'max34451' /sys/class/hwmon/hwmon*/name | head -n1 | xargs -n1 dirname`
      echo $HWMON
      grep -H '' $HWMON/temp1_{input,max,crit}
      grep -H '' $HWMON/temp1_{max_alarm,crit_alarm}
      echo 0 > $HWMON/temp1_max; sleep 1; grep -H '' $HWMON/temp1_{input,max,max_alarm}
      echo 327670 > $HWMON/temp1_max; sleep 1; grep -H '' $HWMON/temp1_{input,max,max_alarm}
      echo 0 > $HWMON/temp1_crit; sleep 1; grep -H '' $HWMON/temp1_{input,crit,crit_alarm}
      echo 327670 > $HWMON/temp1_crit; sleep 1; grep -H '' $HWMON/temp1_{input,crit,crit_alarm}
    
      ipmitool sel list
    }
    
    root@nkwz12-nfd:/run/initramfs# trigger_sensor_event
    /sys/class/hwmon/hwmon19
    /sys/class/hwmon/hwmon19/temp1_input:27370
    /sys/class/hwmon/hwmon19/temp1_max:327670
    /sys/class/hwmon/hwmon19/temp1_crit:327670
    /sys/class/hwmon/hwmon19/temp1_max_alarm:0
    /sys/class/hwmon/hwmon19/temp1_crit_alarm:0
    /sys/class/hwmon/hwmon19/temp1_input:27370
    /sys/class/hwmon/hwmon19/temp1_max:0
    /sys/class/hwmon/hwmon19/temp1_max_alarm:1
    /sys/class/hwmon/hwmon19/temp1_input:27370
    /sys/class/hwmon/hwmon19/temp1_max:327670
    /sys/class/hwmon/hwmon19/temp1_max_alarm:0
    /sys/class/hwmon/hwmon19/temp1_input:27370
    /sys/class/hwmon/hwmon19/temp1_crit:0
    /sys/class/hwmon/hwmon19/temp1_crit_alarm:1
    /sys/class/hwmon/hwmon19/temp1_input:27370
    /sys/class/hwmon/hwmon19/temp1_crit:327670
    /sys/class/hwmon/hwmon19/temp1_crit_alarm:0
       1 |  Pre-Init  |0000038910| Temperature #0x52 | Upper Critical going high | Asserted
       2 |  Pre-Init  |0000038912| Temperature #0x52 | Upper Non-critical going high | Asserted
       3 |  Pre-Init  |0000038912| Temperature #0x52 | Upper Critical going high | Deasserted
    ```
    
    Change-Id: Ic907153ddcc3cf0a42a4209bcfb5ee2ab75f0766
    Signed-off-by: Willy Tu <wltu@google.com>

--- src/PSUSensorMain.cpp
+++ src/PSUSensorMain.cpp
@@ -74,6 +74,8 @@ static std::vector<std::string> pmbusNames = {
     "max20730", "max20734",  "max20796",  "max34451",  "pmbus",
     "pxe1610",  "raa228000", "raa228228", "raa229004", "tps546d24"};
 
+static constexpr const char* sensorPathPrefix = "/xyz/openbmc_project/sensors/";
+
 namespace fs = std::filesystem;
 
 static boost::container::flat_map<std::string, std::shared_ptr<PSUSensor>>
@@ -100,10 +102,11 @@ static std::vector<PSUProperty> psuProperties;
 // sysfs. If the attributes exists in sysfs, then store the complete path
 // of the attribute into eventPathList.
 void checkEvent(
-    const std::string& directory,
+    const std::string& directory, const std::string& dbusName,
     const boost::container::flat_map<std::string, std::vector<std::string>>&
         eventMatch,
-    boost::container::flat_map<std::string, std::vector<std::string>>&
+    boost::container::flat_map<
+        std::string, std::vector<std::pair<std::string, std::string>>>&
         eventPathList)
 {
     for (const auto& match : eventMatch)
@@ -122,7 +125,8 @@ void checkEvent(
                 continue;
             }
 
-            eventPathList[eventName].push_back(eventPath);
+            eventPathList[eventName].push_back(
+                std::make_pair(dbusName, eventPath));
         }
     }
 }
@@ -130,14 +134,15 @@ void checkEvent(
 // Check Group Events which contains more than one targets in each combine
 // events.
 void checkGroupEvent(
-    const std::string& directory,
+    const std::string& directory, const std::string& dbusName,
     const boost::container::flat_map<
         std::string,
         boost::container::flat_map<std::string, std::vector<std::string>>>&
         groupEventMatch,
     boost::container::flat_map<
         std::string,
-        boost::container::flat_map<std::string, std::vector<std::string>>>&
+        boost::container::flat_map<
+            std::string, std::vector<std::pair<std::string, std::string>>>>&
         groupEventPathList)
 {
     for (const auto& match : groupEventMatch)
@@ -145,7 +150,8 @@ void checkGroupEvent(
         const std::string& groupEventName = match.first;
         const boost::container::flat_map<std::string, std::vector<std::string>>
             events = match.second;
-        boost::container::flat_map<std::string, std::vector<std::string>>
+        boost::container::flat_map<
+            std::string, std::vector<std::pair<std::string, std::string>>>
             pathList;
         for (const auto& match : events)
         {
@@ -162,7 +168,8 @@ void checkGroupEvent(
                     continue;
                 }
 
-                pathList[eventName].push_back(eventPath);
+                pathList[eventName].push_back(
+                    std::make_pair(dbusName, eventPath));
             }
         }
         groupEventPathList[groupEventName] = pathList;
@@ -174,10 +181,11 @@ void checkGroupEvent(
 // xxx_min_alarm exist, then store the existing paths of the alarm attributes
 // to eventPathList.
 void checkEventLimits(
-    const std::string& sensorPathStr,
+    const std::string& sensorPathStr, const std::string& dbusName,
     const boost::container::flat_map<std::string, std::vector<std::string>>&
         limitEventMatch,
-    boost::container::flat_map<std::string, std::vector<std::string>>&
+    boost::container::flat_map<
+        std::string, std::vector<std::pair<std::string, std::string>>>&
         eventPathList)
 {
     auto attributePartPos = sensorPathStr.find_last_of('_');
@@ -207,7 +215,8 @@ void checkEventLimits(
             {
                 continue;
             }
-            eventPathList[eventName].push_back(limitEventPath);
+            eventPathList[eventName].push_back(
+                std::make_pair(dbusName, limitEventPath));
         }
     }
 }
@@ -267,11 +276,13 @@ static void createSensorsCallback(
     boost::container::flat_set<std::string> directories;
     for (const auto& pmbusPath : pmbusPaths)
     {
-        boost::container::flat_map<std::string, std::vector<std::string>>
+        boost::container::flat_map<
+            std::string, std::vector<std::pair<std::string, std::string>>>
             eventPathList;
         boost::container::flat_map<
             std::string,
-            boost::container::flat_map<std::string, std::vector<std::string>>>
+            boost::container::flat_map<
+                std::string, std::vector<std::pair<std::string, std::string>>>>
             groupEventPathList;
 
         std::ifstream nameFile(pmbusPath);
@@ -436,8 +447,9 @@ static void createSensorsCallback(
             }
             sensorsChanged->erase(it);
         }
-        checkEvent(directory.string(), eventMatch, eventPathList);
-        checkGroupEvent(directory.string(), groupEventMatch,
+        checkEvent(directory.string(), std::string(), eventMatch,
+                   eventPathList);
+        checkGroupEvent(directory.string(), std::string(), groupEventMatch,
                         groupEventPathList);
 
         /* Check if there are more sensors in the same interface */
@@ -763,8 +775,6 @@ static void createSensorsCallback(
                 }
             }
 
-            checkEventLimits(sensorPathStr, limitEventMatch, eventPathList);
-
             // Similarly, if sensor scaling factor is being customized,
             // then the below power-of-10 constraint becomes unnecessary,
             // as config should be able to specify an arbitrary divisor.
@@ -847,6 +857,16 @@ static void createSensorsCallback(
                           << sensorPathStr << "\" type \"" << sensorType
                           << "\"\n";
             }
+
+            // /xyz/openbmc_project/sensors/<type>/<custom_json_name>
+            std::string dbusName =
+                sensorPathPrefix +
+                sensor_paths::getPathForUnits(findSensorUnit->second) + "/" +
+                boost::replace_all_copy(sensorName, " ", "_");
+
+            checkEventLimits(sensorPathStr, dbusName, limitEventMatch,
+                             eventPathList);
+
             // destruct existing one first if already created
             sensors[sensorName] = nullptr;
             sensors[sensorName] = std::make_shared<PSUSensor>(
