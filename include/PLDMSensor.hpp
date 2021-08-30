b129376439cde3ec55f2859ec69c92e46ba4df2e
Gilbert Chen <gilbert.chen@arm.com>
2021-08-30 19:07:54 +0800

    PLDMSensor: Initial revision
    1. Implement a sensor described in DSP0248 8.3.1 PLDM for access only
    2. Add PldmSensor service file
    3. Update meson files
    
    The PLDMSensor gets the following settings exposed by Entity-Manager to access PLDM
    
    "Eid":            <int: 8-255>
    "SensorId":       <int: 0-255>
    "SensorDataSize": <string: "int8_t"|"uint8_t"|"int16_t"|"uint16_t"|"int32_t"|"uint32_t">
    "SensorType":     <string: "temperature">
    "SensorUnit":     <string: "xyz.openbmc_project.Sensor.Value.Unit.DegressC">
    "Type":           <string: "PLDM">
    
    Signed-off-by: Gilbert Chen <gilbert.chen@arm.com>
    Change-Id: Iba6615af7f24aac8448fcb6976009cf98d335d1f

---
+++ include/PLDMSensor.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include <boost/asio/deadline_timer.hpp>
+#include <boost/container/flat_map.hpp>
+#include <sensor.hpp>
+
+#include <chrono>
+#include <limits>
+#include <memory>
+#include <string>
+#include <vector>
+
+struct PLDMSensor : public Sensor
+{
+  public:
+    PLDMSensor(std::shared_ptr<sdbusplus::asio::connection>& conn,
+               boost::asio::io_service& io, const std::string& name,
+               const std::string& sensorConfiguration,
+               sdbusplus::asio::object_server& objectServer,
+               std::vector<thresholds::Threshold>&& thresholds,
+               const std::string& sensorType, const std::string& sensorUnit,
+               const std::string& DataSize, uint8_t eid, uint16_t sensorId,
+               uint8_t instanceId);
+    ~PLDMSensor() override;
+
+    void checkThresholds(void) override;
+    void read(void);
+    void init(void);
+
+  private:
+    int pldmSendRecv(uint8_t eid, std::vector<uint8_t>& requestMsg,
+                     std::vector<uint8_t>& responseMsg);
+    int getSensorReading(uint8_t sensorId, int32_t* reading);
+    uint8_t eid;
+    uint8_t sensorId;
+    uint8_t sensorDataSize;
+    uint8_t instanceId;
+    std::string sensorPath;
+    sdbusplus::asio::object_server& objectServer;
+    boost::asio::deadline_timer waitTimer;
+};
