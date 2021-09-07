d0eb8b4d1d7af9f201ef295a2798955c93b6caa7
Oskar Senft <osk@google.com>
2021-09-07 13:42:47 -0400

    HwmonTempSensor: Add NCT7802 as a valid type.
    The TYAN S7106 mainboard uses an NCT7802 hardware monitoring IC to
    measure various temperatures on the board. Adding this supported
    type to allow correct references from EntityManager.
    
    Signed-off-by: Oskar Senft <osk@google.com>
    Change-Id: I8151068a0f740d8751af76b217a3ef458bd2e2ca

--- src/HwmonTempMain.cpp
+++ src/HwmonTempMain.cpp
@@ -40,7 +40,7 @@ static constexpr bool debug = false;
 static constexpr float pollRateDefault = 0.5;
 
 namespace fs = std::filesystem;
-static constexpr std::array<const char*, 16> sensorTypes = {
+static constexpr std::array<const char*, 17> sensorTypes = {
     "xyz.openbmc_project.Configuration.EMC1412",
     "xyz.openbmc_project.Configuration.EMC1413",
     "xyz.openbmc_project.Configuration.EMC1414",
@@ -48,6 +48,7 @@ static constexpr std::array<const char*, 16> sensorTypes = {
     "xyz.openbmc_project.Configuration.MAX31730",
     "xyz.openbmc_project.Configuration.MAX6581",
     "xyz.openbmc_project.Configuration.MAX6654",
+    "xyz.openbmc_project.Configuration.NCT7802",
     "xyz.openbmc_project.Configuration.SBTSI",
     "xyz.openbmc_project.Configuration.LM95234",
     "xyz.openbmc_project.Configuration.TMP112",
