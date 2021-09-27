140abf87f1e28224ea78b91aa9d418eb240c1505
Arun Lal K M <arun.lal@intel.com>
2021-09-27 12:01:46 +0000

    Add support for 'Get PMBUS Readings' method.
    VR sensor is currently read in the following way:
    BMC gives read command, and ME proxy forward it to VR sensor.
    
    In 'Get PMBUS Readings' method BMC reads the data from ME.
    This is used in platforms where ME cannot proxy forward PMBUS command.
    
    Command:
    0x2E 0xF5 0x57 0x01 0x00 0x<ID> 0x0F 0x00
    
    0x2E is net function to send raw PMBUS command
    0xF5 is corresponding to 'Get PMBUS Readings'
    0x57 0x01 0x00 is Intel manufacturers ID
    ID is the ID of senssor in ME (not actual sensor address)
    0x0F is PMBUS-enabled Device Index
    0x00 is reserved byte
    
    New configuration parameters in Baseboard.json:
    1) SensorMeAddress: ID of the sensor in ME.
    2) ReadMethod": use "IPMI" for 'Get PMBUS Readings'.
    3) Register": Register to read in the response byte array.
    
    Note:
    This is not a complete replacement for the old method, we are adding
    one more way to read these sensors. And the implementation is backwards
    compatible, as in, new code with old configuration file
    (xx_Baseboard.json) will work (using the old method of reading the
    sensor).
    
    There is a configuration item, which is read in entity manager code.
    And is a baseboard.json file. We configure sensors and there addresses
    there. If the user adds "ReadMethod", "Register" and "SensorMeAddress"
    then the user will be able to switch to new method of reading the
    sensor data.
    
    The new method is used in systems where ME can not proxy forward PMBUS
    command (for example, due to security reasons proxy forward is disabled
    at ME end).
    
    References:
    1) Intelligent Power Node Manager External Interface Specification
       Using IPMI.
    2) PMBus-Specification.
    
    Code clean up:
    1) Abstracted few set fucntions in IpmbSensor
    2) Optimised IpmbSensor::read() fuction, by avoiding switch case.
    3) Added explanation for few magic numbers used in command.
    
    Tested:
    Case 1: Using proxy (Backward compatibility testing)
    Sample configuration in Baseboard.json:
    {
        "Address": "0xXX",
        "Class": "MpsBridgeTemp",
        "Name": "XX VR Temp",
        "Thresholds": [...],
        "Type": "IpmbSensor"
    }
    
    Give command 'ipmitool sdr elist'
    Response:
    CPU1 PVCCD VR Te | 31h | ok  |  0.1 | 37 degrees C
    CPU2 PVCCD VR Te | 36h | ok  |  0.1 | 37 degrees C
    CPU1 PVCCFA EHV  | 32h | ok  |  0.1 | 34 degrees C
    CPU2 PVCCFA EHV  | 37h | ok  |  0.1 | 33 degrees C
    CPU1 VCCIN VR Te | 34h | ok  |  0.1 | 48 degrees C
    CPU2 VCCIN VR Te | 39h | ok  |  0.1 | 58 degrees C
    
    Case 2: Using 'Get PMBUS Readings' method
    Sample configuration in Baseboard.json:
    {
        "Address": "0xXX",
        "SensorMeAddress": "0xXX",
        "Class": "MpsBridgeTemp",
        "Register": 1,
        "ReadMethod": "IPMI",
        "Name": "XX VR Temp",
        "Thresholds": [...],
        "Type": "IpmbSensor"
    }
    
    Give command 'ipmitool sdr elist'
    Response:
    CPU1 PVCCD VR Te | 31h | ok  |  0.1 | 41 degrees C
    CPU2 PVCCD VR Te | 36h | ok  |  0.1 | 43 degrees C
    CPU1 PVCCFA EHV  | 32h | ok  |  0.1 | 37 degrees C
    CPU2 PVCCFA EHV  | 37h | ok  |  0.1 | 37 degrees C
    CPU1 VCCIN VR Te | 34h | ok  |  0.1 | 60 degrees C
    CPU2 VCCIN VR Te | 39h | ok  |  0.1 | 56 degrees C
    
    Signed-off-by: Arun Lal K M <arun.lal@intel.com>
    Change-Id: I15769d33ea2fa53dcc117b1f3c96041acd0f2fcf

--- include/IpmbSensor.hpp
+++ include/IpmbSensor.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <Utils.hpp>
 #include <boost/asio/deadline_timer.hpp>
 #include <boost/container/flat_map.hpp>
 #include <sensor.hpp>
@@ -43,6 +44,66 @@ namespace sensor
 {
 constexpr uint8_t netFn = 0x04;
 constexpr uint8_t getSensorReading = 0x2d;
+constexpr uint8_t manufacturerId[3] = {0x57, 0x01, 0x00};
+
+/*
+ * From PMBus Power System Management Protocol Specification Part II
+ * Command name to code mapping
+ */
+enum CommandName : uint8_t
+{
+    readTemperature = 0x8D,
+    readCurrentOutput = 0x8C
+};
+
+/*
+ * From Intelligent Power Node Manager : External Interface Specification Using
+ * IPMI SMBUS message transaction type
+ */
+enum SmbusMessageType : uint8_t
+{
+    sendByte = 0x00,
+    readByte = 0x01,
+    writeByte = 0x02,
+    readWord = 0x03,
+    writeWord = 0x04,
+    blockRead = 0x05,
+    blockWrite = 0x06,
+    blockWriteReadProcCall = 0x07
+};
+
+namespace read_me
+{
+constexpr uint8_t getSensorReading = 0xF5;
+constexpr uint8_t bytesForTimestamp = 4;
+constexpr uint8_t bytesForDeviceId = 3;
+constexpr uint8_t fixedOffset = bytesForTimestamp + bytesForDeviceId;
+
+void getRawData(uint8_t registerToRead, const std::vector<uint8_t>& input,
+                std::vector<uint8_t>& result)
+{
+    result.clear();
+
+    /* Every register is two bytes*/
+    auto offset = fixedOffset + (registerToRead * 2);
+
+    if (input.size() < static_cast<size_t>(offset + 1))
+    {
+        return;
+    }
+
+    result.reserve(5);
+
+    // ID
+    result.emplace_back(input[0]);
+    result.emplace_back(input[1]);
+    result.emplace_back(input[2]);
+
+    // Value in registerToRead
+    result.emplace_back(input[offset]);
+    result.emplace_back(input[offset + 1]);
+}
+} // namespace read_me
 
 static bool isValid(const std::vector<uint8_t>& data)
 {
@@ -84,32 +145,46 @@ struct IpmbSensor : public Sensor
                const float pollRate, std::string& sensorTypeName);
     ~IpmbSensor() override;
 
-    void checkThresholds(void) override;
-    void read(void);
     void init(void);
-    std::string getSubTypeUnits(void);
-    void loadDefaults(void);
     void runInitCmd(void);
-    bool processReading(const std::vector<uint8_t>& data, double& resp);
+    bool setSensorType(const std::string& sensorClass);
+    void setSensorSubType(const std::string& sensorTypeName);
+    void setScaleAndOffset(const SensorBaseConfigMap& sensorBaseConfig);
+    void setReadMethod(const SensorBaseConfigMap& sensorBaseConfig);
 
+  private:
     IpmbType type;
     IpmbSubType subType;
-    double scaleVal;
-    double offsetVal;
+    double scaleVal = 1;
+    double offsetVal = 0;
     uint8_t commandAddress;
     uint8_t netfn;
     uint8_t command;
     uint8_t deviceAddress;
     uint8_t errorCount;
     uint8_t hostSMbusIndex;
+    uint8_t registerToRead = 0;
+    bool isProxyRead = true;
+    uint8_t sensorMeAddress = 0;
     std::vector<uint8_t> commandData;
     std::optional<uint8_t> initCommand;
     std::vector<uint8_t> initData;
     int sensorPollMs;
-
     ReadingFormat readingFormat;
-
-  private:
     sdbusplus::asio::object_server& objectServer;
     boost::asio::deadline_timer waitTimer;
+    std::function<bool(const std::vector<uint8_t>& data, double& resp)>
+        readFunction;
+
+    void read(void);
+    void loadDefaults(void);
+    void setReadFunction(void);
+    void checkThresholds(void) override;
+    std::string getSubTypeUnits(void);
+    std::vector<uint8_t> getMeCommand();
+    std::vector<uint8_t>
+        getRawPmbusCommand(uint8_t messageType,
+                           const std::vector<uint8_t>& pmbusCommand,
+                           uint8_t readLength, bool isExtendedDeviceAddress,
+                           bool doEnablePec, bool doReportPecErrors);
 };
