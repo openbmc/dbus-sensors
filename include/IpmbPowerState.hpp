0d682b5a29f5fd7e0533dc408e139774356f9b66
Logananth Sundararaj <logananth.s@hcl.com>
2021-10-19 18:31:36 +0530

    dbus-sensors: Add IPMB based support to read ipmbpower states.
    Power states in dbus property represents the ON/OFF state of the
    host, which is connected through IPMB.
    
    x86-power-control uses one of this PGood Dbus property to get PGood
    state for the host, when the x86-power-control receives power ON
    request for a host, it first check the status of the PGood state for
    that host, if the PGood state is true x86-power-control proceeds to
    power ON the host, else the PGood state is false the host is turned OFF.
    
    This is based on OEM specified netfunction & command and this state
    can be used to check the ipmbpower states of each host through IPMB.
    
    Added a new JSON file in entity-manager to define the properties of
    ipmb state from the twinlake IPMB fru for the multihost. From the
    EM file, deviceAddress, bus of each state can also be configured.
    
    Device Address represents the IPMB command and bus represents
    the bus instance. It will be displayed using busctl command.
    
    TESTED: Tested on Facebook YosemiteV2 hardware.
    Verified the host's ipmb states.
    
    root@yosemitev2:~# busctl introspect xyz.openbmc_project.IpmbSensor
     /xyz/openbmc_project/hostcontroller/1_Twinlake/CPU_Good
    
    NAME                                      TYPE      SIGNATURE RESULT/VALUE FLAGS
    org.freedesktop.DBus.Introspectable       interface -         -       -
    .Introspect                               method    -         s       -
    org.freedesktop.DBus.Peer                 interface -         -       -
    .GetMachineId                             method    -         s       -
    .Ping                                     method    -         -       -
    org.freedesktop.DBus.Properties           interface -         -       -
    .Get                                      method    ss        v       -
    .GetAll                                   method    s         a{sv}   -
    .Set                                      method    ssv       -       -
    .PropertiesChanged                        signal    sa{sv}as  -       -
    xyz.openbmc_project.Chassis.Control.Power interface -         -       -
    .PGood                                    property  b         true    emits-change
    
    Signed-off-by: Logananth Sundararaj <logananth.s@hcl.com>
    Change-Id: I1376ef07a23b36db014b64841fe7460e3c98ccef

---
+++ include/IpmbPowerState.hpp
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <boost/container/flat_map.hpp>
+#include <sensor.hpp>
+
+#include <array>
+
+namespace oem
+{
+inline namespace twinlake
+{
+constexpr uint8_t netFn = 0x38;
+} // namespace twinlake
+} // namespace oem
+
+enum class ByteMask
+{
+    BYTE_3 = 3,
+    BYTE_4 = 4,
+    BYTE_5 = 5,
+    BYTE_6 = 6,
+    BYTE_7 = 7,
+    BYTE_8 = 8
+};
+
+enum class BitMask
+{
+    BIT_0 = 0,
+    BIT_1,
+    BIT_2,
+    BIT_3,
+    BIT_4,
+    BIT_5,
+    BIT_6,
+    BIT_7
+};
+
+struct IpmbStateInfo
+{
+    std::string IpmbState;
+    ByteMask byte;
+    BitMask bit;
+};
+static std::array<IpmbStateInfo, 2> twinlakeIpmbStateProp = {
+    {{"CPU_Good", ByteMask::BYTE_3, BitMask::BIT_0},
+     {"PCH_Good", ByteMask::BYTE_3, BitMask::BIT_1}}};
+
+static constexpr uint8_t ipmbHostBit = 2;
+static constexpr uint8_t lun = 0;
+static constexpr uint8_t ipmbRspLength = 9;
+using IpmbMethodType =
+    std::tuple<int, uint8_t, uint8_t, uint8_t, uint8_t, std::vector<uint8_t>>;
+
+class IpmbPowerMonitor : public std::enable_shared_from_this<IpmbPowerMonitor>
+{
+  public:
+    IpmbPowerMonitor(std::shared_ptr<sdbusplus::asio::connection>& conn,
+                     boost::asio::io_service& io, float pollRate,
+                     sdbusplus::asio::object_server& objectServer,
+                     std::string& name, uint8_t ipmbBusIndex,
+                     uint8_t deviceAddress, const std::string& ipmbHostClass,
+                     const std::vector<std::string>& ipmbStateNames);
+    ~IpmbPowerMonitor();
+
+    void read();
+    void incrementError();
+    static IpmbStateInfo getHostCtrlProp(std::string& propName);
+    void setIpmbPowerState(std::vector<uint8_t> ipmbResp);
+
+    int ipmbPollMs;
+    std::string hostName;
+    uint8_t ipmbBusIndex;
+    uint8_t deviceAddress;
+    std::vector<uint8_t> commandData = {0x15, 0xa0, 0};
+    uint8_t commandAddress = ipmbBusIndex << ipmbHostBit;
+    uint8_t netfn = oem::twinlake::netFn;
+    uint8_t retryCount = 0;
+    const std::string ipmbHostClass;
+    std::vector<std::string> ipmbStateNames;
+
+  private:
+    std::map<std::string, std::shared_ptr<sdbusplus::asio::dbus_interface>>
+        ipmbPowerInterfaceMap;
+    sdbusplus::asio::object_server& objectServer;
+    std::shared_ptr<sdbusplus::asio::connection> dbusConnection;
+    boost::asio::deadline_timer ioTimer;
+};
