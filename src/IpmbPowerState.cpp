85fd85c31d82332d7554e3fda7a094598da6f202
Logananth Sundararaj <logananth.s@hcl.com>
2021-10-19 18:31:36 +0530

    dbus-sensors: Add IPMB based support to read ipmbpower states.
    Power states in dbus property represents the ON/OFF state of the
    host, which is connected through IPMB.
    
    x86-power-control uses one of this PGood Dbus property to get PGood
    state for the host, when the x86-power-control receives power ON
    request for a host, it first check the status of the PGood state for
    that host, if the PGood state is true x86-power-control proceeds to
    power ON the host, else the PGood state is false the host is turned OFF.
    
    This is based on OEM specified netfunction & command and this state
    can be used to check the ipmbpower states of each host through IPMB.
    
    Added a new JSON file in entity-manager to define the properties of
    ipmb state from the twinlake IPMB fru for the multihost. From the
    EM file, deviceAddress, bus of each state can also be configured.
    
    Device Address represents the IPMB command and bus represents
    the bus instance. It will be displayed using busctl command.
    
    TESTED: Tested on Facebook YosemiteV2 hardware.
    Verified the host's ipmb states.
    
    root@yosemitev2:~# busctl introspect xyz.openbmc_project.IpmbSensor
     /xyz/openbmc_project/hostcontroller/1_Twinlake/1_CPU_Good
    
    NAME                                      TYPE      SIGNATURE RESULT/VALUE FLAGS
    org.freedesktop.DBus.Introspectable       interface -         -       -
    .Introspect                               method    -         s       -
    org.freedesktop.DBus.Peer                 interface -         -       -
    .GetMachineId                             method    -         s       -
    .Ping                                     method    -         -       -
    org.freedesktop.DBus.Properties           interface -         -       -
    .Get                                      method    ss        v       -
    .GetAll                                   method    s         a{sv}   -
    .Set                                      method    ssv       -       -
    .PropertiesChanged                        signal    sa{sv}as  -       -
    xyz.openbmc_project.Chassis.Control.Power interface -         -       -
    .PGood                                    property  b         true    emits-change
    
    Signed-off-by: Logananth Sundararaj <logananth.s@hcl.com>
    Change-Id: I1376ef07a23b36db014b64841fe7460e3c98ccef

---
+++ src/IpmbPowerState.cpp
@@ -0,0 +1,158 @@
+#include <IpmbPowerState.hpp>
+
+#include <iostream>
+
+IpmbPowerMonitor::IpmbPowerMonitor(
+    std::shared_ptr<sdbusplus::asio::connection>& conn,
+    boost::asio::io_service& io, const float pollRate,
+    sdbusplus::asio::object_server& objectServer, std::string& name,
+    const uint8_t ipmbBusIndex, const uint8_t deviceAddress,
+    const std::string& ipmbHostClass,
+    const std::vector<std::string>& ipmbStateNames) :
+    ipmbPollMs(static_cast<int>(pollRate * 1000)),
+    hostName(escapeName(name)), ipmbBusIndex(ipmbBusIndex),
+    deviceAddress(deviceAddress), ipmbHostClass(ipmbHostClass),
+    ipmbStateNames(ipmbStateNames), objectServer(objectServer),
+    dbusConnection(conn), ioTimer(io)
+{
+    std::string ipmbPowerPathPrefix = "/xyz/openbmc_project/hostcontroller/";
+    try
+    {
+        for (const auto& pathNames : ipmbStateNames)
+        {
+            sdbusplus::message::object_path dbusPath = ipmbPowerPathPrefix;
+            dbusPath /= hostName;
+            dbusPath /= escapeName(pathNames);
+            std::shared_ptr<sdbusplus::asio::dbus_interface> tmpDbusIface =
+                objectServer.add_interface(
+                    dbusPath, "xyz.openbmc_project.Chassis.Control.Power");
+            tmpDbusIface->register_property("PGood", false);
+            if (!tmpDbusIface->initialize())
+            {
+                std::cerr << "error initializing value interface\n";
+                return;
+            }
+            std::string pathName = escapeName(pathNames);
+            std::string ipmbPowerState = pathName.substr(2, pathName.size());
+
+            ipmbPowerInterfaceMap[ipmbPowerState] = tmpDbusIface;
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << ipmbHostClass << "\n";
+        throw e;
+    }
+}
+
+IpmbPowerMonitor::~IpmbPowerMonitor()
+{
+    ioTimer.cancel();
+    for (const auto& iface : ipmbPowerInterfaceMap)
+    {
+        objectServer.remove_interface(iface.second);
+    }
+}
+
+void IpmbPowerMonitor::incrementError()
+{
+    if (retryCount >= 3)
+    {
+        retryCount = 0;
+        return;
+    }
+    retryCount++;
+}
+
+IpmbStateInfo IpmbPowerMonitor::getHostCtrlProp(std::string& propName)
+{
+    for (const IpmbStateInfo& prop : twinlakeIpmbStateProp)
+    {
+        if (prop.IpmbState == propName)
+        {
+            return prop;
+        }
+    }
+    throw std::runtime_error("Invalid config");
+}
+
+void IpmbPowerMonitor::setIpmbPowerState(std::vector<uint8_t> ipmbResp)
+{
+    uint8_t ipmbRspLength = 9;
+
+    if (ipmbResp.size() != ipmbRspLength)
+    {
+        incrementError();
+        read();
+        return;
+    }
+    uint8_t stateByteMask = 0;
+    uint8_t stateBitMask = 0;
+
+    for (const auto& pathNames : ipmbStateNames)
+    {
+        std::string ipmbPowerPathPrefix =
+            "/xyz/openbmc_project/hostcontroller/";
+        sdbusplus::message::object_path dbusPath = ipmbPowerPathPrefix;
+        dbusPath /= hostName;
+        dbusPath /= escapeName(pathNames);
+
+        std::string pathName = escapeName(pathNames);
+        std::string ipmbStateName = pathName.substr(2, pathName.size());
+        IpmbStateInfo hostCtrolPropName = getHostCtrlProp(ipmbStateName);
+
+        stateByteMask = static_cast<uint8_t>(hostCtrolPropName.byte);
+        stateBitMask = static_cast<uint8_t>(hostCtrolPropName.bit);
+        /* First 3 bytes of index is IANA, so it has been ignored
+         * we are reading from a register which holds the pgood ipmb
+         * state register value is read and stored in ipmbResp
+         * variable
+         * using the bit and byte mask(stateByteMask and stateBitMask)
+         * the power good state is read. */
+
+        bool ipmbState =
+            bool(((ipmbResp[stateByteMask] >> stateBitMask) & 1) > 0);
+        ipmbPowerInterfaceMap[ipmbStateName]->set_property("PGood", ipmbState);
+    }
+}
+
+void IpmbPowerMonitor::read()
+{
+    std::weak_ptr<IpmbPowerMonitor> weakRef = weak_from_this();
+    ioTimer.expires_from_now(boost::posix_time::milliseconds(ipmbPollMs));
+    ioTimer.async_wait([weakRef](const boost::system::error_code& ec) {
+        if (ec == boost::asio::error::operation_aborted)
+        {
+            return; // we're being canceled
+        }
+        auto self = weakRef.lock();
+        if (!self)
+        {
+            return;
+        }
+        self->dbusConnection->async_method_call(
+            [weakRef](boost::system::error_code ec,
+                      const IpmbMethodType& response) {
+            const int status = std::get<0>(response);
+            auto self = weakRef.lock();
+            if (!self)
+            {
+                return;
+            }
+            if (ec || (status != 0))
+            {
+                self->incrementError();
+                self->read();
+                return;
+            }
+            const std::vector<uint8_t> ipmbResp = std::get<5>(response);
+
+            self->setIpmbPowerState(ipmbResp);
+            self->read();
+            },
+            "xyz.openbmc_project.Ipmi.Channel.Ipmb",
+            "/xyz/openbmc_project/Ipmi/Channel/Ipmb", "org.openbmc.Ipmb",
+            "sendRequest", self->commandAddress, self->netfn, lun,
+            self->deviceAddress, self->commandData);
+    });
+}
