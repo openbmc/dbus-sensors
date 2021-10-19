5b056a001e4a95705363275d02c9739adff2783e
Logananth Sundararaj <logananth.s@hcl.com>
2021-10-19 18:31:36 +0530

    dbus-sensors: Add IPMB based support to read Gpio states.
    Gpio states dbus property represents the ON/OFF state of the host's
    gpio state which is connected through IPMB.
    
    x86-power-control uses one of this PGood Dbus property to get PGood
    state for the host, when the x86-power-control receives power ON
    request for a host, it first check the status of the PGood state for
    that host, if the PGood state is true x86-power-control proceeds to
    power ON the host, else the PGood state is false the host is turned OFF.
    
    This is based on OEM specified netfunction & command and this state
    can be used to check the gpio states of each host through IPMB.
    
    Added a new JSON file in entity-manager to define the properties of
    gipo state from the twinlake IPMB fru for the multihost. From the
    EM file, deviceAddress, bus of each state can also be configured.
    
    Device Address represents the IPMB command and bus represents
    the bus instance. It will be displayed using busctl command.
    
    TESTED: Tested on Facebook YosemiteV2 hardware.
    Verified all the host Gpio states.
    
    root@yosemitev2:~# busctl introspect xyz.openbmc_project.IpmbSensor
     /xyz/openbmc_project/ipmbgpio/1_TwinlakeipmbGpiostate/1_CPU_Good
    
    NAME                                      TYPE      SIGNATURE RESULT/VALUE FLAGS
    org.freedesktop.DBus.Introspectable       interface -         -       -
    .Introspect                               method    -         s       -
    org.freedesktop.DBus.Peer                 interface -         -       -
    .GetMachineId                             method    -         s       -
    .Ping                                     method    -         -       -
    org.freedesktop.DBus.Properties           interface -         -       -
    .Get                                      method    ss        v       -
    .GetAll                                   method    s         a{sv}   -
    .Set                                      method    ssv       -       -
    .PropertiesChanged                        signal    sa{sv}as  -       -
    xyz.openbmc_project.Chassis.Control.Power interface -         -       -
    .PGood                                    property  b         true    emits-change
    
    Signed-off-by: Logananth Sundararaj <logananth.s@hcl.com>
    Change-Id: I1376ef07a23b36db014b64841fe7460e3c98ccef

--- src/IpmbSensor.cpp
+++ src/IpmbSensor.cpp
@@ -14,6 +14,7 @@
 // limitations under the License.
 */
 
+#include <GpioStateSensor.hpp>
 #include <IpmbSensor.hpp>
 #include <Utils.hpp>
 #include <VariantVisitors.hpp>
@@ -39,21 +40,22 @@ constexpr const bool debug = false;
 
 constexpr const char* configInterface =
     "xyz.openbmc_project.Configuration.IpmbSensor";
+constexpr const char* twinlakeInterface =
+    "xyz.openbmc_project.Configuration.TwinlakeHostController";
 static constexpr double ipmbMaxReading = 0xFF;
 static constexpr double ipmbMinReading = 0;
 
 static constexpr uint8_t meAddress = 1;
-static constexpr uint8_t lun = 0;
 static constexpr uint8_t hostSMbusIndexDefault = 0x03;
 static constexpr uint8_t ipmbBusIndexDefault = 0;
 static constexpr float pollRateDefault = 1; // in seconds
 
 static constexpr const char* sensorPathPrefix = "/xyz/openbmc_project/sensors/";
 
-using IpmbMethodType =
-    std::tuple<int, uint8_t, uint8_t, uint8_t, uint8_t, std::vector<uint8_t>>;
-
 boost::container::flat_map<std::string, std::unique_ptr<IpmbSensor>> sensors;
+
+boost::container::flat_map<std::string, std::unique_ptr<IpmbGpioStateMonitor>>
+    gpios;
 
 std::unique_ptr<boost::asio::deadline_timer> initCmdTimer;
 
@@ -490,6 +492,8 @@ void createSensors(
     boost::asio::io_service& io, sdbusplus::asio::object_server& objectServer,
     boost::container::flat_map<std::string, std::unique_ptr<IpmbSensor>>&
         sensors,
+    boost::container::flat_map<std::string,
+                               std::unique_ptr<IpmbGpioStateMonitor>>& gpios,
     std::shared_ptr<sdbusplus::asio::connection>& dbusConnection)
 {
     if (!dbusConnection)
@@ -508,7 +512,8 @@ void createSensors(
         {
             for (const auto& entry : pathPair.second)
             {
-                if (entry.first != configInterface)
+                if (entry.first != configInterface &&
+                    entry.first != twinlakeInterface)
                 {
                     continue;
                 }
@@ -567,11 +572,33 @@ void createSensors(
                         std::visit(VariantToStringVisitor(), findType->second);
                 }
 
-                auto& sensor = sensors[name];
-                sensor = std::make_unique<IpmbSensor>(
-                    dbusConnection, io, name, pathPair.first, objectServer,
-                    std::move(sensorThresholds), deviceAddress, hostSMbusIndex,
-                    pollRate, sensorTypeName);
+                    if (sensorTypeName == "ipmbGpioState")
+                    {
+                        std::vector<std::string> ipmbGpioStates;
+                        auto findIpmbGpioStates =
+                            entry.second.find("ChannelNames");
+                        if (findIpmbGpioStates != entry.second.end())
+                        {
+                            ipmbGpioStates = std::get<std::vector<std::string>>(
+                                findIpmbGpioStates->second);
+                        }
+                        else
+                        {
+                            std::cerr << "Error ChannelNames read from Json \n";
+                            continue;
+                        }
+                        auto& gpio = gpios[name];
+                        gpio = std::make_unique<IpmbGpioStateMonitor>(
+                            dbusConnection, io, pollRate, objectServer, name,
+                            ipmbBusIndex, deviceAddress, sensorClass,
+                            ipmbGpioStates);
+                        continue;
+                    }
+                    auto& sensor = sensors[name];
+                    sensor = std::make_unique<IpmbSensor>(
+                        dbusConnection, io, name, pathPair.first, objectServer,
+                        std::move(sensorThresholds), deviceAddress,
+                        hostSMbusIndex, pollRate, sensorTypeName);
 
                 sensor->parseConfigValues(entry.second);
                 if (!(sensor->sensorClassType(sensorClass)))
@@ -639,32 +666,40 @@ int main()
 
     initCmdTimer = std::make_unique<boost::asio::deadline_timer>(io);
 
-    io.post([&]() { createSensors(io, objectServer, sensors, systemBus); });
+    io.post(
+        [&]() { createSensors(io, objectServer, sensors, gpios, systemBus); });
 
     boost::asio::deadline_timer configTimer(io);
 
     std::function<void(sdbusplus::message::message&)> eventHandler =
         [&](sdbusplus::message::message&) {
-        configTimer.expires_from_now(boost::posix_time::seconds(1));
-        // create a timer because normally multiple properties change
-        configTimer.async_wait([&](const boost::system::error_code& ec) {
-            if (ec == boost::asio::error::operation_aborted)
-            {
-                return; // we're being canceled
-            }
-            createSensors(io, objectServer, sensors, systemBus);
-            if (sensors.empty())
-            {
-                std::cout << "Configuration not detected\n";
-            }
-        });
-    };
+            configTimer.expires_from_now(boost::posix_time::seconds(1));
+            // create a timer because normally multiple properties change
+            configTimer.async_wait([&](const boost::system::error_code& ec) {
+                if (ec == boost::asio::error::operation_aborted)
+                {
+                    return; // we're being canceled
+                }
+                createSensors(io, objectServer, sensors, gpios, systemBus);
+                if (sensors.empty())
+                {
+                    std::cout << "Configuration not detected\n";
+                }
+            });
+        };
 
     sdbusplus::bus::match::match configMatch(
         static_cast<sdbusplus::bus::bus&>(*systemBus),
         "type='signal',member='PropertiesChanged',path_namespace='" +
             std::string(inventoryPath) + "',arg0namespace='" + configInterface +
             "'",
+        eventHandler);
+
+    sdbusplus::bus::match::match twinlakeMatch(
+        static_cast<sdbusplus::bus::bus&>(*systemBus),
+        "type='signal',member='PropertiesChanged',path_namespace='" +
+            std::string(inventoryPath) + "',arg0namespace='" +
+            twinlakeInterface + "'",
         eventHandler);
 
     sdbusplus::bus::match::match powerChangeMatch(
