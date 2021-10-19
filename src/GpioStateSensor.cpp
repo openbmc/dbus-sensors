5abde843e8b3dbd6f99240547946718cbfad5e46
Logananth Sundararaj <logananth.s@hcl.com>
2021-10-19 18:31:36 +0530

    dbus-sensors: Add IPMB based support to read Gpio PGood state.
    Gpio PGood dbus property represents the ON/OFF state of the host's
    PGood gpio state which is connected through IPMB.
    
    x86-power-control uses this PGood Dbus property to get PGood state
    for the host, when the x86-power-control receives power ON request
    for a host, it first check the status of the PGood state for that
    host, if the PGood state is true x86-power-control proceeds to power
    ON the host, else the PGood state is false the host is turned OFF.
    
    This is based on OEM specified netfunction & command and this state
    can be used to check the PGood state of each host through IPMB.
    
    Added a new JSON file in entity-manager to define the properties of
    gipo state from the twinlake IPMB fru for the multihost. From the
    EM file, deviceAddress, bus of each state can also be configured.
    
    Device Address represents the IPMB command and bus represents
    the bus instance. It will be displayed using busctl command.
    
    TESTED: Tested on Facebook YosemiteV2 hardware.
    Verified all the host Gpio PGood state.
    
    root@yosemitev2:~# busctl introspect xyz.openbmc_project.IpmbSensor
     /xyz/openbmc_project/gpio/ipmbGpioState/1_CPU_Good
    
    NAME                                      TYPE      SIGNATURE RESULT/VALUE FLAGS
    org.freedesktop.DBus.Introspectable       interface -         -       -
    .Introspect                               method    -         s       -
    org.freedesktop.DBus.Peer                 interface -         -       -
    .GetMachineId                             method    -         s       -
    .Ping                                     method    -         -       -
    org.freedesktop.DBus.Properties           interface -         -       -
    .Get                                      method    ss        v       -
    .GetAll                                   method    s         a{sv}   -
    .Set                                      method    ssv       -       -
    .PropertiesChanged                        signal    sa{sv}as  -       -
    xyz.openbmc_project.Chassis.Control.Power interface -         -       -
    .PGood                                    property  b         true    emits-change
    
    Signed-off-by: Logananth Sundararaj <logananth.s@hcl.com>
    Change-Id: I1376ef07a23b36db014b64841fe7460e3c98ccef

---
+++ src/GpioStateSensor.cpp
@@ -0,0 +1,145 @@
+#include <GpioStateSensor.hpp>
+
+#include <iostream>
+
+static inline uint8_t ipmbHostBit = 2;
+static inline float convertToMs = 1000;
+
+static inline const char* gpioPathPrefix = "/xyz/openbmc_project/gpio/";
+static inline const char* gpioInterfaceName =
+    "xyz.openbmc_project.Chassis.Control.Power";
+inline std::string gpioMapName;
+const char* gpioDbusProperty = "PGood";
+
+IpmbGpioStateMonitor::IpmbGpioStateMonitor(
+    std::shared_ptr<sdbusplus::asio::connection>& conn,
+    boost::asio::io_service& io, const float pollRate,
+    sdbusplus::asio::object_server& objectServer, std::string& name,
+    const uint8_t ipmbBusIndex, const uint8_t deviceAddress,
+    const std::string& gpioTypeName, const std::string& gpioClass) :
+    gpioPollMs(static_cast<int>(pollRate * convertToMs)),
+    gpioName(escapeName(name)), ipmbBusIndex(ipmbBusIndex),
+    deviceAddress(deviceAddress), gpioClass(gpioClass),
+    objectServer(objectServer), dbusConnection(conn), ioTimer(io)
+{
+    try
+    {
+        gpioMapName = gpioName.substr(2, gpioName.size());
+        if (init())
+        {
+            std::string dbusPath =
+                gpioPathPrefix + gpioTypeName + "/" + gpioName;
+
+            gpioInterface =
+                objectServer.add_interface(dbusPath, gpioInterfaceName);
+
+            gpioInterface->register_property(gpioDbusProperty, bool(false));
+
+            if (!gpioInterface->initialize())
+            {
+                std::cerr << "error initializing value interface\n";
+                return;
+            }
+        }
+        else
+        {
+            throw std::runtime_error("Failed to initialize");
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << gpioClass << "\n";
+        return;
+    }
+    read();
+}
+
+IpmbGpioStateMonitor::~IpmbGpioStateMonitor()
+{
+    ioTimer.cancel();
+    objectServer.remove_interface(gpioInterface);
+}
+
+bool IpmbGpioStateMonitor::init()
+{
+    if (gpioClass == "twin_lake_gpio")
+    {
+        /* IPMB bus index - first 6 bits is device Index and last 2
+        bits is IPMB/ME channel. Hence shifting the bus to left
+        by 2 bits */
+        commandAddress = ipmbBusIndex << ipmbHostBit;
+        netfn = oem::twinlake_gpio::netFn;
+        gpioByteMask = twinlakeIpmbgpioMap[gpioMapName].byteMask;
+        gpioBitMask = twinlakeIpmbgpioMap[gpioMapName].bitMask;
+    }
+    else
+    {
+        std::cerr << "Invalid class " << gpioClass << " \n";
+        return false;
+    }
+    return true;
+}
+
+void IpmbGpioStateMonitor::incrementError()
+{
+    if (retryCount >= 3)
+    {
+        setProperty(false);
+        retryCount = 0;
+        return;
+    }
+    retryCount++;
+}
+
+void IpmbGpioStateMonitor::setProperty(bool value)
+{
+    gpioInterface->set_property(gpioDbusProperty, value);
+}
+
+void IpmbGpioStateMonitor::read()
+{
+    ioTimer.expires_from_now(boost::posix_time::milliseconds(gpioPollMs));
+    ioTimer.async_wait([this](const boost::system::error_code& ec) {
+        if (ec == boost::asio::error::operation_aborted)
+        {
+            return; // we're being canceled
+        }
+        dbusConnection->async_method_call(
+            [this](boost::system::error_code ec,
+                   const IpmbMethodType& response) {
+                const int status = std::get<0>(response);
+                if (ec || status)
+                {
+                    incrementError();
+                    read();
+                    return;
+                }
+
+                const std::vector<uint8_t> ipmbGpioData = std::get<5>(response);
+
+                /* First 3 bytes of index is IANA, so it has been ignored
+                 * we are reading from a register which holds the pgood gpio
+                 * state register value is read and stored in ipmbgpiodata
+                 * variable
+                 * using the bit mask(gpioBitMask) the pgood state is read. */
+
+                if (gpioByteMask >= ipmbGpioData.size())
+                {
+                    incrementError();
+                    read();
+                    return;
+                }
+
+                bool gpioState =
+                    bool((ipmbGpioData[gpioByteMask] & gpioBitMask) > 0);
+
+                setProperty(gpioState);
+
+                read();
+            },
+            "xyz.openbmc_project.Ipmi.Channel.Ipmb",
+            "/xyz/openbmc_project/Ipmi/Channel/Ipmb", "org.openbmc.Ipmb",
+            "sendRequest", commandAddress, netfn, lun, deviceAddress,
+            commandData);
+    });
+}
