ecd1f9af671550ec5c8f4da0a1a3f5bc959888c3
Zev Weiss <zev@bewilderbeest.net>
2021-12-01 19:01:03 +0000

    psusensor: replace lseek/read with pread
    On an AST1250 system with 50+ pmbus devices, this provides a worthwhile
    (~5-10%) performance improvement by substantially reducing the number of
    syscalls.  By dropping async_read_until() and doing the pread ourselves
    in handleResponse() we also simplify the management of the read buffer,
    converting it to a temporary on-stack array.
    
    Change-Id: I416505a1b001a898429c0526f73002bc45407bf0
    Signed-off-by: Zev Weiss <zev@bewilderbeest.net>

--- src/PSUEvent.cpp
+++ src/PSUEvent.cpp
@@ -17,7 +17,6 @@
 #include <PSUEvent.hpp>
 #include <SensorPaths.hpp>
 #include <boost/asio/io_service.hpp>
-#include <boost/asio/read_until.hpp>
 #include <boost/container/flat_map.hpp>
 #include <phosphor-logging/lg2.hpp>
 #include <sdbusplus/asio/connection.hpp>
@@ -205,20 +204,15 @@ void PSUSubEvent::setupRead(void)
         return;
     }
 
-    std::shared_ptr<boost::asio::streambuf> buffer =
-        std::make_shared<boost::asio::streambuf>();
     std::weak_ptr<PSUSubEvent> weakRef = weak_from_this();
-    boost::asio::async_read_until(
-        inputDev, *buffer, '\n',
-        [weakRef, buffer](const boost::system::error_code& ec,
-                          std::size_t /*bytes_transfered*/) {
-            std::shared_ptr<PSUSubEvent> self = weakRef.lock();
-            if (self)
-            {
-                self->readBuf = buffer;
-                self->handleResponse(ec);
-            }
-        });
+    inputDev.async_wait(boost::asio::posix::descriptor_base::wait_read,
+                        [weakRef](const boost::system::error_code& ec) {
+                            std::shared_ptr<PSUSubEvent> self = weakRef.lock();
+                            if (self)
+                            {
+                                self->handleResponse(ec);
+                            }
+                        });
 }
 
 void PSUSubEvent::restartRead()
@@ -238,6 +232,7 @@ void PSUSubEvent::restartRead()
     });
 }
 
+static constexpr size_t readBufLen = 128;
 void PSUSubEvent::handleResponse(const boost::system::error_code& err)
 {
     if ((err == boost::system::errc::bad_file_descriptor) ||
@@ -245,20 +240,26 @@ void PSUSubEvent::handleResponse(const boost::system::error_code& err)
     {
         return;
     }
-    std::istream responseStream(readBuf.get());
+
     if (!err)
     {
-        std::string response;
-        try
+        char readBuf[readBufLen];
+        ssize_t rdLen = pread(fd, readBuf, readBufLen - 1, 0);
+        if (rdLen >= 0)
         {
-            std::getline(responseStream, response);
-            int nvalue = std::stoi(response);
-            responseStream.clear();
-
-            updateValue(nvalue);
-            errCount = 0;
+            readBuf[rdLen] = '\0';
+            try
+            {
+                int nvalue = std::stoi(readBuf);
+                updateValue(nvalue);
+                errCount = 0;
+            }
+            catch (const std::invalid_argument&)
+            {
+                errCount++;
+            }
         }
-        catch (const std::invalid_argument&)
+        else
         {
             errCount++;
         }
@@ -276,7 +277,6 @@ void PSUSubEvent::handleResponse(const boost::system::error_code& err)
         updateValue(0);
         errCount++;
     }
-    lseek(fd, 0, SEEK_SET);
     restartRead();
 }
 
