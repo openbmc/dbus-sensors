a870e6d8bf324b18b8dea202e37b7be08ccc73a1
Jayashree Dhanapal <jayashree-d@hcl.com>
2021-12-09 12:29:45 +0530

    Add OBMC_HOST_INSTANCES to define the IPMB bus index.
    Added OBMC_HOST_INSTANCES in meson options to identify the number of
    host instances in each platform.
    
    IPMB bus index can be read from entity manager config file.
    It represents which IPMB bus is being used to read the values.
    
    After reading IPMB bus index from EM config file, it will check the
    bus index by comparing OBMC_HOST_INSTANCES for their platform.
    
    TESTED: Tested on Facebook YosemiteV2 hardware for multi host
    instances.
    
    Signed-off-by: Jayashree Dhanapal <jayashree-d@hcl.com>
    Change-Id: I6e1ab04891b81c115a2729c1c81cea1b0715d14c

--- src/IpmbSensor.cpp
+++ src/IpmbSensor.cpp
@@ -45,6 +45,7 @@ static constexpr double ipmbMinReading = 0;
 static constexpr uint8_t meAddress = 1;
 static constexpr uint8_t lun = 0;
 static constexpr uint8_t hostSMbusIndexDefault = 0x03;
+static constexpr uint8_t ipmbBusIndexDefault = 0;
 static constexpr float pollRateDefault = 1; // in seconds
 
 static constexpr const char* sensorPathPrefix = "/xyz/openbmc_project/sensors/";
@@ -467,6 +468,23 @@ void createSensors(
                         if (pollRate <= 0.0f)
                         {
                             pollRate = pollRateDefault;
+                        }
+                    }
+
+                    uint8_t ipmbBusIndex = ipmbBusIndexDefault;
+                    auto findBusType = entry.second.find("Bus");
+                    if (findBusType != entry.second.end())
+                    {
+                        ipmbBusIndex = std::visit(VariantToUnsignedIntVisitor(),
+                                                  findBusType->second);
+
+                        auto hostInstances = findHostInstances(ipmbBusIndex);
+                        if (ipmbBusIndex != hostInstances)
+                        {
+                            ipmbBusIndex = ipmbBusIndexDefault;
+                            std::cerr
+                                << "IPMB bus index exceeds OBMC HOST INSTANCES"
+                                << "\n";
                         }
                     }
 
