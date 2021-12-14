831a2b59b8179937aadf1709e563c8ada90edccf
Jayashree Dhanapal <jayashree-d@hcl.com>
2021-12-14 15:22:23 +0530

    IpmbSensor:Add bus index for multiple IPMB channel
    In multi host platform, IPMB bus channel will be different for each
    host. Therefore, the bus index will be passed as an entity for each
    sensor from EM config file. "Bus" will be used as an entity for
    IPMB bus index.
    
    In IpmbSensor, bus index will be read and specified bus will be used
    to read each sensor value.
    
    Example :
            {
                "Address": "0x01",
                "Bus": "$bus",
                "Name": "Temp_Sensor",
                "Type": "IpmbSensor"
            }
    
    TESTED: Tested on Facebook YosemiteV2 hardware for multi host
    IPMB bus.
    
    Signed-off-by: Jayashree Dhanapal <jayashree-d@hcl.com>
    Change-Id: I0a47057da98ee81471ac9f6fac69a036853db7d1

--- src/IpmbSensor.cpp
+++ src/IpmbSensor.cpp
@@ -45,6 +45,7 @@ static constexpr double ipmbMinReading = 0;
 static constexpr uint8_t meAddress = 1;
 static constexpr uint8_t lun = 0;
 static constexpr uint8_t hostSMbusIndexDefault = 0x03;
+static constexpr uint8_t ipmbBusIndexDefault = 0;
 static constexpr float pollRateDefault = 1; // in seconds
 
 static constexpr const char* sensorPathPrefix = "/xyz/openbmc_project/sensors/";
@@ -468,6 +469,16 @@ void createSensors(
                         {
                             pollRate = pollRateDefault;
                         }
+                    }
+
+                    uint8_t ipmbBusIndex = ipmbBusIndexDefault;
+                    auto findBusType = entry.second.find("Bus");
+                    if (findBusType != entry.second.end())
+                    {
+                        ipmbBusIndex = std::visit(VariantToUnsignedIntVisitor(),
+                                                  findBusType->second);
+                        std::cerr << "Ipmb Bus Index for " << name << " is "
+                                  << ipmbBusIndex << "\n";
                     }
 
                     /* Default sensor type is "temperature" */
