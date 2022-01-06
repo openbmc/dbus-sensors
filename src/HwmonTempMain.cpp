8baf034e656d4b4abc465a4d14d2175c6944e093
Jayashree Dhanapal <jayashree-d@hcl.com>
2022-01-06 12:05:06 +0530

    Fix for hwmontemp sensor to avoid invoking same sensor
    More than one sensor type is present for DPS310 and SI7020 device.
    Therefore, "continue" is removed in the below patch, so the loop
    will not skip the path after finding the first sensor of the device,
    eventhough it is already searched. Because of that, same sensor is
    invoking constructor for another hwmon path.
    
    https://gerrit.openbmc-project.xyz/c/openbmc/dbus-sensors/+/45406
    
    In this commit, once a sensor match is found and a sensor has been
    constructed, config from the sensorConfiguration map is removed.
    Therefore, this will avoid invoking a constructor for same sensor.
    
    Tested: Tested in Facebook YosemiteV2 platform and verified the
    hwmontemp sensors.
    
    Signed-off-by: Jayashree Dhanapal <jayashree-d@hcl.com>
    Change-Id: Iae06b2e1d4245e2f455b1366924694a55d3b0a05

--- src/HwmonTempMain.cpp
+++ src/HwmonTempMain.cpp
@@ -150,7 +150,7 @@ void createSensors(
     auto getter = std::make_shared<GetSensorConfiguration>(
         dbusConnection,
         [&io, &objectServer, &sensors, &dbusConnection,
-         sensorsChanged](const ManagedObjectType& sensorConfigurations) {
+         sensorsChanged](ManagedObjectType& sensorConfigurations) {
             bool firstScan = sensorsChanged == nullptr;
 
             // IIO _raw devices look like this on sysfs:
@@ -219,10 +219,12 @@ void createSensors(
                 const SensorBaseConfigMap* baseConfigMap = nullptr;
 
                 auto thisSensorParameters = getSensorParameters(path);
+                sdbusplus::message::object_path objPath;
 
                 for (const std::pair<sdbusplus::message::object_path,
                                      SensorData>& sensor : sensorConfigurations)
                 {
+                    objPath = sensor.first;
                     sensorData = &(sensor.second);
                     for (const char* type : sensorTypes)
                     {
@@ -402,6 +404,7 @@ void createSensors(
                         sensor->setupRead();
                     }
                 }
+                sensorConfigurations.erase(objPath);
             }
         });
     getter->getConfiguration(
