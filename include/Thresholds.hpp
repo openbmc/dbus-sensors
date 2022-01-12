ed8f4e66090ab7cb33c4fc086363febc4f3377b7
Ed Tanous <edtanous@google.com>
2022-01-12 14:24:45 -0800

    Rearrange thresholds to be pair based
    It was pointed out in another review that the way the dbus interfaces
    are set up, they are all pair-wise, with a positive and negative end
    of the interface.  This changes the data structures to match that
    reality, inventing a "ThresholdDefinition" object.  This reduces the
    code, and allows us to promote the level definitions up to a higher
    scope in the structure.
    
    This greatly simplifies the thresProp structure to be greatly
    simpler, and rely on the helper functions.
    
    Tested: WIP
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: Ic9d5ba8282f78fe928689cb7fbd98d575a570200

--- include/Thresholds.hpp
+++ include/Thresholds.hpp
@@ -112,7 +112,7 @@ struct ThresholdTimer
     std::list<TimerPair> timers;
 };
 
-bool findOrder(Level lev, Direction dir);
+bool isValidLevel(Level lev);
 
 bool parseThresholdsFromConfig(
     const SensorData& sensorData,
