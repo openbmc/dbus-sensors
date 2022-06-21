889b032ab43e1d41dcefdfeb7b13f61805a786e5
Xiaochao Ma <maxiaochao@inspur.com>
2022-06-21 16:54:27 +0800

    Make FanSensor work without gpioLine.event_read method
    The current presence function uses the gpioLine.event_read method
    to detect gpio signal changes, and then to determine the presence status.
    
    I have a board with this feature not working.
    The reason is that this board uses PCA9555 to extend the gpio interface,
    and no interrupt is established between BMC and 9555,
    resulting in event_read never detecting signal changes.
    
    So based on this board, I implemented an in-place detection method that
    does not use the event_read method.
    The presence status is detected by actively pulling the gpio signal
    regularly.
    
    Preconditions:
    Add the configuration of the dual rotor fan in xxx_BaseBoard.json,
    for example:
    ```
    {
    "Address": "0x20",
    "Bus": 8,
    "Index": 0,
    "MaxReading": 30000,
    "PowerState": "Always",
    "Presence": {
    "PinName": "FAN_PRSNT0_D_N",
    "Polarity": "Low"
    },
    "Name": "fan0_0",
    "Type": "I2CFan"
    },
    {
    "Address": "0x20",
    "Bus": 8,
    "Index": 1,
    "MaxReading": 30000,
    "PowerState": "Always",
    "Presence": {
    "PinName": "FAN_PRSNT0_D_N",
    "Polarity": "Low"
    },
    "Name": "fan0_1",
    "Type": "I2CFan"
    },
    ```
    
    Tested
    root@testmc:~# busctl introspect xyz.openbmc_project.FanSensor /xyz/openbmc_project/inventory/fan0_0 xyz.openbmc_project.Inventory.Item
    NAME                               TYPE      SIGNATURE RESULT/VALUE FLAGS
    .Present                           property  b         true         emits-change
    .PrettyName                        property  s         ""           emits-change
    root@testmc:~# busctl introspect xyz.openbmc_project.FanSensor /xyz/openbmc_project/inventory/fan0_1 xyz.openbmc_project.Inventory.Item
    NAME                               TYPE      SIGNATURE RESULT/VALUE FLAGS
    .Present                           property  b         true         emits-change
    .PrettyName                        property  s         ""           emits-change
    
    unplug this fan
    root@testmc:~# busctl introspect xyz.openbmc_project.FanSensor /xyz/openbmc_project/inventory/fan0_0 xyz.openbmc_project.Inventory.Item
    NAME                               TYPE      SIGNATURE RESULT/VALUE FLAGS
    .Present                           property  b         false        emits-change
    .PrettyName                        property  s         ""           emits-change
    root@testmc:~# busctl introspect xyz.openbmc_project.FanSensor /xyz/openbmc_project/inventory/fan0_1 xyz.openbmc_project.Inventory.Item
    NAME                               TYPE      SIGNATURE RESULT/VALUE FLAGS
    .Present                           property  b         false        emits-change
    .PrettyName                        property  s         ""           emits-change
    
    journal log:
    root@testmc:~# journalctl | grep fan
    Jan 01 08:01:43 testmc .........
    Jan 01 08:01:43 testmc fansensor[409]: Fan Removed
    Jan 01 08:01:43 testmc fansensor[409]: Fan Insert
    Jan 01 08:01:43 testmc .........
    
    Signed-off-by: Xiaochao Ma <maxiaochao@inspur.com>
    Change-Id: I15215e6e919fabc8b362016c9bb8a5bfaea842db

--- src/TachSensor.cpp
+++ src/TachSensor.cpp
@@ -29,6 +29,7 @@
 #include <iostream>
 #include <istream>
 #include <limits>
+#include <map>
 #include <memory>
 #include <optional>
 #include <stdexcept>
@@ -37,6 +38,9 @@
 #include <vector>
 
 static constexpr unsigned int pwmPollMs = 500;
+std::map<std::string, gpiod::line>
+    staticGpioMap; // Used to map two rotors to ONE GPIOLine.
+std::map<std::string, bool> gpioPresValueMap;
 
 TachSensor::TachSensor(const std::string& path, const std::string& objectType,
                        sdbusplus::asio::object_server& objectServer,
@@ -206,37 +210,44 @@ void TachSensor::checkThresholds(void)
     }
 }
 
-PresenceSensor::PresenceSensor(const std::string& gpioName, bool inverted,
-                               boost::asio::io_service& io,
-                               const std::string& name) :
-    gpioLine(gpiod::find_line(gpioName)),
-    gpioFd(io), name(name)
+void PresenceSensor::initGpio(const std::string& gpioName, bool inverted)
 {
-    if (!gpioLine)
+    if (staticGpioMap.contains(gpioName))
     {
-        std::cerr << "Error requesting gpio: " << gpioName << "\n";
-        status = false;
+        gpioLine = staticGpioMap.at(gpioName);
         return;
     }
 
+    gpioLine = gpiod::find_line(gpioName);
     try
     {
-        gpioLine.request({"FanSensor", gpiod::line_request::EVENT_BOTH_EDGES,
+        gpioLine.request({"FanSensor", gpiod::line_request::DIRECTION_INPUT,
                           inverted ? gpiod::line_request::FLAG_ACTIVE_LOW : 0});
-        status = gpioLine.get_value();
-
-        int gpioLineFd = gpioLine.event_get_fd();
-        if (gpioLineFd < 0)
-        {
-            std::cerr << "Failed to get " << gpioName << " fd\n";
-            return;
-        }
-
-        gpioFd.assign(gpioLineFd);
     }
     catch (const std::system_error&)
     {
         std::cerr << "Error reading gpio: " << gpioName << "\n";
+        return;
+    }
+
+    std::string gpioPresLabel =
+        gpioLine.get_chip().name() + std::to_string(gpioLine.offset());
+
+    staticGpioMap.emplace(gpioName, gpioLine);
+    gpioPresValueMap.emplace(gpioPresLabel, gpioLine.get_value());
+}
+
+PresenceSensor::PresenceSensor(const std::string& gpioName, bool inverted,
+                               boost::asio::io_service& io,
+                               const std::string& name) :
+    gpioFd(io),
+    name(name), repeatTimer(io)
+{
+    initGpio(gpioName, inverted);
+
+    if (!gpioLine)
+    {
+        std::cerr << "Error requesting gpio: " << gpioName << "\n";
         status = false;
         return;
     }
@@ -246,49 +257,48 @@ PresenceSensor::PresenceSensor(const std::string& gpioName, bool inverted,
 
 PresenceSensor::~PresenceSensor()
 {
-    gpioFd.close();
     gpioLine.release();
 }
 
 void PresenceSensor::monitorPresence(void)
 {
-    gpioFd.async_wait(boost::asio::posix::stream_descriptor::wait_read,
-                      [this](const boost::system::error_code& ec) {
-        if (ec == boost::system::errc::bad_file_descriptor)
+    auto statusTry = gpioLine.get_value();
+    std::string gpioPresLabel =
+        gpioLine.get_chip().name() + std::to_string(gpioLine.offset());
+
+    if (gpioPresValueMap.at(gpioPresLabel) != statusTry)
+    {
+        if (++confirmTimes >= 3)
         {
-            return; // we're being destroyed
+            gpioPresValueMap.at(gpioPresLabel) = statusTry;
+            status = statusTry;
+            if (statusTry)
+            {
+                logFanInserted(name);
+            }
+            else
+            {
+                logFanRemoved(name);
+            }
+
+            confirmTimes = 0;
         }
-        if (ec)
-        {
-            std::cerr << "Error on presence sensor " << name << " \n";
-            ;
-        }
-        else
-        {
-            read();
-        }
-        monitorPresence();
-    });
+    }
+    repeatTimer.expires_after(std::chrono::seconds(1));
+    repeatTimer.async_wait(
+        [this](const boost::system::error_code&) { monitorPresence(); });
 }
 
-void PresenceSensor::read(void)
+bool PresenceSensor::getValue()
 {
-    gpioLine.event_read();
-    status = gpioLine.get_value();
-    // Read is invoked when an edge event is detected by monitorPresence
-    if (status)
+    if (!gpioLine)
     {
-        logFanInserted(name);
+        std::cerr << "Error get gpioLine" << std::endl;
+        return false;
     }
-    else
-    {
-        logFanRemoved(name);
-    }
-}
-
-bool PresenceSensor::getValue(void)
-{
-    return status;
+    std::string gpioPresLabel =
+        gpioLine.get_chip().name() + std::to_string(gpioLine.offset());
+    return gpioPresValueMap.at(gpioPresLabel);
 }
 
 RedundancySensor::RedundancySensor(size_t count,
