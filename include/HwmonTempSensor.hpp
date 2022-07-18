4880844b90c17bc7bb85406b600b1adbbb6df360
Zev Weiss <zev@bewilderbeest.net>
2022-07-18 16:59:35 -0700

    hwmontempsensor: Manage devices on host power-state events
    We now install handler callbacks for power event signals, deactivating
    sensors on power-off and activating them on power-on/POST-complete
    according to their PowerState configuration.  This is needed to properly
    support sensor devices that are in the same power domain as the host,
    and hence may need their drivers bound only after the host is powered on
    so that the driver can properly probe and attach to the device (and
    likewise be unbound when the host is powered off).
    
    Tested: in combination with corresponding entity-manager patches to
    prevent it from trying to manage the same devices, hwmontempsensor
    successfully activates and deactivates host-power-domain nct6779 and
    w83773g sensors on romed8hm3.  After the host is shut off, sensors
    remain on dbus with 'Available' set to false and 'Value' reading NaN.
    Also tested killing hwmontempsor after it instantiated host-power-domain
    sensors, shutting off the host, and then restarting hwmontempsensor to
    verify that it removed and re-created the sensor devices as necessary.
    
    Signed-off-by: Zev Weiss <zev@bewilderbeest.net>
    Change-Id: I2e272fd0870f3abfb06b6f3617f32721861bbc5b

--- include/HwmonTempSensor.hpp
+++ include/HwmonTempSensor.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <DeviceMgmt.hpp>
 #include <Thresholds.hpp>
 #include <boost/asio/random_access_file.hpp>
 #include <sdbusplus/asio/object_server.hpp>
@@ -30,14 +31,20 @@ class HwmonTempSensor :
                     std::vector<thresholds::Threshold>&& thresholds,
                     const struct SensorParams& thisSensorParameters,
                     float pollRate, const std::string& sensorConfiguration,
-                    PowerState powerState);
+                    PowerState powerState,
+                    const std::shared_ptr<I2CDevice>& i2cDevice);
     ~HwmonTempSensor() override;
     void setupRead(void);
+    void activate(const std::string& newPath,
+                  const std::shared_ptr<I2CDevice>& newI2CDevice);
+    void deactivate(void);
+    bool isActive(void);
 
   private:
     // Ordering is important here; readBuf is first so that it's not destroyed
     // while async operations from other member fields might still be using it.
     std::array<char, 128> readBuf{};
+    std::shared_ptr<I2CDevice> i2cDevice;
     sdbusplus::asio::object_server& objServer;
     boost::asio::random_access_file inputDev;
     boost::asio::steady_timer waitTimer;
