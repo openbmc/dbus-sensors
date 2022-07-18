bba872c7f621453c15e63ffcf0e8aba50484b710
Andrew Jeffery <andrew@aj.id.au>
2022-07-18 16:32:11 +0930

    NVMeBasicContext: Properly cleanup resources, allowing destruction
    nvmesensor was terminating with an uncaught exception, e.g:
    
        Jun 03 04:52:09 bmc nvmesensor[507]: terminate called after throwing an instance of 'sdbusplus::exception::SdBusError'
        Jun 03 04:52:09 bmc nvmesensor[507]:   what():  sd_bus_add_object_vtable: org.freedesktop.DBus.Error.FileExists: File exists
    
    This would occur whenever entity-manager published a configuration for a
    new drive in the system. The implementation of nvmesensor isn't the
    smartest, and it tries to just scrap all NVMeContexts it knows of, along
    with their NVMeSensor instances, and reconstruct them all from newly
    captured entity-manager configuration data.
    
    The problem lies in the fact that the NVMeContexts were not getting
    destructed due embedding of std::shared_ptrs obtained via
    shared_from_this() into async callback lambda captures whose context was
    owned by the NVMeContext implementation.
    
    Switch to capturing `this` via weak_from_this() in callback lambdas to
    prevent the circular references. By doing so we are able to successfully
    destruct the NVMeContext derivative instances via the clear() method on
    the context map.
    
    However, there's more to the story, as the NVMeSensors owned by a given
    context are asynchronously iterated for polling purposes. To make sure
    we don't have races from unbounded latency on asynchronous destruction
    of NVMeSensors, we order the std::jthread and async stream member
    variables of NVMeBasicContext such that destruction of the streams exits
    the IO thread due to read() or write() syscall failures on the pipes.
    The use of std::jthread ensures we join() to complete the cleanup prior
    to returning from the NVMeBasicContext destructor.
    
    Preventing active polling of NVMeSensor instances ensures their
    destruction when clearing the NVMe context map, opening the path for
    successful (re-)construction of NVMeSensor DBus objects for the
    published sensor configurations.
    
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    Change-Id: I63d0e7eb3318c209b08551072a3cb7279da21269

--- include/NVMeBasicContext.hpp
+++ include/NVMeBasicContext.hpp
@@ -5,6 +5,8 @@
 #include <boost/asio/io_service.hpp>
 #include <boost/asio/posix/stream_descriptor.hpp>
 
+#include <thread>
+
 class NVMeBasicContext : public NVMeContext
 {
   public:
@@ -19,6 +21,22 @@ class NVMeBasicContext : public NVMeContext
     NVMeBasicContext(boost::asio::io_service& io, int rootBus, int cmdOut,
                      int streamIn, int streamOut, int cmdIn);
     boost::asio::io_service& io;
+
+    // The IO thread must be destructed after the stream descriptors, so
+    // initialise it first. http://eel.is/c++draft/class.base.init#note-6
+    //
+    // Providing a stop-source to the thread execution function isn't
+    // particularly useful as it will spend most of its time blocked in a system
+    // call - ioctl() for the actual device communication, or read() and write()
+    // on the pipes associated with reqStream and respStream. Rather than trying
+    // to force a stop, rely on read()/write() failures from closed pipes to
+    // coerce it to exit and thus allow completion of the join().
+    std::jthread thread;
+
+    // Destruction of the stream descriptors has the effect of issuing cancel(),
+    // destroying the closure of the callback where we might be carrying
+    // weak_ptrs to `this`.
+    // https://www.boost.org/doc/libs/1_79_0/doc/html/boost_asio/reference/posix__basic_descriptor/_basic_descriptor.html
     boost::asio::posix::stream_descriptor reqStream;
     boost::asio::posix::stream_descriptor respStream;
 
--- src/NVMeBasicContext.cpp
+++ src/NVMeBasicContext.cpp
@@ -15,7 +15,6 @@
 #include <cstdio>
 #include <cstring>
 #include <system_error>
-#include <thread>
 
 extern "C"
 {
@@ -231,8 +230,8 @@ NVMeBasicContext::NVMeBasicContext(boost::asio::io_service& io, int rootBus) :
     FileHandle streamOut(responsePipe[1]);
     respStream.assign(responsePipe[0]);
 
-    std::thread thread([streamIn{std::move(streamIn)},
-                        streamOut{std::move(streamOut)}]() mutable {
+    thread = std::jthread([streamIn{std::move(streamIn)},
+                           streamOut{std::move(streamOut)}]() mutable {
         ssize_t rc = 0;
 
         if ((rc = processBasicQueryStream(streamIn, streamOut)) < 0)
@@ -243,7 +242,6 @@ NVMeBasicContext::NVMeBasicContext(boost::asio::io_service& io, int rootBus) :
 
         std::cerr << "Terminating basic query thread\n";
     });
-    thread.detach();
 }
 
 void NVMeBasicContext::readAndProcessNVMeSensor()
@@ -325,7 +323,7 @@ void NVMeBasicContext::readAndProcessNVMeSensor()
         response->prepare(len);
         return len;
         },
-        [self{shared_from_this()}, sensor, response](
+        [weakSelf{weak_from_this()}, sensor, response](
             const boost::system::error_code& ec, std::size_t length) mutable {
         if (ec)
         {
@@ -339,17 +337,20 @@ void NVMeBasicContext::readAndProcessNVMeSensor()
             return;
         }
 
-        /* Deserialise the response */
-        response->consume(1); /* Drop the length byte */
-        std::istream is(response.get());
-        std::vector<char> data(response->size());
-        is.read(data.data(), response->size());
+        if (auto self = weakSelf.lock())
+        {
+            /* Deserialise the response */
+            response->consume(1); /* Drop the length byte */
+            std::istream is(response.get());
+            std::vector<char> data(response->size());
+            is.read(data.data(), response->size());
 
-        /* Update the sensor */
-        self->processResponse(sensor, data.data(), data.size());
+            /* Update the sensor */
+            self->processResponse(sensor, data.data(), data.size());
 
-        /* Enqueue processing of the next sensor */
-        self->readAndProcessNVMeSensor();
+            /* Enqueue processing of the next sensor */
+            self->readAndProcessNVMeSensor();
+        }
     });
 }
 
@@ -358,8 +359,8 @@ void NVMeBasicContext::pollNVMeDevices()
     pollCursor = sensors.begin();
 
     scanTimer.expires_from_now(boost::posix_time::seconds(1));
-    scanTimer.async_wait(
-        [self{shared_from_this()}](const boost::system::error_code errorCode) {
+    scanTimer.async_wait([weakSelf{weak_from_this()}](
+                             const boost::system::error_code errorCode) {
         if (errorCode == boost::asio::error::operation_aborted)
         {
             return;
@@ -371,7 +372,10 @@ void NVMeBasicContext::pollNVMeDevices()
             return;
         }
 
-        self->readAndProcessNVMeSensor();
+        if (auto self = weakSelf.lock())
+        {
+            self->readAndProcessNVMeSensor();
+        }
     });
 }
 
