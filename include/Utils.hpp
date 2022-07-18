7ef57237e6524c5a28ce45c5124a4de1a273aed6
Zev Weiss <zev@bewilderbeest.net>
2022-07-18 14:20:35 -0700

    Utils: Add MgmtParams struct
    This struct contains information retrieved from sensor config data about
    how to manage the sensor's lifecycle.
    
    Change-Id: I27069e14fbd3ff6e4f61379fe44bf5e9e4681da1
    Signed-off-by: Zev Weiss <zev@bewilderbeest.net>

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -338,3 +338,27 @@ std::optional<double> readFile(const std::string& thresholdFile,
                                const double& scaleFactor);
 void setupManufacturingModeMatch(sdbusplus::asio::connection& conn);
 bool getManufacturingMode();
+
+struct MgmtParams
+{
+    // Represents an operation performed by writing 'data' to 'path'
+    struct MgmtOp
+    {
+        std::filesystem::path path;
+        std::string data;
+        int op(void) const;
+    };
+
+    MgmtOp ctor;
+    MgmtOp dtor;
+
+    // If this path exists the sensor device has been successfully instantiated
+    std::filesystem::path presentPath;
+
+    bool present(void) const;
+    int construct(void) const;
+    int destroy(void) const;
+};
+
+void getMgmtParams(const SensorBaseConfigMap& cfg,
+                   std::optional<MgmtParams>& params);
