28d34a2f041fb70a40af5da00256c29751e6a2dc
Zev Weiss <zev@bewilderbeest.net>
2022-08-03 16:24:59 -0700

    hwmontempsensor: Convert sensorTypes to I2CDeviceTypeMap
    We're going to managing these devices internally soon; rather than
    duplicating most of the information in sensorTypes in a separate data
    structure we can instead use a single one that can serve both purposes.
    
    Signed-off-by: Zev Weiss <zev@bewilderbeest.net>
    Change-Id: Ic3444c93674fd485debc27207b6f086ff6c222b9

--- src/HwmonTempMain.cpp
+++ src/HwmonTempMain.cpp
@@ -14,6 +14,7 @@
 // limitations under the License.
 */
 
+#include <DeviceMgmt.hpp>
 #include <HwmonTempSensor.hpp>
 #include <Utils.hpp>
 #include <boost/algorithm/string/predicate.hpp>
@@ -49,29 +50,31 @@ static constexpr double maxValueTemperature = 127;  // DegreesC
 static constexpr double minValueTemperature = -128; // DegreesC
 
 namespace fs = std::filesystem;
-static auto sensorTypes{
-    std::to_array<const char*>({"xyz.openbmc_project.Configuration.DPS310",
-                                "xyz.openbmc_project.Configuration.EMC1412",
-                                "xyz.openbmc_project.Configuration.EMC1413",
-                                "xyz.openbmc_project.Configuration.EMC1414",
-                                "xyz.openbmc_project.Configuration.HDC1080",
-                                "xyz.openbmc_project.Configuration.JC42",
-                                "xyz.openbmc_project.Configuration.LM75A",
-                                "xyz.openbmc_project.Configuration.LM95234",
-                                "xyz.openbmc_project.Configuration.MAX31725",
-                                "xyz.openbmc_project.Configuration.MAX31730",
-                                "xyz.openbmc_project.Configuration.MAX6581",
-                                "xyz.openbmc_project.Configuration.MAX6654",
-                                "xyz.openbmc_project.Configuration.NCT7802",
-                                "xyz.openbmc_project.Configuration.NCT6779",
-                                "xyz.openbmc_project.Configuration.SBTSI",
-                                "xyz.openbmc_project.Configuration.SI7020",
-                                "xyz.openbmc_project.Configuration.TMP112",
-                                "xyz.openbmc_project.Configuration.TMP175",
-                                "xyz.openbmc_project.Configuration.TMP421",
-                                "xyz.openbmc_project.Configuration.TMP441",
-                                "xyz.openbmc_project.Configuration.TMP75",
-                                "xyz.openbmc_project.Configuration.W83773G"})};
+
+static const I2CDeviceTypeMap sensorTypes{
+    {"DPS310", I2CDeviceType{"dps310", false}},
+    {"EMC1412", I2CDeviceType{"emc1412", true}},
+    {"EMC1413", I2CDeviceType{"emc1413", true}},
+    {"EMC1414", I2CDeviceType{"emc1414", true}},
+    {"HDC1080", I2CDeviceType{"hdc1080", false}},
+    {"JC42", I2CDeviceType{"jc42", true}},
+    {"LM75A", I2CDeviceType{"lm75a", true}},
+    {"LM95234", I2CDeviceType{"lm95234", true}},
+    {"MAX31725", I2CDeviceType{"max31725", true}},
+    {"MAX31730", I2CDeviceType{"max31730", true}},
+    {"MAX6581", I2CDeviceType{"max6581", true}},
+    {"MAX6654", I2CDeviceType{"max6654", true}},
+    {"NCT6779", I2CDeviceType{"nct6779", true}},
+    {"NCT7802", I2CDeviceType{"nct7802", true}},
+    {"SBTSI", I2CDeviceType{"sbtsi", true}},
+    {"SI7020", I2CDeviceType{"si7020", false}},
+    {"TMP112", I2CDeviceType{"tmp112", true}},
+    {"TMP175", I2CDeviceType{"tmp175", true}},
+    {"TMP421", I2CDeviceType{"tmp421", true}},
+    {"TMP441", I2CDeviceType{"tmp441", true}},
+    {"TMP75", I2CDeviceType{"tmp75", true}},
+    {"W83773G", I2CDeviceType{"w83773g", true}},
+};
 
 static struct SensorParams
     getSensorParameters(const std::filesystem::path& path)
@@ -478,8 +481,13 @@ void createSensors(
             }
         }
         });
-    getter->getConfiguration(
-        std::vector<std::string>(sensorTypes.begin(), sensorTypes.end()));
+    std::vector<std::string> types(sensorTypes.size());
+    for (const auto& [type, dt] : sensorTypes)
+    {
+        types.push_back(std::string("xyz.openbmc_project.Configuration.") +
+                        type.data());
+    }
+    getter->getConfiguration(types);
 }
 
 void interfaceRemoved(
@@ -559,12 +567,14 @@ int main()
         });
     };
 
-    for (const char* type : sensorTypes)
+    for (const auto& [type, dt] : sensorTypes)
     {
         auto match = std::make_unique<sdbusplus::bus::match_t>(
             static_cast<sdbusplus::bus_t&>(*systemBus),
             "type='signal',member='PropertiesChanged',path_namespace='" +
-                std::string(inventoryPath) + "',arg0namespace='" + type + "'",
+                std::string(inventoryPath) +
+                "',arg0namespace='xyz.openbmc_project.Configuration." +
+                type.data() + "'",
             eventHandler);
         matches.emplace_back(std::move(match));
     }
