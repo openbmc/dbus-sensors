9c4bd3bb4f4702644fa87576daabb593759cfd5f
Zev Weiss <zev@bewilderbeest.net>
2022-08-03 15:43:17 -0700

    Utils: Add device-management infrastructure
    We're moving toward having dbus-sensors take over sensor device
    lifecycle management from entity-manager; this code (loosely based on
    entity-manager's include/devices.hpp and src/overlay.cpp) provides some
    general-purpose utilities for instantiating and destroying devices.
    
    Signed-off-by: Zev Weiss <zev@bewilderbeest.net>
    Change-Id: I39fcd0de2c70cd58b4d132cdcae5f82319732dbb

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -707,3 +707,107 @@ bool getManufacturingMode()
 {
     return manufacturingMode;
 }
+
+std::optional<I2CDevice> getI2CDevice(const I2CDeviceTypeMap& dtmap,
+                                      const SensorBaseConfigMap& cfg)
+{
+    auto findType = cfg.find("Type");
+    auto findBus = cfg.find("Bus");
+    auto findAddr = cfg.find("Address");
+
+    if (findType == cfg.end() || findBus == cfg.end() || findAddr == cfg.end())
+    {
+        return std::nullopt;
+    }
+
+    const std::string* type = std::get_if<std::string>(&findType->second);
+    const uint64_t* bus = std::get_if<uint64_t>(&findBus->second);
+    const uint64_t* addr = std::get_if<uint64_t>(&findAddr->second);
+
+    if (type == nullptr || bus == nullptr || addr == nullptr)
+    {
+        return std::nullopt;
+    }
+
+    auto findDevType = dtmap.find(type->c_str());
+    if (findDevType == dtmap.end())
+    {
+        return std::nullopt;
+    }
+
+    return I2CDevice(findDevType->second, *bus, *addr);
+}
+
+static fs::path i2cBusPath(uint64_t bus)
+{
+    return {"/sys/bus/i2c/devices/i2c-" + std::to_string(bus)};
+}
+
+static std::string deviceDirName(uint64_t bus, uint64_t address)
+{
+    std::ostringstream name;
+    name << bus << "-" << std::hex << std::setw(4) << std::setfill('0')
+         << address;
+    return name.str();
+}
+
+static bool pathExists(const fs::path& path)
+{
+    // Ignore errors; anything but a clean 'true' is fine as 'false'
+    std::error_code ec;
+    return fs::exists(path, ec);
+}
+
+int I2CDevice::create(void) const
+{
+    fs::path busDir = i2cBusPath(bus);
+    fs::path presentPath = busDir / deviceDirName(bus, address);
+
+    if (type->createsHWMon)
+    {
+        presentPath /= "hwmon";
+    }
+
+    // If it's already instantiated, there's nothing we need to do.
+    if (pathExists(presentPath))
+    {
+        return 0;
+    }
+
+    // Try to create it: 'echo $devtype $addr > .../i2c-$bus/new_device'
+    fs::path ctorPath = busDir / "new_device";
+    std::ofstream ctor(ctorPath);
+    if (!ctor.good())
+    {
+        std::cerr << "Failed to open " << ctorPath << "\n";
+        return -1;
+    }
+
+    ctor << type->name << " " << address << "\n";
+    ctor.close();
+
+    // Check if that created the requisite sysfs directory
+    if (!pathExists(presentPath))
+    {
+        destroy();
+        return -1;
+    }
+
+    return 0;
+}
+
+int I2CDevice::destroy(void) const
+{
+    fs::path dtorPath = i2cBusPath(bus) / "delete_device";
+    std::ofstream dtor(dtorPath);
+    if (!dtor.good())
+    {
+        std::cerr << "Failed to open " << dtorPath << "\n";
+        return -1;
+    }
+
+    dtor << address << "\n";
+    dtor.close();
+
+    return 0;
+}
