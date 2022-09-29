a95dfc88d47cc72cf1cb5b93a0f22ee56b58ccb0
Hao Jiang <jianghao@google.com>
2022-09-29 18:47:09 +0000

    Add Identify method to NVMeAdmin interface
    This method is used to send NVMe admin command `Identify` to the target
    controller. It will return a fd to read the raw data field of the
    NVMe-MI admin response message.
    
    Signed-off-by: Hao Jiang <jianghao@google.com>
    Change-Id: I5109af51d6e50e53bda2498bf34656fad4dbe557

--- src/NVMeController.cpp
+++ src/NVMeController.cpp
@@ -54,6 +54,39 @@ NVMeController::NVMeController(
             });
         return sdbusplus::message::unix_fd{pipe[0]};
     });
+
+    adminIntf->register_method(
+        "Identify",
+        [nvmeIntf, nvmeCtrl{ctrl}](uint8_t cns, uint16_t cntid, uint32_t nsid) {
+        std::array<int, 2> pipe;
+        if (::pipe(pipe.data()) < 0)
+        {
+            std::cerr << "Identify fails to open pipe: " << std::strerror(errno)
+                      << std::endl;
+            throw sdbusplus::xyz::openbmc_project::Common::File::Error::Open();
+        }
+
+        nvmeIntf->adminIdentify(
+            nvmeCtrl, static_cast<nvme_identify_cns>(cns), nsid, cntid,
+            [pipe](const std::error_code& ec, std::span<uint8_t> data) {
+            ::close(pipe[0]);
+            int fd = pipe[1];
+            if (ec)
+            {
+                std::cerr << "fail to Identify: " << ec.message() << std::endl;
+                close(fd);
+                return;
+            }
+            if (write(fd, data.data(), data.size()) < 0)
+            {
+                std::cerr << "Identify fails to write fd: "
+                          << std::strerror(errno) << std::endl;
+            };
+            close(fd);
+            });
+        return sdbusplus::message::unix_fd{pipe[0]};
+        });
+
     adminIntf->initialize();
 }
 
