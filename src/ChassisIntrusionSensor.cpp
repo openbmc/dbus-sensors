1240f1ddfdaad5df27c4ece34c93fedf346c7ee6
Chau Ly <chaul@amperecomputing.com>
2022-10-26 04:12:52 +0000

    intrusionsensor: Add Rearm property
    This patch adds Rearm property to intrusionsensor service, under
    "xyz.openbmc_project.Chassis.Intrusion" interface, to specify the method
    for rearming the chassis cover and updating the "Status" property.
    As defined in Redfish schema specification, this property is optional.
    If not set, "Rearm" will default to "Automatic", which will directly
    update the raw chassis intrusion status to the "Status" property.
    If set to "Manual", after an intrusion event, the "Status" property
    will be kept to "HardwareIntrusion" until a reset action happens for it
    to be set to "Normal" (manual rearm). The rearm action comes from
    Redfish by writing "Normal" to the "IntrusionSensor" property in
    /redfish/v1/Chassis/chassis.
    
    Example:
    {
         "Class": "Gpio",
         "Name": "Chassis_Intrusion_Status",
         "GpioPolarity": "Low",
         "Rearm": "Manual",
         "Type": "ChassisIntrusionSensor"
    }
    
    Tested: This has been tested on Ampere's Mt.Mitchell platform using
    hwmon reading method.
    
    Automatic mode (default):
    1. The chassis cover is closed => the Status property shows "Normal"
    2. The chassis cover is open => Status shows "HardwareIntrusion"
    
    Manual mode:
    1. The chassis cover is first open from the last rearm action =>
    "HardwareIntrusion"
    2. The chassis cover is closed => "HardwareIntrusion"
    3. The chassis cover is open again => "HardwareIntrusion"
    4. The rearm action is taken => "Normal"
    5. The chassis cover is closed => "Normal"
    
    Signed-off-by: Chau Ly <chaul@amperecomputing.com>
    Change-Id: I8257a5fee9db67a072e7fafc140f6a143028c429

--- src/ChassisIntrusionSensor.cpp
+++ src/ChassisIntrusionSensor.cpp
@@ -49,32 +49,44 @@ const static constexpr size_t pchRegMaskIntrusion = 0x01;
 
 void ChassisIntrusionSensor::updateValue(const std::string& newValue)
 {
-    // Take no action if value already equal
+    // Take no action if the hardware status does not change
     // Same semantics as Sensor::updateValue(const double&)
     if (newValue == mValue)
     {
         return;
     }
 
-    // indicate that it is internal set call
+    // Automatic ReArm mode allows direct update
+    // Manual ReArm mode requires a rearm action to clear the intrusion
+    // status after the the chassis has been intruded
+    if (mRearm == RearmMode::Manual && mValue != "unknown")
+    {
+        if (newValue == "Normal")
+        {
+            // Chassis is first closed from being open. If it has been
+            // rearmed externally, reset the flag, update mValue and
+            // return, without having to write "Normal" to Dbus (because
+            // the rearm action already did). Otherwise, return with
+            // no more action
+            if (mRearmFlag)
+            {
+                mRearmFlag = false;
+                mValue = newValue;
+            }
+            return;
+        }
+    }
+
+    // Flush the rearm flag everytime it allows an update to Dbus
+    mRearmFlag = false;
+
+    // indicate that this is an internal set call
+    mOverridenState = false;
     mInternalSet = true;
     mIface->set_property("Status", newValue);
     mInternalSet = false;
 
     mValue = newValue;
-
-    if (mOldValue == "Normal" && mValue != "Normal")
-    {
-        std::cerr << "save to SEL for intrusion assert event \n";
-        // TODO: call add SEL log API, depends on patch #13956
-        mOldValue = mValue;
-    }
-    else if (mOldValue != "Normal" && mValue == "Normal")
-    {
-        std::cerr << "save to SEL for intrusion de-assert event \n";
-        // TODO: call add SEL log API, depends on patch #13956
-        mOldValue = mValue;
-    }
 }
 
 int ChassisIntrusionSensor::i2cReadFromPch(int busId, int slaveAddr)
@@ -263,6 +275,16 @@ int ChassisIntrusionSensor::setSensorValue(const std::string& req,
 {
     if (!mInternalSet)
     {
+        if (req != "Normal")
+        {
+            // Only accept Normal value from an external call
+            return 1;
+        }
+        // Turn the rearm flag ON
+        if (mRearm == RearmMode::Manual)
+        {
+            mRearmFlag = true;
+        }
         propertyValue = req;
         mOverridenState = true;
     }
@@ -270,11 +292,23 @@ int ChassisIntrusionSensor::setSensorValue(const std::string& req,
     {
         propertyValue = req;
     }
+    else
+    {
+        return 1;
+    }
+    if (mValue == "Normal" && propertyValue != "Normal")
+    {
+        std::cerr << "Intrusion asserted\n";
+    }
+    else if (mValue != "Normal" && propertyValue == "Normal")
+    {
+        std::cerr << "Intrusion de-asserted\n";
+    }
     return 1;
 }
 
-void ChassisIntrusionSensor::start(IntrusionSensorType type, int busId,
-                                   int slaveAddr, bool gpioInverted)
+void ChassisIntrusionSensor::start(IntrusionSensorType type, RearmMode rearm,
+                                   int busId, int slaveAddr, bool gpioInverted)
 {
     if (debug)
     {
@@ -301,6 +335,7 @@ void ChassisIntrusionSensor::start(IntrusionSensorType type, int busId,
     }
 
     mType = type;
+    mRearm = rearm;
     mBusId = busId;
     mSlaveAddr = slaveAddr;
     mGpioInverted = gpioInverted;
@@ -316,6 +351,7 @@ void ChassisIntrusionSensor::start(IntrusionSensorType type, int busId,
                 [&](const std::string& req, std::string& propertyValue) {
                 return setSensorValue(req, propertyValue);
                 });
+            mIface->register_property("Rearm", convertForMessage(mRearm));
             mIface->initialize();
 
             if (mType == IntrusionSensorType::gpio)
@@ -364,7 +400,7 @@ ChassisIntrusionSensor::ChassisIntrusionSensor(
     boost::asio::io_context& io,
     std::shared_ptr<sdbusplus::asio::dbus_interface> iface) :
     mIface(std::move(iface)),
-    mValue("unknown"), mOldValue("unknown"), mPollTimer(io), mGpioFd(io)
+    mValue("unknown"), mPollTimer(io), mGpioFd(io)
 {}
 
 ChassisIntrusionSensor::~ChassisIntrusionSensor()
