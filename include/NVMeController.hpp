ab6e299f69ff1f976eaf39906d5db75fce5d0d2a
Hao Jiang <jianghao@google.com>
2022-11-29 01:48:52 +0000

    nvmesensor: Using generated controller server
    libnvme-vu include the yaml file for the definition of
    xyz.openbmc_project.NVMe.NVMeAdmin and
    xyz.openbmc_project.NVMe.KnuckleAdmin. The server.hpp and server.cpp
    will be gerenated from the yaml. Move the functionility from dynamic
    insertion of sdbusplus::asio to the inheretence of the generated
    classes.
    
    The goals are:
    * a more clear structure by distributing the functions into the
      overloading instead of a bit chunck of constructor.
    * DBus interface sharing across the server and the client.
    * Get prepared for the future plug-in style knuckle functions by
      splitting the knuckle class out of NVMeController.
    
    Signed-off-by: Hao Jiang <jianghao@google.com>
    Change-Id: I13d6f6b639f892faf27ee1f3170a21c8793be3a6

--- include/NVMeController.hpp
+++ include/NVMeController.hpp
@@ -7,26 +7,18 @@
 #include <sdbusplus/asio/connection.hpp>
 #include <sdbusplus/asio/object_server.hpp>
 #include <xyz/openbmc_project/Inventory/Item/StorageController/server.hpp>
+#include <xyz/openbmc_project/NVMe/NVMeAdmin/server.hpp>
 
 #include <utility>
 
-using ControllerBase =
-    sdbusplus::xyz::openbmc_project::Inventory::Item::server::StorageController;
 class NVMeController :
-    private ControllerBase,
+    private sdbusplus::xyz::openbmc_project::Inventory::Item::server::
+        StorageController,
+    private sdbusplus::xyz::openbmc_project::NVMe::server::NVMeAdmin,
     public std::enable_shared_from_this<NVMeController>
 
 {
   public:
-    enum class FwCommitStatus : uint8_t
-    {
-        Ready,
-        InProgress,
-        Failed,
-        RequireReset,
-        Success,
-    };
-
     NVMeController(boost::asio::io_context& io,
                    sdbusplus::asio::object_server& objServer,
                    std::shared_ptr<sdbusplus::asio::connection> conn,
@@ -35,7 +27,7 @@ class NVMeController :
 
     ~NVMeController() override;
 
-    void start(){};
+    void start();
 
     // setup association to the secondary controllers. Clear the Association if
     // empty.
@@ -60,12 +52,48 @@ class NVMeController :
     // controller
     std::shared_ptr<sdbusplus::asio::dbus_interface> secAssoc;
 
-    // NVMe Admin interface: xyz.openbmc_project.Nvme.NVMeAdmin
-    static constexpr char adminIntfName[] =
-        "xyz.openbmc_project.Nvme.NVMeAdmin";
-    static constexpr char adminDataPath[] = "/run/initramfs";
-    std::shared_ptr<sdbusplus::asio::dbus_interface> adminIntf;
 
-    // FW update status
-    FwCommitStatus commitStatus;
+    /* NVMeAdmin method overload */
+
+    /** @brief Implementation for GetLogPage
+     *  Send GetLogPage command to NVMe device
+     *
+     *  @param[in] lid - Log Page Identifier
+     *  @param[in] nsid - Namespace Identifier
+     *  @param[in] lsp - Log Specific Field
+     *  @param[in] lsi - Log Specific Identifier
+     *
+     *  @return log[sdbusplus::message::unix_fd] - Returned Log Page
+     */
+    sdbusplus::message::unix_fd getLogPage(uint8_t lid, uint32_t nsid,
+                                           uint8_t lsp, uint16_t lsi) override;
+
+    /** @brief Implementation for Identify
+     *  Send Identify command to NVMe device
+     *
+     *  @param[in] cns - Controller or Namespace Structure
+     *  @param[in] nsid - Namespace Identifier
+     *  @param[in] cntid - Controller Identifier
+     *
+     *  @return data[sdbusplus::message::unix_fd] - Identify Data
+     */
+    sdbusplus::message::unix_fd identify(uint8_t cns, uint32_t nsid,
+                                         uint16_t cntid) override;
+    /** Set value of FirmwareCommitStatus
+     * Used to reset the the status back to ready if the commit is not in
+     * process.
+     */
+    NVMeAdmin::FwCommitStatus
+        firmwareCommitStatus(NVMeAdmin::FwCommitStatus) override;
+
+    /** @brief Implementation for FirmwareCommitAsync
+     *  Send Firmware Commit command to NVMe device
+     *
+     *  @param[in] commitAction - Commit Action defined by NVMe base spec
+     * (Figure 175 of rev 1.4)
+     *  @param[in] firmwareSlot - Firmware Slot
+     *  @param[in] bpid - Boot Partition ID
+     */
+    void firmwareCommitAsync(uint8_t commitAction, uint8_t firmwareSlot,
+                             bool bpid) override;
 };
