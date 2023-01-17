openbmc/dbus-sensors

commit 661d4378467a1674eccff223696ecb64899419a7

Identify active sensors via the D-Bus "sensors" service
Upstream modified how sensors are organized on D-Bus. Sensors are now
mapped under the /xyz/openbmc_project/sensors object. Each service
retrieves sensor data via a GetManagedObject call.

Tested:

busctl call -j xyz.openbmc_project.$sensor /xyz/openbmc_project/sensors \
            org.freedesktop.DBus.ObjectManager GetManagedObjects
ipmitool sdr dump sdrs.bin

Confirmed neither function resulted in an error message in the journal

Change-Id: I3a41ea2c28b34c9e28abe2f16f6c325ec715dd27
Signed-off-by: Johnathan Mantey <johnathanx.mantey@intel.com>

src/NVMeSensorMain.cpp
@@ -231,7 +231,8 @@ int main()
     boost::asio::io_service io;
     auto systemBus = std::make_shared<sdbusplus::asio::connection>(io);
     systemBus->request_name("xyz.openbmc_project.NVMeSensor");
-    sdbusplus::asio::object_server objectServer(systemBus);
+    sdbusplus::asio::object_server objectServer(systemBus, true);
+    objectServer.add_manager("/xyz/openbmc_project/sensors/");
 
     io.post([&]() { createSensors(io, objectServer, systemBus); });
 

src/ExitAirTempSensor.cpp
@@ -931,8 +931,9 @@ int main()
 
     boost::asio::io_service io;
     auto systemBus = std::make_shared<sdbusplus::asio::connection>(io);
+    sdbusplus::asio::object_server objectServer(systemBus, true);
+    objectServer.add_manager("/xyz/openbmc_project/sensors");
     systemBus->request_name("xyz.openbmc_project.ExitAirTempSensor");
-    sdbusplus::asio::object_server objectServer(systemBus);
     std::shared_ptr<ExitAirTempSensor> sensor =
         nullptr; // wait until we find the config
 

commit ea148ec6f913c1b1d4a145591097628b01384d96

NVMeSensorMain: Fix sensor object manager path
Change 661d4378467a ("Identify active sensors via the D-Bus "sensors"
service") added a trailing '/' to the path. Remove it, as it breaks
sensor detection by introspection.

Resolves: #23
Fixes: 661d4378467a ("Identify active sensors via the D-Bus "sensors" service")
Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
Change-Id: I6fed1a36b6feae84160cdb5e16a1bdb7cebed262

src/NVMeSensorMain.cpp
@@ -232,7 +232,7 @@ int main()
     auto systemBus = std::make_shared<sdbusplus::asio::connection>(io);
     systemBus->request_name("xyz.openbmc_project.NVMeSensor");
     sdbusplus::asio::object_server objectServer(systemBus, true);
-    objectServer.add_manager("/xyz/openbmc_project/sensors/");
+    objectServer.add_manager("/xyz/openbmc_project/sensors");
 
     io.post([&]() { createSensors(io, objectServer, systemBus); });
 