d7c4d247d2c8a356ec8944d8006bb004cffa7fbf
Oleksandr Shulzhenko <oleksandr.shulzhenko.viktorovych@intel.com>
2023-03-14 14:52:36 +0100

    IntelCPUSensor: Use libpeci when communicating via peci-legacy
    When using peci-legacy the general approach is to use libpeci as
    the middleware to communicate with peci core from userspace.
    Integrate libpeci with IntelCPUSensor and use library functions to issue
    peci commands where necessary.
    
    Tested: hwmons are succesfully created in the same way
    they were created using ioctl.
    
    Change-Id: I4ecd70e604d447a4066dc5fb8d902992cb97cadf
    Signed-off-by: Oleksandr Shulzhenko <oleksandr.shulzhenko.viktorovych@intel.com>

--- src/IntelCPUSensorMain.cpp
+++ src/IntelCPUSensorMain.cpp
@@ -43,6 +43,7 @@
 
 // clang-format off
 // this needs to be included last or we'll have build issues
+#include <peci.h>
 #include <linux/peci-ioctl.h>
 #if !defined(PECI_MBX_INDEX_DDR_DIMM_TEMP)
 #define PECI_MBX_INDEX_DDR_DIMM_TEMP MBX_INDEX_DDR_DIMM_TEMP
@@ -472,29 +473,26 @@ void detectCpu(boost::asio::steady_timer& pingTimer,
             return;
         }
 
+        peci_SetDevName(peciDevPath.data());
         State newState = State::OFF;
-        struct peci_ping_msg msg
-        {};
-        msg.addr = config.addr;
 
         // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg)
-        if (ioctl(file, PECI_IOC_PING, &msg) == 0)
+        if (peci_Ping(config.addr) == PECI_CC_SUCCESS)
         {
             bool dimmReady = false;
             for (unsigned int rank = 0; rank < rankNumMax; rank++)
             {
-                struct peci_rd_pkg_cfg_msg msg
-                {};
-                msg.addr = config.addr;
-                msg.index = PECI_MBX_INDEX_DDR_DIMM_TEMP;
-                msg.param = rank;
-                msg.rx_len = 4;
+                std::array<uint8_t, 8> pkgConfig;
+                uint8_t cc = 0;
 
+                pkgConfig.fill(0);
                 // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg)
-                if (ioctl(file, PECI_IOC_RD_PKG_CFG, &msg) == 0)
+                if (peci_RdPkgConfig(config.addr, PECI_MBX_INDEX_DDR_DIMM_TEMP,
+                                     rank, 4, &pkgConfig[0],
+                                     &cc) == PECI_CC_SUCCESS)
                 {
-                    if ((msg.pkg_config[0] != 0U) ||
-                        (msg.pkg_config[1] != 0U) || (msg.pkg_config[2] != 0U))
+                    if ((pkgConfig[0] != 0U) || (pkgConfig[1] != 0U) ||
+                        (pkgConfig[2] != 0U))
                     {
                         dimmReady = true;
                         break;
@@ -515,8 +513,6 @@ void detectCpu(boost::asio::steady_timer& pingTimer,
                 newState = State::ON;
             }
         }
-
-        close(file);
 
         if (config.state != newState)
         {
