e7d41f5762fedb668b4e934ca396587c85e769b7
Joseph Fu <joseph.fu@quantatw.com>
2023-03-25 10:24:30 +0800

    PSUSensor: add IIO device sensor
    Support IIO device on D-bus via dbus-sensors, expose path to support
    multi-channel labels.
    Reference:
    https://gerrit.openbmc.org/c/openbmc/dbus-sensors/+/47822
    
    This patch is able to support the IIO device ADS1015
    Use with this patch.
    https://gerrit.openbmc.org/c/openbmc/entity-manager/+/58505
    
    Json config
    {
        "Address": "0x49",
        "Bus": 11,
        "Labels": [
            "in_voltage1",
            "in_voltage2"
        ],
        "Name": "vr_prnt_cem",
        "EntityId": "0x07",
        "EntityInstance": "0x17",
        "in_voltage1_Name": "vr_prnt_msas1",
        "in_voltage1_Scale": 1000,
        "in_voltage2_Name": "vr_prnt_msas2",
        "in_voltage2_Scale": 1000,
        "PowerState": "On",
        "Thresholds": [
            {
                "Direction": "greater than",
                "Label": "in_voltage1",
                "Name": "upper critical",
                "Severity": 1,
                "Value": 2.0
            },
            {
                "Direction": "less than",
                "Label": "in_voltage1",
                "Name": "lower critical",
                "Severity": 1,
                "Value": 0.0
            },
            {
                "Direction": "greater than",
                "Label": "in_voltage2",
                "Name": "upper critical",
                "Severity": 1,
                "Value": 2.0
            },
            {
                "Direction": "less than",
                "Label": "in_voltage2",
                "Name": "lower critical",
                "Severity": 1,
                "Value": 0.0
            }
        ],
        "in_voltage1_Max": 2.0,
        "in_voltage1_Min": 0.0,
        "in_voltage2_Max": 2.0,
        "in_voltage2_Min": 0.0,
        "PollRate": 1.0,
        "Type": "ADS1015"
    }
    
    Tested:
    root@qbmc:~# busctl tree xyz.openbmc_project.EntityManager |grep msas
      |-/xyz/openbmc_project/inventory/system/board/Yertle/vr_prnt_msas1
      `-/xyz/openbmc_project/inventory/system/board/Yertle/vr_prnt_msas2
    
    root@qbmc:~# busctl tree xyz.openbmc_project.PSUSensor
    `-/xyz
      `-/xyz/openbmc_project
        |-/xyz/openbmc_project/control
        `-/xyz/openbmc_project/sensors
          `-/xyz/openbmc_project/sensors/voltage
            |-/xyz/openbmc_project/sensors/voltage/vr_prnt_msas1
            `-/xyz/openbmc_project/sensors/voltage/vr_prnt_msas2
    
    root@qbmc:~# busctl get-property xyz.openbmc_project.PSUSensor \
    /xyz/openbmc_project/sensors/voltage/vr_prnt_msas1 \
    xyz.openbmc_project.Sensor.Value Value
    d 1.72
    
    Signed-off-by: Joseph Fu <joseph.fu@quantatw.com>
    Change-Id: I584f5d79850921e702b8572fb6b38e9dcfc44d25

--- src/PSUSensor.hpp
+++ src/PSUSensor.hpp
@@ -12,6 +12,22 @@
 #include <string>
 #include <utility>
 
+enum class DevTypes
+{
+    Unknown = 0,
+    PSU,
+    IIO
+};
+
+struct DevParams
+{
+    DevTypes devType;
+    unsigned int matchIndex;
+    std::string deviceName;
+    std::string matchRegEx;
+    std::string nameRegEx;
+};
+
 class PSUSensor : public Sensor, public std::enable_shared_from_this<PSUSensor>
 {
   public:
