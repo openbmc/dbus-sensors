openbmc/dbus-sensors

commit 77141acb7f090a2205d43d397905c65a0d1e4b6e

IntelCPUSensor: Use libpeci when communicating via peci-legacy
When using peci-legacy the general approach is to use libpeci as
the middleware to communicate with peci core from userspace.
Integrate libpeci with IntelCPUSensor and use library functions to issue
peci commands where necessary.

Tested: hwmons are succesfully created in the same way
they were created using ioctl.

Change-Id: I4ecd70e604d447a4066dc5fb8d902992cb97cadf
Signed-off-by: Oleksandr Shulzhenko <oleksandr.shulzhenko.viktorovych@intel.com>

src/IntelCPUSensorMain.cpp
@@ -19,6 +19,7 @@
 #include "VariantVisitors.hpp"
 
 #include <fcntl.h>
+#include <peci.h>
 
 #include <boost/algorithm/string/replace.hpp>
 #include <boost/container/flat_map.hpp>
@@ -472,29 +473,25 @@ void detectCpu(boost::asio::steady_timer& pingTimer,
             return;
         }
 
+        peci_SetDevName(peciDevPath.data());
         State newState = State::OFF;
-        struct peci_ping_msg msg
-        {};
-        msg.addr = config.addr;
 
         // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg)
-        if (ioctl(file, PECI_IOC_PING, &msg) == 0)
+        if (peci_Ping(config.addr) == PECI_CC_SUCCESS)
         {
             bool dimmReady = false;
             for (unsigned int rank = 0; rank < rankNumMax; rank++)
             {
-                struct peci_rd_pkg_cfg_msg msg
-                {};
-                msg.addr = config.addr;
-                msg.index = PECI_MBX_INDEX_DDR_DIMM_TEMP;
-                msg.param = rank;
-                msg.rx_len = 4;
+                std::array<uint8_t, 8> pkgConfig{};
+                uint8_t cc = 0;
 
                 // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg)
-                if (ioctl(file, PECI_IOC_RD_PKG_CFG, &msg) == 0)
+                if (peci_RdPkgConfig(config.addr, PECI_MBX_INDEX_DDR_DIMM_TEMP,
+                                     rank, 4, &pkgConfig[0],
+                                     &cc) == PECI_CC_SUCCESS)
                 {
-                    if ((msg.pkg_config[0] != 0U) ||
-                        (msg.pkg_config[1] != 0U) || (msg.pkg_config[2] != 0U))
+                    if ((pkgConfig[0] != 0U) || (pkgConfig[1] != 0U) ||
+                        (pkgConfig[2] != 0U))
                     {
                         dimmReady = true;
                         break;
@@ -515,8 +512,6 @@ void detectCpu(boost::asio::steady_timer& pingTimer,
                 newState = State::ON;
             }
         }
-
-        close(file);
 
         if (config.state != newState)
         {

.clang-tidy
@@ -298,7 +298,7 @@ readability-uniqueptr-delete-release,
 readability-uppercase-literal-suffix'
 
 WarningsAsErrors: '*'
-HeaderFilterRegex: '.*'
+HeaderFilterRegex: '(?!^subprojects).*'
 CheckOptions:
   - { key: readability-identifier-naming.ClassCase,     value: CamelCase  }
   - { key: readability-identifier-naming.VariableCase,  value: camelBack }

.gitignore
@@ -3,6 +3,7 @@ subprojects/*
 !subprojects/*.wrap
 !subprojects/.clang-tidy
 !subprojects/.clang-format
+!subprojects/.clang-tidy-ignore
 
 .vscode
 .idea/

src/meson.build
@@ -60,6 +60,17 @@ if not meson.get_compiler('cpp').has_header('linux/peci-ioctl.h')
     peci_incdirs = ['../include']
 endif
 
+if get_option('intel-cpu').enabled()
+    peci_dep = meson.get_compiler('cpp').find_library('libpeci', required : false)
+    if not peci_dep.found()
+      cmake = import('cmake')
+      opt = cmake.subproject_options()
+      opt.append_compile_args('c', '-Wno-pedantic')
+      peci_proj = cmake.subproject('libpeci', options : opt)
+      peci_dep = peci_proj.dependency('peci')
+    endif
+endif
+
 if get_option('adc').enabled()
     executable(
         'adcsensor',
@@ -86,6 +97,7 @@ if get_option('intel-cpu').enabled()
             gpiodcxx,
             thresholds_dep,
             utils_dep,
+            peci_dep,
         ],
         include_directories: peci_incdirs,
         install: true,

subprojects/.clang-tidy-ignore
@@ -1,0 +1,1 @@
+*

subprojects/libpeci.wrap
@@ -1,0 +1,6 @@
+[wrap-git]
+url = https://github.com/openbmc/libpeci.git
+revision = HEAD
+
+[provide]
+libpeci = peci_dep

commit 362b464a91ffa099500a34d6726880c9da7fc0c9

IntelCPUSensor: use libpeci to open peciFd when communicating via peci-legacy
To remain consistent with libpeci usage we should use dedicated functions
to open PECI file descriptors when using peci-legacy.

Fixes: 77141acb7f09 ("IntelCPUSensor: Use libpeci when communicating via peci-legacy")

Tested: hwmons are succesfully created in the same way
they were created opening peciFd directly

Change-Id: I924c3fdc8029f47aed80480ebc03d2dca71d134f
Signed-off-by: Oleksandr Shulzhenko <oleksandr.shulzhenko.viktorovych@intel.com>

src/IntelCPUSensorMain.cpp
@@ -480,6 +480,7 @@ void detectCpu(boost::asio::steady_timer& pingTimer,
 {
     size_t rescanDelaySeconds = 0;
     static bool keepPinging = false;
+    int peciFd = -1;
 
     for (CPUConfig& config : cpuConfigs)
     {
@@ -490,9 +491,10 @@ void detectCpu(boost::asio::steady_timer& pingTimer,
 
         std::string peciDevPath = peciDev + std::to_string(config.bus);
 
+        peci_SetDevName(peciDevPath.data());
+
         // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg)
-        auto file = open(peciDevPath.c_str(), O_RDWR | O_CLOEXEC);
-        if (file < 0)
+        if (peci_Lock(&peciFd, PECI_NO_WAIT) != PECI_CC_SUCCESS)
         {
             std::cerr << "unable to open " << peciDevPath << " "
                       << std::strerror(errno) << "\n";
@@ -501,7 +503,6 @@ void detectCpu(boost::asio::steady_timer& pingTimer,
             return;
         }
 
-        peci_SetDevName(peciDevPath.data());
         State newState = State::OFF;
 
         // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg)
@@ -591,6 +592,8 @@ void detectCpu(boost::asio::steady_timer& pingTimer,
             std::cout << config.name << ", state: " << config.state << "\n";
         }
     }
+
+    peci_Unlock(peciFd);
 
     if (rescanDelaySeconds != 0U)
     {