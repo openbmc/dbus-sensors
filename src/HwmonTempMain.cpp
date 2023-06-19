9a7db6af2010ce9ca6ca9e1e53850aa6456b6b38
Hieu Huynh <hieuh@os.amperecomputing.com>
2023-06-19 11:02:07 +0000

    hwmontemp: add tmp464
    Change-Id: Ic7337e9450a9cb83222b99d70c666f10211f1668
    Signed-off-by: Hieu Huynh <hieuh@os.amperecomputing.com>

--- src/HwmonTempMain.cpp
+++ src/HwmonTempMain.cpp
@@ -73,6 +73,7 @@ static const I2CDeviceTypeMap sensorTypes{
     {"TMP175", I2CDeviceType{"tmp175", true}},
     {"TMP421", I2CDeviceType{"tmp421", true}},
     {"TMP441", I2CDeviceType{"tmp441", true}},
+    {"TMP464", I2CDeviceType{"tmp464", true}},
     {"TMP75", I2CDeviceType{"tmp75", true}},
     {"W83773G", I2CDeviceType{"w83773g", true}},
 };
