5674ac99304076077def9c2cdad29a05fc46a356
Potin Lai <potin.lai@quantatw.com>
2023-11-20 14:33:56 +0800

    PSUSensor: add ina238 support
    Add device ina238 reading support.
    
    Change-Id: If8dd323498ca661d2e3cfa116ddef60d91501c55
    Signed-off-by: Potin Lai <potin.lai@quantatw.com>

--- src/PSUSensorMain.cpp
+++ src/PSUSensorMain.cpp
@@ -60,6 +60,7 @@ static const I2CDeviceTypeMap sensorTypes{
     {"DPS800", I2CDeviceType{"dps800", true}},
     {"INA219", I2CDeviceType{"ina219", true}},
     {"INA230", I2CDeviceType{"ina230", true}},
+    {"INA238", I2CDeviceType{"ina238", true}},
     {"IPSPS1", I2CDeviceType{"ipsps1", true}},
     {"IR38060", I2CDeviceType{"ir38060", true}},
     {"IR38164", I2CDeviceType{"ir38164", true}},
