afe5b9d8ba34bff5dc8ea9718e1a9cc53e5f5114
Chau Ly <chaul@amperecomputing.com>
2023-12-01 04:05:24 +0000

    intrusionsensor: Add association with chassis
    Chassis intrusion sensor does not have the Sensor.Value interface to be
    legit into the "sensors" association with the chassis but there's a need
    to associate the sensor with the chassis that it is monitoring physical
    security state.
    
    The commit [1] in PDI introduces the monitoring/monitored_by association
    between a chassis intrusion sensor and the inventory item that it is
    currently monitoring the physical security state.
    
    This commit adds the association interface to the sensor, and creates
    the monitoring/monitored_by association between it and the inventory
    object path of the item that it is configured in.
    
    [1] https://github.com/openbmc/phosphor-dbus-interfaces/commit/6cfd0d08a7083263102cb8e7a216f818a4c210eb
    
    Tested in Ampere's MtMitchell platform:
    
    The sensor is configured in the motherboard's Entity Manager json
    
    $ busctl tree xyz.openbmc_project.ObjectMapper | grep -i -e monitored \
    -e monitoring
    
    |-/xyz/openbmc_project/Chassis/Intrusion/monitoring
    |-/xyz/openbmc_project/inventory/system/board/Mt_Mitchell_Motherboard/mo
    nitored_by
    
    $ busctl get-property xyz.openbmc_project.ObjectMapper \
    /xyz/openbmc_project/Chassis/Intrusion/monitoring \
    xyz.openbmc_project.Association endpoints
    
    as 1 "/xyz/openbmc_project/inventory/system/board/\
    Mt_Mitchell_Motherboard"
    
    $ busctl get-property xyz.openbmc_project.IntrusionSensor \
    /xyz/openbmc_project/Chassis/Intrusion \
    xyz.openbmc_project.Association.Definitions Associations
    
    a(sss) 1 "monitoring" "monitored_by" "/xyz/openbmc_project/inventory/sys
    tem/board/Mt_Mitchell_Motherboard"
    
    Signed-off-by: Chau Ly <chaul@amperecomputing.com>
    Change-Id: Ibe686816dda0dea0c06210cf8e5b53fe74ffba79

--- src/IntrusionSensorMain.cpp
+++ src/IntrusionSensorMain.cpp
@@ -131,7 +131,7 @@ static void createSensorsFromConfig(
                         (std::get<std::string>(findGpioPolarity->second) ==
                          "Low");
                     pSensor = std::make_shared<ChassisIntrusionGpioSensor>(
-                        autoRearm, io, objServer, gpioInverted);
+                        autoRearm, io, path, objServer, gpioInverted);
                     pSensor->start();
                     if (debug)
                     {
@@ -171,7 +171,7 @@ static void createSensorsFromConfig(
                 try
                 {
                     pSensor = std::make_shared<ChassisIntrusionHwmonSensor>(
-                        autoRearm, io, objServer, hwmonName);
+                        autoRearm, io, path, objServer, hwmonName);
                     pSensor->start();
                     return;
                 }
@@ -197,7 +197,7 @@ static void createSensorsFromConfig(
                     int busId = std::get<uint64_t>(findBus->second);
                     int slaveAddr = std::get<uint64_t>(findAddress->second);
                     pSensor = std::make_shared<ChassisIntrusionPchSensor>(
-                        autoRearm, io, objServer, busId, slaveAddr);
+                        autoRearm, io, path, objServer, busId, slaveAddr);
                     pSensor->start();
                     if (debug)
                     {
