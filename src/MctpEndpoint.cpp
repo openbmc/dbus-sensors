08c1d32a7be9bd8aa64c0609819fa387ff388832
Andrew Jeffery <andrew@codeconstruct.com.au>
2024-01-31 12:41:14 +1030

    Introduce MCTP-related interfaces and implementations
    The supported implementation of MCTP in OpenBMC exploits Linux's AF_MCTP
    sockets alongside the mctp[1] tools in userspace.
    
    Provide interfaces exposing abstract MCTP management behaviors and
    classes implementing these interfaces. The implementations drive the
    mctp tools to configure the kernel with required networks and endpoints.
    
    [1]: https://github.com/CodeConstruct/mctp/
    
    Change-Id: I5e3125010a24530a07aeba7d0fe9dee15e93184a
    Signed-off-by: Andrew Jeffery <andrew@codeconstruct.com.au>

---
+++ src/MctpEndpoint.cpp
@@ -0,0 +1,398 @@
+#include "MctpEndpoint.hpp"
+
+#include "Utils.hpp"
+
+#include <boost/system/detail/errc.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/bus/match.hpp>
+#include <sdbusplus/exception.hpp>
+#include <sdbusplus/message.hpp>
+#include <sdbusplus/message/native_types.hpp>
+
+#include <exception>
+#include <iterator>
+#include <memory>
+#include <sstream>
+#include <system_error>
+
+static constexpr const char* mctpdBusName = "xyz.openbmc_project.MCTP";
+static constexpr const char* mctpdControlPath = "/xyz/openbmc_project/mctp";
+static constexpr const char* mctpdControlInterface =
+    "au.com.CodeConstruct.MCTP";
+static constexpr const char* mctpdEndpointControlInterface =
+    "au.com.CodeConstruct.MCTP.Endpoint";
+
+static constexpr bool debug = false;
+
+MctpdDevice::MctpdDevice(
+    const std::shared_ptr<sdbusplus::asio::connection>& connection,
+    const std::string& interface, const std::vector<uint8_t>& physaddr,
+    std::optional<std::uint8_t> eid) :
+    connection(connection),
+    interface(interface), physaddr(physaddr), eid{eid}
+{}
+
+void MctpdDevice::onEndpointInterfacesRemoved(
+    const std::weak_ptr<MctpdDevice>& weak, const std::string& objpath,
+    sdbusplus::message_t& msg)
+{
+    sdbusplus::message::object_path path;
+    msg.read(path);
+
+    if (path.str != objpath)
+    {
+        return;
+    }
+
+    std::vector<std::string> removedIfaces;
+    msg.read(removedIfaces);
+
+    auto entry = std::find(removedIfaces.begin(), removedIfaces.end(),
+                           mctpdEndpointControlInterface);
+
+    if (entry == removedIfaces.end())
+    {
+        return;
+    }
+
+    if (auto self = weak.lock())
+    {
+        self->endpointRemoved();
+    }
+}
+
+void MctpdDevice::finaliseEndpoint(
+    const std::string& objpath, uint8_t eid, int network,
+    std::function<void(const std::error_code& ec,
+                       const std::shared_ptr<MctpEndpoint>& ep)>&& added)
+{
+    const auto matchSpec =
+        sdbusplus::bus::match::rules::interfacesRemovedAtPath(objpath);
+    removeMatch = std::make_unique<sdbusplus::bus::match_t>(
+        *connection, matchSpec,
+        std::bind_front(MctpdDevice::onEndpointInterfacesRemoved,
+                        weak_from_this(), objpath));
+    endpoint = std::make_shared<MctpdEndpoint>(shared_from_this(), connection,
+                                               objpath, network, eid);
+    added({}, endpoint);
+}
+
+void MctpdDevice::setup(
+    std::function<void(const std::error_code& ec,
+                       const std::shared_ptr<MctpEndpoint>& ep)>&& added)
+{
+    auto onSetup = [weak{weak_from_this()}, added{std::move(added)}](
+                       const boost::system::error_code& ec, uint8_t eid,
+                       int network, const std::string& objpath,
+                       bool allocated [[maybe_unused]]) mutable {
+        if (ec)
+        {
+            added(ec, {});
+            return;
+        }
+
+        if (auto self = weak.lock())
+        {
+            self->finaliseEndpoint(objpath, eid, network, std::move(added));
+        }
+    };
+    try
+    {
+        if (eid)
+        {
+            connection->async_method_call(
+                onSetup, mctpdBusName, mctpdControlPath, mctpdControlInterface,
+                "AssignEndpointStatic", interface, physaddr, *eid);
+        }
+        else
+        {
+            connection->async_method_call(
+                onSetup, mctpdBusName, mctpdControlPath, mctpdControlInterface,
+                "SetupEndpoint", interface, physaddr);
+        }
+    }
+    catch (const sdbusplus::exception::SdBusError& err)
+    {
+        if constexpr (debug)
+        {
+            std::cerr << "Caught exception while configuring endpoint: "
+                      << err.description() << std::endl;
+        }
+        auto errc = std::errc::no_such_device_or_address;
+        auto ec = std::make_error_code(errc);
+        added(ec, {});
+    }
+}
+
+void MctpdDevice::endpointRemoved()
+{
+    if (endpoint)
+    {
+        if constexpr (debug)
+        {
+            std::cerr << "Endpoint removed @ [" << endpoint->describe() << "]"
+                      << std::endl;
+        }
+        removeMatch.reset();
+        endpoint->removed();
+        endpoint.reset();
+    }
+}
+
+void MctpdDevice::remove()
+{
+    if (endpoint)
+    {
+        if constexpr (debug)
+        {
+            std::cerr << "Removing endpoint @ [" << endpoint->describe() << "]"
+                      << std::endl;
+        }
+        endpoint->remove();
+    }
+}
+
+std::string MctpdDevice::describe() const
+{
+    std::ostringstream oss;
+    oss << "interface: " << interface;
+    if (!physaddr.empty())
+    {
+        oss << ", address: 0x [ ";
+        oss << std::setfill('0') << std::setw(2) << std::hex;
+        auto it = physaddr.begin();
+        for (; it != physaddr.end() - 1; it++)
+        {
+            oss << static_cast<unsigned int>(*it) << " ";
+        }
+        oss << static_cast<unsigned int>(*it);
+        oss << " ]";
+    }
+    return oss.str();
+}
+
+std::string MctpdEndpoint::path(const std::shared_ptr<MctpEndpoint>& ep)
+{
+    return std::format("/xyz/openbmc_project/mctp/{}/{}", ep->network(),
+                       ep->eid());
+}
+
+MctpdEndpoint::MctpdEndpoint(
+    const std::shared_ptr<MctpdDevice>& dev,
+    const std::shared_ptr<sdbusplus::asio::connection>& connection,
+    sdbusplus::message::object_path objpath, int network, uint8_t eid) :
+    dev(dev),
+    connection(connection), objpath(std::move(objpath)), mctp{network, eid}
+{}
+
+void MctpdEndpoint::onMctpEndpointChange(sdbusplus::message_t& msg)
+{
+    std::string iface;
+    std::map<std::string, BasicVariantType> changed;
+    std::vector<std::string> invalidated;
+
+    msg.read(iface);
+    msg.read(changed);
+    msg.read(invalidated);
+
+    if (iface != mctpdEndpointControlInterface)
+    {
+        return;
+    }
+
+    auto it = changed.find("Connectivity");
+    if (it == changed.end())
+    {
+        return;
+    }
+
+    updateEndpointConnectivity(std::get<std::string>(it->second));
+}
+
+void MctpdEndpoint::updateEndpointConnectivity(const std::string& connectivity)
+{
+    if (connectivity == "Degraded")
+    {
+        if (notifyDegraded)
+        {
+            notifyDegraded(shared_from_this());
+        }
+    }
+    else if (connectivity == "Available")
+    {
+        if (notifyAvailable)
+        {
+            notifyAvailable(shared_from_this());
+        }
+    }
+    else
+    {
+        if constexpr (debug)
+        {
+            std::cerr << "Unrecognised connectivity state: '" << connectivity
+                      << "'" << std::endl;
+        }
+    }
+}
+
+int MctpdEndpoint::network() const
+{
+    return mctp.network;
+}
+
+uint8_t MctpdEndpoint::eid() const
+{
+    return mctp.eid;
+}
+
+void MctpdEndpoint::subscribe(
+    std::function<void(const std::shared_ptr<MctpEndpoint>& ep)>&& degraded,
+    std::function<void(const std::shared_ptr<MctpEndpoint>& ep)>&& available,
+    std::function<void(const std::shared_ptr<MctpEndpoint>& ep)>&& removed)
+{
+    const auto matchSpec =
+        sdbusplus::bus::match::rules::propertiesChangedNamespace(
+            objpath.str, mctpdEndpointControlInterface);
+
+    this->notifyDegraded = degraded;
+    this->notifyAvailable = available;
+    this->notifyRemoved = removed;
+
+    try
+    {
+        connectivityMatch.emplace(
+            static_cast<sdbusplus::bus_t&>(*connection), matchSpec,
+            [weak{weak_from_this()}](sdbusplus::message_t& msg) {
+            if (auto self = weak.lock())
+            {
+                self->onMctpEndpointChange(msg);
+            }
+        });
+        connection->async_method_call(
+            [weak{weak_from_this()}](const boost::system::error_code& ec,
+                                     const std::variant<std::string>& value) {
+            if (ec)
+            {
+                if constexpr (debug)
+                {
+                    std::cerr
+                        << "Failed to get current connectivity state: " << ec
+                        << std::endl;
+                }
+                return;
+            }
+
+            if (auto self = weak.lock())
+            {
+                const std::string& connectivity = std::get<std::string>(value);
+                self->updateEndpointConnectivity(connectivity);
+            }
+        },
+            mctpdBusName, objpath.str, "org.freedesktop.DBus.Properties", "Get",
+            mctpdEndpointControlInterface, "Connectivity");
+    }
+    catch (const sdbusplus::exception::SdBusError& err)
+    {
+        this->notifyDegraded = nullptr;
+        this->notifyAvailable = nullptr;
+        this->notifyRemoved = nullptr;
+        std::throw_with_nested(
+            MctpException("Failed to register connectivity signal match"));
+    }
+}
+
+void MctpdEndpoint::recover()
+{
+    try
+    {
+        connection->async_method_call(
+            [weak{weak_from_this()}](const boost::system::error_code& ec
+                                     [[maybe_unused]]) {
+            if (ec)
+            {
+                if (auto self = weak.lock())
+                {
+                    if constexpr (debug)
+                    {
+                        std::cerr << "Failed to recover device at '"
+                                  << self->objpath.str << "'" << std::endl;
+                    }
+                }
+            }
+        },
+            mctpdBusName, objpath.str, mctpdEndpointControlInterface,
+            "Recover");
+    }
+    catch (const sdbusplus::exception::SdBusError& err)
+    {
+        std::throw_with_nested(
+            MctpException("Failed to schedule endpoint recovery"));
+    }
+}
+
+void MctpdEndpoint::remove()
+{
+    try
+    {
+        connection->async_method_call(
+            [self{shared_from_this()}](const boost::system::error_code& ec) {
+            if (ec)
+            {
+                if constexpr (debug)
+                {
+                    std::cerr << "Failed to remove endpoint @ ["
+                              << self->describe() << "]: " << ec.message()
+                              << std::endl;
+                }
+                return;
+            }
+        },
+            mctpdBusName, objpath.str, mctpdEndpointControlInterface, "Remove");
+    }
+    catch (const sdbusplus::exception::SdBusError& err)
+    {
+        std::throw_with_nested(
+            MctpException("Failed schedule endpoint removal"));
+    }
+}
+
+void MctpdEndpoint::removed()
+{
+    if (notifyRemoved)
+    {
+        notifyRemoved(shared_from_this());
+    }
+}
+
+void MctpdEndpoint::setMtu(
+    uint32_t mtu, std::function<void(const std::error_code& ec)>&& completed)
+{
+    try
+    {
+        connection->async_method_call(
+            [cb{std::move(completed)}](const boost::system::error_code& bsec) {
+            cb(static_cast<const std::error_code&>(bsec));
+        },
+            mctpdBusName, objpath.str, mctpdEndpointControlInterface, "SetMTU",
+            mtu);
+    }
+    catch (const sdbusplus::exception::SdBusError& err)
+    {
+        completed(std::error_code(err.get_errno(), std::system_category()));
+    }
+}
+
+std::string MctpdEndpoint::describe() const
+{
+    return std::string("network: ")
+        .append(std::to_string(mctp.network))
+        .append(", EID: ")
+        .append(std::to_string(mctp.eid))
+        .append(" | ")
+        .append(dev->describe());
+}
+
+std::shared_ptr<MctpDevice> MctpdEndpoint::device() const
+{
+    return dev;
+}
---
+++ src/MctpEndpoint.hpp
@@ -0,0 +1,317 @@
+#pragma once
+
+#include <boost/asio/steady_timer.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/bus/match.hpp>
+#include <sdbusplus/message.hpp>
+#include <sdbusplus/message/native_types.hpp>
+
+#include <cstdint>
+#include <iostream>
+
+/**
+ * @file
+ * @brief Abstract and concrete classes representing MCTP concepts and
+ *        behaviours.
+ */
+
+/**
+ * @brief An exception type that may be thrown by implementations of the MCTP
+ * abstract classes.
+ *
+ * This exception should be the basis for all exceptions thrown out of the MCTP
+ * APIs, and should capture any other exceptions that occur.
+ */
+class MctpException : public std::exception
+{
+  public:
+    MctpException() = delete;
+    explicit MctpException(const char* desc) : desc(desc) {}
+    const char* what() const noexcept override
+    {
+        return desc;
+    }
+
+  private:
+    const char* desc;
+};
+
+/**
+ * @brief An enum of the MCTP transports described in DSP0239 v1.10.0 Section 7.
+ *
+ * https://www.dmtf.org/sites/default/files/standards/documents/DSP0239_1.10.0.pdf
+ */
+enum class MctpTransport
+{
+    Reserved = 0x00,
+    SMBus = 0x01,
+};
+
+/**
+ * @brief Captures properties of MCTP interfaces.
+ *
+ * https://github.com/CodeConstruct/mctp/blob/v1.1/src/mctp.c#L672-L703
+ */
+struct MctpInterface
+{
+    std::string name;
+    MctpTransport transport;
+
+    auto operator<=>(const MctpInterface& r) const = default;
+};
+
+class MctpDevice;
+
+/**
+ * @brief Captures the behaviour of an endpoint at the MCTP layer
+ *
+ * The lifetime of an instance of MctpEndpoint is proportional to the lifetime
+ * of the endpoint configuration. If an endpoint is deconfigured such that its
+ * device has no assigned EID, then any related MctpEndpoint instance must be
+ * destructed as a consequence.
+ */
+class MctpEndpoint
+{
+  public:
+    /**
+     * @return The Linux network ID of the network in which the endpoint
+               participates
+     */
+    virtual int network() const = 0;
+
+    /**
+     * @return The MCTP endpoint ID of the endpoint in its network
+     */
+    virtual uint8_t eid() const = 0;
+
+    /**
+     * @brief Subscribe to events produced by an endpoint object across its
+     *        lifecycle
+     *
+     * @param degraded The callback to execute when the MCTP layer indicates the
+     *                 endpoint is unresponsive
+     *
+     * @param available The callback to execute when the MCTP layer indicates
+     *                  that communication with the degraded endpoint has been
+     *                  recovered
+     *
+     * @param removed The callback to execute when the MCTP layer indicates the
+     *                endpoint has been removed.
+     */
+    virtual void subscribe(
+        std::function<void(const std::shared_ptr<MctpEndpoint>& ep)>&& degraded,
+        std::function<void(const std::shared_ptr<MctpEndpoint>& ep)>&&
+            available,
+        std::function<void(const std::shared_ptr<MctpEndpoint>& ep)>&&
+            removed) = 0;
+
+    /**
+     * @brief Configures the Maximum Transmission Unit (MTU) for the route to
+     *        the endpoint
+     *
+     * @param mtu The route MTU to be used for the endpoint. Note that MTU
+     *            value refers to the size of the entire MCTP packet. The value
+     *            provided is interpreted as accounting for the MCTP header.
+     *
+     * @param completed The callback to invoke once the MTU configuration has
+     *                  completed. The provided error code must be checked as
+     *                  the request may not have succeeded.
+     */
+    virtual void
+        setMtu(uint32_t mtu,
+               std::function<void(const std::error_code& ec)>&& completed) = 0;
+
+    /**
+     * @brief Request that the MCTP layer attempt to recover communication with
+     *        an unresponsive endpoint.
+     *
+     * Recovery progress is indicated by invocation of the @c degraded,
+     * @c available and @c removed handlers registered using the @c events()
+     * API.
+     */
+    virtual void recover() = 0;
+
+    /**
+     * @brief Remove the endpoint from its associated network
+     */
+    virtual void remove() = 0;
+
+    /**
+     * @return A formatted string representing the endpoint in terms of its
+     *         address properties
+     */
+    virtual std::string describe() const = 0;
+
+    /**
+     * @return A shared pointer to the device instance associated with the
+     *         endpoint.
+     */
+    virtual std::shared_ptr<MctpDevice> device() const = 0;
+};
+
+/**
+ * @brief Represents an MCTP-capable device on a bus.
+ *
+ * It is often known that an MCTP-capable device exists on a bus prior to the
+ * MCTP stack configuring the device for communication. MctpDevice exposes the
+ * ability to set-up the endpoint device for communication.
+ *
+ * The lifetime of an MctpDevice instance is proportional to the existence of an
+ * MCTP-capable device in the system. If a device represented by an MctpDevice
+ * instance is removed from the system then any related MctpDevice instance must
+ * be destructed a consequence.
+ *
+ * Successful set-up of the device as an endpoint yields an MctpEndpoint
+ * instance. The lifetime of the MctpEndpoint instance produced must not exceed
+ * the lifetime of its parent MctpDevice.
+ */
+class MctpDevice
+{
+  public:
+    /**
+     * @brief Configure the device for MCTP communication
+     *
+     * @param added The callback to invoke once the setup process has
+     *              completed. The provided error code @p ec must be
+     *              checked as the request may not have succeeded. If
+     *              the request was successful then @p ep contains a
+     *              valid MctpEndpoint instance.
+     */
+    virtual void
+        setup(std::function<void(const std::error_code& ec,
+                                 const std::shared_ptr<MctpEndpoint>& ep)>&&
+                  added) = 0;
+
+    /**
+     * @brief Remove the device and any associated endpoint from the MCTP stack.
+     */
+    virtual void remove() = 0;
+
+    /**
+     * @return A formatted string representing the device in terms of its
+     *         address properties.
+     */
+    virtual std::string describe() const = 0;
+};
+
+class MctpdDevice;
+
+/**
+ * @brief An implementation of MctpEndpoint in terms of the D-Bus interfaces
+ *        exposed by @c mctpd.
+ *
+ * The lifetime of an MctpdEndpoint is proportional to the lifetime of the
+ * endpoint object exposed by @c mctpd. The lifecycle of @c mctpd endpoint
+ * objects is discussed here:
+ *
+ * https://github.com/CodeConstruct/mctp/pull/23/files#diff-00234f5f2543b8b9b8a419597e55121fe1cc57cf1c7e4ff9472bed83096bd28e
+ */
+class MctpdEndpoint :
+    public MctpEndpoint,
+    public std::enable_shared_from_this<MctpdEndpoint>
+{
+  public:
+    static std::string path(const std::shared_ptr<MctpEndpoint>& ep);
+
+    MctpdEndpoint() = delete;
+    MctpdEndpoint(
+        const std::shared_ptr<MctpdDevice>& dev,
+        const std::shared_ptr<sdbusplus::asio::connection>& connection,
+        sdbusplus::message::object_path objpath, int network, uint8_t eid);
+    MctpdEndpoint& McptdEndpoint(const MctpdEndpoint& other) = delete;
+    MctpdEndpoint(MctpdEndpoint&& other) noexcept = default;
+    virtual ~MctpdEndpoint() = default;
+
+    int network() const override;
+    uint8_t eid() const override;
+    void subscribe(
+        std::function<void(const std::shared_ptr<MctpEndpoint>& ep)>&& degraded,
+        std::function<void(const std::shared_ptr<MctpEndpoint>& ep)>&&
+            available,
+        std::function<void(const std::shared_ptr<MctpEndpoint>& ep)>&& removed)
+        override;
+    void setMtu(
+        uint32_t mtu,
+        std::function<void(const std::error_code& ec)>&& completed) override;
+    void recover() override;
+    void remove() override;
+
+    std::string describe() const override;
+
+    std::shared_ptr<MctpDevice> device() const override;
+
+    /**
+     * @brief Indicate the endpoint has been removed
+     *
+     * Called from the implementation of MctpdDevice::removed() for resource
+     * cleanup prior to destruction.
+     */
+    void removed();
+
+  private:
+    std::shared_ptr<MctpdDevice> dev;
+    std::shared_ptr<sdbusplus::asio::connection> connection;
+    sdbusplus::message::object_path objpath;
+    struct
+    {
+        int network;
+        uint8_t eid;
+    } mctp;
+    std::function<void(const std::shared_ptr<MctpEndpoint>& ep)>
+        notifyAvailable;
+    std::function<void(const std::shared_ptr<MctpEndpoint>& ep)> notifyDegraded;
+    std::function<void(const std::shared_ptr<MctpEndpoint>& ep)> notifyRemoved;
+    std::optional<sdbusplus::bus::match_t> connectivityMatch;
+
+    void onMctpEndpointChange(sdbusplus::message_t& msg);
+    void updateEndpointConnectivity(const std::string& connectivity);
+};
+
+/**
+ * @brief An implementation of MctpDevice in terms of D-Bus interfaces exposed
+ *        by @c mctpd.
+ *
+ * The construction or destruction of an MctpdDevice is not required to be
+ * correlated with signals from @c mctpd. For instance, EntityManager may expose
+ * the existance of an MCTP-capable device through its usual configuration
+ * mechanisms.
+ */
+class MctpdDevice :
+    public MctpDevice,
+    public std::enable_shared_from_this<MctpdDevice>
+{
+  public:
+    MctpdDevice() = delete;
+    MctpdDevice(const std::shared_ptr<sdbusplus::asio::connection>& connection,
+                const std::string& interface,
+                const std::vector<uint8_t>& physaddr,
+                std::optional<std::uint8_t> eid = {});
+    MctpdDevice(const MctpdDevice& other) = delete;
+    MctpdDevice(MctpdDevice&& other) = delete;
+    virtual ~MctpdDevice() = default;
+
+    void setup(std::function<void(const std::error_code& ec,
+                                  const std::shared_ptr<MctpEndpoint>& ep)>&&
+                   added) override;
+    void remove() override;
+    std::string describe() const override;
+
+  private:
+    static void
+        onEndpointInterfacesRemoved(const std::weak_ptr<MctpdDevice>& weak,
+                                    const std::string& objpath,
+                                    sdbusplus::message_t& msg);
+
+    std::shared_ptr<sdbusplus::asio::connection> connection;
+    const std::string interface;
+    const std::vector<uint8_t> physaddr;
+    const std::optional<uint8_t> eid;
+    std::shared_ptr<MctpdEndpoint> endpoint;
+    std::unique_ptr<sdbusplus::bus::match_t> removeMatch;
+
+    void finaliseEndpoint(
+        const std::string& objpath, uint8_t eid, int network,
+        std::function<void(const std::error_code& ec,
+                           const std::shared_ptr<MctpEndpoint>& ep)>&& added);
+    void endpointRemoved();
+};
