68ad8ae3067e9fcee8570db6743eeb56aeee0f43
Ed Tanous <ed@tanous.net>
2024-02-09 16:59:22 -0800

    Add IpmbSensor unit tests
    Because everything was packed into IpmbSensor.cpp, which also included
    main(), this commit breaks it up into the patterns we have in other
    sensors, with <X>main.cpp containing the things for main.
    
    While this doesn't clean up everything, it at least makes
    processResponse unit testable, and adds one minor test for it.
    
    Change-Id: I1eabf650ff635211e5b9a94816c2ffce250ed252
    Signed-off-by: Ed Tanous <ed@tanous.net>

---
+++ tests/test_IpmbSensor.cpp
@@ -0,0 +1,19 @@
+#include "IpmbSensor.hpp"
+
+#include <gtest/gtest.h>
+
+namespace
+{
+
+TEST(IPMBSensor, Byte0)
+{
+    std::vector<uint8_t> data;
+    data.push_back(42);
+
+    double responseValue = 0.0;
+    size_t errCount = 0;
+    EXPECT_TRUE(IpmbSensor::processReading(ReadingFormat::byte0, 0, data,
+                                           responseValue, errCount));
+    EXPECT_EQ(responseValue, 42.0);
+
+} // namespace
