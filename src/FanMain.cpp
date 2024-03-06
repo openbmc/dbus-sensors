506e5c88c24edcb8097b22a5966df90d3eabc98b
Chris Cain <cjcain@us.ibm.com>
2024-03-06 14:20:09 -0600

    Refactor PresenceSensor for GPIO presence monitoring
    This commit moves the PresenceSensor object from TachSensors to its own
    file so that it can be used for more than just fans.
    
    This commit maintains the same functionality as the original code.
    Once the objects is created for a GPIO, it will be monitoring for
    gpiod::line_request::EVENT_BOTH_EDGES changes. The user can call
    isPresent() anytime to read the presence status.
    
    Interface:
        EventPresenceSensor(const std::string& iSensorType,
                            const std::string& iSensorName,
                            const std::string& gpioName, bool inverted,
                            boost::asio::io_context& io);
    Sample usage:
        presenceSensor = std::make_unique<EventPresenceSensor>(
            "Fan", "Fan4b", "FAN4_PRESENCE_R_N", true, io);
        if (presenceSensor->isPresent())
        {
            // Fan is present
        }
    
    Testing: Unable to test this refactor because no hardware supporting
    event driven GPIO detection available.
    
    Change-Id: I1f1a4cbab39d3e3ab38b30288f6aa199ee0cfe3c
    Signed-off-by: Chris Cain <cjcain@us.ibm.com>

--- src/FanMain.cpp
+++ src/FanMain.cpp
@@ -14,6 +14,7 @@
 // limitations under the License.
 */
 
+#include "PresenceSensor.hpp"
 #include "PwmSensor.hpp"
 #include "TachSensor.hpp"
 #include "Thresholds.hpp"
@@ -470,8 +471,9 @@ void createSensors(
                             if (!presenceSensor)
                             {
                                 presenceSensor =
-                                    std::make_shared<PresenceSensor>(
-                                        *pinName, inverted, io, sensorName);
+                                    std::make_shared<EventPresenceSensor>(
+                                        "Fan", sensorName, *pinName, inverted,
+                                        io);
                                 presenceSensors[*pinName] = presenceSensor;
                             }
                         }
---
+++ src/PresenceSensor.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "sensor.hpp"
+
+#include <gpiod.hpp>
+#include <phosphor-logging/lg2.hpp>
+
+class PresenceSensor : private boost::noncopyable
+{
+  public:
+    PresenceSensor(const std::string& type, const std::string& name) :
+        sensorType(type), sensorName(name) {};
+    virtual ~PresenceSensor() = 0;
+
+    bool isPresent() const
+    {
+        return status;
+    }
+
+  protected:
+    gpiod::line gpioLine;
+    bool status = false;
+    std::string sensorType;
+    std::string sensorName;
+
+    virtual void monitorPresence() = 0;
+
+    void logPresent(const std::string& device)
+    {
+        std::string summary = sensorType + " Present (" + sensorName + ")";
+        std::string msg = "OpenBMC.0.1." + sensorType + "Present";
+        lg2::info(summary.c_str(), "REDFISH_MESSAGE_ID", msg.c_str(),
+                  "REDFISH_MESSAGE_ARGS", device);
+    }
+
+    void logRemoved(const std::string& device)
+    {
+        std::string summary = sensorType + " Not Present (" + sensorName + ")";
+        std::string msg = "OpenBMC.0.1." + sensorType + "Removed";
+        lg2::error(summary.c_str(), "REDFISH_MESSAGE_ID", msg.c_str(),
+                   "REDFISH_MESSAGE_ARGS", device);
+    }
+
+    void updateAndTracePresence();
+};
+
+class EventPresenceSensor :
+    public PresenceSensor,
+    public std::enable_shared_from_this<EventPresenceSensor>
+{
+  public:
+    EventPresenceSensor(const std::string& iSensorType,
+                        const std::string& iSensorName,
+                        const std::string& gpioName, bool inverted,
+                        boost::asio::io_context& io);
+
+    ~EventPresenceSensor() override
+    {
+        gpioFd.close();
+    }
+
+  private:
+    boost::asio::posix::stream_descriptor gpioFd;
+
+    void monitorPresence() override;
+    void read();
+};
