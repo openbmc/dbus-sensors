f49c24b2a55c13e8d83115fc47020be87efb1d36
Vincent Chou <vincent.chou@fii-na.com>
2024-03-27 09:14:14 -0500

    Defer initial fan poll to prevent low threshold alarm
    The fan controller(max31790) that we are using measures fan speed once
    per second. However, dbus-sensors polls the fan speed right after
    enable pwm, and update value property and check thresholds right away
    if the powerState is configured to "always". This causes the initial
    read of fan speed gets 0 value and assert low threshold at start up.
    
    This change makes the initial poll wait for a configurable delay so
    the fan controller has enough time to populate the value to fan_input
    file before dbus-sensors reads the value.
    
    Tested:
    1. Add log to waitAndRead and setupRead function.
    2. Flash and hotswap the unit.
    3. Run `journalctl | grep fansensor` to check the log and see only
       the initial poll has been delayed.
    4. Check journal and `ipmitool sel elist` and no longer see the
       low threshold alarm at start up.
    
    Change-Id: I14b62b4cc8ed5c72f004c7c4e9d7509c5d0407cd
    Signed-off-by: Vincent Chou <vincent.chou@fii-na.com>

--- src/TachSensor.cpp
+++ src/TachSensor.cpp
@@ -123,10 +123,10 @@ void TachSensor::setupRead()
     });
 }
 
-void TachSensor::restartRead(size_t pollTime)
+void TachSensor::waitAndRead(size_t waitTime)
 {
     std::weak_ptr<TachSensor> weakRef = weak_from_this();
-    waitTimer.expires_after(std::chrono::milliseconds(pollTime));
+    waitTimer.expires_after(std::chrono::milliseconds(waitTime));
     waitTimer.async_wait([weakRef](const boost::system::error_code& ec) {
         if (ec == boost::asio::error::operation_aborted)
         {
@@ -188,7 +188,7 @@ void TachSensor::handleResponse(const boost::system::error_code& err,
         }
     }
 
-    restartRead(pollTime);
+    waitAndRead(pollTime);
 }
 
 void TachSensor::checkThresholds()
