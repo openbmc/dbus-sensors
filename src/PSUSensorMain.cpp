9110dfc6905db2e749506f76922c1b5f2f0b59e5
Yi-Shum <EasonChen1@quantatw.com>
2024-06-04 15:19:58 +0800

    PSUSensor: add xdp710 support
    Add device xdp710 reading support.
    
    Change-Id: Id4b79dd4c782c39d75ac0305b9f66c94810531fe
    Signed-off-by: Yi-Shum <EasonChen1@quantatw.com>

--- src/PSUSensorMain.cpp
+++ src/PSUSensorMain.cpp
@@ -124,6 +124,7 @@ static const I2CDeviceTypeMap sensorTypes{
     {"TDA38640", I2CDeviceType{"tda38640", true}},
     {"TPS53679", I2CDeviceType{"tps53679", true}},
     {"TPS546D24", I2CDeviceType{"tps546d24", true}},
+    {"XDP710", I2CDeviceType{"xdp710", true}},
     {"XDPE11280", I2CDeviceType{"xdpe11280", true}},
     {"XDPE12284", I2CDeviceType{"xdpe12284", true}},
     {"XDPE152C4", I2CDeviceType{"xdpe152c4", true}},
