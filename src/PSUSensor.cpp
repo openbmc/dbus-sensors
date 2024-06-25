45b5bb14d6932d7125584d201073fe7c7d2c290c
Peter Yin <peter.yin@quantatw.com>
2024-06-25 14:57:08 +0800

    psusensor: avoid crash by activate sensor twice
    Sensor should just only activate or deactivate when power state change.
    
    Simplify powerStateChanged(), just do "sensor->activate()" instead of
    calling "createSensors()".
    
    Also add check in activate(), just return if sensor already activated.
    
    Tested result:
    Do a host power cycle and wait until ostate change to "Standby",
    psusensor is not crushing and PID remain same.
    ```
    root@harma:~# systemctl status xyz.openbmc_project.psusensor
    ● xyz.openbmc_project.psusensor.service - PSU Sensor
         Loaded: loaded (/usr/lib/systemd/system/xyz.openbmc_project.psusensor.service; enabled; preset: enabled)
        Drop-In: /usr/lib/systemd/system/xyz.openbmc_project.psusensor.service.d
                 └─psusensor-wait-host-state-ready.conf
         Active: active (running) since Mon 2024-02-26 21:42:47 PST; 1min 10s ago
       Main PID: 13355 (psusensor)
            CPU: 8.344s
         CGroup: /system.slice/xyz.openbmc_project.psusensor.service
                 └─13355 /usr/bin/psusensor
    
    Feb 26 21:43:54 harma psusensor[13355]: PSUSubEvent asserted by /sys/class/hwmon/hwmon25/in2_alarm
    Feb 26 21:43:54 harma psusensor[13355]: Failure assert
    Feb 26 21:43:55 harma psusensor[13355]: MB_DIMM_A10_TEMP_C read failed
    Feb 26 21:43:55 harma psusensor[13355]: MB_DIMM_A10_PWR_W read failed
    Feb 26 21:43:55 harma psusensor[13355]: Failure deassert
    Feb 26 21:43:56 harma psusensor[13355]: MB_DIMM_A10_TEMP_C read failed
    Feb 26 21:43:56 harma psusensor[13355]: MB_DIMM_A10_PWR_W read failed
    Feb 26 21:43:56 harma psusensor[13355]: PSUSubEvent asserted by /sys/class/hwmon/hwmon26/in2_alarm
    Feb 26 21:43:56 harma psusensor[13355]: Failure assert
    Feb 26 21:43:57 harma psusensor[13355]: MB_DIMM_A10_TEMP_C read failed
    root@harma:~#
    root@harma:~# obmcutil poweroff
    root@harma:~# sleep 10
    root@harma:~# obmcutil poweron
    root@harma:~# sleep 60
    root@harma:~# obmcutil osstate
    OperatingSystemState: xyz.openbmc_project.State.OperatingSystem.Status.OSStatus.Standby
    root@harma:~#
    root@harma:~# systemctl status xyz.openbmc_project.psusensor
    ● xyz.openbmc_project.psusensor.service - PSU Sensor
         Loaded: loaded (/usr/lib/systemd/system/xyz.openbmc_project.psusensor.service; enabled; preset: enabled)
        Drop-In: /usr/lib/systemd/system/xyz.openbmc_project.psusensor.service.d
                 └─psusensor-wait-host-state-ready.conf
         Active: active (running) since Mon 2024-02-26 21:42:47 PST; 3min 41s ago
       Main PID: 13355 (psusensor)
            CPU: 22.802s
         CGroup: /system.slice/xyz.openbmc_project.psusensor.service
                 └─13355 /usr/bin/psusensor
    
    Feb 26 21:46:25 harma psusensor[13355]: MB_DIMM_A10_PWR_W read failed
    Feb 26 21:46:26 harma psusensor[13355]: PSUSubEvent asserted by /sys/class/hwmon/hwmon34/in2_alarm
    Feb 26 21:46:26 harma psusensor[13355]: Failure assert
    Feb 26 21:46:26 harma psusensor[13355]: MB_DIMM_A10_TEMP_C read failed
    Feb 26 21:46:26 harma psusensor[13355]: MB_DIMM_A10_PWR_W read failed
    Feb 26 21:46:26 harma psusensor[13355]: Failure deassert
    Feb 26 21:46:27 harma psusensor[13355]: MB_DIMM_A10_TEMP_C read failed
    Feb 26 21:46:27 harma psusensor[13355]: MB_DIMM_A10_PWR_W read failed
    Feb 26 21:46:27 harma psusensor[13355]: PSUSubEvent asserted by /sys/class/hwmon/hwmon25/in2_alarm
    Feb 26 21:46:27 harma psusensor[13355]: Failure assert
    ```
    
    Change-Id: I3fb55c0f10ad26472470bb783ea5df5498a4c1a8
    Signed-off-by: Potin Lai <potin.lai@quantatw.com>
    Signed-off-by: Peter Yin <peter.yin@quantatw.com>

--- src/PSUSensor.cpp
+++ src/PSUSensor.cpp
@@ -122,8 +122,7 @@ bool PSUSensor::isActive()
     return inputDev.is_open();
 }
 
-void PSUSensor::activate(const std::string& newPath,
-                         const std::shared_ptr<I2CDevice>& newI2CDevice)
+void PSUSensor::activate()
 {
     if (isActive())
     {
@@ -143,8 +142,6 @@ void PSUSensor::deactivate()
     // close the input dev to cancel async operations
     inputDev.close();
     waitTimer.cancel();
-    i2cDevice = nullptr;
-    path = "";
 }
 
 void PSUSensor::setupRead()
