326f75479f3bfe8d8038a9fcbadf32aeecbc7504
Bright Cheng <bright_cheng@wiwynn.com>
2024-08-26 11:37:53 +0800

    Utils: support powerState for multi-node system
    The current code of dbus-sensors only get powerState values for Chassis0
    and Host0 which cannot be used for the system with multiple server
    nodes.
    
    This commit add "slotId" in Sensor class to indicate the index of
    server. When setupPowerMatchCallback is called, code will call
    GetSubTreePaths method to get chassis and host objects to set match
    rules for each object. Meanwhile, sensor can get corresponding
    powerState value with its slotId.
    
    Schema update of Emtity-Manager:
    https://gerrit.openbmc.org/c/openbmc/entity-manager/+/74072
    
    Test Plan:
    1. Add setting "SlotId" and "PowerState" to some sensors
    2. Modify property "CurrentPowerState" to
    xyz.openbmc_project.State.Chassis.PowerState.Off, the sensor value of
    corresponding server should be nan.
    3. Change property "CurrentPowerState" to
    xyz.openbmc_project.State.Chassis.PowerState.On, the sensor value of
    corresponding server should be real value.
    
    Test Result:
    root@bmc:~# busctl get-property xyz.openbmc_project.State.Chassis7
    /xyz/openbmc_project/state/chassis7 xyz.openbmc_project.State.Chassis
    CurrentPowerState
    s "xyz.openbmc_project.State.Chassis.PowerState.On"
    root@bmc:~# busctl get-property xyz.openbmc_project.PSUSensor
    /xyz/openbmc_project/sensors/voltage/MEDUSA_MB7_12VHSC_INPUT_VOLT_V
    xyz.openbmc_project.Sensor.Value Value
    d 12.214
    root@bmc:~# busctl set-property xyz.openbmc_project.State.Chassis7
    /xyz/openbmc_project/state/chassis7 xyz.openbmc_project.State.Chassis
    CurrentPowerState s "xyz.openbmc_project.State.Chassis.PowerState.Off"
    root@bmc:~# busctl get-property xyz.openbmc_project.State.Chassis7
    /xyz/openbmc_project/state/chassis7 xyz.openbmc_project.State.Chassis
    CurrentPowerState
    s "xyz.openbmc_project.State.Chassis.PowerState.Off"
    root@bmc:~# busctl get-property xyz.openbmc_project.PSUSensor
    /xyz/openbmc_project/sensors/voltage/MEDUSA_MB7_12VHSC_INPUT_VOLT_V
    xyz.openbmc_project.Sensor.Value Value
    d nan
    root@bmc:~# busctl set-property xyz.openbmc_project.State.Chassis7
    /xyz/openbmc_project/state/chassis7 xyz.openbmc_project.State.Chassis
    CurrentPowerState s "xyz.openbmc_project.State.Chassis.PowerState.On"
    root@bmc:~# busctl get-property xyz.openbmc_project.State.Chassis7
    /xyz/openbmc_project/state/chassis7 xyz.openbmc_project.State.Chassis
    CurrentPowerState
    s "xyz.openbmc_project.State.Chassis.PowerState.On"
    root@bmc:~# busctl get-property xyz.openbmc_project.PSUSensor
    /xyz/openbmc_project/sensors/voltage/MEDUSA_MB7_12VHSC_INPUT_VOLT_V
    xyz.openbmc_project.Sensor.Value Value
    d 12.209
    
    Change-Id: Iaf4459e9774ac88dd625abed7644c00f8808ed46
    Signed-off-by: Bright Cheng <bright.cheng.wiwynn@gmail.com>

--- src/PSUSensorMain.cpp
+++ src/PSUSensorMain.cpp
@@ -510,6 +510,7 @@ static void createSensorsCallback(
         checkGroupEvent(directory.string(), groupEventPathList);
 
         PowerState readState = getPowerState(*baseConfig);
+        size_t readSlot = getSlotId(*baseConfig);
 
         /* Check if there are more sensors in the same interface */
         int i = 1;
@@ -945,7 +946,7 @@ static void createSensorsCallback(
                 readState, findSensorUnit->second, factor,
                 psuProperty.maxReading, psuProperty.minReading,
                 psuProperty.sensorOffset, labelHead, thresholdConfSize,
-                pollRate, i2cDev);
+                pollRate, i2cDev, readSlot);
             sensors[sensorName]->setupRead();
             ++numCreated;
             if constexpr (debug)
@@ -1128,7 +1129,11 @@ static void powerStateChanged(
         {
             if (sensor != nullptr && sensor->readState == type)
             {
-                sensor->activate();
+                if ((type == PowerState::chassisOn) &&
+                    (isChassisOn(sensor->slotId)))
+                {
+                    sensor->activate();
+                }
             }
         }
     }
@@ -1138,7 +1143,11 @@ static void powerStateChanged(
         {
             if (sensor != nullptr && sensor->readState == type)
             {
-                sensor->deactivate();
+                if ((type == PowerState::chassisOn) &&
+                    (!isChassisOn(sensor->slotId)))
+                {
+                    sensor->deactivate();
+                }
             }
         }
     }
