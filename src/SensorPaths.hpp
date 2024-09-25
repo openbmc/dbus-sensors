8f08a8ead9b584340e21a376a524eb39623f8e05
P.K. Lee <p.k.lee@quantatw.com>
2024-09-25 19:41:00 +0800

    Add Modbus Sensor
    New sensors have been added to enable access to remote devices via
    Modbus RTU and TCP. This implementation references the Belimo 22PF
    Thermal Energy Meter,and several measurement units have been added
    as required, including DegreesF,CF (Cubic Feet), CM (Cubic Meters),
    CMH (Cubic Meters per Hour), CMS (Cubic Meters per Second), Gallon,
    GPM (Gallons Per Minute), Litre, LPH (Litres Per Hour), LPM (Litres
    Per Minute), and LPS (Litres Per Second).
    
    Tested:
    Currently, verification has been conducted using the Modbus RTU
    method, successfully obtaining the relevant sensor values.
    
    Change-Id: I4e9c2d0a73180fa0d214f8e0747a0204f2819510
    Signed-off-by: P.K. Lee <p.k.lee@quantatw.com>

--- src/SensorPaths.hpp
+++ src/SensorPaths.hpp
@@ -13,6 +13,8 @@ namespace sensor_paths
 
 constexpr const char* unitDegreesC =
     "xyz.openbmc_project.Sensor.Value.Unit.DegreesC";
+constexpr const char* unitDegreesF =
+    "xyz.openbmc_project.Sensor.Value.Unit.DegreesF";
 constexpr const char* unitRPMs = "xyz.openbmc_project.Sensor.Value.Unit.RPMS";
 constexpr const char* unitVolts = "xyz.openbmc_project.Sensor.Value.Unit.Volts";
 constexpr const char* unitMeters =
@@ -24,11 +26,30 @@ constexpr const char* unitJoules =
     "xyz.openbmc_project.Sensor.Value.Unit.Joules";
 constexpr const char* unitPercent =
     "xyz.openbmc_project.Sensor.Value.Unit.Percent";
+constexpr const char* unitCF = "xyz.openbmc_project.Sensor.Value.Unit.CF";
 constexpr const char* unitCFM = "xyz.openbmc_project.Sensor.Value.Unit.CFM";
 constexpr const char* unitPascals =
     "xyz.openbmc_project.Sensor.Value.Unit.Pascals";
 constexpr const char* unitPercentRH =
     "xyz.openbmc_project.Sensor.Value.Unit.PercentRH";
+constexpr const char* unitCM =
+    "xyz.openbmc_project.Sensor.Value.Unit.CM";
+constexpr const char* unitCMH =
+    "xyz.openbmc_project.Sensor.Value.Unit.CMH";
+constexpr const char* unitCMS =
+    "xyz.openbmc_project.Sensor.Value.Unit.CMS";
+constexpr const char* unitGallon =
+    "xyz.openbmc_project.Sensor.Value.Unit.Gallon";
+constexpr const char* unitGPM =
+    "xyz.openbmc_project.Sensor.Value.Unit.GPM";
+constexpr const char* unitLitre =
+    "xyz.openbmc_project.Sensor.Value.Unit.Litre";
+constexpr const char* unitLPH =
+    "xyz.openbmc_project.Sensor.Value.Unit.LPH";
+constexpr const char* unitLPM =
+    "xyz.openbmc_project.Sensor.Value.Unit.LPM";
+constexpr const char* unitLPS =
+    "xyz.openbmc_project.Sensor.Value.Unit.LPS";
 
 std::string getPathForUnits(const std::string& units);
 
