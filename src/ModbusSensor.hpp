28c39cbea63c5eeabd7b2e127b47fd2e2b5dc22f
P.K. Lee <p.k.lee@quantatw.com>
2024-09-25 19:41:00 +0800

    Add Modbus Sensor
    New sensors have been added to enable access to remote devices via Modbus RTU
    and TCP. This implementation references the Belimo 22PF Thermal Energy Meter,
    and several measurement units have been added as required, including DegreesF,
    CF (Cubic Feet), CM (Cubic Meters), CMH (Cubic Meters per Hour), CMS (Cubic
    Meters per Second), Gallon, GPM (Gallons Per Minute), Litre, LPH (Litres Per
    Hour), LPM (Litres Per Minute), and LPS (Litres Per Second).
    
    Tested:
    Currently, verification has been conducted using the Modbus RTU method,
    successfully obtaining the relevant sensor values.
    
    Change-Id: I4e9c2d0a73180fa0d214f8e0747a0204f2819510
    Signed-off-by: P.K. Lee <p.k.lee@quantatw.com>

---
+++ src/ModbusSensor.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <sensor.hpp>
+
+#include <memory>
+#include <string>
+#include <vector>
+
+extern "C"
+{
+#include <modbus.h>
+}
+
+using word = std::vector<uint16_t>;
+using halfword = std::vector<uint8_t>;
+
+class ModbusSensor : public Sensor
+{
+  public:
+    ModbusSensor(std::shared_ptr<sdbusplus::asio::connection>& conn,
+                 boost::asio::io_context& io, const std::string& name,
+                 const std::string& sensorConfiguration,
+                 sdbusplus::asio::object_server& objectServer,
+                 std::vector<thresholds::Threshold>&& thresholdData,
+                 const std::pair<double, double>& limits, std::string& protocol,
+                 uint8_t deviceId, std::string& ipAddress, uint16_t portNumber,
+                 std::string& device, uint32_t baud, std::string& parity,
+                 uint8_t dataBit, uint8_t stopBit, uint16_t requestFunctionCode,
+                 uint16_t requestAddress, uint16_t requestQuantity,
+                 uint16_t requestByteCount, uint16_t requestValue,
+                 uint16_t readLength, double scaleFactor, bool signedInteger,
+                 bool readTwoWords, std::string sensorUnit);
+    ~ModbusSensor() override;
+
+    void checkThresholds() override;
+
+    void init();
+
+  private:
+    void setSensorValue();
+    int request();
+    void routine();
+    int setupModbusConnection();
+    int startConnection();
+
+    modbus_t* mb;
+
+    std::string protocol;
+    uint8_t deviceId;
+    std::string ipAddress;
+    uint16_t portNumber;
+    std::string device;
+    uint32_t baud;
+    std::string parity;
+    uint8_t dataBit;
+    uint8_t stopBit;
+
+    uint16_t requestFunctionCode;
+    uint16_t requestAddress;
+    uint16_t requestQuantity;
+    uint16_t requestByteCount;
+    uint16_t requestValue;
+    uint16_t readLength; // A readLength value of 1 represents one register.
+    double scaleFactor;
+    bool signedInteger;
+    bool readTwoWords; // Combine two registers into a single value.
+    std::string sensorUnit;
+    word responseWord;
+    halfword responseHalfword;
+
+    sdbusplus::asio::object_server& objectServer;
+    boost::asio::steady_timer waitTimer;
+};
