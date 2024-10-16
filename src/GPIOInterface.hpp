24620eb7bbb356d66efe42b0bc70d662aa35803c
Jagpal Singh Gill <paligill@gmail.com>
2024-10-16 09:42:54 -0700

    Implement Liquid Leak Detector
    Implement liquid leak detector based on design
    https://gerrit.openbmc.org/c/openbmc/docs/+/73152.
    
    Related PRs:
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/73151
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/73707
    https://gerrit.openbmc.org/c/openbmc/sdbusplus/+/75461
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/75999
    
    Tested:
    ```
    >> busctl tree xyz.openbmc_project.LeakDetector
    └─ /xyz
      └─ /xyz/openbmc_project
        └─ /xyz/openbmc_project/state
          └─ /xyz/openbmc_project/state/leak
            └─ /xyz/openbmc_project/state/leak/detector
              ├─ /xyz/openbmc_project/state/leak/detector/TrayDetector1
              └─ /xyz/openbmc_project/state/leak/detector/TrayDetector2
    
    >> echo pull-up > /sys/devices/platform/gpio-sim.0/gpiochip2/sim_gpio0/pull
    
    >> curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Systems/system/LogServices/EventLog/Entries
    {
      "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries",
      "@odata.type": "#LogEntryCollection.LogEntryCollection",
      "Description": "Collection of System Event Log Entries",
      "Members": [
      	...
        {
          "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/2",
          "@odata.type": "#LogEntry.v1_9_0.LogEntry",
          "AdditionalDataURI": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/2/attachment",
          "Created": "2024-05-10T06:40:30.423+00:00",
          "EntryType": "Event",
          "Id": "2",
          "Message": "xyz.openbmc_project.State.Leak.Detector.LeakDetectedWarning",
          "Modified": "2024-05-10T06:40:30.423+00:00",
          "Name": "System Event Log Entry",
          "Resolved": false,
          "Severity": "Warning"
        }
      ],
      "Members@odata.count": 2,
      "Name": "System Event Log Entries"
    }
    
    >> echo pull-down > /sys/devices/platform/gpio-sim.0/gpiochip2/sim_gpio0/pull
    
    >> curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Systems/system/LogServices/EventLog/Entries
    {
      "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries",
      "@odata.type": "#LogEntryCollection.LogEntryCollection",
      "Description": "Collection of System Event Log Entries",
      "Members": [
    	...
        {
          "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/2",
          "@odata.type": "#LogEntry.v1_9_0.LogEntry",
          "AdditionalDataURI": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/2/attachment",
          "Created": "2024-05-10T06:40:30.423+00:00",
          "EntryType": "Event",
          "Id": "2",
          "Message": "xyz.openbmc_project.State.Leak.Detector.LeakDetectedWarning",
          "Modified": "2024-05-10T06:42:23.989+00:00",
          "Name": "System Event Log Entry",
          "Resolved": true,
          "Severity": "Warning"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/3",
          "@odata.type": "#LogEntry.v1_9_0.LogEntry",
          "AdditionalDataURI": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/3/attachment",
          "Created": "2024-05-10T06:42:24.024+00:00",
          "EntryType": "Event",
          "Id": "3",
          "Message": "xyz.openbmc_project.State.Leak.Detector.LeakDetectedNormal",
          "Modified": "2024-05-10T06:42:24.024+00:00",
          "Name": "System Event Log Entry",
          "Resolved": false,
          "Severity": "OK"
        }
      ],
      "Members@odata.count": 3,
      "Name": "System Event Log Entries"
    }
    ```
    
    Change-Id: Id6981deb314ca6852c5e31b932b28e601c2f3976
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

---
+++ src/GPIOInterface.hpp
@@ -0,0 +1,110 @@
+#pragma once
+
+#include <gpiod.hpp>
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/async/context.hpp>
+#include <sdbusplus/async/fdio.hpp>
+#include <sdbusplus/async/task.hpp>
+
+#include <exception>
+#include <memory>
+#include <stdexcept>
+#include <string>
+
+namespace gpio
+{
+
+PHOSPHOR_LOG2_USING;
+
+template <typename Instance>
+class GPIOInterface
+{
+  public:
+    GPIOInterface() = delete;
+
+    GPIOInterface(sdbusplus::async::context& ctx,
+                  const std::string& consumerName, const std::string& pinName,
+                  bool activeLow) :
+        ctx(ctx), consumerName(consumerName), pinName(pinName),
+        activeLow(activeLow)
+    {
+        line = gpiod::find_line(pinName);
+        if (!line)
+        {
+            throw std::runtime_error("Failed to find GPIO line for " + pinName);
+        }
+        try
+        {
+            line.request(
+                {consumerName, gpiod::line_request::EVENT_BOTH_EDGES,
+                 activeLow ? gpiod::line_request::FLAG_ACTIVE_LOW : 0});
+        }
+        catch (std::exception& e)
+        {
+            throw std::runtime_error("Failed to request line for " + pinName +
+                                     " with error " + e.what());
+        }
+
+        int lineFd = line.event_get_fd();
+        if (lineFd < 0)
+        {
+            throw std::runtime_error(
+                "Failed to get event fd for GPIO line " + pinName);
+        }
+
+        fdioInstance = std::make_unique<sdbusplus::async::fdio>(ctx, lineFd);
+        if (!fdioInstance)
+        {
+            throw std::runtime_error(
+                "Failed to create fdio for GPIO line " + pinName);
+        }
+
+        // Start the async read for the GPIO line
+        ctx.spawn(readGPIOAsyncEvent());
+
+        // Read the initial GPIO value
+        ctx.spawn(readGPIOAsync());
+    }
+
+  private:
+    /** @brief Read the gpio state asynchronously */
+    sdbusplus::async::task<> readGPIOAsync()
+    {
+        auto lineValue = line.get_value();
+        if (lineValue < 0)
+        {
+            error("Failed to read GPIO line {LINENAME}", "LINENAME", pinName);
+            co_return;
+        }
+        static_cast<Instance*>(this)->updateGPIOState(
+            lineValue == gpiod::line_event::RISING_EDGE);
+
+        co_return;
+    }
+
+    /** @brief Read the gpio state asynchronously based on gpio event */
+    sdbusplus::async::task<> readGPIOAsyncEvent()
+    {
+        while (!ctx.stop_requested())
+        {
+            // Wait for the fd event for the line to change
+            co_await fdioInstance->next();
+
+            line.event_read();
+            auto lineValue = line.get_value();
+
+            co_await static_cast<Instance*>(this)->updateGPIOStateAsync(
+                lineValue == gpiod::line_event::RISING_EDGE);
+        }
+    }
+
+    sdbusplus::async::context& ctx;
+    const std::string& consumerName;
+    const std::string& pinName;
+    bool activeLow = false;
+    gpiod::line line;
+    /** File descriptor based async event handler */
+    std::unique_ptr<sdbusplus::async::fdio> fdioInstance;
+};
+
+} // namespace gpio
---
+++ src/leakdetector/LeakDetectionManager.cpp
@@ -0,0 +1,155 @@
+#include "LeakDetectionManager.hpp"
+
+#include "LeakGPIODetector.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/async/task.hpp>
+#include <sdbusplus/message/native_types.hpp>
+#include <sdbusplus/server/manager.hpp>
+
+#include <exception>
+#include <memory>
+#include <optional>
+#include <string>
+
+PHOSPHOR_LOG2_USING;
+
+namespace leak
+{
+
+void DetectionManager::processInventoryAdded(
+    const sdbusplus::message::object_path& objectPath,
+    const std::string& /*unused*/)
+{
+    ctx.spawn(processConfigAddedAsync(objectPath));
+}
+
+void DetectionManager::processInventoryRemoved(
+    const sdbusplus::message::object_path& objectPath,
+    const std::string& /*unused*/)
+{
+    if (detectors.find(objectPath.str) == detectors.end())
+    {
+        return;
+    }
+    detectors.erase(objectPath.str);
+    debug("Removed detector {DETECTOR}", "DETECTOR", objectPath.filename());
+}
+
+sdbusplus::async::task<> DetectionManager::startup()
+{
+    ctx.spawn(handleInventoryAdded());
+    ctx.spawn(handleInventoryRemoved());
+    ctx.spawn(handleInventoryGet());
+    co_return;
+}
+
+sdbusplus::async::task<> DetectionManager::processConfigAddedAsync(
+    sdbusplus::message::object_path objectPath)
+{
+    auto res = co_await getDetectorConfig(objectPath);
+    if (!res)
+    {
+        co_return;
+    }
+    auto config = res.value();
+
+    if (detectors.find(objectPath.str) != detectors.end())
+    {
+        warning("Detector {DETECTOR} already exist", "DETECTOR", config.name);
+        co_return;
+    }
+
+    try
+    {
+        detectors[objectPath.str] =
+            std::make_unique<GPIODetector>(ctx, leakEvents, config);
+    }
+    catch (std::exception& e)
+    {
+        error("Failed to create detector {DETECTOR}: {ERROR}", "DETECTOR",
+              config.name, "ERROR", e.what());
+    }
+
+    co_return;
+}
+
+sdbusplus::async::task<std::optional<config::DetectorConfig>>
+    DetectionManager::getDetectorConfig(
+        sdbusplus::message::object_path objectPath)
+{
+    config::DetectorConfig config = {};
+
+    auto client =
+        GPIODetectorConfigIntf(ctx).service(serviceName).path(objectPath.str);
+
+    config.name = co_await client.name();
+
+    auto type = co_await client.sub_type();
+    for (const auto& [key, value] : config::validDetectorTypes)
+    {
+        if (type == key)
+        {
+            config.type = value;
+            break;
+        }
+    }
+
+    config.pinName = co_await client.pin_name();
+
+    auto polarity = co_await client.polarity();
+    for (const auto& [key, value] : config::validPinPolarity)
+    {
+        if (polarity == key)
+        {
+            config.polarity = value;
+            break;
+        }
+    }
+    if (config.polarity == config::PinPolarity::unknown)
+    {
+        error("Invalid polarity {POLARITY} for {NAME}", "POLARITY", polarity,
+              "NAME", config.name);
+        co_return std::nullopt;
+    }
+
+    auto level = co_await client.level();
+    for (const auto& [key, value] : config::validDetectorLevel)
+    {
+        if (level == key)
+        {
+            config.level = value;
+            break;
+        }
+    }
+    if (config.level == config::DetectorLevel::unknown)
+    {
+        error("Invalid level {LEVEL} for {NAME}", "LEVEL", level, "NAME",
+              config.name);
+        co_return std::nullopt;
+    }
+
+    debug("Detector config: {NAME} {PIN_NAME} {POLARITY} {LEVEL}", "NAME",
+          config.name, "PIN_NAME", config.pinName, "POLARITY", config.polarity,
+          "LEVEL", config.level);
+
+    co_return config;
+}
+
+} // namespace leak
+
+int main()
+{
+    constexpr auto path = leak::DetectorIntf::namespace_path::value;
+    constexpr auto serviceName = "xyz.openbmc_project.leakdetector";
+    sdbusplus::async::context ctx;
+    sdbusplus::server::manager_t manager{ctx, path};
+
+    info("Creating leak detection manager at {PATH}", "PATH", path);
+    leak::DetectionManager leakDetectionManager{ctx};
+
+    ctx.request_name(serviceName);
+
+    ctx.run();
+    return 0;
+}
