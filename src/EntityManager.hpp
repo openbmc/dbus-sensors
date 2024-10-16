9d2e9f17738e57fae89791df6ba04e50b1e4c9b2
Jagpal Singh Gill <paligill@gmail.com>
2024-10-16 09:42:54 -0700

    Implement Liquid Leak Detector
    Implement liquid leak detector based on design
    https://gerrit.openbmc.org/c/openbmc/docs/+/73152.
    
    Related PRs:
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/73151
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/73707
    https://gerrit.openbmc.org/c/openbmc/sdbusplus/+/75461
    
    Tested:
    ```
    >> busctl tree xyz.openbmc_project.LeakDetector
    `- /xyz
      `- /xyz/openbmc_project
        `- /xyz/openbmc_project/leak
          `- /xyz/openbmc_project/leak/detector
            |- /xyz/openbmc_project/leak/detector/TrayDetector1
            |- /xyz/openbmc_project/leak/detector/TrayDetector2
            |- /xyz/openbmc_project/leak/detector/TrayDetector3
            `- /xyz/openbmc_project/leak/detector/TrayDetector4
    
    >> busctl introspect xyz.openbmc_project.LeakDetector /xyz/openbmc_project/leak/detector/TrayDetector1
    NAME                                        TYPE      SIGNATURE RESULT/VALUE                             FLAGS
    org.freedesktop.DBus.Introspectable         interface -         -                                        -
    .Introspect                                 method    -         s                                        -
    org.freedesktop.DBus.Peer                   interface -         -                                        -
    .GetMachineId                               method    -         s                                        -
    .Ping                                       method    -         -                                        -
    org.freedesktop.DBus.Properties             interface -         -                                        -
    .Get                                        method    ss        v                                        -
    .GetAll                                     method    s         a{sv}                                    -
    .Set                                        method    ssv       -                                        -
    .PropertiesChanged                          signal    sa{sv}as  -                                        -
    xyz.openbmc_project.Association.Definitions interface -         -                                        -
    .Associations                               property  a(sss)    0                                        emits-change writable
    xyz.openbmc_project.LeakDetection.Detector  interface -         -                                        -
    .PrettyName                                 property  s         "TrayDetector1"                          emits-change
    .State                                      property  b         false                                    emits-change
    .Type                                       property  s         "xyz.openbmc_project.LeakDetection.De... emits-change writable
    
    >> cat /sys/devices/platform/gpio-sim.0/gpiochip2/sim_gpio0/pull
    pull-down
    >> echo pull-up > /sys/devices/platform/gpio-sim.0/gpiochip2/sim_gpio0/pull
    >> busctl get-property xyz.openbmc_project.LeakDetector /xyz/openbmc_project/leak/detector/TrayDetector1 xyz.openbmc_project.LeakDetection.Detector State
    b true
    ```
    
    Change-Id: Id6981deb314ca6852c5e31b932b28e601c2f3976
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

---
+++ src/EntityManager.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "Utils.hpp"
+
+#include <sdbusplus/async.hpp>
+
+namespace phosphor::entity::manager
+{
+namespace RulesIntf = sdbusplus::bus::match::rules;
+
+template <typename Instance>
+class EntityManager
+{
+  public:
+    using interface_list_t = std::set<std::string>;
+
+    EntityManager(sdbusplus::async::context& ctx) : ctx(ctx)
+    {
+        // Setup the D-Bus match rules
+        configAddedMatch = std::make_unique<sdbusplus::async::match>(
+            ctx, RulesIntf::interfacesAdded() + RulesIntf::sender(serviceName));
+
+        configRemovedMatch = std::make_unique<sdbusplus::async::match>(
+            ctx,
+            RulesIntf::interfacesRemoved() + RulesIntf::sender(serviceName));
+    }
+
+    /** Get the initial config from Entity Manager */
+    auto handleConfigGet() -> sdbusplus::async::task<>
+    {
+        constexpr auto entityManager =
+            sdbusplus::async::proxy()
+                .service(serviceName)
+                .path("/xyz/openbmc_project/inventory")
+                .interface("org.freedesktop.DBus.ObjectManager");
+
+        for (const auto& [objectPath, detectorConfig] :
+             co_await entityManager.call<ManagedObjectType>(
+                 ctx, "GetManagedObjects"))
+        {
+            co_await static_cast<Instance*>(this)->processConfigAdded(
+                objectPath, detectorConfig);
+        }
+
+        co_return;
+    }
+
+    /** @brief Handle async config add from Entity Manager */
+    auto handleConfigAdded() -> sdbusplus::async::task<>
+    {
+        while (!ctx.stop_requested())
+        {
+            auto [objectPath, detectorConfig] =
+                co_await configAddedMatch
+                    ->next<sdbusplus::message::object_path, SensorData>();
+
+            co_await static_cast<Instance*>(this)->processConfigAdded(
+                objectPath, detectorConfig);
+        }
+
+        co_return;
+    }
+
+    /** @brief Handle async config remove from Entity Manager */
+    auto handleConfigRemoved() -> sdbusplus::async::task<>
+    {
+        while (!ctx.stop_requested())
+        {
+            auto [objectPath, interfaces] =
+                co_await configRemovedMatch
+                    ->next<sdbusplus::message::object_path, interface_list_t>();
+
+            co_await static_cast<Instance*>(this)->processConfigRemoved(
+                objectPath, interfaces);
+        }
+
+        co_return;
+    }
+
+  private:
+    static constexpr auto serviceName = "xyz.openbmc_project.EntityManager";
+
+    /** @brief D-Bus context */
+    sdbusplus::async::context& ctx;
+    /** @brief sdbusplus::bus::match_t for config added */
+    std::unique_ptr<sdbusplus::async::match> configAddedMatch;
+    /** @brief sdbusplus::bus::match_t for config removed */
+    std::unique_ptr<sdbusplus::async::match> configRemovedMatch;
+};
+
+} // namespace phosphor::entity::manager
