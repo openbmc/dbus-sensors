385e3b88d06f5c300c233139cbe3bcdc30c3fba9
Jagpal Singh Gill <paligill@gmail.com>
2024-10-16 09:42:54 -0700

    Implement Liquid Leak Detector
    Implement liquid leak detector based on design
    https://gerrit.openbmc.org/c/openbmc/docs/+/73152.
    
    Related PRs:
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/73151
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/73707
    https://gerrit.openbmc.org/c/openbmc/sdbusplus/+/75461
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/75999
    
    Tested:
    ```
    >> busctl tree xyz.openbmc_project.LeakDetector
    └─ /xyz
      └─ /xyz/openbmc_project
        └─ /xyz/openbmc_project/state
          └─ /xyz/openbmc_project/state/leak
            └─ /xyz/openbmc_project/state/leak/detector
              ├─ /xyz/openbmc_project/state/leak/detector/TrayDetector1
              └─ /xyz/openbmc_project/state/leak/detector/TrayDetector2
    
    >> echo pull-up > /sys/devices/platform/gpio-sim.0/gpiochip2/sim_gpio0/pull
    
    >> curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Systems/system/LogServices/EventLog/Entries
    {
      "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries",
      "@odata.type": "#LogEntryCollection.LogEntryCollection",
      "Description": "Collection of System Event Log Entries",
      "Members": [
      	...
        {
          "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/2",
          "@odata.type": "#LogEntry.v1_9_0.LogEntry",
          "AdditionalDataURI": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/2/attachment",
          "Created": "2024-05-10T06:40:30.423+00:00",
          "EntryType": "Event",
          "Id": "2",
          "Message": "xyz.openbmc_project.State.Leak.Detector.LeakDetectedWarning",
          "Modified": "2024-05-10T06:40:30.423+00:00",
          "Name": "System Event Log Entry",
          "Resolved": false,
          "Severity": "Warning"
        }
      ],
      "Members@odata.count": 2,
      "Name": "System Event Log Entries"
    }
    
    >> echo pull-down > /sys/devices/platform/gpio-sim.0/gpiochip2/sim_gpio0/pull
    
    >> curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Systems/system/LogServices/EventLog/Entries
    {
      "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries",
      "@odata.type": "#LogEntryCollection.LogEntryCollection",
      "Description": "Collection of System Event Log Entries",
      "Members": [
    	...
        {
          "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/2",
          "@odata.type": "#LogEntry.v1_9_0.LogEntry",
          "AdditionalDataURI": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/2/attachment",
          "Created": "2024-05-10T06:40:30.423+00:00",
          "EntryType": "Event",
          "Id": "2",
          "Message": "xyz.openbmc_project.State.Leak.Detector.LeakDetectedWarning",
          "Modified": "2024-05-10T06:42:23.989+00:00",
          "Name": "System Event Log Entry",
          "Resolved": true,
          "Severity": "Warning"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/3",
          "@odata.type": "#LogEntry.v1_9_0.LogEntry",
          "AdditionalDataURI": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/3/attachment",
          "Created": "2024-05-10T06:42:24.024+00:00",
          "EntryType": "Event",
          "Id": "3",
          "Message": "xyz.openbmc_project.State.Leak.Detector.LeakDetectedNormal",
          "Modified": "2024-05-10T06:42:24.024+00:00",
          "Name": "System Event Log Entry",
          "Resolved": false,
          "Severity": "OK"
        }
      ],
      "Members@odata.count": 3,
      "Name": "System Event Log Entries"
    }
    ```
    
    Change-Id: Id6981deb314ca6852c5e31b932b28e601c2f3976
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

---
+++ src/EntityManager.hpp
@@ -0,0 +1,118 @@
+#pragma once
+
+#include "Utils.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/async/context.hpp>
+#include <sdbusplus/async/match.hpp>
+#include <sdbusplus/async/proxy.hpp>
+#include <sdbusplus/async/task.hpp>
+#include <sdbusplus/bus/match.hpp>
+#include <sdbusplus/message/native_types.hpp>
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
+namespace entity_manager
+{
+PHOSPHOR_LOG2_USING;
+namespace RulesIntf = sdbusplus::bus::match::rules;
+
+template <typename Instance>
+class EntityManager
+{
+  public:
+    using interface_list_t = std::vector<std::string>;
+    static constexpr auto serviceName = "xyz.openbmc_project.EntityManager";
+
+    EntityManager() = delete;
+
+    explicit EntityManager(sdbusplus::async::context& ctx,
+                           const std::vector<std::string>& interfaceNames) :
+        ctx(ctx), interfaceNames(interfaceNames),
+        inventoryAddedMatch(ctx, RulesIntf::interfacesAdded() +
+                                     RulesIntf::sender(serviceName)),
+        inventoryRemovedMatch(ctx, RulesIntf::interfacesRemoved() +
+                                       RulesIntf::sender(serviceName))
+    {}
+
+    /** Get the inventory info from Entity Manager */
+    sdbusplus::async::task<> handleInventoryGet()
+    {
+        constexpr auto entityManager =
+            sdbusplus::async::proxy()
+                .service(serviceName)
+                .path("/xyz/openbmc_project/inventory")
+                .interface("org.freedesktop.DBus.ObjectManager");
+
+        for (const auto& [objectPath, detectorConfig] :
+             co_await entityManager.call<ManagedObjectType>(
+                 ctx, "GetManagedObjects"))
+        {
+            for (const auto& interfaceName : interfaceNames)
+            {
+                if (detectorConfig.find(interfaceName) != detectorConfig.end())
+                {
+                    static_cast<Instance*>(this)->processInventoryAdded(
+                        objectPath, interfaceName);
+                }
+            }
+        }
+
+        co_return;
+    }
+
+    /** @brief Handle async inventory add from Entity Manager */
+    sdbusplus::async::task<> handleInventoryAdded()
+    {
+        while (!ctx.stop_requested())
+        {
+            auto [objectPath, inventoryData] =
+                co_await inventoryAddedMatch
+                    .next<sdbusplus::message::object_path, SensorData>();
+
+            for (const auto& interfaceName : interfaceNames)
+            {
+                if (inventoryData.find(interfaceName) != inventoryData.end())
+                {
+                    static_cast<Instance*>(this)->processInventoryAdded(
+                        objectPath, interfaceName);
+                }
+            }
+        }
+
+        co_return;
+    }
+
+    /** @brief Handle async inventory remove from Entity Manager */
+    sdbusplus::async::task<> handleInventoryRemoved()
+    {
+        while (!ctx.stop_requested())
+        {
+            auto [objectPath, interfaces] =
+                co_await inventoryRemovedMatch
+                    .next<sdbusplus::message::object_path, interface_list_t>();
+
+            for (const auto& interfaceName : interfaceNames)
+            {
+                if (std::find(interfaces.begin(), interfaces.end(),
+                              interfaceName) != interfaces.end())
+                {
+                    static_cast<Instance*>(this)->processInventoryRemoved(
+                        objectPath, interfaceName);
+                }
+            }
+        }
+
+        co_return;
+    }
+
+  private:
+    sdbusplus::async::context& ctx;
+    std::vector<std::string> interfaceNames;
+    sdbusplus::async::match inventoryAddedMatch;
+    sdbusplus::async::match inventoryRemovedMatch;
+};
+
+} // namespace entity_manager
---
+++ src/GPIOInterface.cpp
@@ -0,0 +1,102 @@
+#include "GPIOInterface.hpp"
+
+#include <gpiod.hpp>
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/async/context.hpp>
+#include <sdbusplus/async/fdio.hpp>
+#include <sdbusplus/async/task.hpp>
+
+#include <exception>
+#include <memory>
+#include <stdexcept>
+#include <string>
+
+namespace gpio
+{
+
+PHOSPHOR_LOG2_USING;
+
+GPIOInterface::GPIOInterface(sdbusplus::async::context& ctx,
+                             const std::string& consumerName,
+                             const std::string& pinName, bool activeLow,
+                             UpdateStateCallback_t updateGPIOStateCallback) :
+    ctx(ctx), pinName(pinName), updateGPIOStateCallback(updateGPIOStateCallback)
+{
+    line = gpiod::find_line(pinName);
+    if (!line)
+    {
+        throw std::runtime_error("Failed to find GPIO line for " + pinName);
+    }
+    try
+    {
+        line.request({consumerName, gpiod::line_request::EVENT_BOTH_EDGES,
+                      activeLow ? gpiod::line_request::FLAG_ACTIVE_LOW : 0});
+    }
+    catch (std::exception& e)
+    {
+        throw std::runtime_error("Failed to request line for " + pinName +
+                                 " with error " + e.what());
+    }
+
+    int lineFd = line.event_get_fd();
+    if (lineFd < 0)
+    {
+        throw std::runtime_error(
+            "Failed to get event fd for GPIO line " + pinName);
+    }
+
+    fdioInstance = std::make_unique<sdbusplus::async::fdio>(ctx, lineFd);
+    if (!fdioInstance)
+    {
+        throw std::runtime_error(
+            "Failed to create fdio for GPIO line " + pinName);
+    }
+}
+
+sdbusplus::async::task<> GPIOInterface::start()
+{
+    // Start the async read for the GPIO line
+    ctx.spawn(readGPIOAsyncEvent());
+
+    // Read the initial GPIO value
+    co_return co_await readGPIOAsync();
+}
+
+sdbusplus::async::task<> GPIOInterface::readGPIOAsync()
+{
+    auto lineValue = line.get_value();
+    if (lineValue < 0)
+    {
+        error("Failed to read GPIO line {LINENAME}", "LINENAME", pinName);
+        co_return;
+    }
+    if (updateGPIOStateCallback)
+    {
+        co_await updateGPIOStateCallback(
+            lineValue == gpiod::line_event::RISING_EDGE);
+    }
+
+    co_return;
+}
+
+sdbusplus::async::task<> GPIOInterface::readGPIOAsyncEvent()
+{
+    while (!ctx.stop_requested())
+    {
+        // Wait for the fd event for the line to change
+        co_await fdioInstance->next();
+
+        line.event_read();
+        auto lineValue = line.get_value();
+
+        if (updateGPIOStateCallback)
+        {
+            co_await updateGPIOStateCallback(
+                lineValue == gpiod::line_event::RISING_EDGE);
+        }
+    }
+
+    co_return;
+}
+
+} // namespace gpio
