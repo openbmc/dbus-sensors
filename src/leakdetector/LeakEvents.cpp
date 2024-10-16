3573c40bdc3ea27148ef1703c2a3a1af96f0437d
Jagpal Singh Gill <paligill@gmail.com>
2024-10-16 09:42:54 -0700

    Implement Liquid Leak Detector
    Implement liquid leak detector based on design
    https://gerrit.openbmc.org/c/openbmc/docs/+/73152.
    
    Related PRs:
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/73151
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/73707
    https://gerrit.openbmc.org/c/openbmc/sdbusplus/+/75461
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/75999
    
    Tested:
    ```
    >> busctl tree xyz.openbmc_project.LeakDetector
    └─ /xyz
      └─ /xyz/openbmc_project
        └─ /xyz/openbmc_project/state
          └─ /xyz/openbmc_project/state/leak
            └─ /xyz/openbmc_project/state/leak/detector
              ├─ /xyz/openbmc_project/state/leak/detector/TrayDetector1
              └─ /xyz/openbmc_project/state/leak/detector/TrayDetector2
    
    >> echo pull-up > /sys/devices/platform/gpio-sim.0/gpiochip2/sim_gpio0/pull
    
    >> curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Systems/system/LogServices/EventLog/Entries
    {
      "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries",
      "@odata.type": "#LogEntryCollection.LogEntryCollection",
      "Description": "Collection of System Event Log Entries",
      "Members": [
      	...
        {
          "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/2",
          "@odata.type": "#LogEntry.v1_9_0.LogEntry",
          "AdditionalDataURI": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/2/attachment",
          "Created": "2024-05-10T06:40:30.423+00:00",
          "EntryType": "Event",
          "Id": "2",
          "Message": "xyz.openbmc_project.State.Leak.Detector.LeakDetectedWarning",
          "Modified": "2024-05-10T06:40:30.423+00:00",
          "Name": "System Event Log Entry",
          "Resolved": false,
          "Severity": "Warning"
        }
      ],
      "Members@odata.count": 2,
      "Name": "System Event Log Entries"
    }
    
    >> echo pull-down > /sys/devices/platform/gpio-sim.0/gpiochip2/sim_gpio0/pull
    
    >> curl -k -H "X-Auth-Token: $token" -X GET https://${bmc}/redfish/v1/Systems/system/LogServices/EventLog/Entries
    {
      "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries",
      "@odata.type": "#LogEntryCollection.LogEntryCollection",
      "Description": "Collection of System Event Log Entries",
      "Members": [
    	...
        {
          "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/2",
          "@odata.type": "#LogEntry.v1_9_0.LogEntry",
          "AdditionalDataURI": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/2/attachment",
          "Created": "2024-05-10T06:40:30.423+00:00",
          "EntryType": "Event",
          "Id": "2",
          "Message": "xyz.openbmc_project.State.Leak.Detector.LeakDetectedWarning",
          "Modified": "2024-05-10T06:42:23.989+00:00",
          "Name": "System Event Log Entry",
          "Resolved": true,
          "Severity": "Warning"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/3",
          "@odata.type": "#LogEntry.v1_9_0.LogEntry",
          "AdditionalDataURI": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/3/attachment",
          "Created": "2024-05-10T06:42:24.024+00:00",
          "EntryType": "Event",
          "Id": "3",
          "Message": "xyz.openbmc_project.State.Leak.Detector.LeakDetectedNormal",
          "Modified": "2024-05-10T06:42:24.024+00:00",
          "Name": "System Event Log Entry",
          "Resolved": false,
          "Severity": "OK"
        }
      ],
      "Members@odata.count": 3,
      "Name": "System Event Log Entries"
    }
    ```
    
    Change-Id: Id6981deb314ca6852c5e31b932b28e601c2f3976
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

---
+++ src/leakdetector/LeakEvents.cpp
@@ -0,0 +1,79 @@
+#include "LeakEvents.hpp"
+
+#include "LeakGPIODetector.hpp"
+
+#include <phosphor-logging/commit.hpp>
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/async/task.hpp>
+#include <sdbusplus/message/native_types.hpp>
+#include <xyz/openbmc_project/Logging/Entry/client.hpp>
+#include <xyz/openbmc_project/State/Leak/Detector/event.hpp>
+
+#include <format>
+
+PHOSPHOR_LOG2_USING;
+
+namespace leak
+{
+
+// NOLINTNEXTLINE(readability-static-accessed-through-instance)
+sdbusplus::async::task<> Events::generateLeakEvent(
+    sdbusplus::message::object_path detectorPath,
+    DetectorStateIntf::DetectorState state, config::DetectorLevel level)
+{
+    auto eventName =
+        std::format("{}_{}", detectorPath.str, static_cast<int>(level));
+
+    if (state == DetectorStateIntf::DetectorState::Normal)
+    {
+        auto pendingEvent = pendingEvents.find(eventName);
+        if (pendingEvent != pendingEvents.end())
+        {
+            co_await resolveLeakEvent(pendingEvent->second);
+
+            using DetectorNormal = sdbusplus::event::xyz::openbmc_project::
+                state::leak::Detector::LeakDetectedNormal;
+            co_await lg2::commit(ctx,
+                                 DetectorNormal("DETECTOR_NAME", detectorPath));
+
+            pendingEvents.erase(eventName);
+        }
+    }
+    else
+    {
+        namespace ErrorIntf =
+            sdbusplus::error::xyz::openbmc_project::state::leak::Detector;
+        sdbusplus::message::object_path eventPath{};
+        if (level == config::DetectorLevel::critical)
+        {
+            eventPath = co_await lg2::commit(
+                ctx,
+                ErrorIntf::LeakDetectedCritical("DETECTOR_NAME", detectorPath));
+            error("Critical leak detected for {PATH}", "PATH", detectorPath);
+        }
+        else
+        {
+            eventPath = co_await lg2::commit(
+                ctx,
+                ErrorIntf::LeakDetectedWarning("DETECTOR_NAME", detectorPath));
+            warning("Warning leak detected for {PATH}", "PATH", detectorPath);
+        }
+        pendingEvents[eventName] = eventPath;
+    }
+    co_return;
+}
+
+// NOLINTNEXTLINE(readability-static-accessed-through-instance)
+sdbusplus::async::task<> Events::resolveLeakEvent(
+    sdbusplus::message::object_path eventPath)
+{
+    using LoggingEntry =
+        sdbusplus::client::xyz::openbmc_project::logging::Entry<>;
+
+    auto entryClient = LoggingEntry(ctx)
+                           .service(LoggingEntry::default_service)
+                           .path(eventPath.str);
+    co_await entryClient.resolved(true);
+}
+
+} // namespace leak
