23594f9c8d643c3002c434c69c413b4390a455ac
Jagpal Singh Gill <paligill@gmail.com>
2024-10-16 09:42:54 -0700

    Implement Liquid Leak Detector
    Implement liquid leak detector based on design
    https://gerrit.openbmc.org/c/openbmc/docs/+/73152.
    
    Related PRs:
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/73151
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/73707
    https://gerrit.openbmc.org/c/openbmc/sdbusplus/+/75461
    
    Tested: WIP
    
    Change-Id: Id6981deb314ca6852c5e31b932b28e601c2f3976
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

---
+++ src/Systemd.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <sdbusplus/async.hpp>
+
+namespace phosphor::systemd
+{
+
+class Systemd
+{
+  public:
+    Systemd() = delete;
+    explicit Systemd(sdbusplus::bus_t& bus) : bus(bus) {}
+
+    /** @brief Start a systemd unit.
+     * Returns true on success, otherwise false.
+     */
+    auto startUnit(const std::string& sysdUnit) -> bool;
+
+  private:
+    sdbusplus::bus_t& bus;
+};
+
+} // namespace phosphor::systemd
