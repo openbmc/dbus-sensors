deab93a25fe3b0d3c44d256ad4b915c672c12498
Jagpal Singh Gill <paligill@gmail.com>
2024-10-16 09:42:54 -0700

    Implement Liquid Leak Detector
    Implement liquid leak detector based on design
    https://gerrit.openbmc.org/c/openbmc/docs/+/73152.
    
    Related PRs:
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/73151
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/73707
    https://gerrit.openbmc.org/c/openbmc/sdbusplus/+/75461
    
    Tested: WIP
    
    Change-Id: Id6981deb314ca6852c5e31b932b28e601c2f3976
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

---
+++ src/LeakDetection/LeakDetector.hpp
@@ -0,0 +1,123 @@
+#pragma once
+
+#include "GPIOInterface.hpp"
+#include "LeakEvents.hpp"
+#include "Systemd.hpp"
+#include "Utils.hpp"
+
+// #include <gpiod.hpp>
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/async.hpp>
+#include <sdbusplus/async/server.hpp>
+#include <xyz/openbmc_project/Association/Definitions/aserver.hpp>
+#include <xyz/openbmc_project/Configuration/Common/client.hpp>
+#include <xyz/openbmc_project/Configuration/GPIOLeakDetector/client.hpp>
+#include <xyz/openbmc_project/State/Leak/Detector/aserver.hpp>
+
+PHOSPHOR_LOG2_USING;
+
+constexpr auto dbusService = "xyz.openbmc_project.LeakDetector";
+
+using LeakEventsIntf = phosphor::leak::events::LeakEvents;
+using SystemdIntf = phosphor::systemd::Systemd;
+
+namespace phosphor::leak::detector
+{
+
+class LeakDetector;
+
+using DetectorConfigIntf =
+    sdbusplus::client::xyz::openbmc_project::configuration::GPIOLeakDetector<>;
+
+using CommonConfigIntf =
+    sdbusplus::client::xyz::openbmc_project::configuration::Common<>;
+
+using DetectorIntf = sdbusplus::async::server_t<
+    LeakDetector,
+    sdbusplus::aserver::xyz::openbmc_project::association::Definitions,
+    sdbusplus::aserver::xyz::openbmc_project::state::leak::Detector>;
+
+using GPIOIntf = phosphor::gpio::GPIOInterface<LeakDetector>;
+
+namespace config
+{
+
+/** @brief Leak detector configuration */
+struct DetectorConfig
+{
+    /** @brief Detector name */
+    std::string name = defaults::name;
+    /** @brief Type */
+    DetectorIntf::DetectorType type = DetectorIntf::DetectorType::Unknown;
+    /** @brief Pin name */
+    std::string pinName = defaults::pinName;
+    /** @brief Polarity */
+    CommonConfigIntf::PinPolarity polarity =
+        CommonConfigIntf::PinPolarity::ActiveLow;
+    /** @brief Level */
+    DetectorConfigIntf::DetectorLevel level =
+        DetectorConfigIntf::DetectorLevel::Critical;
+    /** @brief Default Action */
+    DetectorConfigIntf::DetectorDefaultAction action =
+        DetectorConfigIntf::DetectorDefaultAction::Unknown;
+
+    /** @brief Default values */
+    struct defaults
+    {
+        static constexpr auto name = "unknown";
+        static constexpr auto pinName = "unknown";
+    };
+};
+
+/** @brief Get the detector configuration from the Entity Manager
+ */
+auto processConfig(sdbusplus::async::context& ctx,
+                   const sdbusplus::message::object_path& objectPath)
+    -> sdbusplus::async::task<DetectorConfig>;
+
+}; // namespace config
+
+class LeakDetector : public DetectorIntf, public GPIOIntf
+{
+  public:
+    explicit LeakDetector(sdbusplus::async::context& ctx,
+                          LeakEventsIntf& leakEvents, SystemdIntf& systemd,
+                          const config::DetectorConfig& config) :
+        DetectorIntf(ctx, getObjectPath(config.name).str.c_str()),
+        GPIOIntf(ctx, dbusService, config.pinName), ctx(ctx),
+        leakEvents(leakEvents), systemd(systemd), config(config)
+    {
+        try
+        {
+            createLeakDetector();
+        }
+        catch (const std::exception& e)
+        {
+            throw;
+        }
+    }
+
+    /** @brief Update the GPIO state */
+    auto updateGPIOState(bool gpioState) -> void;
+
+  private:
+    /** @brief Create the leak detector */
+    auto createLeakDetector() -> void;
+    /**  @brief Get the detector object path for the given detector name */
+    auto getObjectPath(std::string detectorName)
+        -> sdbusplus::message::object_path;
+    /** @brief Update the leak detector state */
+    auto updateState(DetectorIntf::DetectorState newState)
+        -> sdbusplus::async::task<>;
+
+    /** @brief D-Bus context */
+    sdbusplus::async::context& ctx;
+    /** @brief Leak events instance */
+    LeakEventsIntf& leakEvents;
+    /** @brief Systemd instance */
+    SystemdIntf& systemd;
+    /** @brief Detector configuration  */
+    config::DetectorConfig config;
+};
+
+} // namespace phosphor::leak::detector
