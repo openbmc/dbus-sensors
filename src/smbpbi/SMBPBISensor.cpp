cb11758e95ff5337a9923c1dd65cf486c580bcf8
Peter Delevoryas <peter@pjd.dev>
2024-11-26 20:23:12 -0800

    dbus-sensors: Add SMBus Post-Box Interface (SMBPBI) sensor skeleton
    This sensor will expose the NVIDIA GB200 HMC i2c sensors that are
    implemented in NVIDIA's downstream fork [here][1].
    
    There's quite a few other things to setup after this:
    
    - A configuration specification in `entity-manager`
    - An interface in `phosphor-dbus-interfaces`?
    - The actual implementation of sensor reading
    - Some unit tests
    
    ```
    $ meson setup build -Dsmbpbi=enabled
    $ meson compile -C build
    $ ./build/src/smbpbi/smbpbisensor
    ```
    ```
    $ busctl list | rg smb
    :1.131                           96518 smbpbisensor root :1.131        -    -       -
    xyz.openbmc_project.SMBPBISensor 96518 smbpbisensor root :1.131        -    -       -
    ```
    
    [1] https://github.com/NVIDIA/dbus-sensors/blob/develop/src/SatelliteSensor.cpp
    
    Change-Id: I1d541dc55386b1cfc2b26a4989982dc77723332b
    Signed-off-by: Peter Delevoryas <peter@pjd.dev>

---
+++ src/smbpbi/SMBPBISensor.cpp
@@ -0,0 +1,16 @@
+/// SMBPBI: SMBus Post-Box Interface. It's a "virtual EEPROM" I2C endpoint.
+/// NVIDIA uses this on their HMCs to give low-er latency access to important
+/// sensors, to avoid the whole TCP/IP-over-USB stack when using Redfish.
+
+#include <sdbusplus/async/context.hpp>
+#include <sdbusplus/server/manager.hpp>
+
+int main()
+{
+    sdbusplus::async::context ctx;
+    sdbusplus::server::manager_t manager{ctx,
+                                         "xyz/openbmc_project/smbpbisensor"};
+    ctx.request_name("xyz.openbmc_project.SMBPBISensor");
+    ctx.run();
+    return 0;
+}
