b316aaadc35ac24b26285896bdbcc3dc7d7c8e1b
Chris Sides <Christopher.Sides@hpe.com>
2025-02-05 15:18:45 -0600

    DeviceMgmt: unbind drivers on i2cdevice::destroy
    I2CDevice::destroy now checks for drivers to unbind in the event
    that an i2c 'delete_device' write fails.
    
    Writes to delete_device are expected to fail for devices added
    to a bus by a driver from outside of userspace.
    
    TESTED:
    
    Confirmed on a live system that HwmonTempSensor daemon is
    able to unbind sensors originally bound by the daemon
    when I2CDevice::destroy() is called on
    devices that cannot be removed via delete_device in sysfs
    
    Paths on failed delete_device write:
    1. no driver unbind needed (occurs when driver probe failed)
    2. driver unbind successful
    3. driver unbind failed (no instances of this in testing)
    
    Adding debug outputs to stderr produces output showing
    each path behaves as expected:
    
    '''
    The 'Exited' outputs indicate a successful unbind call
    
    Feb 05 22:49:40 sanjac-b0 hwmontempsensor[521]: CTS: about to flush delete_device call @ "/sys/bus/i2c/devices/i2c-50/delete_device"
    Feb 05 22:49:40 sanjac-b0 hwmontempsensor[521]: CTS:Nothing to unbind @ "/sys/bus/i2c/devices/i2c-50/50-0049/driver/unbind" with: 50-0049
    Feb 05 22:49:57 sanjac-b0 hwmontempsensor[521]: CTS: about to flush delete_device call @ "/sys/bus/i2c/devices/i2c-50/delete_device"
    Feb 05 22:49:57 sanjac-b0 hwmontempsensor[521]: CTS:Nothing to unbind @ "/sys/bus/i2c/devices/i2c-50/50-0049/driver/unbind" with: 50-0049
    Feb 05 22:50:35 sanjac-b0 hwmontempsensor[521]: CTS: about to flush delete_device call @ "/sys/bus/i2c/devices/i2c-50/delete_device"
    Feb 05 22:50:35 sanjac-b0 hwmontempsensor[521]: CTS: Exited destroy success for @"/sys/bus/i2c/devices/i2c-50/50-004c/driver/unbind" with: 50-004c
    Feb 05 22:50:35 sanjac-b0 hwmontempsensor[521]: CTS: about to flush delete_device call @ "/sys/bus/i2c/devices/i2c-49/delete_device"
    Feb 05 22:50:35 sanjac-b0 hwmontempsensor[521]: CTS: Exited destroy success for @"/sys/bus/i2c/devices/i2c-49/49-004c/driver/unbind" with: 49-004c
    Feb 05 22:50:35 sanjac-b0 hwmontempsensor[521]: CTS: about to flush delete_device call @ "/sys/bus/i2c/devices/i2c-50/delete_device"
    Feb 05 22:50:35 sanjac-b0 hwmontempsensor[521]: CTS: Exited destroy success for @"/sys/bus/i2c/devices/i2c-50/50-0048/driver/unbind" with: 50-0048
    '''
    
    Change-Id: Icb6931d1ac8204cc5c8c54c044c347dc24cfdf26
    Signed-off-by: Chris Sides <Christopher.Sides@hpe.com>

--- src/DeviceMgmt.cpp
+++ src/DeviceMgmt.cpp
@@ -1,7 +1,9 @@
+
 #include "DeviceMgmt.hpp"
 
 #include "Utils.hpp"
 
+#include <algorithm>
 #include <cstdint>
 #include <filesystem>
 #include <fstream>
@@ -14,44 +16,36 @@
 #include <string>
 #include <system_error>
 #include <variant>
-
+#include <vector>
 namespace fs = std::filesystem;
-
 std::optional<I2CDeviceParams> getI2CDeviceParams(
     const I2CDeviceTypeMap& dtmap, const SensorBaseConfigMap& cfg)
 {
     auto findType = cfg.find("Type");
     auto findBus = cfg.find("Bus");
     auto findAddr = cfg.find("Address");
-
     if (findType == cfg.end() || findBus == cfg.end() || findAddr == cfg.end())
     {
         return std::nullopt;
     }
-
     const std::string* type = std::get_if<std::string>(&findType->second);
     const uint64_t* bus = std::get_if<uint64_t>(&findBus->second);
     const uint64_t* addr = std::get_if<uint64_t>(&findAddr->second);
-
     if (type == nullptr || bus == nullptr || addr == nullptr)
     {
         return std::nullopt;
     }
-
     auto findDevType = dtmap.find(type->c_str());
     if (findDevType == dtmap.end())
     {
         return std::nullopt;
     }
-
     return I2CDeviceParams(findDevType->second, *bus, *addr);
 }
-
 static fs::path i2cBusPath(uint64_t bus)
 {
     return {"/sys/bus/i2c/devices/i2c-" + std::to_string(bus)};
 }
-
 static std::string deviceDirName(uint64_t bus, uint64_t address)
 {
     std::ostringstream name;
@@ -59,46 +53,43 @@ static std::string deviceDirName(uint64_t bus, uint64_t address)
          << address;
     return name.str();
 }
-
 // check if device has already been added to bus
 bool I2CDeviceParams::devicePresent() const
 {
     fs::path path = i2cBusPath(bus) / deviceDirName(bus, address);
-
     // Ignore errors; anything but a clean 'true' is fine as 'false'
     std::error_code ec;
     return fs::exists(path, ec);
 }
-
 // check if bindable-devices have been bound
 bool I2CDeviceParams::deviceReady() const
 {
     fs::path path = i2cBusPath(bus) / deviceDirName(bus, address);
-
     if (type->createsHWMon)
     {
         path /= "hwmon";
     }
-
     // Ignore errors; anything but a clean 'true' is fine as 'false'
     std::error_code ec;
     return fs::exists(path, ec);
 }
-
 bool I2CDeviceParams::deviceStatic() const
 {
     if (!deviceReady())
     {
         return false;
     }
-
     fs::path ofNode = i2cBusPath(bus) / deviceDirName(bus, address) / "of_node";
-
     // Ignore errors -- if of_node is present the device is a static DT node;
     // otherwise we can assume we created it from userspace.
     std::error_code ec;
     return fs::exists(ofNode, ec);
 }
+
+// paths for devices added to a bus from outside userspace,
+// but then had driver bindings set from userspace by this
+// daemon
+std::vector<std::filesystem::path> deviceDriverBindings;
 
 I2CDevice::I2CDevice(I2CDeviceParams params) : params(params)
 {
@@ -107,12 +98,10 @@ I2CDevice::I2CDevice(I2CDeviceParams params) : params(params)
         throw std::runtime_error("failed to instantiate i2c device");
     }
 }
-
 I2CDevice::~I2CDevice()
 {
     destroy();
 }
-
 int I2CDevice::create() const
 {
     // If it's already instantiated, there's nothing we need to do.
@@ -123,29 +112,29 @@ int I2CDevice::create() const
 
     // If device is already added to the bus, but an expected driver binding
     // hasn't happened yet
-    else if (params.devicePresent()) // if device is 'present' (handle exists on
+    if (params.devicePresent()) // if device is 'present' (handle exists on
                                 // bus) but not 'ready' (missing an
                                 // expected hwmon node)
     {
         fs::path ctorPath = "/sys/bus/i2c/drivers_probe";
         std::ofstream ctor(ctorPath);
-
         if (!ctor.good())
         {
             std::cerr << "Failed to open " << ctorPath << "\n";
             return -1;
         }
-
         ctor << deviceDirName(params.bus, params.address) << "\n";
-
         ctor.flush();
         if (!ctor.good())
         {
             std::cerr << "Failed to write to " << ctorPath << "\n";
             return -1;
         }
-    }
 
+        // keep track of drivers we needed to handle bindings for
+        deviceDriverBindings.push_back(
+            i2cBusPath(params.bus) / deviceDirName(params.bus, params.address));
+    }
     else // if device does not exist at all
     {
         // Try to create it: 'echo $devtype $addr > .../i2c-$bus/new_device'
@@ -156,7 +145,6 @@ int I2CDevice::create() const
             std::cerr << "Failed to open " << ctorPath << "\n";
             return -1;
         }
-
         ctor << params.type->name << " " << params.address << "\n";
         ctor.flush();
         if (!ctor.good())
@@ -165,24 +153,20 @@ int I2CDevice::create() const
             return -1;
         }
     }
-
     // Check if that created the requisite sysfs directory
     if (!params.deviceReady())
     {
         destroy();
         return -1;
     }
-
     return 0;
 }
-
 int I2CDevice::destroy() const
 {
     // No params.deviceReady() check on this like in create(), since it
     // might be used to clean up after a device instantiation that was only
     // partially successful (i.e. when params.deviceReady() would return
     // false but there's still a dummy i2c client device to remove)
-
     fs::path dtorPath = i2cBusPath(params.bus) / "delete_device";
     std::ofstream dtor(dtorPath);
     if (!dtor.good())
@@ -190,15 +174,44 @@ int I2CDevice::destroy() const
         std::cerr << "Failed to open " << dtorPath << "\n";
         return -1;
     }
-
     dtor << params.address << "\n";
     dtor.flush();
+
     if (!dtor.good())
     {
-        // Writes to delete_device will fail for devices added to bus by a
-        // driver instead a daemon
-        std::cerr << "Failed to write to " << dtorPath << "\n";
-        return -1;
+        // Writing to delete_device will fail for devices added to bus from
+        // outside userspace In that case, try to unbind the driver if - but
+        // only if  we set the binding for it in the first place
+
+        fs::path basePath =
+            i2cBusPath(params.bus) / deviceDirName(params.bus, params.address);
+
+        auto bindIt = std::find(deviceDriverBindings.begin(),
+                                deviceDriverBindings.end(), basePath);
+
+        if (bindIt != deviceDriverBindings.end())
+        {
+            fs::path unbindPath = basePath / "driver/unbind";
+            std::ofstream unbind(unbindPath);
+            if (!unbind.good())
+            {
+                // indicates there is no bound driver to unbind
+                // something else must have unbound it, so
+                // take it off the list of managed paths and return
+                deviceDriverBindings.erase(bindIt);
+
+                return 0;
+            }
+
+            dtor << deviceDirName(params.bus, params.address) << "\n";
+            unbind.flush();
+            if (!unbind.good())
+            {
+                return -1;
+            }
+
+            deviceDriverBindings.erase(bindIt);
+        }
     }
 
     return 0;
