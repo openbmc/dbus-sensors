20d16d3dfa4c46162f3e107ad1e3b6d1ed476797
MandyMCHung <mandy.hung.wiwynn@gmail.com>
2025-03-03 10:39:31 +0800

    Avoid recreating hwmon temp when blade cycle
    Problem:
    AC/DC cycles cause all hwmon temperature sensors to become unavailable.
    
    Cause:
    During AC/DC cycles on YV4 (8-host system), the hwmon temp sensor
    service detects power changes and improperly creates duplicate
    chassis-level sensors. These sensors should remain static and
    not disappear.
    
    Impact:
    The creation of duplicate sensors causes the hwmon service to
    crash and restart, leading to temporary sensor unavailability
    while the service resets and retrieves new readings.
    
    Change-Id: If2e022cf0aa2baf7a1703a945fa304db0b5b1816
    Signed-off-by: Mandy Hung <mandy.hung.wiwynn@gmail.com>

--- src/hwmon-temp/HwmonTempMain.cpp
+++ src/hwmon-temp/HwmonTempMain.cpp
@@ -580,6 +580,12 @@ static void powerStateChanged(
     boost::asio::io_context& io, sdbusplus::asio::object_server& objectServer,
     std::shared_ptr<sdbusplus::asio::connection>& dbusConnection)
 {
+    if (DisableHostPowerMonitoring == 1)
+    {
+        std::cout
+            << "[INFO] Host power monitoring is disabled, skipping power state change handling.\n";
+        return;
+    }
     if (newState)
     {
         createSensors(io, objectServer, sensors, dbusConnection, nullptr, true);
