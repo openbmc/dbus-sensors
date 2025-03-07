9afe60b3bfb0e204f057c4fff9ec307960baea0b
Harshit Aghera <haghera@nvidia.com>
2025-03-07 09:48:49 +0530

    nvidia-gpu : add temperature sensor dbus service
    This commit introduces an app that can read the temperature sensor
    reading from an nvidia GPU that implements an nvidia vendor extension
    spec based on the OCP AMI specification
    
    The commit adds the following:
    - A D-Bus app that uses the library libocpami to prepare/parse
      request/response messages and to transmit/receive the message via
      in-kernel mctp stack.
    
    Tested:
    Discovery of GPUs from MCTP DBus tree and GPU temperature read is tested
    with in-kernel mctp stack on the nvidia GB200NVL platform BMC
    
    ```
    root@hgxb:~# busctl tree xyz.openbmc_project.GpuSensor
    `- /xyz
      `- /xyz/openbmc_project
        `- /xyz/openbmc_project/sensors
          `- /xyz/openbmc_project/sensors/temperature
            `- /xyz/openbmc_project/sensors/temperature/GPU_4_TEMP_0
    
    root@hgxb:~# busctl introspect  xyz.openbmc_project.GpuSensor /xyz/openbmc_project/sensors/temperature/GPU_4_TEMP_0
    NAME                                        TYPE      SIGNATURE RESULT/VALUE                             FLAGS
    org.freedesktop.DBus.Introspectable         interface -         -                                        -
    .Introspect                                 method    -         s                                        -
    org.freedesktop.DBus.Peer                   interface -         -                                        -
    .GetMachineId                               method    -         s                                        -
    .Ping                                       method    -         -                                        -
    org.freedesktop.DBus.Properties             interface -         -                                        -
    .Get                                        method    ss        v                                        -
    .GetAll                                     method    s         a{sv}                                    -
    .Set                                        method    ssv       -                                        -
    .PropertiesChanged                          signal    sa{sv}as  -                                        -
    xyz.openbmc_project.Association.Definitions interface -         -                                        -
    .Associations                               property  a(sss)    1 "chassis" "all_sensors" "/xyz/openb... emits-change writable
    xyz.openbmc_project.Sensor.Value            interface -         -                                        -
    .MaxAllowableValue                          property  d         inf                                      emits-change writable
    .MaxValue                                   property  d         inf                                      emits-change writable
    .MinAllowableValue                          property  d         -inf                                     emits-change writable
    .MinValue                                   property  d         -inf                                     emits-change writable
    .Unit                                       property  s         "xyz.openbmc_project.Sensor.Value.Uni... emits-change writable
    .Value                                      property  d         42                                       emits-change writable
    ```
    
    Change-Id: I5fc0c197737e19214d0f58df45c86e018aadfd1a
    Signed-off-by: Harshit Aghera <haghera@nvidia.com>

---
+++ src/nvidia-gpu/apps/gpu-temperature-sensor/MctpRequester.hpp
@@ -0,0 +1,109 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION &
+ * AFFILIATES. All rights reserved. SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <af-mctp.h>
+#include <transport.h>
+
+#include <boost/asio/awaitable.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/local/datagram_protocol.hpp>
+#include <boost/asio/socket_base.hpp>
+
+#include <cstdint>
+#include <utility>
+#include <vector>
+
+namespace mctp
+{
+/**
+ * @brief MCTP requester class
+ *
+ * This class provides a simple interface for sending and receiving MCTP
+ * messages.
+ */
+class MctpRequester
+{
+  public:
+    /**
+     * @brief Default constructor
+     */
+    MctpRequester() = delete;
+
+    /**
+     * @brief Copy constructor
+     */
+    MctpRequester(const MctpRequester&) = delete;
+
+    /**
+     * @brief Move constructor
+     */
+    MctpRequester(MctpRequester&&) = delete;
+
+    /**
+     * @brief Copy assignment operator (deleted)
+     * @return Reference to this MctpRequester
+     */
+    MctpRequester& operator=(const MctpRequester&) = delete;
+
+    /**
+     * @brief Move assignment operator (deleted)
+     * @return Reference to this MctpRequester
+     */
+    MctpRequester& operator=(MctpRequester&&) = delete;
+
+    /**
+     * @brief Constructor
+     * @param ctx - The IO context to use
+     * @param msgType - The message type to use
+     * @param verbose - Whether to enable verbose logging
+     */
+    MctpRequester(boost::asio::io_context& ctx, uint8_t msgType,
+                  bool verbose = false);
+
+    /**
+     * @brief Destructor
+     *
+     * Cleans up resources used by the MctpRequester instance.
+     */
+    ~MctpRequester();
+
+    /**
+     * @brief Send an MCTP request message and receive the response
+     *
+     * This function sends a request message to the specified endpoint ID and
+     * waits for a response. It uses the MCTP transport layer to handle the
+     * communication.
+     *
+     * @param[in] eid - The endpoint ID to send the message to
+     * @param[in] reqMsg - The request message to send
+     * @return A pair containing:
+     *         - An integer status code
+     *         - A vector containing the response message bytes
+     */
+    boost::asio::awaitable<std::pair<int, std::vector<uint8_t>>>
+        sendRecvMsg(mctp_eid_t eid, const std::vector<uint8_t>& reqMsg);
+
+  private:
+    /** @brief Enable verbose logging if true */
+    bool verbose;
+
+    /** @brief IO context to use */
+    boost::asio::io_context& ctx;
+
+    /** @brief AF MCTP transport layer */
+    mctp_transport_af_mctp* afMctp{};
+
+    /** @brief Transport layer */
+    mctp_transport* transport;
+
+    /** @brief Local socket */
+    boost::asio::local::datagram_protocol::socket socket;
+
+    /** @brief Message flags */
+    boost::asio::socket_base::message_flags flag;
+};
+} // namespace mctp
