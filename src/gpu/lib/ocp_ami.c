539d7fcb222d64602cdc010bc3ffa56ebac2a721
Harshit Aghera <haghera@nvidia.com>
2025-03-07 09:04:09 +0530

    nvidia-gpu : add libocpami
    The commit adds the following:
    - A library containing encode/decode APIs for the OCP AMI specification
    - OCP AMI specification -
    '''
      https://www.opencompute.org/documents/ocp-gpu-accelerator-management-interfaces-v1-pdf
    '''
    - The specification defines a protocol framework for MCTP OEM VDM that
      allows device vendors to implement their own monitoring and control
      interfaces. The specification acknowledges that device vendors will
      have additional OEM sensors.
    - The specification provides message bindings
       - PCI Vendor ID-based messages (using vendor's PCI ID)
       - IANA Enterprise ID-based messages (using OCP's IANA ID: 42623)
    - The specification provides following protocol features
       - Message type identification
       - Support for request/response and event messages
       - Instance ID tracking for message pairing
       - Completion codes and error handling
       - Timing specifications (retry counts, timeouts)
    
    Tested:
    Unit tests for encode/decode APIs for the OCP AMI specification
    
    Change-Id: Iee7376a9116489052c690f2e3a1ca8d0f29564dd
    Signed-off-by: Harshit Aghera <haghera@nvidia.com>

---
+++ src/gpu/lib/ocp_ami.c
@@ -0,0 +1,95 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION &
+ * AFFILIATES. All rights reserved. SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "ocp_ami.h"
+
+#include <endian.h>
+#include <string.h>
+
+int ocp_ami_pack_header(uint16_t pci_vendor_id,
+			const struct ocp_ami_binding_pci_vid_info *hdr,
+			struct ocp_ami_binding_pci_vid *msg)
+{
+	if (msg == NULL || hdr == NULL) {
+		return OCP_AMI_ERR_NULL;
+	}
+
+	if (hdr->ocp_ami_msg_type != OCP_AMI_RESPONSE &&
+	    hdr->ocp_ami_msg_type != OCP_AMI_REQUEST &&
+	    hdr->ocp_ami_msg_type != OCP_AMI_EVENT &&
+	    hdr->ocp_ami_msg_type != OCP_AMI_EVENT_ACKNOWLEDGMENT) {
+		return OCP_AMI_ERR_INVALID_DATA;
+	}
+
+	if (hdr->instance_id > OCP_AMI_INSTANCE_MAX) {
+		return OCP_AMI_ERR_INVALID_DATA;
+	}
+
+	msg->datagram = 0;
+	if (hdr->ocp_ami_msg_type == OCP_AMI_EVENT_ACKNOWLEDGMENT ||
+	    hdr->ocp_ami_msg_type == OCP_AMI_EVENT) {
+		msg->datagram = 1;
+	}
+
+	msg->request = 0;
+	if (hdr->ocp_ami_msg_type == OCP_AMI_REQUEST ||
+	    hdr->ocp_ami_msg_type == OCP_AMI_EVENT) {
+		msg->request = 1;
+	}
+
+	msg->pci_vendor_id = htobe16(pci_vendor_id);
+	msg->reserved = 0;
+	msg->instance_id = hdr->instance_id;
+	msg->ocp_type = OCP_TYPE;
+	msg->ocp_version = OCP_VERSION;
+	msg->ocp_ami_msg_type = hdr->msg_type;
+
+	return OCP_AMI_SUCCESS;
+}
+
+int ocp_ami_encode_reason_code(uint8_t cc, uint16_t reason_code,
+			       uint8_t command_code, struct ocp_ami_msg *msg)
+{
+	if (msg == NULL) {
+		return OCP_AMI_ERR_NULL;
+	}
+
+	struct ocp_ami_common_non_success_resp *response =
+		(struct ocp_ami_common_non_success_resp *)msg->payload;
+
+	response->command = command_code;
+	response->completion_code = cc;
+	reason_code = htole16(reason_code);
+	response->reason_code = reason_code;
+
+	return OCP_AMI_SUCCESS;
+}
+
+int ocp_ami_decode_reason_code_and_cc(const struct ocp_ami_msg *msg,
+				      size_t msg_len, uint8_t *cc,
+				      uint16_t *reason_code)
+{
+	if (msg == NULL || cc == NULL || reason_code == NULL) {
+		return OCP_AMI_ERR_NULL;
+	}
+
+	*cc = ((struct ocp_ami_common_resp *)msg->payload)->completion_code;
+	if (*cc == OCP_AMI_SUCCESS) {
+		return OCP_AMI_SUCCESS;
+	}
+
+	if (msg_len != (sizeof(struct ocp_ami_binding_pci_vid) +
+			sizeof(struct ocp_ami_common_non_success_resp))) {
+		return OCP_AMI_ERR_INVALID_DATA_LENGTH;
+	}
+
+	struct ocp_ami_common_non_success_resp *response =
+		(struct ocp_ami_common_non_success_resp *)msg->payload;
+
+	// reason code is expected to be present if CC != OCP_AMI_SUCCESS
+	*reason_code = le16toh(response->reason_code);
+
+	return OCP_AMI_SUCCESS;
+}
