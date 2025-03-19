f401f1e463a56397ed6ee1991c8f999ff1779d46
Harshit Aghera <haghera@nvidia.com>
2025-03-19 10:36:59 +0530

    nvidia-gpu : add temperature sensor dbus service
    This commit introduces an app that can read the temperature sensor
    reading from an nvidia GPU that implements an nvidia vendor extension
    spec based on the OCP AMI specification
    
    The commit adds the following:
    - A D-Bus app that uses the library libocpami to prepare/parse
      request/response messages and to transmit/receive the message via
      in-kernel mctp stack.
    
    Tested:
    Discovery of GPUs from MCTP DBus tree and GPU temperature read is tested
    with in-kernel mctp stack on the nvidia GB200NVL platform BMC
    
    ```
    root@hgxb:~# busctl tree xyz.openbmc_project.GpuSensor
    `- /xyz
      `- /xyz/openbmc_project
        `- /xyz/openbmc_project/sensors
          `- /xyz/openbmc_project/sensors/temperature
            `- /xyz/openbmc_project/sensors/temperature/GPU_4_TEMP_0
    
    root@hgxb:~# busctl introspect  xyz.openbmc_project.GpuSensor /xyz/openbmc_project/sensors/temperature/GPU_4_TEMP_0
    NAME                                        TYPE      SIGNATURE RESULT/VALUE                             FLAGS
    org.freedesktop.DBus.Introspectable         interface -         -                                        -
    .Introspect                                 method    -         s                                        -
    org.freedesktop.DBus.Peer                   interface -         -                                        -
    .GetMachineId                               method    -         s                                        -
    .Ping                                       method    -         -                                        -
    org.freedesktop.DBus.Properties             interface -         -                                        -
    .Get                                        method    ss        v                                        -
    .GetAll                                     method    s         a{sv}                                    -
    .Set                                        method    ssv       -                                        -
    .PropertiesChanged                          signal    sa{sv}as  -                                        -
    xyz.openbmc_project.Association.Definitions interface -         -                                        -
    .Associations                               property  a(sss)    1 "chassis" "all_sensors" "/xyz/openb... emits-change writable
    xyz.openbmc_project.Sensor.Value            interface -         -                                        -
    .MaxAllowableValue                          property  d         inf                                      emits-change writable
    .MaxValue                                   property  d         inf                                      emits-change writable
    .MinAllowableValue                          property  d         -inf                                     emits-change writable
    .MinValue                                   property  d         -inf                                     emits-change writable
    .Unit                                       property  s         "xyz.openbmc_project.Sensor.Value.Uni... emits-change writable
    .Value                                      property  d         42                                       emits-change writable
    ```
    
    Change-Id: I5fc0c197737e19214d0f58df45c86e018aadfd1a
    Signed-off-by: Harshit Aghera <haghera@nvidia.com>

---
+++ src/gpu/apps/gpu-temperature-sensor/GpuSensorTask.cpp
@@ -0,0 +1,295 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION &
+ * AFFILIATES. All rights reserved. SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "GpuSensorTask.hpp"
+
+#include "GpuTempSensor.hpp"
+#include "MctpRequester.hpp"
+
+#include <gpu_sensors.h>
+#include <ocp_ami.h>
+#include <transport.h>
+
+#include <boost/asio/awaitable.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/steady_timer.hpp>
+#include <boost/asio/use_awaitable.hpp>
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/bus.hpp>
+
+#include <algorithm>
+#include <chrono>
+#include <cstddef>
+#include <cstdint>
+#include <exception>
+#include <memory>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <utility>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using SensorId = uint8_t;
+using SensorName = std::string;
+using ChassisPath = std::string;
+using ConfigData = std::tuple<mctp_eid_t, SensorId, SensorName, ChassisPath,
+                              std::chrono::milliseconds>;
+
+using ObjectPath = std::string;
+using ServiceName = std::string;
+using InterfaceName = std::string;
+using getSubTreeRet = std::vector<
+    std::pair<ObjectPath,
+              std::vector<std::pair<ServiceName, std::vector<InterfaceName>>>>>;
+
+using GpuSensorConfigMap = std::unordered_map<
+    std::string, std::variant<std::vector<std::string>, std::vector<uint8_t>,
+                              std::string, int64_t, uint64_t, double, int32_t,
+                              uint32_t, int16_t, uint16_t, uint8_t, bool>>;
+
+struct GpuInfo
+{
+    uint8_t eid;
+    uint8_t instanceId;
+};
+
+constexpr uint8_t gpuTempSensorId{0};
+constexpr std::chrono::milliseconds samplingInterval{1000ms};
+const std::string gpuTempSensorNamePrefix{"TEMP"};
+const std::string gpuNamePrefix{"GPU"};
+const std::string gpuChassisPathPrefix{
+    "/xyz/openbmc_project/inventory/chassis/GPU"};
+
+template <typename RetType, typename CompletionToken, typename... InputArgs>
+static auto asyncDbusMethodCall(
+    CompletionToken&& token,
+    const std::shared_ptr<sdbusplus::asio::connection>& dbusConnection,
+    const std::string& service, const std::string& objpath,
+    const std::string& interf, const std::string& method, const InputArgs&... a)
+{
+    auto initiate = [&](auto handler) {
+        dbusConnection->async_method_call(
+            [hand = std::move(handler)](const boost::system::error_code& ec,
+                                        const RetType& response) mutable {
+                hand(ec, std::move(response));
+            },
+            service, objpath, interf, method, a...);
+    };
+
+    // Disable clang-tidy check as the error is in header
+    // boost/asio/impl/awaitable.hpp and to exclude that header from clang-tidy
+    // checks, clang-tidy configuration option ExcludeHeaderFilterRegex is only
+    // supported in clang-tidy version 19.
+
+    // NOLINTNEXTLINE
+    return boost::asio::async_initiate<
+        CompletionToken, void(boost::system::error_code, RetType)>(
+        initiate, std::forward<CompletionToken>(token));
+}
+
+static boost::asio::awaitable<int> getGPUEIDs(
+    std::shared_ptr<sdbusplus::asio::connection> dbusConnection,
+    mctp::MctpRequester& mctpRequester, std::vector<GpuInfo>& eids)
+{
+    std::string searchPath{"/au/com/codeconstruct/mctp/"};
+    std::vector<std::string> ifaceList{{"xyz.openbmc_project.MCTP.Endpoint"}};
+
+    getSubTreeRet ret{};
+
+    try
+    {
+        ret = co_await asyncDbusMethodCall<getSubTreeRet>(
+            boost::asio::use_awaitable, dbusConnection,
+            "xyz.openbmc_project.ObjectMapper",
+            "/xyz/openbmc_project/object_mapper",
+            "xyz.openbmc_project.ObjectMapper", "GetSubTree", searchPath, 0,
+            ifaceList);
+    }
+    catch (const std::exception& e)
+    {
+        lg2::error("getGPUEIDs(): Error:{ERROR}", "ERROR", e);
+        co_return -1;
+    }
+
+    if (ret.empty())
+    {
+        co_return -1;
+    }
+
+    std::vector<uint8_t> ocpamiEndpoints;
+
+    for (const auto& [objPath, services] : ret)
+    {
+        for (const auto& [service, ifaces] : services)
+        {
+            for (const auto& iface : ifaces)
+            {
+                if (iface == "xyz.openbmc_project.MCTP.Endpoint")
+                {
+                    try
+                    {
+                        auto configs =
+                            co_await asyncDbusMethodCall<GpuSensorConfigMap>(
+                                boost::asio::use_awaitable, dbusConnection,
+                                service, objPath,
+                                "org.freedesktop.DBus.Properties", "GetAll",
+                                iface);
+
+                        auto eid = std::get<size_t>(configs["EID"]);
+                        auto mctpTypes = std::get<std::vector<uint8_t>>(
+                            configs["SupportedMessageTypes"]);
+
+                        if (std::find(mctpTypes.begin(), mctpTypes.end(),
+                                      OCP_AMI_MESSAGE_TYPE) != mctpTypes.end())
+                        {
+                            ocpamiEndpoints.push_back(eid);
+
+                            lg2::info("Found OCPAMI Endpoint with ID {EID}",
+                                      "EID", eid);
+                        }
+                    }
+                    catch (const std::exception& e)
+                    {
+                        lg2::error("getGPUEIDs(): Error:{ERROR}", "ERROR", e);
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+
+    for (const auto ocpamiEndpoint : ocpamiEndpoints)
+    {
+        std::vector<uint8_t> reqMsg(
+            sizeof(struct ocp_ami_binding_pci_vid) +
+            sizeof(gpu_query_device_identification_req));
+
+        auto* msg = new (reqMsg.data()) struct ocp_ami_msg;
+
+        auto rc = gpu_encode_query_device_identification_req(
+            OCP_AMI_INSTANCE_MIN, msg);
+        if (rc != 0)
+        {
+            lg2::error(
+                "getGPUEIDs(): gpu_encode_query_device_identification_req failed, rc={RC}",
+                "RC", rc);
+            continue;
+        }
+
+        std::vector<uint8_t> respMsg;
+        std::tie(rc, respMsg) =
+            co_await mctpRequester.sendRecvMsg(ocpamiEndpoint, reqMsg);
+
+        if (rc != 0)
+        {
+            lg2::error(
+                "getGPUEIDs(): MctpRequester::sendRecvMsg() failed, rc={RC}",
+                "RC", rc);
+            continue;
+        }
+
+        if (respMsg.empty())
+        {
+            lg2::error(
+                "getGPUEIDs(): MctpRequester::sendRecvMsg() failed, respMsgLen=0");
+            continue;
+        }
+
+        uint8_t cc = 0;
+        uint16_t reasonCode = 0;
+        uint8_t deviceIdentification = 0;
+        uint8_t instanceId = 0;
+
+        rc = gpu_decode_query_device_identification_resp(
+            new (respMsg.data()) ocp_ami_msg, respMsg.size(), &cc, &reasonCode,
+            &deviceIdentification, &instanceId);
+        if (rc != 0)
+        {
+            lg2::error(
+                "getGPUEIDs(): gpu_decode_query_device_identification_resp() failed, rc={RC} cc={CC} reasonCode={RESC}\n",
+                "RC", rc, "CC", cc, "RESC", reasonCode);
+            continue;
+        }
+
+        if (deviceIdentification == GPU_DEV_ID_GPU)
+        {
+            lg2::info(
+                "getGPUEIDs(): found GPU with EID {EID} and InstanceId {IID}",
+                "EID", ocpamiEndpoint, "IID", instanceId);
+
+            eids.push_back({ocpamiEndpoint, instanceId});
+        }
+    }
+
+    co_return 0;
+}
+
+boost::asio::awaitable<void> gpuSensorTask(
+    boost::asio::io_context& ctx,
+    std::shared_ptr<sdbusplus::asio::connection> dbusConnection, bool verbose)
+{
+    mctp::MctpRequester mctpRequester(ctx, 0x7e);
+
+    std::vector<GpuInfo> gpuInfos;
+    auto rc = co_await getGPUEIDs(dbusConnection, mctpRequester, gpuInfos);
+
+    if (rc < 0 || gpuInfos.empty())
+    {
+        lg2::error("gpuSensorTask(): failed to get OCP AMI GPU list.");
+        co_return;
+    }
+
+    // create temp sensors
+    std::vector<GpuTempSensor> tempSensors{};
+
+    for (const auto gpuInfo : gpuInfos)
+    {
+        if (verbose)
+        {
+            lg2::info(
+                "gpuSensorTask(): GPU_TEMP_SENSOR EID={EID} INSTANCE_ID={IID}.",
+                "EID", gpuInfo.eid, "IID", gpuInfo.instanceId);
+        }
+
+        std::string sensorName = gpuNamePrefix;
+        sensorName += '_';
+        sensorName += std::to_string(gpuInfo.instanceId);
+        sensorName += '_';
+        sensorName += gpuTempSensorNamePrefix;
+        sensorName += '_';
+        sensorName += std::to_string(gpuTempSensorId);
+
+        tempSensors.emplace_back(
+            static_cast<sdbusplus::bus::bus&>(*dbusConnection), gpuInfo.eid,
+            gpuTempSensorId, sensorName,
+            gpuChassisPathPrefix + '_' + std::to_string(gpuInfo.instanceId),
+            verbose);
+    }
+
+    // update sensors regularly
+    while (true)
+    {
+        if (verbose)
+        {
+            lg2::info("gpuSensorTask(): updating the sensors.");
+        }
+
+        for (auto& sensor : tempSensors)
+        {
+            co_await sensor.update(mctpRequester);
+        }
+
+        boost::asio::steady_timer timeoutTimer{ctx};
+
+        timeoutTimer.expires_after(samplingInterval);
+        co_await timeoutTimer.async_wait(boost::asio::use_awaitable);
+    }
+
+    co_return;
+}
