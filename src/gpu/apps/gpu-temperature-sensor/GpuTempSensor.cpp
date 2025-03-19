fcaa7aaa9c440658ea6ce89edde459b67de094bd
Harshit Aghera <haghera@nvidia.com>
2025-03-19 10:36:59 +0530

    nvidia-gpu : add temperature sensor dbus service
    This commit introduces an app that can read the temperature sensor
    reading from an nvidia GPU that implements an nvidia vendor extension
    spec based on the OCP AMI specification
    
    The commit adds the following:
    - A D-Bus app that uses the library libocpami to prepare/parse
      request/response messages and to transmit/receive the message via
      in-kernel mctp stack.
    
    Tested:
    Discovery of GPUs from MCTP DBus tree and GPU temperature read is tested
    with in-kernel mctp stack on the nvidia GB200NVL platform BMC
    
    ```
    root@hgxb:~# busctl tree xyz.openbmc_project.GpuSensor
    `- /xyz
      `- /xyz/openbmc_project
        `- /xyz/openbmc_project/sensors
          `- /xyz/openbmc_project/sensors/temperature
            `- /xyz/openbmc_project/sensors/temperature/GPU_4_TEMP_0
    
    root@hgxb:~# busctl introspect  xyz.openbmc_project.GpuSensor /xyz/openbmc_project/sensors/temperature/GPU_4_TEMP_0
    NAME                                        TYPE      SIGNATURE RESULT/VALUE                             FLAGS
    org.freedesktop.DBus.Introspectable         interface -         -                                        -
    .Introspect                                 method    -         s                                        -
    org.freedesktop.DBus.Peer                   interface -         -                                        -
    .GetMachineId                               method    -         s                                        -
    .Ping                                       method    -         -                                        -
    org.freedesktop.DBus.Properties             interface -         -                                        -
    .Get                                        method    ss        v                                        -
    .GetAll                                     method    s         a{sv}                                    -
    .Set                                        method    ssv       -                                        -
    .PropertiesChanged                          signal    sa{sv}as  -                                        -
    xyz.openbmc_project.Association.Definitions interface -         -                                        -
    .Associations                               property  a(sss)    1 "chassis" "all_sensors" "/xyz/openb... emits-change writable
    xyz.openbmc_project.Sensor.Value            interface -         -                                        -
    .MaxAllowableValue                          property  d         inf                                      emits-change writable
    .MaxValue                                   property  d         inf                                      emits-change writable
    .MinAllowableValue                          property  d         -inf                                     emits-change writable
    .MinValue                                   property  d         -inf                                     emits-change writable
    .Unit                                       property  s         "xyz.openbmc_project.Sensor.Value.Uni... emits-change writable
    .Value                                      property  d         42                                       emits-change writable
    ```
    
    Change-Id: I5fc0c197737e19214d0f58df45c86e018aadfd1a
    Signed-off-by: Harshit Aghera <haghera@nvidia.com>

---
+++ src/gpu/apps/gpu-temperature-sensor/GpuTempSensor.cpp
@@ -0,0 +1,119 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION &
+ * AFFILIATES. All rights reserved. SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "GpuTempSensor.hpp"
+
+#include <bits/basic_string.h>
+#include <gpu_sensors.h>
+#include <ocp_ami.h>
+#include <transport.h>
+
+#include <MctpRequester.hpp>
+#include <boost/asio/awaitable.hpp>
+#include <boost/regex.hpp>
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/bus.hpp>
+
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <tuple>
+#include <vector>
+
+using namespace std::literals;
+
+static const boost::regex invalidDBusNameSubString{"[^a-zA-Z0-9._/]+"};
+static std::string makeDBusNameValid(const std::string& name)
+{
+    return boost::regex_replace(name, invalidDBusNameSubString, "_");
+}
+
+GpuTempSensor::GpuTempSensor(sdbusplus::bus::bus& bus, mctp_eid_t eid,
+                             uint8_t sensorId, const std::string& name,
+                             const std::string& chassisPath, bool verbose) :
+    eid(eid), sensorId(sensorId), verbose(verbose)
+
+{
+    auto sensorPath =
+        "/xyz/openbmc_project/sensors/temperature/"s + makeDBusNameValid(name);
+    valueIntf = std::make_unique<ValueIntf>(bus, sensorPath.c_str());
+    valueIntf->unit(SensorUnit::DegreesC);
+
+    associationIntf =
+        std::make_unique<AssociationDefinitionsInft>(bus, sensorPath.c_str());
+    std::vector<std::tuple<std::string, std::string, std::string>> associations;
+    associations.emplace_back("chassis", "all_sensors", chassisPath);
+    associationIntf->associations(associations);
+}
+
+boost::asio::awaitable<void> GpuTempSensor::update(
+    mctp::MctpRequester& mctpRequester)
+{
+    std::vector<uint8_t> reqMsg(sizeof(struct ocp_ami_binding_pci_vid) +
+                                sizeof(gpu_get_temperature_reading_req));
+
+    auto* msg = new (reqMsg.data()) ocp_ami_msg;
+
+    auto rc = gpu_encode_get_temperature_reading_req(OCP_AMI_INSTANCE_MIN,
+                                                     sensorId, msg);
+
+    // Disable clang-tidy check as the error is in header
+    // boost/asio/impl/awaitable.hpp and to exclude that header from clang-tidy
+    // checks, clang-tidy configuration option ExcludeHeaderFilterRegex is only
+    // supported in clang-tidy version 19.
+
+    // NOLINTNEXTLINE
+    if (rc != 0)
+    {
+        lg2::error(
+            "GpuTempSensor::update(): encode_get_temperature_reading_req failed, rc={RC}",
+            "RC", rc);
+        co_return;
+    }
+
+    std::vector<uint8_t> respMsg;
+    std::tie(rc, respMsg) = co_await mctpRequester.sendRecvMsg(eid, reqMsg);
+
+    if (rc != 0)
+    {
+        lg2::error(
+            "GpuTempSensor::update(): MctpRequester::sendRecvMsg() failed, rc={RC}",
+            "RC", rc);
+        co_return;
+    }
+
+    uint8_t cc = 0;
+    uint16_t reasonCode = 0;
+    double tempValue = 0;
+
+    if (respMsg.empty())
+    {
+        lg2::error(
+            "GpuTempSensor::update(): MctpRequester::sendRecvMsg() failed, respMsgLen=0");
+        co_return;
+    }
+
+    rc = gpu_decode_get_temperature_reading_resp(
+        new (respMsg.data()) ocp_ami_msg, respMsg.size(), &cc, &reasonCode,
+        &tempValue);
+    if (rc != 0)
+    {
+        lg2::error(
+            "GpuTempSensor::update(): decode_get_temperature_reading_resp() failed, rc={RC} cc={CC} reasonCode={RESC}\n",
+            "RC", rc, "CC", cc, "RESC", reasonCode);
+        co_return;
+    }
+
+    valueIntf->value(tempValue);
+
+    if (verbose)
+    {
+        lg2::info(
+            "GpuTempSensor::update(): eid={EID} sensorId={SID} temp={TEMP}",
+            "EID", eid, "SID", sensorId, "TEMP", tempValue);
+    }
+
+    co_return;
+}
---
+++ src/gpu/apps/gpu-temperature-sensor/MctpRequester.cpp
@@ -0,0 +1,176 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION &
+ * AFFILIATES. All rights reserved. SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "MctpRequester.hpp"
+
+#include <af-mctp.h>
+#include <ocp_ami.h>
+#include <transport.h>
+
+#include <boost/asio/awaitable.hpp>
+#include <boost/asio/buffer.hpp>
+#include <boost/asio/experimental/awaitable_operators.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/local/datagram_protocol.hpp>
+#include <boost/asio/steady_timer.hpp>
+#include <boost/asio/this_coro.hpp>
+#include <boost/asio/use_awaitable.hpp>
+#include <boost/exception/diagnostic_information.hpp>
+#include <boost/exception/exception.hpp>
+#include <boost/system/system_error.hpp>
+#include <phosphor-logging/lg2.hpp>
+
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std::literals;
+
+namespace mctp
+{
+using namespace boost::asio::experimental::awaitable_operators;
+
+MctpRequester::MctpRequester(boost::asio::io_context& ctx, uint8_t msgType,
+                             bool verbose) :
+    verbose(verbose), ctx(ctx), socket(ctx), flag(MSG_PEEK | MSG_TRUNC)
+{
+    auto rc = mctp_transport_af_mctp_init(&afMctp, msgType);
+    if (rc < 0)
+    {
+        lg2::error("mctp_transport_af_mctp_init() failed");
+        return;
+    }
+
+    transport = mctp_transport_af_mctp_core(afMctp);
+
+    auto sockfd = mctp_transport_get_fd(transport);
+
+    try
+    {
+        socket.assign(boost::asio::local::datagram_protocol{}, sockfd);
+    }
+    catch (const boost::exception& ex)
+    {
+        std::string info = boost::diagnostic_information(ex);
+        lg2::error(
+            "MctpRequester failed to connect to the MCTP socket - Error={ER}.",
+            "ER", info);
+        return;
+    }
+
+    socket.non_blocking(true);
+}
+
+MctpRequester::~MctpRequester()
+{
+    mctp_transport_af_mctp_destroy(afMctp);
+}
+
+boost::asio::awaitable<std::pair<int, std::vector<uint8_t>>>
+    MctpRequester::sendRecvMsg(mctp_eid_t eid,
+                               const std::vector<uint8_t>& reqMsg)
+{
+    std::vector<uint8_t> respMsg{};
+
+    auto rc =
+        mctp_transport_send_msg(transport, eid, reqMsg.data(), reqMsg.size());
+
+    // Disable clang-tidy check as the error is in header
+    // boost/asio/impl/awaitable.hpp and to exclude that header from clang-tidy
+    // checks, clang-tidy configuration option ExcludeHeaderFilterRegex is only
+    // supported in clang-tidy version 19.
+
+    // NOLINTNEXTLINE
+    if (rc < 0)
+    {
+        lg2::error(
+            "MctpRequester failed send data to the MCTP Socket - Error={EC}.",
+            "EC", rc);
+
+        co_return std::make_pair(rc, respMsg);
+    }
+
+    try
+    {
+        boost::asio::steady_timer timoutTimer(
+            co_await boost::asio::this_coro::executor);
+
+        timoutTimer.expires_after(2s);
+
+        const auto result = co_await (
+            socket.async_receive(boost::asio::buffer(respMsg), flag,
+                                 boost::asio::use_awaitable) ||
+            timoutTimer.async_wait(boost::asio::use_awaitable));
+
+        if (result.index() == 1)
+        {
+            // timeout
+            co_return std::make_pair(-1, respMsg);
+        }
+    }
+    catch (const boost::system::system_error& e)
+    {
+        boost::system::error_code ec = e.code();
+
+        lg2::error(
+            "MctpRequester failed to receive data from the MCTP socket - ErrorCode={EC}, Error={ER}.",
+            "EC", ec.value(), "ER", ec.message());
+
+        co_return std::make_pair(-2, respMsg);
+    }
+
+    mctp_eid_t respEid = 0;
+    void* recvMsg = nullptr;
+    size_t recvMsgLen = 0;
+
+    rc = mctp_transport_recv_msg(transport, &respEid, &recvMsg, &recvMsgLen);
+
+    boost::system::error_code ec;
+
+    if (rc < 0)
+    {
+        lg2::error(
+            "MctpRequester failed to receive data from the MCTP socket - ErrorCode={EC}, Error={ER}.",
+            "EC", ec.value(), "ER", ec.message());
+
+        co_return std::make_pair(rc, respMsg);
+    }
+
+    if (respEid != eid)
+    {
+        lg2::error(
+            "MctpRequester failed to receive data from the MCTP socket - ErrorCode={EC}, Error={ER}.",
+            "EC", ec.value(), "ER", ec.message());
+
+        co_return std::make_pair(-3, respMsg);
+    }
+
+    if (recvMsgLen > sizeof(struct ocp_ami_binding_pci_vid))
+    {
+        ocp_ami_binding_pci_vid reqHdr{};
+        std::memcpy(&reqHdr, reqMsg.data(), sizeof(ocp_ami_binding_pci_vid));
+
+        ocp_ami_binding_pci_vid respHdr{};
+        std::memcpy(&respHdr, recvMsg, sizeof(ocp_ami_binding_pci_vid));
+
+        if (reqHdr.instance_id != respHdr.instance_id)
+        {
+            lg2::error(
+                "MctpRequester failed to receive data from the MCTP socket - ErrorCode={EC}, Error={ER}.",
+                "EC", ec.value(), "ER", ec.message());
+
+            co_return std::make_pair(-4, respMsg);
+        }
+    }
+
+    uint8_t* recvMsgData = static_cast<uint8_t*>(recvMsg);
+    respMsg.insert(respMsg.begin(), recvMsgData, recvMsgData + recvMsgLen);
+
+    co_return std::make_pair(0, std::move(respMsg));
+}
+} // namespace mctp
