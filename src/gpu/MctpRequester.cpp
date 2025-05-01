a8aafe07db2725af5d8946a81782e4f0e9ecfaa0
Aditya Kurdunkar <akurdunkar@nvidia.com>
2025-05-01 20:24:53 +0530

    gpu: add support for per EID request queuing
    The OCP MCTP VDM Protocol has requirements for the protocol requester
    for handling overlapping requests and responses, The requirements are as
    follows:
    
    '''
    A protocol requester shall wait until one of the following conditions
    occur before submitting a new request:
    
    1. It times out waiting for response to a previous request or,
    2. It receives the response for a previous request or,
    3. It receives an indication that the transmission of a particular
      request failed.
    
    The requester must be prepared to handle responses which may not arrive
    in the order in which the requests were issued. To match the response to
    a particular request the requester should verify the NVIDIA Message
    Type, Command Code, and Instance ID values match with the corresponding
    outstanding command.
    '''
    
    These requirements, especially 2, introduces a requirement for request
    queuing per EID. This patch introduces the same.
    
    Change-Id: Ic3b892ef2c76c4c703aa55f5b2a66c22a5d71bdf
    Signed-off-by: Aditya Kurdunkar <akurdunkar@nvidia.com>

--- src/gpu/MctpRequester.cpp
+++ src/gpu/MctpRequester.cpp
@@ -5,11 +5,12 @@
 
 #include "MctpRequester.hpp"
 
+#include "OcpMctpVdm.hpp"
+
 #include <linux/mctp.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
-#include <OcpMctpVdm.hpp>
 #include <boost/asio/buffer.hpp>
 #include <boost/asio/error.hpp>
 #include <boost/asio/io_context.hpp>
@@ -24,8 +25,6 @@
 #include <memory>
 #include <utility>
 #include <vector>
-
-using namespace std::literals;
 
 namespace mctp
 {
@@ -53,141 +52,240 @@ MctpRequester::MctpRequester(boost::asio::io_context& ctx, uint8_t msgType) :
     }
 
     mctpSocket.non_blocking(true);
+    asyncRecv();
 }
 
-void MctpRequester::processRecvMsg(
-    mctp_eid_t eid, const std::vector<uint8_t>& reqMsg,
-    const std::function<void(int, std::vector<uint8_t>)>& callback,
-    size_t peekedLength) const
+void MctpRequester::asyncRecv()
 {
-    // Receive message
-    struct sockaddr sockAddr{};
-    struct sockaddr_mctp respAddr{};
-    socklen_t addrlen = sizeof(respAddr);
-    size_t receivedLength = 0;
+    static std::vector<uint8_t> tmp{};
+    mctpSocket.async_receive(
+        boost::asio::buffer(tmp), MSG_PEEK | MSG_TRUNC,
+        [this](const boost::system::error_code& ec, size_t peekedLength) {
+            if (ec)
+            {
+                lg2::error(
+                    "MctpRequester failed to receive data from the MCTP socket - ErrorCode={EC}, Error={ER}.",
+                    "EC", ec.value(), "ER", ec.message());
+                processPendingRequests();
+                asyncRecv();
+                return;
+            }
 
-    std::vector<uint8_t> fullRespMsg(peekedLength);
+            struct sockaddr sockAddr{};
+            struct sockaddr_mctp respAddr{};
+            socklen_t addrlen = sizeof(respAddr);
+            size_t receivedLength = 0;
 
-    receivedLength = recvfrom(sockfd, fullRespMsg.data(), peekedLength,
-                              MSG_TRUNC, &sockAddr, &addrlen);
+            Response resp(peekedLength);
 
-    std::memcpy(&respAddr, &sockAddr, sizeof(respAddr));
+            receivedLength = recvfrom(sockfd, resp.data(), peekedLength,
+                                      MSG_TRUNC, &sockAddr, &addrlen);
 
-    if (receivedLength <= 0)
+            std::memcpy(&respAddr, &sockAddr, sizeof(respAddr));
+
+            if (receivedLength <= 0)
+            {
+                lg2::error(
+                    "MctpRequester: Failed to receive message - Error={EC}.",
+                    "EC", receivedLength);
+                processPendingRequests();
+                asyncRecv();
+                return;
+            }
+
+            if (peekedLength != receivedLength)
+            {
+                lg2::error(
+                    "MctpRequester: Received message of unexpected length - expected={EL}, received={RL}",
+                    "EL", peekedLength, "RL", receivedLength);
+                processPendingRequests();
+                asyncRecv();
+                return;
+            }
+
+            if (receivedLength <
+                sizeof(ocp::accelerator_management::BindingPciVid))
+            {
+                lg2::error(
+                    "MctpRequester: Received Message of Invalid length - {LEN}",
+                    "LEN", receivedLength);
+                processPendingRequests();
+                asyncRecv();
+                return;
+            }
+
+            if (respAddr.smctp_type != msgType)
+            {
+                lg2::error(
+                    "MctpRequester: Message type mismatch - expected={MT}, received={RM}",
+                    "MT", msgType, "RM", respAddr.smctp_type);
+                processPendingRequests();
+                asyncRecv();
+                return;
+            }
+
+            const mctp_eid_t eid = respAddr.smctp_addr.s_addr;
+
+            handleResponse(eid, resp);
+        });
+}
+
+inline void MctpRequester::handleResponse(const mctp_eid_t eid,
+                                          const Response& response)
+{
+    if (!hasActiveRequest(eid))
     {
-        lg2::error("MctpRequester: Failed to receive message");
-        callback(-2, std::vector<uint8_t>{});
+        lg2::error("MctpRequester: No active request found for EID={EID}, "
+                   "Either this is a response for a timed out request"
+                   "or we received a response for a request that was not"
+                   "sent out by us",
+                   "EID", eid);
+        processPendingRequests();
+        asyncRecv();
         return;
     }
 
-    if (respAddr.smctp_type != msgType)
-    {
-        lg2::error("MctpRequester: Message type mismatch");
-        callback(-3, std::move(fullRespMsg));
-        return;
-    }
+    std::unique_ptr<Request> req = std::move(activeRequests[eid]);
 
-    mctp_eid_t respEid = respAddr.smctp_addr.s_addr;
+    req->timer->cancel();
+    cleanupActiveRequest(eid);
 
-    if (respEid != eid)
+    const auto* respHdr =
+        std::bit_cast<const ocp::accelerator_management::BindingPciVid*>(
+            response.data());
+
+    if (req->instanceId != respHdr->instance_id)
     {
         lg2::error(
-            "MctpRequester: EID mismatch - expected={EID}, received={REID}",
-            "EID", eid, "REID", respEid);
-        callback(-4, std::move(fullRespMsg));
+            "MctpRequester: Instance ID mismatch for EID={EID} - expected={IID}, received={RIID}",
+            "EID", eid, "IID", req->instanceId, "RIID",
+            static_cast<uint8_t>(respHdr->instance_id));
+
+        req->callback(-5, Response{});
+
+        processPendingRequests(eid);
+        asyncRecv();
+
         return;
     }
 
-    if (receivedLength > sizeof(ocp::accelerator_management::BindingPciVid))
-    {
-        ocp::accelerator_management::BindingPciVid reqHdr{};
-        std::memcpy(&reqHdr, reqMsg.data(),
-                    sizeof(ocp::accelerator_management::BindingPciVid));
+    req->callback(0, response);
 
-        ocp::accelerator_management::BindingPciVid respHdr{};
-        std::memcpy(&respHdr, fullRespMsg.data(),
-                    sizeof(ocp::accelerator_management::BindingPciVid));
-
-        if (reqHdr.instance_id != respHdr.instance_id)
-        {
-            lg2::error(
-                "MctpRequester: Instance ID mismatch - request={REQ}, response={RESP}",
-                "REQ", static_cast<int>(reqHdr.instance_id), "RESP",
-                static_cast<int>(respHdr.instance_id));
-            callback(-5, std::move(fullRespMsg));
-            return;
-        }
-    }
-
-    callback(0, std::move(fullRespMsg));
+    processPendingRequests(eid);
+    asyncRecv();
 }
 
-void MctpRequester::sendRecvMsg(
-    mctp_eid_t eid, const std::vector<uint8_t>& reqMsg,
-    const std::function<void(int, std::vector<uint8_t>)>& callback)
+void MctpRequester::processPendingRequests()
 {
-    std::vector<uint8_t> respMsg{};
-
-    if (reqMsg.size() < sizeof(ocp::accelerator_management::BindingPciVid))
+    for (auto& [eid, _] : pendingRequests)
     {
-        lg2::error("MctpRequester: Message too small");
-        callback(-2, respMsg);
+        processPendingRequests(eid);
+    }
+}
+
+void MctpRequester::processPendingRequests(mctp_eid_t eid)
+{
+    using namespace std::chrono_literals;
+
+    if (hasActiveRequest(eid) || !hasPendingRequests(eid))
+    {
         return;
     }
 
-    // Create address structure
-    struct sockaddr sockAddr{};
+    std::unique_ptr<Request> req = std::move(pendingRequests[eid].front());
+    pendingRequests[eid].pop();
+
     struct sockaddr_mctp addr{};
     addr.smctp_family = AF_MCTP;
     addr.smctp_addr.s_addr = eid;
     addr.smctp_type = msgType;
     addr.smctp_tag = MCTP_TAG_OWNER;
 
+    // Create address structure
+    struct sockaddr sockAddr{};
     std::memcpy(&sockAddr, &addr, sizeof(addr));
 
     // Send message
-    ssize_t rc = sendto(sockfd, reqMsg.data(), reqMsg.size(), 0, &sockAddr,
+    ssize_t rc = sendto(sockfd, req->vec.data(), req->size(), 0, &sockAddr,
                         sizeof(addr));
     if (rc < 0)
     {
         lg2::error(
             "MctpRequester failed send data to the MCTP Socket - Error={EC}.",
             "EC", rc);
-        callback(rc, respMsg);
+        req->callback(rc, Response{});
+        boost::asio::post(ctx, [this, eid]() { processPendingRequests(eid); });
         return;
     }
 
     // Set up async receive with timeout
-    auto timer = std::make_shared<boost::asio::steady_timer>(ctx);
-    timer->expires_after(2s);
+    auto timer = std::make_unique<boost::asio::steady_timer>(ctx);
 
-    // Set up handler for when the timer expires
-    timer->async_wait([callback, timer](const boost::system::error_code& ec) {
+    timer->expires_after(2s);
+    timer->async_wait([callback = req->callback, eid,
+                       this](const boost::system::error_code& ec) {
         if (ec != boost::asio::error::operation_aborted)
         {
-            callback(-1, std::vector<uint8_t>{});
+            callback(-1, Response{});
+            cleanupActiveRequest(eid);
+            processPendingRequests(eid);
         }
-    });
+    }); // Set up handler for when the timer expires
 
-    // Set up asynchronous receive
-    mctpSocket.async_receive(
-        boost::asio::buffer(respMsg), MSG_PEEK | MSG_TRUNC,
-        [this, eid, reqMsg, callback,
-         timer](const boost::system::error_code& ec, size_t peekedLength) {
-            // Cancel the timer since we got a response
-            timer->cancel();
+    req->timer = std::move(timer);
+    activeRequests.emplace(std::move(req));
+}
 
-            if (ec)
-            {
-                lg2::error(
-                    "MctpRequester failed to receive data from the MCTP socket - ErrorCode={EC}, Error={ER}.",
-                    "EC", ec.value(), "ER", ec.message());
-                callback(-1, std::vector<uint8_t>{});
-                return;
-            }
+bool MctpRequester::enqueueRequest(mctp_eid_t eid, std::unique_ptr<Request> req)
+{
+    if (req->size() < sizeof(ocp::accelerator_management::BindingPciVid))
+    {
+        lg2::error(
+            "MctpRequester: Message too small - expected={ES}, received={RS}",
+            "ES", sizeof(ocp::accelerator_management::BindingPciVid), "RS",
+            req->size());
+        return false;
+    }
 
-            this->processRecvMsg(eid, reqMsg, callback, peekedLength);
-        });
+    const auto* reqMsg =
+        std::bit_cast<const ocp::accelerator_management::Message*>(
+            req->vec.data());
+
+    req->instanceId = reqMsg->hdr.instance_id;
+
+    pendingRequests[eid].emplace(std::move(req));
+    return true;
+}
+
+void MctpRequester::sendRecvMsg(mctp_eid_t eid, std::vector<uint8_t> reqMsg,
+                                ResponseCallback callback)
+{
+    auto req =
+        std::make_unique<Request>(std::move(reqMsg), std::move(callback));
+    req->callback = std::move(callback);
+
+    if (!enqueueRequest(eid, std::move(req)))
+    {
+        req->callback(-2, Response{});
+        return;
+    }
+
+    processPendingRequests(eid);
+}
+
+bool MctpRequester::hasPendingRequests(mctp_eid_t eid) const
+{
+    return pendingRequests.contains(eid) && !pendingRequests.at(eid).empty();
+}
+
+bool MctpRequester::hasActiveRequest(mctp_eid_t eid) const
+{
+    return (activeRequests.contains(eid));
+}
+
+void MctpRequester::cleanupActiveRequest(mctp_eid_t eid)
+{
+    activeRequests.erase(eid);
 }
 
 } // namespace mctp
