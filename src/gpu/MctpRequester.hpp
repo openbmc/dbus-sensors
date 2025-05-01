5dfdf5e41bfe565f38839139c27e32ffa747edb8
Aditya Kurdunkar <akurdunkar@nvidia.com>
2025-05-01 20:24:53 +0530

    gpu: add support for per EID request queuing
    The OCP MCTP VDM Protocol has requirements for the protocol requester
    for handling overlapping requests and responses, The requirements are as
    follows:
    
    '''
    A protocol requester shall wait until one of the following conditions
    occur before submitting a new request:
    
    1. It times out waiting for response to a previous request or,
    2. It receives the response for a previous request or,
    3. It receives an indication that the transmission of a particular
      request failed.
    
    The requester must be prepared to handle responses which may not arrive
    in the order in which the requests were issued. To match the response to
    a particular request the requester should verify the NVIDIA Message
    Type, Command Code, and Instance ID values match with the corresponding
    outstanding command.
    '''
    
    These requirements, especially 2, introduces a requirement for request
    queuing per EID. This patch introduces the same.
    
    Change-Id: Ic3b892ef2c76c4c703aa55f5b2a66c22a5d71bdf
    Signed-off-by: Aditya Kurdunkar <akurdunkar@nvidia.com>

--- src/gpu/MctpRequester.hpp
+++ src/gpu/MctpRequester.hpp
@@ -7,17 +7,65 @@
 
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/local/datagram_protocol.hpp>
+#include <boost/asio/steady_timer.hpp>
 
-#include <cstddef>
 #include <cstdint>
 #include <functional>
+#include <queue>
+#include <utility>
 #include <vector>
-
-// Define MCTP EID type
-using mctp_eid_t = uint8_t;
 
 namespace mctp
 {
+
+/** @brief MCTP EID type */
+using mctp_eid_t = uint8_t;
+
+/** @brief Response */
+using Response = std::vector<uint8_t>;
+
+/** @brief Response callback */
+using ResponseCallback = std::function<void(int, const Response&)>;
+
+/** @brief Request */
+struct Request
+{
+  public:
+    /** @brief Constructor
+     *
+     * @param[in] data - The data to send
+     * @param[in] callback - The callback to invoke when the response is
+     * received
+     */
+    explicit Request(std::vector<uint8_t>&& data,
+                     ResponseCallback&& callback) noexcept :
+        callback(std::move(callback)), vec(std::move(data)) {};
+
+    Request() = delete;
+    Request(const Request& r) = delete;
+
+    Request(Request&& r) noexcept = default;
+    Request& operator=(Request&& r) noexcept = default;
+    ~Request() noexcept = default;
+
+    /** @brief Get the size of the request
+     *
+     * @return The size of the request
+     */
+    std::size_t size() const noexcept
+    {
+        return vec.size();
+    }
+
+    uint8_t instanceId{};
+    ResponseCallback callback;
+    std::unique_ptr<boost::asio::steady_timer> timer;
+    std::vector<uint8_t> vec;
+};
+
+/** @brief Request queue */
+using RequestQueue = std::queue<std::unique_ptr<Request>>;
+
 /**
  * @brief MCTP requester class
  *
@@ -58,26 +106,80 @@ class MctpRequester
      *            - An integer status code (0 for success, negative for error)
      *            - A vector containing the response message bytes
      */
-    void sendRecvMsg(
-        mctp_eid_t eid, const std::vector<uint8_t>& reqMsg,
-        const std::function<void(int, std::vector<uint8_t>)>& callback);
+    void sendRecvMsg(mctp_eid_t eid, std::vector<uint8_t> reqMsg,
+                     ResponseCallback callback);
 
   private:
     /**
-     * @brief Process received message
+     * @brief Poll for received messages
      *
-     * This function processes a received message and invokes the callback with
+     * This function polls for received messages and invokes the callback with
      * the appropriate status code and response message bytes.
-     *
-     * @param[in] eid - The endpoint ID from which the message was received
-     * @param[in] reqMsg - The received request message bytes
-     * @param[in] callback - The callback function to invoke with the result
-     * @param[in] peekedLength - The length of the peeked data
      */
-    void processRecvMsg(
-        mctp_eid_t eid, const std::vector<uint8_t>& reqMsg,
-        const std::function<void(int, std::vector<uint8_t>)>& callback,
-        size_t peekedLength) const;
+    void asyncRecv();
+
+    /** @brief Enqueue a request
+     *
+     * @param[in] eid - The endpoint ID to send the message to
+     * @param[in] reqMsg - The request message to send
+     * @param[in] callback - Callback function to be invoked when response is
+     * received The callback takes two parameters:
+     *            - An integer status code (0 for success, negative for error)
+     *            - A vector containing the response message bytes
+     */
+    bool enqueueRequest(mctp_eid_t eid, std::unique_ptr<Request> reqMsg);
+
+    /** @brief Process pending requests
+     *
+     * This function processes pending requests for a given endpoint ID. It
+     * retrieves the next request from the queue and sends it to the endpoint.
+     *
+     * @param[in] eid - The endpoint ID to process requests for
+     */
+    void processPendingRequests(mctp_eid_t eid);
+
+    /** @brief Process pending requests for all endpoints
+     *
+     * This function processes pending requests for all endpoints.
+     */
+    void processPendingRequests();
+
+    /** @brief Helper function to check if there are pending requests
+     *
+     * This function checks if there are pending requests for a given endpoint
+     * ID.
+     *
+     * @param[in] eid - The endpoint ID to check for pending requests
+     * @return True if there are pending requests, false otherwise
+     */
+    bool hasPendingRequests(mctp_eid_t eid) const;
+
+    /** @brief Helper function to check if there is an active request
+     *
+     * This function checks if there is an active request for a given endpoint
+     * ID.
+     *
+     * @param[in] eid - The endpoint ID to check for active request
+     * @return True if there is an active request, false otherwise
+     */
+    bool hasActiveRequest(mctp_eid_t eid) const;
+
+    /** @brief Cancel an active request
+     *
+     * This function cancels an active request for a given endpoint ID.
+     *
+     * @param[in] eid - The endpoint ID to cancel the request for
+     */
+    void cleanupActiveRequest(mctp_eid_t eid);
+
+    /** @brief Handle a response
+     *
+     * This function handles a response for a given endpoint ID.
+     *
+     * @param[in] eid - The endpoint ID to handle the response for
+     * @param[in] response - The response to handle
+     */
+    inline void handleResponse(mctp_eid_t eid, const Response& response);
 
     /** @brief IO context to use */
     boost::asio::io_context& ctx;
@@ -90,5 +192,11 @@ class MctpRequester
 
     /** @brief MCTP message type */
     uint8_t msgType;
+
+    /** @brief Pending requests */
+    std::unordered_map<mctp_eid_t, RequestQueue> pendingRequests;
+
+    /** @brief Active request callbacks */
+    std::unordered_map<mctp_eid_t, std::unique_ptr<Request>> activeRequests;
 };
 } // namespace mctp
