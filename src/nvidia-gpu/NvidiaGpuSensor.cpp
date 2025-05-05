0e1718cc92169db42c3b5b1f8f7fcbef345d58b2
Harshit Aghera <haghera@nvidia.com>
2025-05-05 12:26:35 +0530

    nvidia-gpu: add TLimit sensor
    This commit introduces a new thermal limit (TLimit) sensor for the GPU,
    enhancing the existing temperature monitoring capabilities.
    
    Tested: Build an image for gb200nvl-obmc machine with the following
    patches cherry picked. This patches are needed to enable the mctp stack.
    
    https://gerrit.openbmc.org/c/openbmc/openbmc/+/79422
    
    ```
    $ curl -s -k -u 'root:0penBmc' https://10.137.203.137/redfish/v1/Chassis/NVIDIA_GB200_1/Sensors/temperature_NVIDIA_GB200_GPU_0_TEMP_1
    {
      "@odata.id": "/redfish/v1/Chassis/NVIDIA_GB200_1/Sensors/temperature_NVIDIA_GB200_GPU_0_TEMP_1",
      "@odata.type": "#Sensor.v1_2_0.Sensor",
      "Id": "temperature_NVIDIA_GB200_GPU_0_TEMP_1",
      "Name": "NVIDIA GB200 GPU 0 TEMP 1",
      "Reading": 57.3984375,
      "ReadingRangeMax": 127.0,
      "ReadingRangeMin": -128.0,
      "ReadingType": "Temperature",
      "ReadingUnits": "Cel",
      "Status": {
        "Health": "OK",
        "State": "Enabled"
      }
    }%
    ```
    
    Change-Id: Ib8e0ef93a4acbb8870671665b098fb61d0205cb2
    Signed-off-by: Harshit Aghera <haghera@nvidia.com>

--- src/nvidia-gpu/NvidiaGpuSensor.cpp
+++ src/nvidia-gpu/NvidiaGpuSensor.cpp
@@ -31,20 +31,19 @@
 
 using namespace std::literals;
 
-constexpr uint8_t gpuTempSensorId{0};
 static constexpr double gpuTempSensorMaxReading = 127;
 static constexpr double gpuTempSensorMinReading = -128;
 
 NvidiaGpuTempSensor::NvidiaGpuTempSensor(
     std::shared_ptr<sdbusplus::asio::connection>& conn,
     mctp::MctpRequester& mctpRequester, const std::string& name,
-    const std::string& sensorConfiguration, const uint8_t eid,
+    const std::string& sensorConfiguration, const uint8_t eid, uint8_t sensorId,
     sdbusplus::asio::object_server& objectServer,
     std::vector<thresholds::Threshold>&& thresholdData) :
     Sensor(escapeName(name), std::move(thresholdData), sensorConfiguration,
            "temperature", false, true, gpuTempSensorMaxReading,
            gpuTempSensorMinReading, conn),
-    eid(eid), sensorId{gpuTempSensorId}, mctpRequester(mctpRequester),
+    eid(eid), sensorId{sensorId}, mctpRequester(mctpRequester),
     objectServer(objectServer)
 {
     std::string dbusPath =
