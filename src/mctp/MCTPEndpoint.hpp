9f2faa2a45802c933f6fd920f91f29a5c9be1a11
Saptarsi Saha <saptarsis@nvidia.com>
2025-05-20 21:38:37 +0530

    Changes enable the Dynamic Endpoint Assignment for Schema
    Changes enable EID assignment for MCTPUSBDevice
    following schema defined in mctp.json
    
    Tested
    '''
    The EID's are assigned correctly in the range defined
    (12-27) as per the hardware.
    |- /au/com/codeconstruct/mctp1/networks/1/endpoints/12
    |- /au/com/codeconstruct/mctp1/networks/1/endpoints/13
    |- /au/com/codeconstruct/mctp1/networks/1/endpoints/14
    |- /au/com/codeconstruct/mctp1/networks/1/endpoints/15
    |- /au/com/codeconstruct/mctp1/networks/1/endpoints/16
    |- /au/com/codeconstruct/mctp1/networks/1/endpoints/17
    |- /au/com/codeconstruct/mctp1/networks/1/endpoints/18
    |- /au/com/codeconstruct/mctp1/networks/1/endpoints/19
    |- /au/com/codeconstruct/mctp1/networks/1/endpoints/20
    |- /au/com/codeconstruct/mctp1/networks/1/endpoints/21
    |- /au/com/codeconstruct/mctp1/networks/1/endpoints/22
    |- /au/com/codeconstruct/mctp1/networks/1/endpoints/23
    |- /au/com/codeconstruct/mctp1/networks/1/endpoints/24
    |- /au/com/codeconstruct/mctp1/networks/1/endpoints/25
    |- /au/com/codeconstruct/mctp1/networks/1/endpoints/26
    |- /au/com/codeconstruct/mctp1/networks/1/endpoints/27
    '''
    
    Change-Id: I2a2c30605b2738f64d30b3f3efcdefdece8e5652
    Signed-off-by: Saptarsi Saha <saptarsis@nvidia.com>

--- src/mctp/MCTPEndpoint.hpp
+++ src/mctp/MCTPEndpoint.hpp
@@ -267,6 +267,8 @@ class MCTPDDevice :
     MCTPDDevice(const std::shared_ptr<sdbusplus::asio::connection>& connection,
                 const std::string& interface,
                 const std::vector<uint8_t>& physaddr);
+    MCTPDDevice(const std::shared_ptr<sdbusplus::asio::connection>& connection,
+                const std::string& usbInterfaceName);
     MCTPDDevice(const MCTPDDevice& other) = delete;
     MCTPDDevice(MCTPDDevice&& other) = delete;
     ~MCTPDDevice() override = default;
@@ -285,6 +287,7 @@ class MCTPDDevice :
     std::shared_ptr<sdbusplus::asio::connection> connection;
     const std::string interface;
     const std::vector<uint8_t> physaddr;
+    const std::string usbInterfaceName;
     std::shared_ptr<MCTPDEndpoint> endpoint;
     std::unique_ptr<sdbusplus::bus::match_t> removeMatch;
 
@@ -325,4 +328,25 @@ class I2CMCTPDDevice : public MCTPDDevice
     static constexpr const char* configType = "MCTPI2CTarget";
 
     static std::string interfaceFromBus(int bus);
+};
+
+class USBMCTPDDevice : public MCTPDDevice
+{
+  public:
+    static std::optional<SensorBaseConfigMap> match(const SensorData& config);
+    static bool match(const std::set<std::string>& interfaces);
+    static std::shared_ptr<USBMCTPDDevice> from(
+        const std::shared_ptr<sdbusplus::asio::connection>& connection,
+        const SensorBaseConfigMap& iface);
+
+    USBMCTPDDevice() = delete;
+    USBMCTPDDevice(
+        const std::shared_ptr<sdbusplus::asio::connection>& connection,
+        const std::string& usbInterfaceName) :
+        MCTPDDevice(connection, usbInterfaceName)
+    {}
+    ~USBMCTPDDevice() override = default;
+
+  private:
+    static constexpr const char* configType = "MCTPUSBDevice";
 };
