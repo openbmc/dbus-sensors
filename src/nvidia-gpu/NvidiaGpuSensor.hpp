e37f666f4fe82b0617dd8fb6e03134420ddef06d
Harshit Aghera <haghera@nvidia.com>
2025-05-27 12:19:29 +0530

    nvidia-gpu: add dram temperature sensor
    This commit introduces a dram temperature sensor for the GPU, enhancing
    the existing temperature monitoring capabilities.
    
    Tested: Build an image for gb200nvl-obmc machine with the following
    patches cherry picked. This patches are needed to enable the mctp stack.
    
    https://gerrit.openbmc.org/c/openbmc/openbmc/+/79422
    
    ```
    $ curl -s -k -u 'root:0penBmc' https://10.137.203.137/redfish/v1/Chassis/NVIDIA_GB200_1/Sensors/temperature_NVIDIA_GB200_GPU_0_DRAM_0_TEMP_0
    {
      "@odata.id": "/redfish/v1/Chassis/NVIDIA_GB200_1/Sensors/temperature_NVIDIA_GB200_GPU_0_DRAM_0_TEMP_0",
      "@odata.type": "#Sensor.v1_2_0.Sensor",
      "Id": "temperature_NVIDIA_GB200_GPU_0_DRAM_0_TEMP_0",
      "Name": "NVIDIA GB200 GPU 0 DRAM 0 TEMP 0",
      "Reading": 30.0,
      "ReadingRangeMax": 127.0,
      "ReadingRangeMin": -128.0,
      "ReadingType": "Temperature",
      "ReadingUnits": "Cel",
      "Status": {
        "Health": "OK",
        "State": "Enabled"
      },
      "Thresholds": {
        "UpperCritical": {
          "Reading": 95.0
        }
      }
    }%
    ```
    
    Change-Id: I914bb94f85e2d4163397b71a08b4ddd8b171e7d7
    Signed-off-by: Harshit Aghera <haghera@nvidia.com>

--- src/nvidia-gpu/NvidiaGpuSensor.hpp
+++ src/nvidia-gpu/NvidiaGpuSensor.hpp
@@ -22,6 +22,7 @@
 
 constexpr uint8_t gpuTempSensorId{0};
 constexpr uint8_t gpuTLimitSensorId{2};
+constexpr uint8_t gpuDramTempSensorId{1};
 constexpr uint8_t smaTempSensorId{5};
 
 struct NvidiaGpuTempSensor :
