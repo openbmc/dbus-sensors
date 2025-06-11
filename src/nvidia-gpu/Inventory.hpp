8b84f711f4cfa80f68e2f550c77a33d4dde37481
Rohit PAI <ropai@nvidia.com>
2025-06-11 08:52:29 +0530

    Nvidia-gpu: Create GPU Inventory device
    GPU device class implements Item.accelerator interface to get identified
    as as GPU device. This will be used in RF to populate the GPU processor
    schema.
    
    Tested -
    ```
    root@gb200nvl-obmc:~# busctl introspect xyz.openbmc_project.GpuSensor /xyz/openbmc_project/inventory/NVIDIA_GB200_GPU_0
    NAME                                           TYPE      SIGNATURE RESULT/VALUE FLAGS
    org.freedesktop.DBus.Introspectable            interface -         -            -
    .Introspect                                    method    -         s            -
    org.freedesktop.DBus.Peer                      interface -         -            -
    .GetMachineId                                  method    -         s            -
    .Ping                                          method    -         -            -
    org.freedesktop.DBus.Properties                interface -         -            -
    .Get                                           method    ss        v            -
    .GetAll                                        method    s         a{sv}        -
    .Set                                           method    ssv       -            -
    .PropertiesChanged                             signal    sa{sv}as  -            -
    xyz.openbmc_project.Inventory.Item.Accelerator interface -         -            -
    .Type                                          property  s         "GPU"        emits-change
    ```
    
    Change-Id: I20434529860cb37889e63651bbcd97cadfa9d54e
    Signed-off-by: Rohit PAI <ropai@nvidia.com>

---
+++ src/nvidia-gpu/Inventory.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "MctpRequester.hpp"
+
+#include <NvidiaGpuMctpVdm.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+
+#include <array>
+#include <memory>
+#include <string>
+
+class Inventory
+{
+  public:
+    enum class DeviceType
+    {
+        Unknown,
+        GPU,
+    };
+
+    Inventory(const std::shared_ptr<sdbusplus::asio::connection>& conn,
+              sdbusplus::asio::object_server& objectServer,
+              const std::string& inventoryName, DeviceType deviceType);
+
+  private:
+    std::shared_ptr<sdbusplus::asio::dbus_interface> acceleratorInterface;
+
+    std::string name;
+};
