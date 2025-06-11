53d16dd81f6097296fcbb9cc7d5959bf8ae823d1
Rohit PAI <ropai@nvidia.com>
2025-06-11 08:52:29 +0530

    WIP - Create GPU Inventory device
    adds Item.accelerator interface for GPU devices
    
    Change-Id: I20434529860cb37889e63651bbcd97cadfa9d54e
    Signed-off-by: Rohit PAI <ropai@nvidia.com>

---
+++ src/nvidia-gpu/Inventory.cpp
@@ -0,0 +1,25 @@
+#include "Inventory.hpp"
+
+static constexpr const char* inventoryPrefix =
+    "/xyz/openbmc_project/inventory/";
+static constexpr const char* acceleratorIfaceName =
+    "xyz.openbmc_project.Inventory.Item.Accelerator";
+static constexpr const char* assetIfaceName =
+    "xyz.openbmc_project.Inventory.Decorator.Asset";
+
+Inventory::Inventory(
+    const std::shared_ptr<sdbusplus::asio::connection>& /*conn*/,
+    sdbusplus::asio::object_server& objectServer,
+    const std::string& inventoryName, mctp::MctpRequester& mctpRequester,
+    DeviceType deviceType, uint8_t eid) :
+    path(std::string(inventoryPrefix) + inventoryName),
+    mctpRequester(mctpRequester), deviceType(deviceType), eid(eid)
+{
+    if (deviceType == DeviceType::GPU)
+    {
+        acceleratorInterface =
+            objectServer.add_interface(path, acceleratorIfaceName);
+        acceleratorInterface->register_property("Type", std::string("GPU"));
+        acceleratorInterface->initialize();
+    }
+}
