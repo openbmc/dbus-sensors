626d5e7c3d3ad7b801fcb14512a1923e75f95164
Rohit PAI <ropai@nvidia.com>
2025-06-12 17:11:46 +0530

    WIP - add support for GPU inventory properties
    support for serial number and part number fetch is added in inventory
    class.
    
    Change-Id: Id2b33a66ff6d5480f8e229fa233528afc0bdcfc0
    Signed-off-by: Rohit PAI <ropai@nvidia.com>

--- src/nvidia-gpu/Inventory.hpp
+++ src/nvidia-gpu/Inventory.hpp
@@ -3,14 +3,22 @@
 #include "MctpRequester.hpp"
 
 #include <NvidiaGpuMctpVdm.hpp>
+#include <boost/asio/steady_timer.hpp>
 #include <sdbusplus/asio/connection.hpp>
 #include <sdbusplus/asio/object_server.hpp>
 
 #include <array>
 #include <memory>
+#include <optional>
 #include <string>
+#include <string_view>
 
-class Inventory
+using InventoryRequestBuffer =
+    std::array<uint8_t, sizeof(gpu::GetInventoryInformationRequest)>;
+using InventoryResponseBuffer =
+    std::array<uint8_t, sizeof(gpu::GetInventoryInformationResponse)>;
+
+class Inventory : public std::enable_shared_from_this<Inventory>
 {
   public:
     enum class DeviceType
@@ -23,13 +31,37 @@ class Inventory
               sdbusplus::asio::object_server& objectServer,
               const std::string& inventoryName,
               mctp::MctpRequester& mctpRequester, DeviceType deviceType,
-              uint8_t eid);
+              uint8_t eid, boost::asio::io_context& io);
+
+    void fetchPartNumber();
+    void fetchSerialNumber();
+    void update();
+
+    static std::optional<std::string_view> dbusPropertyNameForId(
+        gpu::InventoryPropertyId propertyId);
 
   private:
+    void requestInventoryProperty(
+        gpu::InventoryPropertyId propertyId,
+        std::shared_ptr<InventoryRequestBuffer> requestBuffer,
+        std::shared_ptr<InventoryResponseBuffer> responseBuffer);
+
+    void handleInventoryPropertyResponse(
+        gpu::InventoryPropertyId propertyId, int sendRecvMsgResult,
+        std::shared_ptr<InventoryRequestBuffer> requestBuffer,
+        std::shared_ptr<InventoryResponseBuffer> responseBuffer);
+
+    void retryRequest(gpu::InventoryPropertyId propertyId,
+                      std::shared_ptr<InventoryRequestBuffer> requestBuffer,
+                      std::shared_ptr<InventoryResponseBuffer> responseBuffer);
+
+    std::shared_ptr<sdbusplus::asio::dbus_interface> assetIface;
     std::shared_ptr<sdbusplus::asio::dbus_interface> acceleratorInterface;
 
     std::string path;
     mctp::MctpRequester& mctpRequester;
     DeviceType deviceType;
     uint8_t eid;
+    boost::asio::steady_timer retryTimer;
+    static constexpr std::chrono::seconds retryDelay{5};
 };
