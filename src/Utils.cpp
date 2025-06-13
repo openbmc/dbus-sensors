91cdffcda767048cc654ccced23e5af47d522906
Rohit PAI <ropai@nvidia.com>
2025-06-13 18:20:02 +0530

    Nvidia-GPU: Add UUID support for GPU device
    Support for fetching UUID information using Get Inventory command
    
    Tested
    - Able to get UUID info from the GPU device and populate that on Dbus
    
    ```
     busctl introspect xyz.openbmc_project.GpuSensor  /xyz/openbmc_project/inventory/NVIDIA_GB200_GPU_0
    NAME                                                 TYPE      SIGNATURE RESULT/VALUE            FLAGS
    org.freedesktop.DBus.Introspectable                  interface -         -                       -
    .Introspect                                          method    -         s                       -
    org.freedesktop.DBus.Peer                            interface -         -                       -
    .GetMachineId                                        method    -         s                       -
    .Ping                                                method    -         -                       -
    org.freedesktop.DBus.Properties                      interface -         -                       -
    .Get                                                 method    ss        v                       -
    .GetAll                                              method    s         a{sv}                   -
    .Set                                                 method    ssv       -                       -
    .PropertiesChanged                                   signal    sa{sv}as  -                       -
    xyz.openbmc_project.Common.UUID                      interface -         -                       -
    .UUID                                                property  s         "6c13dc0f-ec0c-1fc9-db63-4d9f1053b5ef"                      emits-change
    xyz.openbmc_project.Inventory.Decorator.Asset        interface -         -                       -
    .PartNumber                                          property  s         "B40GPU"                      emits-change
    .SerialNumber                                        property  s         "1641425000136"                      emits-change
    xyz.openbmc_project.Inventory.Item.Accelerator       interface -         -                       -
    .Type                                                property  s         "GPU"                   emits-change
    
    ```
    
    Change-Id: I4600e85b3bf00e68032bb2b960cb803a76f6af96
    Signed-off-by: Rohit PAI <ropai@nvidia.com>

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -37,16 +37,20 @@
 #include <array>
 #include <chrono>
 #include <cstddef>
+#include <cstdint>
 #include <cstring>
 #include <filesystem>
 #include <fstream>
 #include <functional>
+#include <iomanip>
+#include <ios>
 #include <iterator>
 #include <memory>
 #include <optional>
 #include <regex>
 #include <set>
 #include <span>
+#include <sstream>
 #include <stdexcept>
 #include <string>
 #include <string_view>
@@ -881,4 +885,25 @@ std::vector<std::unique_ptr<sdbusplus::bus::match_t>>
         types.push_back(type.data());
     }
     return setupPropertiesChangedMatches(bus, {types}, handler);
+}
+
+std::string formatUuid(const std::vector<uint8_t>& uuidBytes)
+{
+    if (uuidBytes.size() < 16)
+    {
+        lg2::error("formatUuid: input size {SIZE} is less than 16 bytes",
+                   "SIZE", uuidBytes.size());
+        return "";
+    }
+    std::ostringstream oss;
+    oss << std::hex << std::setfill('0');
+    for (size_t i = 0; i < 16; ++i)
+    {
+        if (i == 4 || i == 6 || i == 8 || i == 10)
+        {
+            oss << '-';
+        }
+        oss << std::setw(2) << static_cast<int>(uuidBytes[i]);
+    }
+    return oss.str();
 }
