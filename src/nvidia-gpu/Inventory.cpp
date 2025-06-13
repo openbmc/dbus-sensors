9f61d8674caadfde1673deb64d908638894de5af
Rohit PAI <ropai@nvidia.com>
2025-06-13 19:29:20 +0530

    Nvidia-Gpu: Support to fetch model,revision inventory properties
    Add capability to fetch model and revision from Nvidia GPU devices using
    GET inventory command
    
    Tested
    Able to get model and revision info from the GPU
    
    ```
     busctl introspect xyz.openbmc_project.GpuSensor  /xyz/openbmc_project/inventory/NVIDIA_GB200_GPU_0
    NAME                                                 TYPE      SIGNATURE RESULT/VALUE            FLAGS
    org.freedesktop.DBus.Introspectable                  interface -         -                       -
    .Introspect                                          method    -         s                       -
    org.freedesktop.DBus.Peer                            interface -         -                       -
    .GetMachineId                                        method    -         s                       -
    .Ping                                                method    -         -                       -
    org.freedesktop.DBus.Properties                      interface -         -                       -
    .Get                                                 method    ss        v                       -
    .GetAll                                              method    s         a{sv}                   -
    .Set                                                 method    ssv       -                       -
    .PropertiesChanged                                   signal    sa{sv}as  -                       -
    xyz.openbmc_project.Common.UUID                      interface -         -                       -
    .UUID                                                property  s         "6c13dc0f-ec0c-1fc9-db63-4d9f1053b5ef"                      emits-change
    xyz.openbmc_project.Inventory.Decorator.Asset        interface -         -                       -
    .Manufacturer                                        property  s         "NVIDIA"                emits-change
    .Model                                               property  s         "RTXPRO6000BlackwellDC" emits-change
    .PartNumber                                          property  s         "B40GPU"                      emits-change
    .SerialNumber                                        property  s         "1641425000136"                      emits-change
    xyz.openbmc_project.Inventory.Decorator.Revision interface -         -                                      -
    .Version                                         property  s         "2BB5-895-A1"                          emits-change
    xyz.openbmc_project.Inventory.Item.Accelerator       interface -         -                       -
    .Type                                                property  s         "GPU"                   emits-change
    
    ```
    
    Change-Id: Ib0870f1680687272e58c49726618aeee332e3d4a
    Signed-off-by: Rohit PAI <ropai@nvidia.com>

--- src/nvidia-gpu/Inventory.cpp
+++ src/nvidia-gpu/Inventory.cpp
@@ -24,6 +24,8 @@ static constexpr const char* acceleratorIfaceName =
 static constexpr const char* assetIfaceName =
     "xyz.openbmc_project.Inventory.Decorator.Asset";
 static constexpr const char* uuidIfaceName = "xyz.openbmc_project.Common.UUID";
+static constexpr const char* revisionIfaceName =
+    "xyz.openbmc_project.Inventory.Decorator.Revision";
 
 Inventory::Inventory(
     const std::shared_ptr<sdbusplus::asio::connection>& /*conn*/,
@@ -41,6 +43,7 @@ Inventory::Inventory(
         objectServer.add_interface(path, acceleratorIfaceName);
     assetIface = objectServer.add_interface(path, assetIfaceName);
     uuidInterface = objectServer.add_interface(path, uuidIfaceName);
+    revisionIface = objectServer.add_interface(path, revisionIfaceName);
 
     // Static properties
     if (deviceType == DeviceType::GPU)
@@ -54,12 +57,17 @@ Inventory::Inventory(
                      "SerialNumber");
     registerProperty(gpu::InventoryPropertyId::BOARD_PART_NUMBER,
                      assetIfaceName, "PartNumber");
+    registerProperty(gpu::InventoryPropertyId::MARKETING_NAME, assetIfaceName,
+                     "Model");
+    registerProperty(gpu::InventoryPropertyId::DEVICE_PART_NUMBER,
+                     revisionIfaceName, "Version");
     registerProperty(gpu::InventoryPropertyId::DEVICE_GUID, uuidIfaceName,
                      "UUID");
 
     acceleratorInterface->initialize();
     assetIface->initialize();
     uuidInterface->initialize();
+    revisionIface->initialize();
     processNextProperty();
 }
 
@@ -79,6 +87,10 @@ void Inventory::registerProperty(gpu::InventoryPropertyId propertyId,
     else if (interfaceName == uuidIfaceName)
     {
         interface = uuidInterface;
+    }
+    else if (interfaceName == revisionIfaceName)
+    {
+        interface = revisionIface;
     }
 
     if (interface)
@@ -101,6 +113,16 @@ void Inventory::fetchSerialNumber()
 void Inventory::fetchUUID()
 {
     fetchInventoryProperty(gpu::InventoryPropertyId::DEVICE_GUID);
+}
+
+void Inventory::fetchMarketingName()
+{
+    fetchInventoryProperty(gpu::InventoryPropertyId::MARKETING_NAME);
+}
+
+void Inventory::fetchDevicePartNumber()
+{
+    fetchInventoryProperty(gpu::InventoryPropertyId::DEVICE_PART_NUMBER);
 }
 
 void Inventory::fetchInventoryProperty(gpu::InventoryPropertyId propertyId)
@@ -312,4 +334,6 @@ void Inventory::update()
     fetchBoardPartNumber();
     fetchSerialNumber();
     fetchUUID();
+    fetchMarketingName();
+    fetchDevicePartNumber();
 }
