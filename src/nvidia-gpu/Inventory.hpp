55108842fd4ddb82040d96cdedd34febc3eceba8
Rohit PAI <ropai@nvidia.com>
2025-06-13 20:27:49 +0530

    WIP - Support for location code
    Add location code property which is obtained from EM config
    
    Change-Id: I603ff404a794bc0ba9647452f7368f10852c7885
    Signed-off-by: Rohit PAI <ropai@nvidia.com>

--- src/nvidia-gpu/Inventory.hpp
+++ src/nvidia-gpu/Inventory.hpp
@@ -41,6 +41,7 @@ class Inventory : public std::enable_shared_from_this<Inventory>
     void fetchMarketingName();
     void fetchDevicePartNumber();
     void update();
+    void setLocationCode(const std::string& locationCode);
 
     static std::optional<std::string_view> dbusPropertyNameForId(
         gpu::InventoryPropertyId propertyId);
@@ -59,7 +60,7 @@ class Inventory : public std::enable_shared_from_this<Inventory>
                                        int sendRecvMsgResult);
     void processNextProperty();
     void fetchInventoryProperty(gpu::InventoryPropertyId propertyId);
-    void initializeInterfaces(sdbusplus::asio::object_server& objectServer);
+    void initializeInterfaces();
     void registerProperty(gpu::InventoryPropertyId propertyId,
                          const std::string& interfaceName,
                          const std::string& propertyName);
@@ -71,12 +72,14 @@ class Inventory : public std::enable_shared_from_this<Inventory>
     std::shared_ptr<sdbusplus::asio::dbus_interface> acceleratorInterface;
     std::shared_ptr<sdbusplus::asio::dbus_interface> uuidInterface;
     std::shared_ptr<sdbusplus::asio::dbus_interface> revisionIface;
+    std::shared_ptr<sdbusplus::asio::dbus_interface> locationCodeIface;
 
     std::string path;
     mctp::MctpRequester& mctpRequester;
     DeviceType deviceType;
     uint8_t eid;
     boost::asio::steady_timer retryTimer;
+    sdbusplus::asio::object_server& objectServer;
     std::unordered_map<gpu::InventoryPropertyId, PropertyInfo> properties;
     std::shared_ptr<InventoryRequestBuffer> requestBuffer;
     std::shared_ptr<InventoryResponseBuffer> responseBuffer;
