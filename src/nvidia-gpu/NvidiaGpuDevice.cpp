f7f6092a9ef8219870101789a8e949fffb0a7a68
Rohit PAI <ropai@nvidia.com>
2025-06-13 18:20:02 +0530

    WIP - Add UUID support for GPU device
    Support for fetching UUID information
    
    Change-Id: I4600e85b3bf00e68032bb2b960cb803a76f6af96
    Signed-off-by: Rohit PAI <ropai@nvidia.com>

--- src/nvidia-gpu/NvidiaGpuDevice.cpp
+++ src/nvidia-gpu/NvidiaGpuDevice.cpp
@@ -26,6 +26,8 @@
 #include <string>
 #include <vector>
 
+static constexpr const char* uuidInterfaceName = "xyz.openbmc_project.Common.UUID";
+
 GpuDevice::GpuDevice(const SensorConfigs& configs, const std::string& name,
                      const std::string& path,
                      const std::shared_ptr<sdbusplus::asio::connection>& conn,
@@ -39,8 +41,70 @@ GpuDevice::GpuDevice(const SensorConfigs& configs, const std::string& name,
 {
     inventory =
         std::make_unique<Inventory>(conn, objectServer, name, mctpRequester,
-                                    Inventory::DeviceType::GPU, eid, io);
+                                   Inventory::DeviceType::GPU, eid, io);
+    
+    uuidInterface = objectServer.add_interface(path, uuidInterfaceName);
+    uuidInterface->register_property("UUID", std::string{});
+    uuidInterface->initialize();
+    
+    fetchUUID();
     makeSensors();
+}
+
+void GpuDevice::fetchUUID()
+{
+    auto req = std::make_shared<InventoryRequestBuffer>();
+    auto resp = std::make_shared<InventoryResponseBuffer>();
+    
+    int rc = gpu::encodeGetInventoryInformationRequest(
+        0, static_cast<uint8_t>(gpu::InventoryPropertyId::DEVICE_GUID), *req);
+    if (rc != 0)
+    {
+        lg2::error("Failed to encode UUID request: rc={RC}", "RC", rc);
+        return;
+    }
+
+    mctpRequester.sendRecvMsg(
+        eid, *req, *resp,
+        [self = weak_from_this(), resp](int sendRecvMsgResult) {
+            if (auto device = self.lock())
+            {
+                device->handleUUIDResponse(sendRecvMsgResult, resp);
+            }
+        });
+}
+
+void GpuDevice::handleUUIDResponse(
+    int sendRecvMsgResult,
+    std::shared_ptr<InventoryResponseBuffer> responseBuffer)
+{
+    if (sendRecvMsgResult != 0)
+    {
+        lg2::error("Failed to get UUID from device: rc={RC}", "RC", sendRecvMsgResult);
+        return;
+    }
+
+    ocp::accelerator_management::CompletionCode cc{};
+    uint16_t reasonCode = 0;
+    gpu::InventoryInfo info;
+    int rc = gpu::decodeGetInventoryInformationResponse(
+        *responseBuffer, cc, reasonCode, gpu::InventoryPropertyId::DEVICE_GUID, info);
+    
+    if (rc != 0 || cc != ocp::accelerator_management::CompletionCode::SUCCESS)
+    {
+        lg2::error("Failed to decode UUID response: rc={RC}, cc={CC}", "RC", rc, "CC", static_cast<int>(cc));
+        return;
+    }
+
+    if (!std::holds_alternative<std::string>(info))
+    {
+        lg2::error("Invalid UUID format received from device");
+        return;
+    }
+
+    uuid = std::get<std::string>(info);
+    uuidInterface->set_property("UUID", uuid);
+    lg2::info("Successfully retrieved UUID from device: {UUID}", "UUID", uuid);
 }
 
 void GpuDevice::makeSensors()
