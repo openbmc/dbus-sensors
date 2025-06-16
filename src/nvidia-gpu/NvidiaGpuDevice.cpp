18a9cf91cc70893ee9b9b36c026cdbc04d2d46b9
Rohit PAI <ropai@nvidia.com>
2025-06-16 13:05:11 +0530

    Nvidia-GPU: Add support for GPU DRAM information
    Nvidia GPU has DRAM memory and its clock frequency can be obtained using
    GET Current Clock command
    
    Tested
    - able to see memory inventory object created for GPU
    - memory type is statically set as HBM and Speed is read from the device
    
    ```
    root@gb200nvl-obmc:~# busctl introspect xyz.openbmc_project.GpuSensor /xyz/openbmc_project/inventory/NVIDIA_GB200_GPU_0_DRAM_0
    NAME                                    TYPE      SIGNATURE RESULT/VALUE FLAGS
    org.freedesktop.DBus.Introspectable     interface -         -            -
    .Introspect                             method    -         s            -
    org.freedesktop.DBus.Peer               interface -         -            -
    .GetMachineId                           method    -         s            -
    .Ping                                   method    -         -            -
    org.freedesktop.DBus.Properties         interface -         -            -
    .Get                                    method    ss        v            -
    .GetAll                                 method    s         a{sv}        -
    .Set                                    method    ssv       -            -
    .PropertiesChanged                      signal    sa{sv}as  -            -
    xyz.openbmc_project.Inventory.Item.Dimm interface -         -            -
    .Manufacturer                           property  s         "Nvidia"     emits-change
    .MemoryConfiguredSpeedInMhz             property  u         0            emits-change
    .MemorySizeInKB                         property  t         0            emits-change
    .MemoryType                             property  s         "HBM"        emits-change
    
    ```
    
    Change-Id: I4414dd0bedef427f97f2f08e0656c37d9385d741
    Signed-off-by: Rohit PAI <ropai@nvidia.com>

--- src/nvidia-gpu/NvidiaGpuDevice.cpp
+++ src/nvidia-gpu/NvidiaGpuDevice.cpp
@@ -7,6 +7,7 @@
 #include "NvidiaGpuDevice.hpp"
 
 #include "Inventory.hpp"
+#include "Memory.hpp"
 #include "NvidiaDeviceDiscovery.hpp"
 #include "NvidiaGpuSensor.hpp"
 #include "Thresholds.hpp"
@@ -46,6 +47,15 @@ GpuDevice::GpuDevice(const SensorConfigs& configs, const std::string& name,
         std::make_unique<Inventory>(conn, objectServer, name, mctpRequester,
                                     Inventory::DeviceType::GPU, eid, io);
     makeSensors();
+
+    std::string dramName = "DRAM_0";
+    std::string memoryName = escapeName(name) + "_" + dramName;
+
+    memoryModule = std::make_shared<Memory>(conn, objectServer, memoryName,
+                                            mctpRequester, eid, io);
+
+    memoryModule->setMemoryType("HBM");
+    memoryModule->update();
 }
 
 void GpuDevice::makeSensors()
