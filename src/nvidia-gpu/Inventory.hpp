e32c95a4d86bddfde05b7f494bbccc1967782973
Rohit PAI <ropai@nvidia.com>
2025-06-19 10:57:47 +0530

    nvidia-gpu: Add processor and chassis association
    Processors will have association with its chassis resource. This is
    required in Redfish to show links between GPU processor and its
    containing chassis object.
    
    Tested
    - able to read the association property between GPU and its parent
      chassis
    ```
    busctl get-property xyz.openbmc_project.GpuSensor
    /xyz/openbmc_project/inventory/NVIDIA_GB200_GPU_0
    xyz.openbmc_project.Association.Definitions Associations
    
    a(sss) 1 "contained_by" "containing"
    "/xyz/openbmc_project/inventory/system/board
    /NVIDIA_GB200_1/NVIDIA_GB200_GPU"
    
    ```
    
    Change-Id: I5337e4c38a2f64d0bf7208229b2efde893385d0c
    Signed-off-by: Rohit PAI <ropai@nvidia.com>

--- src/nvidia-gpu/Inventory.hpp
+++ src/nvidia-gpu/Inventory.hpp
@@ -2,6 +2,7 @@
 
 #include "MctpRequester.hpp"
 #include "NvidiaGpuMctpVdm.hpp"
+#include "Utils.hpp"
 
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/steady_timer.hpp>
@@ -15,6 +16,7 @@
 #include <optional>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 using InventoryRequestBuffer =
     std::array<uint8_t, sizeof(gpu::GetInventoryInformationRequest)>;
@@ -30,6 +32,8 @@ class Inventory : public std::enable_shared_from_this<Inventory>
               mctp::MctpRequester& mctpRequester,
               gpu::DeviceIdentification deviceType, uint8_t eid,
               boost::asio::io_context& io);
+
+    void setAssociation(const std::string& chassisPath);
 
   private:
     struct PropertyInfo
@@ -61,6 +65,7 @@ class Inventory : public std::enable_shared_from_this<Inventory>
     std::shared_ptr<sdbusplus::asio::dbus_interface> uuidInterface;
     std::shared_ptr<sdbusplus::asio::dbus_interface> revisionIface;
     std::shared_ptr<sdbusplus::asio::dbus_interface> locationCodeIface;
+    std::shared_ptr<sdbusplus::asio::dbus_interface> associationInterface;
 
     std::string name;
     mctp::MctpRequester& mctpRequester;
@@ -72,4 +77,7 @@ class Inventory : public std::enable_shared_from_this<Inventory>
     std::shared_ptr<InventoryResponseBuffer> responseBuffer;
     static constexpr std::chrono::seconds retryDelay{5};
     static constexpr int maxRetryAttempts = 3;
+    std::string inventoryPath;
+    std::vector<Association> associations;
+    sdbusplus::asio::object_server& objectServer;
 };
