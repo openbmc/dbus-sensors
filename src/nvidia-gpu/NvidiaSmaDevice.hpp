fa6fb7f0e1bff085fc57f467d35e4f6f8dfe69e4
Harshit Aghera <haghera@nvidia.com>
2025-06-25 15:25:33 +0530

    nvidia-gpu: add SMA Temperature Sensor
    Add support for device type SMA (System Management Agent) and its
    temperature sensor. It is typically an MCU device.
    
    Tested: Build an image for gb200nvl-obmc machine with the following
    patches cherry picked. This patches are needed to enable the mctp stack.
    
    https://gerrit.openbmc.org/c/openbmc/openbmc/+/79422
    
    ```
    $ curl -s -k -u 'root:0penBmc' https://10.137.203.193/redfish/v1/Chassis/NVIDIA_GB200_1/Sensors/temperature_NVIDIA_GB200_GPU_SMA_255_TEMP_0
    {
      "@odata.id": "/redfish/v1/Chassis/NVIDIA_GB200_1/Sensors/temperature_NVIDIA_GB200_GPU_SMA_255_TEMP_0",
      "@odata.type": "#Sensor.v1_2_0.Sensor",
      "Id": "temperature_NVIDIA_GB200_GPU_SMA_255_TEMP_0",
      "Name": "NVIDIA GB200 GPU SMA 255 TEMP 0",
      "Reading": 34.0,
      "ReadingRangeMax": 127.0,
      "ReadingRangeMin": -128.0,
      "ReadingType": "Temperature",
      "ReadingUnits": "Cel",
      "Status": {
        "Health": "OK",
        "State": "Enabled"
      }
    }%
    ```
    
    Change-Id: I560864758036a5b6ea6c1745145736c7bfa0a1c5
    Signed-off-by: Harshit Aghera <haghera@nvidia.com>

---
+++ src/nvidia-gpu/NvidiaSmaDevice.hpp
@@ -0,0 +1,62 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION &
+ * AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include "MctpRequester.hpp"
+#include "NvidiaDeviceDiscovery.hpp"
+#include "NvidiaGpuSensor.hpp"
+
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/steady_timer.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+
+#include <chrono>
+#include <cstdint>
+#include <memory>
+#include <string>
+
+class SmaDevice
+{
+  public:
+    SmaDevice(const SensorConfigs& configs, const std::string& name,
+              const std::string& path,
+              const std::shared_ptr<sdbusplus::asio::connection>& conn,
+              uint8_t eid, boost::asio::io_context& io,
+              mctp::MctpRequester& mctpRequester,
+              sdbusplus::asio::object_server& objectServer);
+
+    const std::string& getPath() const
+    {
+        return path;
+    }
+
+  private:
+    void makeSensors();
+
+    void read();
+
+    uint8_t eid{};
+
+    std::chrono::milliseconds sensorPollMs;
+
+    boost::asio::steady_timer waitTimer;
+
+    mctp::MctpRequester& mctpRequester;
+
+    std::shared_ptr<sdbusplus::asio::connection> conn;
+
+    sdbusplus::asio::object_server& objectServer;
+
+    std::shared_ptr<NvidiaGpuTempSensor> tempSensor;
+
+    SensorConfigs configs;
+
+    std::string name;
+
+    std::string path;
+};
