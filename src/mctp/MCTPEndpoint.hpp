195452cb56f9c415edd270d852d2c18200b2234b
Saptarsi Saha <saptarsis@nvidia.com>
2025-07-14 08:09:37 +0530

    MctpReactor: LocalEID changes
    Changes enable a new property for adding LocalEID
    from the EM config[1] in the mctp reactor reducing
    the need of being setup using the mctp command.
    
    Tested
    '''
    Changes were tested on gb200obmc platform where the
    localEIDs were setup correctly for the interfaces
    via the mctpreactor.
    '''
    
    [1]https://gerrit.openbmc.org/c/openbmc/entity-manager/+/81931
    
    Change-Id: Iee4a8eaa8aaeefdffb0fc399effdf6e3e5a117b1
    Signed-off-by: Saptarsi Saha <saptarsis@nvidia.com>

--- src/mctp/MCTPEndpoint.hpp
+++ src/mctp/MCTPEndpoint.hpp
@@ -266,7 +266,8 @@ class MCTPDDevice :
     MCTPDDevice() = delete;
     MCTPDDevice(const std::shared_ptr<sdbusplus::asio::connection>& connection,
                 const std::string& interface,
-                const std::vector<uint8_t>& physaddr);
+                const std::vector<uint8_t>& physaddr,
+                const std::string& localEID);
     MCTPDDevice(const MCTPDDevice& other) = delete;
     MCTPDDevice(MCTPDDevice&& other) = delete;
     ~MCTPDDevice() override = default;
@@ -285,6 +286,7 @@ class MCTPDDevice :
     std::shared_ptr<sdbusplus::asio::connection> connection;
     const std::string interface;
     const std::vector<uint8_t> physaddr;
+    const std::string localEID;
     std::shared_ptr<MCTPDEndpoint> endpoint;
     std::unique_ptr<sdbusplus::bus::match_t> removeMatch;
 
@@ -316,8 +318,8 @@ class I2CMCTPDDevice : public MCTPDDevice
     I2CMCTPDDevice() = delete;
     I2CMCTPDDevice(
         const std::shared_ptr<sdbusplus::asio::connection>& connection, int bus,
-        uint8_t physaddr) :
-        MCTPDDevice(connection, interfaceFromBus(bus), {physaddr})
+        uint8_t physaddr, const std::string& localEID) :
+        MCTPDDevice(connection, interfaceFromBus(bus), {physaddr}, localEID)
     {}
     ~I2CMCTPDDevice() override = default;
 
@@ -339,8 +341,8 @@ class I3CMCTPDDevice : public MCTPDDevice
     I3CMCTPDDevice() = delete;
     I3CMCTPDDevice(
         const std::shared_ptr<sdbusplus::asio::connection>& connection, int bus,
-        const std::vector<uint8_t>& physaddr) :
-        MCTPDDevice(connection, interfaceFromBus(bus), physaddr)
+        uint8_t physaddr, const std::string& localEID) :
+        MCTPDDevice(connection, interfaceFromBus(bus), {physaddr}, localEID)
     {}
     ~I3CMCTPDDevice() override = default;
 
