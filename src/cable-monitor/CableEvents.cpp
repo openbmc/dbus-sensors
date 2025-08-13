eb1455f67792c017a24a7f78ba72b32c8ec85296
Eldin Lee <eldin.lee@quantatw.com>
2025-08-13 16:41:18 +0800

    cable-monitor: fix generateCableEvent behavior and logging
    Updates:
    - Resolve any pending CableConnected/CableDisconnected event for the
    same port before committing a new one.
    - Restore the CableConnected warning log that was not emitted as
    expected.
    
    Change-Id: I142a91f76ea148c563f69240c272295212af7753
    Signed-off-by: Eldin Lee <eldin.lee@quantatw.com>

--- src/cable-monitor/CableEvents.cpp
+++ src/cable-monitor/CableEvents.cpp
@@ -17,19 +17,22 @@ auto Events::generateCableEvent(Type type, std::string name)
 {
     // Added NO_LINT to bypass clang-tidy warning about STDEXEC_ASSERT as clang
     // seems to be confused about context being uninitialized.
-    // NOLINTNEXTLINE(clang-analyzer-core.uninitialized.Branch)
+    // NOLINTBEGIN(clang-analyzer-core.uninitialized.Branch)
+    auto pendingEvent = pendingEvents.find(name);
+    if (pendingEvent != pendingEvents.end())
+    {
+        co_await lg2::resolve(ctx, pendingEvent->second);
+        pendingEvents.erase(pendingEvent);
+    }
+
     if (type == Type::connected)
     {
-        auto pendingEvent = pendingEvents.find(name);
-        if (pendingEvent != pendingEvents.end())
-        {
-            co_await lg2::resolve(ctx, pendingEvent->second);
-
-            using CableConnected = sdbusplus::event::xyz::openbmc_project::
-                state::Cable::CableConnected;
+        using CableConnected = sdbusplus::event::xyz::openbmc_project::state::
+            Cable::CableConnected;
+        auto eventPath =
             co_await lg2::commit(ctx, CableConnected("PORT_ID", name));
-            pendingEvents.erase(pendingEvent);
-        }
+        warning("Generate CableConnected for {NAME}", "NAME", name);
+        pendingEvents.emplace(name, eventPath);
     }
     else if (type == Type::disconnected)
     {
@@ -37,6 +40,7 @@ auto Events::generateCableEvent(Type type, std::string name)
             state::Cable::CableDisconnected;
         auto eventPath =
             co_await lg2::commit(ctx, CableDisconnected("PORT_ID", name));
+
         warning("Generate CableDisconnected for {NAME}", "NAME", name);
         pendingEvents.emplace(name, eventPath);
     }
@@ -45,6 +49,7 @@ auto Events::generateCableEvent(Type type, std::string name)
         error("Unknown cable event type");
     }
     co_return;
+    // NOLINTEND(clang-analyzer-core.uninitialized.Branch)
 }
 
 } // namespace cable
