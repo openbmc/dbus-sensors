93b34b79e179a551b05a79613222d865dd709ddc
Eldin Lee <eldin.lee@quantatw.com>
2025-08-13 14:13:24 +0800

    cable-monitor: fix generateCableEvent behavior and logging
    Updates:
    - Resolve any pending CableConnected/CableDisconnected event for the
    same port before committing a new one.
    - Restore the CableConnected warning log that was not emitted as
    expected.
    
    Change-Id: I142a91f76ea148c563f69240c272295212af7753
    Signed-off-by: Eldin Lee <eldin.lee@quantatw.com>

--- src/cable-monitor/CableEvents.cpp
+++ src/cable-monitor/CableEvents.cpp
@@ -15,21 +15,25 @@ namespace cable
 auto Events::generateCableEvent(Type type, std::string name)
     -> sdbusplus::async::task<>
 {
-    // Added NO_LINT to bypass clang-tidy warning about STDEXEC_ASSERT as clang
-    // seems to be confused about context being uninitialized.
-    // NOLINTNEXTLINE(clang-analyzer-core.uninitialized.Branch)
+
+    auto pendingEvent = pendingEvents.find(name);
+    if (pendingEvent != pendingEvents.end())
+    {
+        // Added NO_LINT to bypass clang-tidy warning about STDEXEC_ASSERT as
+        // clang seems to be confused about context being uninitialized.
+        // NOLINTNEXTLINE(clang-analyzer-core.uninitialized.Branch)
+        co_await lg2::resolve(ctx, pendingEvent->second);
+        pendingEvents.erase(pendingEvent);
+    }
+
     if (type == Type::connected)
     {
-        auto pendingEvent = pendingEvents.find(name);
-        if (pendingEvent != pendingEvents.end())
-        {
-            co_await lg2::resolve(ctx, pendingEvent->second);
-
-            using CableConnected = sdbusplus::event::xyz::openbmc_project::
-                state::Cable::CableConnected;
+        using CableConnected = sdbusplus::event::xyz::openbmc_project::state::
+            Cable::CableConnected;
+        auto eventPath =
             co_await lg2::commit(ctx, CableConnected("PORT_ID", name));
-            pendingEvents.erase(pendingEvent);
-        }
+        warning("Generate CableConnected for {NAME}", "NAME", name);
+        pendingEvents.emplace(name, eventPath);
     }
     else if (type == Type::disconnected)
     {
@@ -37,6 +41,7 @@ auto Events::generateCableEvent(Type type, std::string name)
             state::Cable::CableDisconnected;
         auto eventPath =
             co_await lg2::commit(ctx, CableDisconnected("PORT_ID", name));
+
         warning("Generate CableDisconnected for {NAME}", "NAME", name);
         pendingEvents.emplace(name, eventPath);
     }
