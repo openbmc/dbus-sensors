e4f094f8202d05a181d7364ee153b07d72ce5e14
Andy Hsu <andy.hsu.wiwynn@gmail.com>
2025-08-15 15:44:31 +0800

    PSUSensor:add property to sensorTable and labelMatch
    Description
    Add "current" to the sensorTable, and add "in_current" and "in_power"
     to labelMatch.
    
    Motivation
    Using the RTQ6056 to monitor current and power requires additional
     properties.
    
    Test Plan
    Confirmed expected sensors appeared on dbus
    
    Change-Id: I4767352847d2e43ad356ebb57f61b5a0d9f6180d
    Signed-off-by: Andy Hsu <andy.hsu.wiwynn@gmail.com>

--- src/psu/PSUSensorMain.cpp
+++ src/psu/PSUSensorMain.cpp
@@ -185,8 +185,9 @@ struct SensorUnit
     auto operator<=>(const SensorUnit&) const = default;
 };
 
-static constexpr const std::array<SensorUnit, 6> sensorTable{{
+static constexpr const std::array<SensorUnit, 7> sensorTable{{
     {"curr", sensor_paths::unitAmperes},
+    {"current", sensor_paths::unitAmperes},
     {"fan", sensor_paths::unitRPMs},
     {"in", sensor_paths::unitVolts},
     {"power", sensor_paths::unitWatts},
@@ -194,10 +195,12 @@ static constexpr const std::array<SensorUnit, 6> sensorTable{{
     {"voltage", sensor_paths::unitVolts},
 }};
 
-static constexpr std::array<PSUProperty, 20> labelMatch{{
+static constexpr std::array<PSUProperty, 22> labelMatch{{
     {"curr", "Output Current", 255, 0, 3, 0},
     {"fan", "Fan Speed ", 30000, 0, 0, 0},
     {"iin", "Input Current", 20, 0, 3, 0},
+    {"in_current", "Output Current", 255, 0, 3, 0},
+    {"in_power", "Output Power", 3000, 0, 6, 0},
     {"in_voltage", "Output Voltage", 255, 0, 3, 0},
     {"in", "Output Voltage", 255, 0, 3, 0},
     {"iout", "Output Current", 255, 0, 3, 0},
