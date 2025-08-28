3b46ad1358a3c26dab7bfa48a8b88a4fe10ac0e7
Harshit Aghera <haghera@nvidia.com>
2025-08-28 14:05:29 +0530

    nvidia-gpu: add support for ConnectX device
    Add support to discover ConnectX devices and to populate PCIe interface
    properties using Phosphor DBus Interface
    xyz.openbmc_project.Inventory.Item.PCIeDevice.
    
    ConnectX device has an integrated PCIe Switch. The patch uses
    xyz.openbmc_project.Inventory.Item.PCIeSwitch PDI to define the PCIe
    Switch resource.
    
    Tested: Build an image for nvl32-obmc machine with the following patches
    cherry picked.
    
    https://gerrit.openbmc.org/c/openbmc/openbmc/+/84222
    
    ```
    root@nvl32-bmc:~# busctl tree xyz.openbmc_project.GpuSensor
    `- /xyz
      `- /xyz/openbmc_project
        |- /xyz/openbmc_project/inventory
        | `- /xyz/openbmc_project/inventory/pcie_devices
        |   |- /xyz/openbmc_project/inventory/pcie_devices/Nvidia_ConnectX_0
        |   |- /xyz/openbmc_project/inventory/pcie_devices/Nvidia_ConnectX_1
        |   |- /xyz/openbmc_project/inventory/pcie_devices/Nvidia_ConnectX_2
        |   `- /xyz/openbmc_project/inventory/pcie_devices/Nvidia_ConnectX_3
    
    root@nvl32-obmc:~# busctl introspect xyz.openbmc_project.GpuSensor /xyz/openbmc_project/inventory/pcie_devices/Nvidia_ConnectX_0
    NAME                                          TYPE      SIGNATURE RESULT/VALUE                             FLAGS
    org.freedesktop.DBus.Introspectable           interface -         -                                        -
    .Introspect                                   method    -         s                                        -
    org.freedesktop.DBus.Peer                     interface -         -                                        -
    .GetMachineId                                 method    -         s                                        -
    .Ping                                         method    -         -                                        -
    org.freedesktop.DBus.Properties               interface -         -                                        -
    .Get                                          method    ss        v                                        -
    .GetAll                                       method    s         a{sv}                                    -
    .Set                                          method    ssv       -                                        -
    .PropertiesChanged                            signal    sa{sv}as  -                                        -
    xyz.openbmc_project.Inventory.Item.PCIeDevice interface -         -                                        -
    .GenerationInUse                              property  s         "xyz.openbmc_project.Inventory.Item.P... emits-change
    .GenerationSupported                          property  s         "xyz.openbmc_project.Inventory.Item.P... emits-change
    .LanesInUse                                   property  u         8                                        emits-change
    .MaxLanes                                     property  u         16                                       emits-change
    xyz.openbmc_project.Inventory.Item.PCIeSwitch interface -         -                                        -
    
    $ curl -s -k -u 'root:0penBmc' https://${bmc_ip}/redfish/v1/Systems/system/PCIeDevices/Nvidia_ConnectX_0
    {
      "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/Nvidia_ConnectX_0",
      "@odata.type": "#PCIeDevice.v1_19_0.PCIeDevice",
      "Id": "Nvidia_ConnectX_0",
      "Name": "PCIe Device",
      "PCIeFunctions": {
        "@odata.id": "/redfish/v1/Systems/system/PCIeDevices/Nvidia_ConnectX_0/PCIeFunctions"
      },
      "PCIeInterface": {
        "LanesInUse": 8,
        "MaxLanes": 16,
        "MaxPCIeType": "Gen5",
        "PCIeType": "Gen5"
      },
      "Status": {
        "Health": "OK",
        "State": "Enabled"
      }
    }%
    ```
    
    Change-Id: Id89ce8a298ebb16934e94efcb9ca4679f91a7b26
    Signed-off-by: Harshit Aghera <haghera@nvidia.com>

---
+++ src/nvidia-gpu/NvidiaPcieDevice.hpp
@@ -0,0 +1,66 @@
+/*
+ * SPDX-FileCopyrightText: Copyright OpenBMC Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include "MctpRequester.hpp"
+#include "NvidiaDeviceDiscovery.hpp"
+#include "NvidiaPcieInterface.hpp"
+
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/steady_timer.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+
+#include <chrono>
+#include <cstdint>
+#include <memory>
+#include <string>
+
+constexpr const char* pcieDevicePathPrefix =
+    "/xyz/openbmc_project/inventory/pcie_devices/";
+
+class PcieDevice
+{
+  public:
+    PcieDevice(const SensorConfigs& configs, const std::string& name,
+               const std::string& path,
+               const std::shared_ptr<sdbusplus::asio::connection>& conn,
+               uint8_t eid, boost::asio::io_context& io,
+               mctp::MctpRequester& mctpRequester,
+               sdbusplus::asio::object_server& objectServer);
+
+    const std::string& getPath() const
+    {
+        return path;
+    }
+
+    void init();
+
+  private:
+    void makeSensors();
+
+    void read();
+
+    uint8_t eid{};
+
+    std::chrono::milliseconds sensorPollMs;
+
+    boost::asio::steady_timer waitTimer;
+
+    mctp::MctpRequester& mctpRequester;
+
+    std::shared_ptr<sdbusplus::asio::connection> conn;
+
+    sdbusplus::asio::object_server& objectServer;
+
+    SensorConfigs configs;
+
+    std::string name;
+
+    std::string path;
+
+    std::shared_ptr<NvidiaPcieInterface> pcieInterface;
+};
