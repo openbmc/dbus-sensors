143d00e0eca5c186ae793c1b168aae645f0f5944
Peter Yin <peter.yin@quantatw.com>
2025-09-16 16:56:41 +0800

    PSUSensor: add SQ52206 support
    Add device SQ52206 reading support.
    
    Change-Id: I42c57ceb7f7308a73ede91c87857619e47c846a0
    Signed-off-by: Peter Yin <peter.yin@quantatw.com>

--- src/psu/PSUSensorMain.cpp
+++ src/psu/PSUSensorMain.cpp
@@ -126,6 +126,7 @@ static const I2CDeviceTypeMap sensorTypes{
     {"PLI1209BC", I2CDeviceType{"pli1209bc", true}},
     {"pmbus", I2CDeviceType{"pmbus", true}},
     {"PXE1610", I2CDeviceType{"pxe1610", true}},
+    {"SQ52206", I2CDeviceType{"sq52206", true}},
     {"RAA228000", I2CDeviceType{"raa228000", true}},
     {"RAA228004", I2CDeviceType{"raa228004", true}},
     {"RAA228228", I2CDeviceType{"raa228228", true}},
