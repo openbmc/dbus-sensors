a9397b28492529821fef42638e3e6d1761176a08
Alexander Hansen <alexander.hansen@9elements.com>
2025-09-19 17:21:25 +0200

    meson: introduce folder structure for common code
    Establish these folders to contain common code.
    
    What's been created:
    
    - src/asio        common code for sdbusplus::asio based code
    - src/async_task  common code for sdbusplus::async::task based code
    - src/utils       for common utils
    - src/config      for generated config header
    
    This has some advantage:
    
    - easy to visually separate includes of common code from
      headers which are private to a sensor.
    
    - patterns like `#include "Inventory.hpp"` disambiguated since it is
      clear we are not including any common header here, even though it
      looks like it could be common.
    
    - easier to spot patterns where a sensor daemon accidentally compiles
      part of common code which is already defined as a static library.
      Those have been cleaned up.
    
    - easier to distribute/share maintenance responsibilities (e.g. [1])
    
    Possible follow-up changes:
    
    - mirror this folder structure in 'tests/' folder
    
    Tested: Code compiles.
    
    References:
    [1] https://gerrit.openbmc.org/c/openbmc/dbus-sensors/+/82895
    
    Change-Id: Ia256745123090e93d4fdcf85a08a672620211d32
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- src/nvidia-gpu/NvidiaGpuVoltageSensor.cpp
+++ src/nvidia-gpu/NvidiaGpuVoltageSensor.cpp
@@ -5,10 +5,10 @@
 
 #include "NvidiaGpuVoltageSensor.hpp"
 
-#include "SensorPaths.hpp"
-#include "Thresholds.hpp"
-#include "Utils.hpp"
-#include "sensor.hpp"
+#include "asio/Thresholds.hpp"
+#include "asio/sensor.hpp"
+#include "utils/SensorPaths.hpp"
+#include "utils/Utils.hpp"
 
 #include <bits/basic_string.h>
 
