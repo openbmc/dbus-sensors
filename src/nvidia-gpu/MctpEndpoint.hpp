41bdc2a055a40301cb03ce51045f133780f04299
Ed Tanous <etanous@nvidia.com>
2025-09-29 10:47:58 -0700

    Make MctpEndpoint class
    This class wraps the boost endpoint to provide an MCTP specific class
    that implements various size checks around use of an MCTP endpoint.
    
    Change-Id: Id43e44591b5cccdce46149cfc4c84834342ec663
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

---
+++ src/nvidia-gpu/MctpEndpoint.hpp
@@ -0,0 +1,65 @@
+
+#include <boost/asio/generic/datagram_protocol.hpp>
+#include <phosphor-logging/lg2.hpp>
+
+// Becuase of issues with glibc not matching linux, we need to make sure these
+// are included AFTER the system headers, which are implictly included by boost.
+// clang-format off
+#include <linux/mctp.h>
+#include <sys/socket.h>
+// clang-format off
+
+// Wrapper around boost::asio::generic::datagram_protocol::endpoint to provide
+// MCTP specific APIs
+struct MctpEndpoint
+{
+    MctpEndpoint() = default;
+    MctpEndpoint(const MctpEndpoint&) = delete;
+    MctpEndpoint(MctpEndpoint&&) = delete;
+    MctpEndpoint& operator=(const MctpEndpoint&) = delete;
+    MctpEndpoint& operator=(MctpEndpoint&&) = delete;
+
+    boost::asio::generic::datagram_protocol::endpoint endpoint;
+
+    MctpEndpoint(boost::asio::generic::datagram_protocol::endpoint endpoint) :
+        endpoint(endpoint)
+    {}
+
+    uint8_t eid() const
+    {
+        if (endpoint.size() < sizeof(struct sockaddr_mctp))
+        {
+            lg2::error("MctpRequester: Received endpoint is too small?");
+            return std::numeric_limits<uint8_t>::max();
+        }
+
+        struct sockaddr_mctp* respAddr = getSockAddr();
+        if (respAddr == nullptr)
+        {
+            return std::numeric_limits<uint8_t>::max();
+        }
+
+        return respAddr->smctp_addr.s_addr;
+    }
+
+    uint8_t type() const
+    {
+        struct sockaddr_mctp* respAddr = getSockAddr();
+        if (respAddr == nullptr)
+        {
+            return std::numeric_limits<uint8_t>::max();
+        }
+        return respAddr->smctp_type;
+    }
+    private:
+    struct sockaddr_mctp* getSockAddr() const
+    {
+        if (endpoint.size() < sizeof(struct sockaddr_mctp))
+        {
+            lg2::error("MctpRequester: Received endpoint is too small?");
+            return nullptr;
+        }
+
+        return std::bit_cast<struct sockaddr_mctp*>(endpoint.data());
+    }
+};
