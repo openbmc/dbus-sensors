42ad11c2e5a0e63a5a02294dce00e25adc072cb8
Harshit Aghera <haghera@nvidia.com>
2025-11-10 12:32:01 +0530

    nvidia-gpu: fix crash caused by device instance ID collision
    When multiple devices of the same device type return identical instance
    IDs in response of the QueryDeviceIdentification MCTP VDM command, their
    device names become similar due to the instance ID being appended. This
    leads to multiple devices sharing the same dbus object path for their
    sensors, resulting in a dbus object path collision and an exception.
    This patch ensures that no dbus objects are created in the device class
    constructor, and that the init method is only invoked when a new device
    is instantiated. Additionally, it introduces checks for exceptions that
    may occur during device creation.
    
    Tested: Build an image for nvl32-obmc machine with the following patch
    cherry picked.
    
    https://gerrit.openbmc.org/c/openbmc/openbmc/+/85490
    
    The patch cherry-picks the following patches that are currently under
    review.
    
    ```
    1. device tree
    https://lore.kernel.org/all/aRbLqH8pLWCQryhu@molberding.nvidia.com/
    2. mctpd patches
    https://github.com/CodeConstruct/mctp/pull/85
    3. u-boot changes
    https://lore.kernel.org/openbmc/20251121-msx4-v1-0-fc0118b666c1@nvidia.com/T/#t
    4. kernel changes as specified in the openbmc patch (for espi)
    5. entity-manager changes
    https://gerrit.openbmc.org/c/openbmc/entity-manager/+/85455
    6. platform-init changes
    https://gerrit.openbmc.org/c/openbmc/platform-init/+/85456
    7. spi changes
    https://lore.kernel.org/all/20251121-w25q01jv_fixup-v1-1-3d175050db73@nvidia.com/
    ```
    
    nvidiagpusensor application does not crash.
    
    Change-Id: I44afe0233ad5dfe634b42f906c736ada82640b79
    Signed-off-by: Harshit Aghera <haghera@nvidia.com>

--- src/nvidia-gpu/NvidiaGpuDevice.cpp
+++ src/nvidia-gpu/NvidiaGpuDevice.cpp
@@ -51,18 +51,19 @@ GpuDevice::GpuDevice(const SensorConfigs& configs, const std::string& name,
                      sdbusplus::asio::object_server& objectServer) :
     eid(eid), sensorPollMs(std::chrono::milliseconds{configs.pollRate}),
     waitTimer(io, std::chrono::steady_clock::duration(0)),
-    mctpRequester(mctpRequester), conn(conn), objectServer(objectServer),
-    configs(configs), name(escapeName(name)), path(path)
+    mctpRequester(mctpRequester), io(io), conn(conn),
+    objectServer(objectServer), configs(configs), name(escapeName(name)),
+    path(path)
+{}
+
+void GpuDevice::init()
 {
     inventory = std::make_shared<Inventory>(
         conn, objectServer, name, mctpRequester,
         gpu::DeviceIdentification::DEVICE_GPU, eid, io);
-}
-
-void GpuDevice::init()
-{
-    makeSensors();
     inventory->init();
+
+    makeSensors();
 }
 
 void GpuDevice::makeSensors()
