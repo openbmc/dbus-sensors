549ae5b3566eed0a88c3d4d6026423430944b8f1
George Liu <liuxiwei@ieisystem.com>
2025-11-17 14:21:38 +0800

    Remove redundant is_method_error() checks
    The handlers registered through sdbusplus::bus::match_t only receive
    D-Bus signals. Signal messages are never sent as method-error
    replies, and therefore message.is_method_error() can never be true in
    these callbacks.
    
    This change removes all unnecessary is_method_error() checks from
    signal handlers to simplify the code and avoid confusion.
    
    Change-Id: I43e4a564c1bf401a5da9819dd201464e4a59c871
    Signed-off-by: George Liu <liuxiwei@ieisystem.com>

--- src/adc/ADCSensorMain.cpp
+++ src/adc/ADCSensorMain.cpp
@@ -333,11 +333,6 @@ int main()
     boost::asio::steady_timer filterTimer(io);
     std::function<void(sdbusplus::message_t&)> eventHandler =
         [&](sdbusplus::message_t& message) {
-            if (message.is_method_error())
-            {
-                lg2::error("callback method error");
-                return;
-            }
             sensorsChanged->insert(message.get_path());
             // this implicitly cancels the timer
             filterTimer.expires_after(std::chrono::seconds(1));
