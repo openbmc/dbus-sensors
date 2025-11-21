947d95fe7d1859d22db256469387739aeaa1282c
FreddieJheng <Freddie.Jheng@quantatw.com>
2025-11-21 10:28:06 +0800

    ExternalSensor: fix Units configuration
    ExternalSensor configuration should use "DegreesC" for Units,
    but setInitialProperties() requires full D-Bus
    format (e.g., "xyz.openbmc_project.Sensor.Value.Unit.DegreesC").
    
    Add convertToFullUnits() to convert short format to full D-Bus
    format using static map lookup before setting properties.
    
    Change-Id: I8716225ee9a991a221e1783bcca1b42425b6b2c2
    Signed-off-by: Freddie Jheng <Freddie.Jheng@quantatw.com>

--- src/external/ExternalSensor.cpp
+++ src/external/ExternalSensor.cpp
@@ -60,7 +60,13 @@ ExternalSensor::ExternalSensor(
 
     association =
         objectServer.add_interface(objectPath, association::interface);
-    setInitialProperties(sensorUnits);
+    
+    std::string fullUnits = sensor_paths::convertToFullUnits(sensorUnits);
+    if (fullUnits.empty())
+    {
+        throw std::invalid_argument("Invalid units for sensor");
+    }
+    setInitialProperties(fullUnits);
 
     lg2::debug(
         "ExternalSensor '{NAME}' constructed: path '{PATH}', type '{TYPE}', "
