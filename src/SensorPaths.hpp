4f05be7e26a90d87f4a6b032ad435c8f55d3e088
Freddie Jheng <Freddie.Jheng@quantatw.com>
2025-11-24 16:17:47 +0800

    ExternalSensor: fix Units configuration
    ExternalSensor configuration should use "DegreesC" for Units,
    but setInitialProperties() requires full D-Bus
    format (e.g., "xyz.openbmc_project.Sensor.Value.Unit.DegreesC").
    
    Add convertToFullUnits() to convert short format to full D-Bus
    format using static map lookup before setting properties.
    
    Change-Id: I8716225ee9a991a221e1783bcca1b42425b6b2c2
    Signed-off-by: Freddie Jheng <Freddie.Jheng@quantatw.com>

--- src/SensorPaths.hpp
+++ src/SensorPaths.hpp
@@ -1,10 +1,33 @@
 #pragma once
 
+#include <array>
 #include <string>
 
 namespace sensor_paths
 {
 
+struct UnitMap
+{
+    std::string_view dbusPathName;
+    std::string_view dbusUnitName;
+    std::string_view emUnitName;
+};
+
+constexpr std::array<UnitMap, 11> unitsMap = {{
+    {"xyz.openbmc_project.Sensor.Value.Unit.DegreesC", "DegreesC",
+     "temperature"},
+    {"xyz.openbmc_project.Sensor.Value.Unit.RPMS", "RPMS", "fan_tach"},
+    {"xyz.openbmc_project.Sensor.Value.Unit.Volts", "Volts", "voltage"},
+    {"xyz.openbmc_project.Sensor.Value.Unit.Meters", "Meters", "distance"},
+    {"xyz.openbmc_project.Sensor.Value.Unit.Amperes", "Amperes", "current"},
+    {"xyz.openbmc_project.Sensor.Value.Unit.Watts", "Watts", "power"},
+    {"xyz.openbmc_project.Sensor.Value.Unit.Joules", "Joules", "energy"},
+    {"xyz.openbmc_project.Sensor.Value.Unit.Percent", "Percent", "percent"},
+    {"xyz.openbmc_project.Sensor.Value.Unit.CFM", "CFM", "airflow"},
+    {"xyz.openbmc_project.Sensor.Value.Unit.Pascals", "Pascals", "pressure"},
+    {"xyz.openbmc_project.Sensor.Value.Unit.PercentRH", "PercentRH",
+     "humidity"},
+}};
 // This is an allowlist of the units a sensor can measure. Should be in sync
 // with
 // phosphor-dbus-interfaces/blob/master/yaml/xyz/openbmc_project/Sensor/Value.interface.yaml#L38
@@ -31,5 +54,7 @@ constexpr const char* unitPercentRH =
 std::string getPathForUnits(const std::string& units);
 
 std::string escapePathForDbus(const std::string& name);
+
+std::string convertToFullUnits(const std::string& units);
 
 } // namespace sensor_paths
