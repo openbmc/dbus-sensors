openbmc/dbus-sensors

commit 838bd71374cf1969467d86d9097d1bb85ee33639

hwmontempsensor: Add support for BME280
This patch adds support for the Bosch BME280 environmental sensor, which
provides temperature, pressure, and humidity readings.

In addition, the sensor name mapping logic has been improved to use more
intuitive key names for pressure and humidity sensors.  Previously, both
used the same key ("Name1"), which could lead to confusion.  The mapping
has now been corrected to:

  - Name  -> Temperature
  - NamePressure -> Pressure
  - NameHumidity -> Humidity

This ensures correct identification and indexing of multi-function
sensors like the BME280, and generally improves the clarity of sensor
configuration.

This requires corresponding in entity-manager.
https://gerrit.openbmc.org/c/openbmc/entity-manager/+/82560

Change-Id: I128131c7c71c9d414a491fad18aac5e6c2aabb02
Signed-off-by: Naresh Solanki <naresh.solanki@9elements.com>

src/hwmon-temp/HwmonTempMain.cpp
@@ -64,6 +64,7 @@ static constexpr double minValueTemperature = -128;     // DegreesC
 
 static const I2CDeviceTypeMap sensorTypes{
     {"ADM1021", I2CDeviceType{"adm1021", true}},
+    {"BME280", I2CDeviceType{"bme280", false}},
     {"DPS310", I2CDeviceType{"dps310", false}},
     {"EMC1403", I2CDeviceType{"emc1403", true}},
     {"EMC1412", I2CDeviceType{"emc1412", true}},
@@ -385,11 +386,15 @@ void createSensors(
                 // Temperature has "Name", pressure has "Name1"
                 auto findSensorName = baseConfigMap.find("Name");
                 int index = 1;
-                if (thisSensorParameters.typeName == "pressure" ||
-                    thisSensorParameters.typeName == "humidity")
+                if (thisSensorParameters.typeName == "pressure")
                 {
-                    findSensorName = baseConfigMap.find("Name1");
+                    findSensorName = baseConfigMap.find("NamePressure");
                     index = 2;
+                }
+                if (thisSensorParameters.typeName == "humidity")
+                {
+                    findSensorName = baseConfigMap.find("NameHumidity");
+                    index = 3;
                 }
 
                 if (findSensorName == baseConfigMap.end())

commit 67e98e9f98e11bfe743d3bcbb79404ab8a3eb07d

hwmon: collect all Name* variants in buildSensorConfigMap
Previously the function used an index loop (Name, Name1, Name2,..)
to collect sensor names.  This broke when NameHumidity and NamePressure
were introduced in recent refactoring

Replaced the indexed loop with a range-based loop over all cfg entries,
collecting any key that starts with 'Name'.  This handles all current
and future Name* variants without requiring further code changes.

Tested against bletchley hardware with humidity sensor: humidity
was missing (how we discovered the problem), and returned with patch
in place.

Fixes: 838bd71374cf ("hwmontempsensor: Add support for BME280")

Change-Id: I6be9e60e74add06ddc6c45c2b172e7f2bbafaea3
Signed-off-by: Rick Yazwinski <rickyaz@meta.com>

src/hwmon-temp/HwmonTempMain.cpp
@@ -224,7 +224,6 @@ static SensorConfigMap buildSensorConfigMap(
             {
                 continue;
             }
-
             if ((std::get_if<uint64_t>(&busCfg->second) == nullptr) ||
                 (std::get_if<uint64_t>(&addrCfg->second) == nullptr))
             {
@@ -233,28 +232,20 @@ static SensorConfigMap buildSensorConfigMap(
             }
 
             std::vector<std::string> hwmonNames;
-            auto nameCfg = cfg.find("Name");
-            if (nameCfg != cfg.end())
+            for (const auto& [confKey, confValue] : cfg)
             {
-                hwmonNames.push_back(std::get<std::string>(nameCfg->second));
-                size_t i = 1;
-                while (true)
+                // Collect all "Name" and "Name<...>" variants (e.g. Name,
+                // Name1, Name2, NameHumidity).
+                if (confKey.starts_with("Name"))
                 {
-                    auto sensorNameCfg = cfg.find("Name" + std::to_string(i));
-                    if (sensorNameCfg == cfg.end())
-                    {
-                        break;
-                    }
-                    hwmonNames.push_back(
-                        std::get<std::string>(sensorNameCfg->second));
-                    i++;
+                    hwmonNames.push_back(std::get<std::string>(confValue));
                 }
             }
 
             SensorConfigKey key = {std::get<uint64_t>(busCfg->second),
                                    std::get<uint64_t>(addrCfg->second)};
-            SensorConfig val = {path.str, cfgData, intf, cfg, hwmonNames};
-
+            SensorConfig val = {path.str, cfgData, intf, cfg,
+                                std::move(hwmonNames)};
             auto [it, inserted] = configMap.emplace(key, std::move(val));
             if (!inserted)
             {