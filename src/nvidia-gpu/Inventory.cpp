6ec099c0758588d0e49d2dc0c479e224bcc36dfb
Eric Liu <liuer@nvidia.com>
2026-03-06 14:13:46 +0900

    nvidia-gpu: add DRAM inventory properties
    Add inventory properties to the GPU DRAM object for
    Memory Inventory GET operations:
    - xyz.openbmc_project.Inventory.Item.Dram with
      MemoryType (HBM), ECC (SingleBitECC), MemorySizeInKB
    - xyz.openbmc_project.Inventory.Decorator.Location
      with LocationType (Embedded)
    
    MemorySizeInKB is fetched via the Inventory class
    pipeline using MCTP VDM GET_INVENTORY_INFORMATION with
    property MAX_MEMORY_CAPACITY, converted from MiB to KiB.
    
    Changes:
    - Inventory.hpp/cpp: Add registerDramInventory() and
      MAX_MEMORY_CAPACITY case in handleInventoryPropertyResponse
    - NvidiaGpuDevice.cpp: Add registerDramInventory call
      and getDramItemInterface getter
    - NvidiaGpuMctpVdm.cpp: Add MAX_MEMORY_CAPACITY decode
      case in decodeGetInventoryInformationResponse
    - NvidiaGpuMemoryDevice.hpp/cpp: Add Item.Dram
      properties, Location interface, getDramItemInterface
    
    Tested: Build an image for nvl32-obmc machine with
    the following patch cherry picked.
    https://gerrit.openbmc.org/c/openbmc/dbus-sensors/+/86946
    https://gerrit.openbmc.org/c/openbmc/dbus-sensors/+/87166
    https://gerrit.openbmc.org/c/openbmc/dbus-sensors/+/85080
    https://gerrit.openbmc.org/c/openbmc/bmcweb/+/87974
    
    Verified via busctl that DRAM inventory properties
    appear:
    ```
    busctl introspect xyz.openbmc_project.GpuSensor \
      /xyz/openbmc_project/inventory/\
      NVIDIA_RTX_PRO_6000_VDM_0_DRAM_0 \
      xyz.openbmc_project.Inventory.Item.Dram
    .ECC          s  "...Ecc.SingleBitECC"  emits-change
    .MemorySizeInKB  u  100236288           emits-change
    .MemoryType   s  "...DeviceType.HBM"   emits-change
    ```
    
    Verified Location:
    ```
    busctl introspect xyz.openbmc_project.GpuSensor \
      /xyz/openbmc_project/inventory/\
      NVIDIA_RTX_PRO_6000_VDM_0_DRAM_0 \
      xyz.openbmc_project.Inventory.Decorator.Location
    .LocationType s "...LocationTypes.Embedded" emits-change
    ```
    1. All DRAM inventory properties readable on all GPUs
    2. MemorySizeInKB populated via Inventory pipeline
    3. SRAM ECC and DRAM ECC unaffected
    
    Change-Id: Idc2ec2542e02fb9bef1981d501e633f17239f652
    Signed-off-by: Eric Liu <liuer@nvidia.com>

--- src/nvidia-gpu/Inventory.cpp
+++ src/nvidia-gpu/Inventory.cpp
@@ -13,6 +13,7 @@
 #include <sdbusplus/asio/object_server.hpp>
 
 #include <algorithm>
+#include <cstddef>
 #include <cstdint>
 #include <limits>
 #include <memory>
@@ -88,6 +89,13 @@ Inventory::Inventory(
 void Inventory::init()
 {
     processNextProperty();
+}
+
+void Inventory::registerDramInventory(
+    const std::shared_ptr<sdbusplus::asio::dbus_interface>& dramItemIface)
+{
+    properties[gpu::InventoryPropertyId::MAX_MEMORY_CAPACITY] = {
+        dramItemIface, "MemorySizeInKB", 0, true};
 }
 
 void Inventory::registerProperty(
@@ -269,6 +277,25 @@ void Inventory::handleInventoryPropertyResponse(
                             static_cast<uint64_t>(clockSpeed);
                         it->second.interface->set_property(
                             it->second.propertyName, clockSpeed64);
+                        success = true;
+                    }
+                    else
+                    {
+                        lg2::error(
+                            "Property ID {PROP_ID} for {NAME} expected uint32_t but got different type",
+                            "PROP_ID", static_cast<uint8_t>(propertyId), "NAME",
+                            name);
+                    }
+                    break;
+
+                case gpu::InventoryPropertyId::MAX_MEMORY_CAPACITY:
+                    if (std::holds_alternative<uint32_t>(info))
+                    {
+                        const size_t memorySizeInKB =
+                            static_cast<size_t>(std::get<uint32_t>(info)) *
+                            1024;
+                        it->second.interface->set_property(
+                            it->second.propertyName, memorySizeInKB);
                         success = true;
                     }
                     else
