1284eac1b15c1681307cd4597ac98a629966a61f
Eric Liu <liuer@nvidia.com>
2026-03-06 19:32:44 +0900

    nvidia-gpu: add GPU to DRAM memory association
    Add D-Bus association from GPU inventory object to its
    DRAM memory object to enable Redfish MemorySummary
    MemoryMetrics endpoint on Processors. The bmcweb
    implementation uses getAssociationEndPoints with
    "all_memory" to locate DRAM objects associated with
    a GPU processor and aggregate ECC error counts.
    
    The association is created on the GPU inventory path
    with forward name "all_memory" and reverse name
    "parent_processor" pointing to the DRAM_0 object.
    
    This implementation follows the interface
    definition introduced in:
    TODO:
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/XXXXX
    
    Key changes:
    - NvidiaGpuMemoryDevice.cpp: Add association
      interface on GPU path with ("all_memory",
      "parent_processor", dramPath). Add Utils.hpp and
      vector includes. Add remove_interface in destructor.
    - NvidiaGpuMemoryDevice.hpp: Add
      gpuAssociationInterface member.
    
    Tested: Build an image for nvl32-obmc with the
    following patch cherry picked.
    
    ```
    1. openbmc configuration:
       https://gerrit.openbmc.org/c/openbmc/openbmc/+/85490
    2. mctpd configuration:
       https://gerrit.openbmc.org/c/openbmc/openbmc/+/87390
    3. Device tree (upstream, in flight):
       https://lore.kernel.org/all/aRbLqH8pLWCQryhu@molberding.nvidia.com/
    4. U-Boot changes (in flight):
       https://lore.kernel.org/openbmc/20251121-msx4-v1-0-fc0118b666c1@nvidia.com/T/#t
    5. Linux changes need proper fixing (for espi)
    6. dbus-sensors patches:
       https://gerrit.openbmc.org/c/openbmc/dbus-sensors/+/86946
       https://gerrit.openbmc.org/c/openbmc/dbus-sensors/+/87166
       https://gerrit.openbmc.org/c/openbmc/dbus-sensors/+/87975
    
    busctl introspect xyz.openbmc_project.GpuSensor \
      /xyz/openbmc_project/inventory/\
      NVIDIA_RTX_PRO_6000_VDM_0 \
      xyz.openbmc_project.Association.Definitions
    .Associations  a(sss)  1 "all_memory" \
      "parent_processor" "/xyz/openbmc_project/\
      inventory/NVIDIA_RTX_PRO_6000_VDM_0_DRAM_0"
    ```
    1. Verified Association interface on GPU object
       with all_memory pointing to DRAM.
    2. Verified DRAM ECC and SRAM ECC unaffected.
    
    Change-Id: I506f2162bb4ff901a7796dae8876c151fb7315c4
    Signed-off-by: Eric Liu <liuer@nvidia.com>

--- src/nvidia-gpu/NvidiaGpuMemoryDevice.cpp
+++ src/nvidia-gpu/NvidiaGpuMemoryDevice.cpp
@@ -7,6 +7,7 @@
 
 #include "NvidiaGpuMctpVdm.hpp"
 #include "OcpMctpVdm.hpp"
+#include "Utils.hpp"
 
 #include <MctpRequester.hpp>
 #include <phosphor-logging/lg2.hpp>
@@ -19,6 +20,7 @@
 #include <span>
 #include <string>
 #include <system_error>
+#include <vector>
 
 static constexpr auto locationIfaceName =
     "xyz.openbmc_project.Inventory.Decorator.Location";
@@ -75,6 +77,19 @@ NvidiaGpuMemoryDevice::NvidiaGpuMemoryDevice(
         lg2::error("Failed to initialize DRAM ECC interface for {NAME}", "NAME",
                    dramName);
     }
+
+    std::vector<Association> associations;
+    associations.emplace_back("all_memory", "parent_processor", dramPath);
+
+    gpuAssociationInterface =
+        objectServer.add_interface(gpuPath, association::interface);
+    gpuAssociationInterface->register_property("Associations", associations);
+
+    if (!gpuAssociationInterface->initialize())
+    {
+        lg2::error("Failed to initialize Association interface for {NAME}",
+                   "NAME", gpuName);
+    }
 }
 
 NvidiaGpuMemoryDevice::~NvidiaGpuMemoryDevice()
@@ -82,6 +97,7 @@ NvidiaGpuMemoryDevice::~NvidiaGpuMemoryDevice()
     objectServer.remove_interface(sramEccInterface);
     objectServer.remove_interface(dramEccInterface);
     objectServer.remove_interface(dramLocationInterface);
+    objectServer.remove_interface(gpuAssociationInterface);
 }
 
 void NvidiaGpuMemoryDevice::update()
