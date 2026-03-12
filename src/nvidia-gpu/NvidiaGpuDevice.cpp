1cf6e8a606308b9619b5fcce1fcce85955e57b3d
Ender Hsieh <andhsieh@nvidia.com>
2026-03-12 17:57:00 +0900

    nvidia-gpu: add chassis association for GPU inventory
    Add a D-Bus association between each GPU inventory object and its
    parent chassis, allowing bmcweb to discover GPU processors via the
    ObjectMapper all_processors association endpoint.
    
    Key changes:
    - Inventory: add chassisPath parameter, register
      Association.Definitions interface with parent_chassis association
    - NvidiaGpuDevice: derive chassisPath via parent_path() and pass
      to Inventory via init()
    
    Tested: Build an image for nvl32-obmc machine with the following
    patches cherry-picked:
    
    1. openbmc configuration:
       https://gerrit.openbmc.org/c/openbmc/openbmc/+/85490
    2. mctpd configuration:
       https://gerrit.openbmc.org/c/openbmc/openbmc/+/87390
    3. Device tree (upstream, in flight):
       https://lore.kernel.org/all/aRbLqH8pLWCQryhu@molberding.nvidia.com/
    4. U-Boot changes (in flight):
       https://lore.kernel.org/openbmc/20251121-msx4-v1-0-fc0118b666c1@nvidia.com/T/#t
    5. Linux changes need proper fixing (for espi)
    
    ```
    $ busctl introspect xyz.openbmc_project.GpuSensor \
        /xyz/openbmc_project/inventory/GPU_*_*
    ...
    xyz.openbmc_project.Association.Definitions  interface  -  -  -
    .Associations  property  a(sss)  1 "parent_chassis" "all_processors"
    "{}"  emits-change
    ...
    ```
    
    Change-Id: I19fa84fbb465ea475c1f44d0aeabb0927f926f0b
    Signed-off-by: Ender Hsieh <andhsieh@nvidia.com>

--- src/nvidia-gpu/NvidiaGpuDevice.cpp
+++ src/nvidia-gpu/NvidiaGpuDevice.cpp
@@ -25,6 +25,7 @@
 #include <phosphor-logging/lg2.hpp>
 #include <sdbusplus/asio/connection.hpp>
 #include <sdbusplus/asio/object_server.hpp>
+#include <sdbusplus/message/native_types.hpp>
 
 #include <array>
 #include <chrono>
@@ -88,8 +89,10 @@ GpuDevice::~GpuDevice()
 
 void GpuDevice::init()
 {
+    const std::string chassisPath =
+        sdbusplus::message::object_path(path).parent_path();
     inventory = std::make_shared<Inventory>(
-        conn, objectServer, name, mctpRequester,
+        conn, objectServer, name, chassisPath, mctpRequester,
         gpu::DeviceIdentification::DEVICE_GPU, eid, io,
         operatingConfigInterface);
 
