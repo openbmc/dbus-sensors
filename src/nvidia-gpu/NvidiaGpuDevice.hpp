4160ecc5bfa84a5b8c9d6838d23771afe96463f6
JY Voon <jvoon@nvidia.com>
2026-03-24 14:44:59 +0900

    nvidia-gpu: expose GPU inventory interfaces
    Expose GPU inventory data through standard D-Bus interfaces so bmcweb
    can surface FirmwareVersion and ServiceLabel in the Redfish Processor
    resource.
    
    Tested: Build an image for nvl32-obmc machine with the following
    patches cherry-picked:
    
    ```
    1. Align with upstream u-boot dts tree:
       https://gerrit.openbmc.org/c/openbmc/openbmc/+/89932
    2. mctpd configuration:
       https://gerrit.openbmc.org/c/openbmc/openbmc/+/87390
    3. Enable nvidia-gpu sensor:
       https://gerrit.openbmc.org/c/openbmc/openbmc/+/89933
    ```
    
    ```
      $ busctl get-property xyz.openbmc_project.GpuSensor \
        /xyz/openbmc_project/software/Nvidia_GPU_10_Firmware \
        xyz.openbmc_project.Software.Version Version
      s "98.02.AF.00.01"
    
      $ busctl get-property xyz.openbmc_project.GpuSensor \
        /xyz/openbmc_project/software/Nvidia_GPU_10_Firmware \
        xyz.openbmc_project.Association.Definitions Associations
      a(sss) 1 "running" "ran_on" \
        "/xyz/openbmc_project/inventory/Nvidia_GPU_10"
    
      $ busctl get-property xyz.openbmc_project.GpuSensor \
        /xyz/openbmc_project/inventory/Nvidia_GPU_10 \
        xyz.openbmc_project.Inventory.Decorator.LocationCode \
        LocationCode
      s "Nvidia_GPU_10"
    
      $ busctl get-property xyz.openbmc_project.GpuSensor \
        /xyz/openbmc_project/software/Nvidia_GPU_10_Driver \
        xyz.openbmc_project.Software.Version Version
      s "580.82.07"
    ```
    
    The GPU inventory item resolves to a single Software.Version object,
    so the Redfish Processor resource reports both properties. Response
    trimmed to the relevant fields:
    
    ```
      $ busctl get-property xyz.openbmc_project.ObjectMapper \
        /xyz/openbmc_project/inventory/Nvidia_GPU_10/ran_on \
        xyz.openbmc_project.Association endpoints
      as 1 "/xyz/openbmc_project/software/Nvidia_GPU_10_Firmware"
    
      $ curl -sk -u root:0penBmc \
        https://localhost/redfish/v1/Systems/system/Processors/Nvidia_GPU_10
      {
        "@odata.id": "/redfish/v1/Systems/system/Processors/Nvidia_GPU_10",
        "@odata.type": "#Processor.v1_18_0.Processor",
        "FirmwareVersion": "98.02.AF.00.01",
        "Id": "Nvidia_GPU_10",
        "Location": {
          "PartLocation": {
            "ServiceLabel": "Nvidia_GPU_10"
          }
        },
        "Manufacturer": "NVIDIA",
        "Model": "RTXPRO6000BlackwellDC",
        "Name": "Processor",
        "ProcessorType": "Accelerator",
        "Status": {
          "Health": "OK",
          "State": "Enabled"
        }
      }
    ```
    
    Change-Id: Id0c09f4ced40dbe505ca7cbd99f6de0e847afe3b
    Signed-off-by: JY Voon <jvoon@nvidia.com>

--- src/nvidia-gpu/NvidiaGpuDevice.hpp
+++ src/nvidia-gpu/NvidiaGpuDevice.hpp
@@ -21,6 +21,7 @@
 #include <NvidiaDriverInformation.hpp>
 #include <NvidiaGpuEccMode.hpp>
 #include <NvidiaGpuEnergySensor.hpp>
+#include <NvidiaGpuFirmwareInformation.hpp>
 #include <NvidiaGpuMemoryCapacityUtilization.hpp>
 #include <NvidiaGpuPowerPeakReading.hpp>
 #include <NvidiaGpuUtilizationMetrics.hpp>
@@ -102,6 +103,7 @@ class GpuDevice : public std::enable_shared_from_this<GpuDevice>
     std::shared_ptr<NvidiaGpuVoltageSensor> voltageSensor;
     std::shared_ptr<NvidiaDriverInformation> driverInfo;
     std::shared_ptr<NvidiaGpuPowerControl> gpuPowerControl;
+    std::shared_ptr<NvidiaGpuFirmwareInformation> firmwareInfo;
     std::shared_ptr<sdbusplus::asio::dbus_interface> powerCapInterface;
     std::shared_ptr<sdbusplus::asio::dbus_interface> dramAssociationInterface;
     std::shared_ptr<sdbusplus::asio::dbus_interface> dramItemInterface;
