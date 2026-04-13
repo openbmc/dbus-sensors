e0db803fd50d9a6add00775ce7631a41ed029be4
Eric Liu <liuer@nvidia.com>
2026-04-13 16:01:42 +0900

    nvidia-gpu: expose NetworkDeviceFunction on ConnectX ports
    Set PortProtocol to Ethernet on the existing Connector.Port
    interface and create a NetworkDeviceFunction object for each
    ConnectX Ethernet port to expose its MAC address.
    
    The Permanent MAC Address value is extracted from the
    GetPortNetworkAddresses MCTP VDM response (tag 2) and exposed via
    Item.NetworkInterface on a separate NetworkDeviceFunction object,
    linked to the NetworkAdapter and the physical Port through
    associations.
    
    This enables bmcweb to:
    - Walk the adapter's exposing association to discover
      NetworkDeviceFunctions
    - Populate Ethernet.PermanentMACAddress from
      Item.NetworkInterface MACAddress
    - Walk the assigned_to association to populate
      AssignablePhysicalNetworkPorts and read Connector.Port
      PortProtocol for NetDevFuncType
    
    Object layout per Ethernet port:
    - /.../<NIC>/<Port_N>
      - Connector.Port (PortProtocol, already existed)
      - Association.Definitions (connected_to -> NIC,
        already existed)
    - /.../<NIC>/NetworkDeviceFunctions/<Port_N>  (new)
      - Item.NetworkInterface (MACAddress)
      - Association.Definitions:
        - exposed_by -> NIC
        - assigned_to -> <NIC>/<Port_N>
    
    PDI dependencies:
    - Connector.Port: add Ethernet PortProtocol
      https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/89248
    - Inventory: add Item.NetworkAdapter interface
      https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/89247
    - Inventory: add NetworkDeviceFunction associations on
      Item.NetworkInterface
      https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/89649
    
    Tested: Built an image for nvl32-obmc with the following patches
    cherry-picked:
    
    ```
    1. Align with upstream u-boot dts tree:
       https://gerrit.openbmc.org/c/openbmc/openbmc/+/89932
    2. U-Boot change phy mode (in-flight on mailing list):
       https://lore.kernel.org/openbmc/20260504044702.2613879-1-andhsieh@nvidia.com/T/#t
    3. Kernel device tree add mac mode (in-flight on mailing list):
       https://lore.kernel.org/linux-aspeed/20260505050541.3031447-1-andhsieh@nvidia.com/T/#t
    4. platform-init enable LCLK and espiCLK:
       https://gerrit.openbmc.org/c/openbmc/platform-init/+/89900
    5. mctpd configuration:
       https://gerrit.openbmc.org/c/openbmc/openbmc/+/87390
    6. Enable nvidia-gpu sensor:
       https://gerrit.openbmc.org/c/openbmc/openbmc/+/89933
    7. PDI Connector.Port Ethernet PortProtocol:
       https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/89248
    8. PDI Item.NetworkAdapter interface:
       https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/89247
    9. PDI Item.NetworkInterface associations:
       https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/89649
    ```
    
    ```
    $ busctl get-property xyz.openbmc_project.GpuSensor \
        /xyz/openbmc_project/inventory/Nvidia_ConnectX_0_NIC/Port_1 \
        xyz.openbmc_project.Inventory.Connector.Port PortProtocol
    s "xyz.openbmc_project.Inventory.Connector.Port.\
    PortProtocol.Ethernet"
    
    $ busctl get-property xyz.openbmc_project.GpuSensor \
        /xyz/openbmc_project/inventory/Nvidia_ConnectX_0_NIC\
    /NetworkDeviceFunctions/Port_1 \
        xyz.openbmc_project.Inventory.Item.NetworkInterface MACAddress
    s "00:00:00:00:00:0B"
    
    $ busctl get-property xyz.openbmc_project.GpuSensor \
        /xyz/openbmc_project/inventory/Nvidia_ConnectX_0_NIC\
    /NetworkDeviceFunctions/Port_1 \
        xyz.openbmc_project.Association.Definitions Associations
    a(sss) 2 "exposed_by" "exposing" \
    "/xyz/openbmc_project/inventory/Nvidia_ConnectX_0_NIC" \
    "assigned_to" "assigning" \
    "/xyz/openbmc_project/inventory/Nvidia_ConnectX_0_NIC/Port_1"
    ```
    
    1. Verified PortProtocol on Port_1/Port_2 and MACAddress on the
       NetworkDeviceFunctions/Port_1/Port_2 objects for all four
       ConnectX devices.
    2. Verified the exposed_by and assigned_to associations on the
       NDF object resolve via the mapper to the NetworkAdapter and
       the physical Port, so bmcweb can walk them.
    
    Change-Id: I99e3b33a7245f3afe470c75b09503d0ac0f62db2
    Signed-off-by: Eric Liu <liuer@nvidia.com>

--- src/nvidia-gpu/NvidiaPcieDevice.cpp
+++ src/nvidia-gpu/NvidiaPcieDevice.cpp
@@ -213,7 +213,7 @@ void PcieDevice::processGetNetworkPortAddressesResponse(
 
         ethPortMetrics.emplace_back(std::make_shared<NvidiaEthPortMetrics>(
             conn, mctpRequester, portName, nicDeviceName, path, eid, portNumber,
-            objectServer));
+            objectServer, addresses));
     }
 }
 
