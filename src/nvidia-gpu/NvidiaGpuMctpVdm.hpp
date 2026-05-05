0e77949b5c05fa01fdb373ab2646e14310ae6198
Mike Hsieh <mike_hsieh@quantatw.com>
2026-05-05 20:34:03 +0800

    nvidia-gpu: add support for SMA leak sensors
    The patch adds support for SMA leak sensor reading. It introduces a
    voltage sensor for leakage detection.
    
    Change-Id: Id2bbf490beaca9408dbda7019dd506eb68e65ee0
    Signed-off-by: Mike Hsieh <mike_hsieh@quantatw.com>

--- src/nvidia-gpu/NvidiaGpuMctpVdm.hpp
+++ src/nvidia-gpu/NvidiaGpuMctpVdm.hpp
@@ -61,6 +61,7 @@ enum class PlatformEnvironmentalCommands : uint8_t
     GET_CURRENT_UTILIZATION = 0x47,
     GET_ECC_MODE = 0x4F,
     GET_ECC_ERROR_COUNTS = 0x7D,
+    GET_LEAK_DETECTION_INFO = 0x17,
 };
 
 enum class PlatformEnvironmentalEvent : uint8_t
@@ -186,6 +187,9 @@ constexpr size_t maxInventoryDataSize = 256;
 
 constexpr size_t queryDeviceIdentificationRequestSize =
     ocp::accelerator_management::commonRequestSize;
+
+constexpr size_t getLeakDetectionInfoRequestSize =
+    queryDeviceIdentificationRequestSize;
 
 constexpr size_t getNumericSensorReadingRequestSize =
     ocp::accelerator_management::commonRequestSize + 1;
@@ -477,5 +481,21 @@ int decodeGetEccModeResponse(std::span<const uint8_t> buf,
 int decodeGetEccModeResponse(std::span<const uint8_t> buf,
                              bool& currentEccModeEnabled,
                              bool& pendingEccModeEnabled);
+
+int encodeGetLeakDetectionInfoRequest(uint8_t instanceId,
+                                      std::span<uint8_t> buf);
+
+struct LeakSensorData
+{
+    uint8_t sensorId{};
+    uint8_t leakState{};
+    std::vector<uint16_t> thresholds;
+    uint16_t adcReadingMv{};
+};
+
+int decodeGetLeakDetectionInfoResponse(
+    std::span<const uint8_t> buf,
+    ocp::accelerator_management::CompletionCode& cc, uint16_t& reasonCode,
+    std::vector<LeakSensorData>& parsedSensors);
 
 } // namespace gpu
