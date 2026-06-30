58908f69a91c44590f1e79daae974c8dc9c7deba
Ender Hsieh <andhsieh@nvidia.com>
2026-06-30 18:36:31 +0900

    nvidia-gpu: align constexpr string decl style
    Three declarations used non-standard forms:
    - `const constexpr char*` qualifier order (metricPath)
    - `static constexpr auto` for string literals
      (embeddedIfaceName, dramIfaceName)
    
    Normalize all to `[static] constexpr const char*` to match
    NvidiaDeviceDiscovery.hpp and other constants in nvidia-gpu.
    No behavior change.
    
    Tested: Build an image for nvl32-obmc machine with the following
    patches cherry-picked:
    
    ```
    1. Align with upstream u-boot dts tree:
       https://gerrit.openbmc.org/c/openbmc/openbmc/+/89932
    2. platform-init enable LCLK and espiCLK:
       https://gerrit.openbmc.org/c/openbmc/platform-init/+/89900
    3. mctpd configuration:
       https://gerrit.openbmc.org/c/openbmc/openbmc/+/87390
    4. Enable nvidia-gpu sensor:
       https://gerrit.openbmc.org/c/openbmc/openbmc/+/89933
    ```
    1. Pure token-level rewrite; binary identical on BMC.
    2. No journalctl regressions after rebuild.
    
    Change-Id: I41d854e3042d26ebdfb06f78b5147206378b9017
    Signed-off-by: Ender Hsieh <andhsieh@nvidia.com>

--- src/nvidia-gpu/NvidiaUtils.hpp
+++ src/nvidia-gpu/NvidiaUtils.hpp
@@ -7,13 +7,13 @@
 
 #include <sdbusplus/message/native_types.hpp>
 
-const constexpr char* metricPath = "/xyz/openbmc_project/metric/";
+constexpr const char* metricPath = "/xyz/openbmc_project/metric/";
 
 // Suffix appended to a GPU inventory path to form its DRAM inventory object,
 // e.g. /xyz/openbmc_project/inventory/Nvidia_GPU_0 -> .../Nvidia_GPU_0_DRAM_0
-const constexpr char* dramInventorySuffix = "_DRAM_0";
+constexpr const char* dramInventorySuffix = "_DRAM_0";
 
-const constexpr char* nvidiaManufacturer = "NVIDIA";
+constexpr const char* nvidiaManufacturer = "NVIDIA";
 
 inline const sdbusplus::object_path inventoryPrefix{
     "/xyz/openbmc_project/inventory"};
