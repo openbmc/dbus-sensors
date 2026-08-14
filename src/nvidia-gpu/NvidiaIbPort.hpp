ebeabbb66ed86572f253ed4d2290a62841800919
Eric Liu <liuer@nvidia.com>
2026-08-14 12:07:40 +0900

    nvidia-gpu: add InfiniBand network device function
    A ConnectX port configured for InfiniBand exposes its permanent
    node and port GUIDs instead of a MAC address. Extend the network
    port discovery to model an InfiniBand port as a Redfish
    NetworkDeviceFunction:
    
    - Connector.Port with PortProtocol InfiniBand.
    - Item.NetworkInterface on a NetworkDeviceFunctions object
      carrying PermanentNodeGUID and PermanentPortGUID, extracted from
      the GetPortNetworkAddresses response (tag 3 and tag 4).
    - exposed_by / assigned_to associations to the NetworkAdapter and
      the physical Port.
    
    This mirrors the existing Ethernet path (MAC address, tag 2); a
    given port exposes either the Ethernet MAC or the InfiniBand
    GUIDs. bmcweb reads these to populate NetDevFuncType and the
    InfiniBand.PermanentNodeGUID / PermanentPortGUID properties.
    
    This follows PDI interface:
    ```
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/92492
    ```
    
    Key changes:
    - NvidiaIbPort: new port and NetworkDeviceFunction object for an
      InfiniBand port.
    - NvidiaPcieDevice: create an InfiniBand port when the port link
      type is InfiniBand.
    
    Tested: Built an image for the vr-nvl-obmc machine with the
    following patches cherry-picked:
    
    ```
    1. Kernel
       https://lore.kernel.org/all/20260702165524.2168091-1-jackyhuang@nvidia.com/
    2. u-boot
       https://lore.kernel.org/openbmc/20260629105202.1417679-1-jackyhuang@nvidia.com/
    3. meta-nvidia
       https://gerrit.openbmc.org/c/openbmc/openbmc/+/90959
       https://gerrit.openbmc.org/c/openbmc/openbmc/+/92346
    4. platform-init
       https://gerrit.openbmc.org/c/openbmc/platform-init/+/92189
       https://gerrit.openbmc.org/c/openbmc/platform-init/+/92190
       https://gerrit.openbmc.org/c/openbmc/platform-init/+/92191
       https://gerrit.openbmc.org/c/openbmc/platform-init/+/92192
    5. mctp-over-usb-config
       https://gerrit.openbmc.org/c/openbmc/dbus-sensors/+/80452
       https://gerrit.openbmc.org/c/openbmc/dbus-sensors/+/86236
       https://gerrit.openbmc.org/c/openbmc/dbus-sensors/+/86791
    ```
    
    On that BMC a ConnectX-9 port was switched to InfiniBand mode
    (mlxconfig LINK_TYPE_P1=IB). Port 1 of that adapter reports
    InfiniBand GUIDs while port 2 stays Ethernet, so one device covers
    both branches. Object paths below drop the leading
    /xyz/openbmc_project prefix (service xyz.openbmc_project.GpuSensor):
    
    ```
    $ busctl tree xyz.openbmc_project.GpuSensor
    inventory/CX9_16_NIC
    |- inventory/CX9_16_NIC/NetworkDeviceFunctions
    |  |- inventory/CX9_16_NIC/NetworkDeviceFunctions/Port_1
    |  `- inventory/CX9_16_NIC/NetworkDeviceFunctions/Port_2
    |- inventory/CX9_16_NIC/Port_1
    `- inventory/CX9_16_NIC/Port_2
    
    $ busctl introspect xyz.openbmc_project.GpuSensor \
        .../inventory/CX9_16_NIC/Port_1
    xyz.openbmc_project.Inventory.Connector.Port
    .PortProtocol       property s "...Port.PortProtocol.InfiniBand"
    
    $ busctl introspect xyz.openbmc_project.GpuSensor \
        .../CX9_16_NIC/NetworkDeviceFunctions/Port_1
    xyz.openbmc_project.Inventory.Item.NetworkInterface
    .PermanentNodeGUID  property s "F0FB:7F03:00A4:2B64"
    .PermanentPortGUID  property s "F0FB:7F03:00A4:2B64"
    
    $ busctl introspect xyz.openbmc_project.GpuSensor \
        .../inventory/CX9_16_NIC/Port_2
    .PortProtocol       property s "...Port.PortProtocol.Ethernet"
    
    $ busctl introspect xyz.openbmc_project.GpuSensor \
        .../CX9_16_NIC/NetworkDeviceFunctions/Port_2
    .MACAddress         property s "F0:FB:7F:A4:2B:65"
    ```
    
    Change-Id: Ief66d8ede4a26e0a25ad0a2ab8f834ff8b9318fe
    Signed-off-by: Eric Liu <liuer@nvidia.com>

---
+++ src/nvidia-gpu/NvidiaIbPort.hpp
@@ -0,0 +1,38 @@
+/*
+ * SPDX-FileCopyrightText: Copyright OpenBMC Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <sdbusplus/asio/object_server.hpp>
+
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+struct NvidiaIbPort
+{
+  public:
+    NvidiaIbPort(const std::string& name, const std::string& deviceName,
+                 uint8_t eid, uint16_t portNumber,
+                 sdbusplus::asio::object_server& objectServer,
+                 const std::vector<std::pair<uint8_t, uint64_t>>& addresses);
+
+    ~NvidiaIbPort();
+
+  private:
+    sdbusplus::asio::object_server& objectServer;
+
+    std::shared_ptr<sdbusplus::asio::dbus_interface> portInterface;
+
+    std::shared_ptr<sdbusplus::asio::dbus_interface> associationInterface;
+
+    std::shared_ptr<sdbusplus::asio::dbus_interface>
+        networkDeviceFunctionInterface;
+
+    std::shared_ptr<sdbusplus::asio::dbus_interface>
+        networkDeviceFunctionAssociationInterface;
+};
