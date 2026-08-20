48527fc23f1e4c6c3d065773a47698a5e43ffb10
Praveen Balakrishnan Vasantha <praveenbv@ami.com>
2026-08-20 10:19:07 +0530

    external: add sensor number support
    Added support for parsing SensorNumber and LUN from the external
    sensor configuration and passing them to the base Sensor object.
    
    This allows external sensors to override the default IPMI sensor
    number and LUN when they are supplied in the configuration.
    
    Change-Id: I6e5357623b87f5e3788f3e1ff61ad52a9e243178
    Signed-off-by: Praveen Balakrishnan Vasantha <praveenbv@ami.com>

--- src/external/ExternalSensorMain.cpp
+++ src/external/ExternalSensorMain.cpp
@@ -2,6 +2,7 @@
 #include "Thresholds.hpp"
 #include "Utils.hpp"
 #include "VariantVisitors.hpp"
+#include "sensor.hpp"
 
 #include <boost/asio/error.hpp>
 #include <boost/asio/io_context.hpp>
@@ -20,8 +21,10 @@
 #include <array>
 #include <chrono>
 #include <cmath>
+#include <cstdint>
 #include <exception>
 #include <functional>
+#include <limits>
 #include <memory>
 #include <string>
 #include <string_view>
@@ -58,6 +61,7 @@
 // https://github.com/openbmc/docs/tree/master/designs/
 
 static constexpr std::string_view sensorType = "ExternalSensor";
+static constexpr unsigned int maxLun = 3;
 
 void updateReaper(
     boost::container::flat_map<std::string, std::shared_ptr<ExternalSensor>>&
@@ -303,6 +307,40 @@ void createSensors(
 
                 PowerState readState = getPowerState(baseConfigMap);
 
+                uint16_t sensorNumber = defaultSensorNumber;
+                uint8_t lun = defaultLun;
+                auto findSensorNum = baseConfigMap.find("SensorNumber");
+                if (findSensorNum != baseConfigMap.end())
+                {
+                    auto val = std::visit(VariantToUnsignedIntVisitor(),
+                                          findSensorNum->second);
+                    if (val > std::numeric_limits<uint16_t>::max())
+                    {
+                        lg2::error(
+                            "SensorNumber '{VALUE}' out of range for '{PATH}'",
+                            "VALUE", val, "PATH", interfacePath);
+                    }
+                    else
+                    {
+                        sensorNumber = static_cast<uint16_t>(val);
+                    }
+                }
+                auto findLun = baseConfigMap.find("LUN");
+                if (findLun != baseConfigMap.end())
+                {
+                    auto val = std::visit(VariantToUnsignedIntVisitor(),
+                                          findLun->second);
+                    if (val > maxLun)
+                    {
+                        lg2::error("LUN '{VALUE}' out of range for '{PATH}'",
+                                   "VALUE", val, "PATH", interfacePath);
+                    }
+                    else
+                    {
+                        lun = static_cast<uint8_t>(val);
+                    }
+                }
+
                 auto& sensorEntry = sensors[sensorName];
                 sensorEntry = nullptr;
                 try
@@ -310,7 +348,8 @@ void createSensors(
                     sensorEntry = std::make_shared<ExternalSensor>(
                         sensorType, objectServer, dbusConnection, sensorName,
                         sensorUnits, std::move(sensorThresholds), interfacePath,
-                        maxValue, minValue, timeoutSecs, readState);
+                        maxValue, minValue, timeoutSecs, readState,
+                        sensorNumber, lun);
                     sensorEntry->initWriteHook(
                         [&sensors, &reaperTimer](
                             const std::chrono::steady_clock::time_point& now) {
